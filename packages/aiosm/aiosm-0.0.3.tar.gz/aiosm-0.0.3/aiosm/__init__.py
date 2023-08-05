c17ab33f4bc554c8884d3f69ed2215e774aa7f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:09 +0000

    Add package aiosm/0.0.3/aiosm-0.0.3.tar.gz

---
+++ packages/aiosm/aiosm-0.0.3.tar.gz/aiosm-0.0.3/aiosm/__init__.py
@@ -0,0 +1,18 @@
+"""An asynchronous TCP connection hub
+
+todo:
+    implement heartbeat
+    implement inheritable class structure for client side to represent a node with header functions for RPC
+    support loop.create_datagram_endpoint()
+
+"""
+
+__version__ = '0.0.3'
+
+from . import run_host
+from .radio import Radio
+from .clinet import Client
+from .host import Host
+from .node import Node
+from .responder import Responder
+
