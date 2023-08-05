b953b3de4bd7b4650c3ec2af3a0a31663e3b8cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:24 +0000

    Add package edamame/0.35/edamame-0.35.tar.gz

---
+++ packages/edamame/edamame-0.35.tar.gz/edamame-0.35/src/edamame/__init__.py
@@ -0,0 +1,26 @@
+from .eda import (dimensions,
+describe_distribution,
+identify_types,
+num_to_categorical,
+missing,
+handling_missing,
+drop_columns,
+plot_categorical,
+plot_numerical,
+view_cardinality,
+modify_cardinality,
+correlation_pearson,
+correlation_categorical,
+correlation_phik,
+num_variable_study,
+interaction,
+inspection,
+split_and_scaling)
+
+#from .regression import (TrainRegressor, regression_metrics)
+
+from .tools import (load_model, setup, scaling)
+
+#from .classification import TrainClassifier
+
+__version__ = "0.35"
