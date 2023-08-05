dca8dd4d5cf832af56265796283a66b5c72fb5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package secp256k1-zkp-mw/0.4.0/secp256k1-zkp-mw-0.4.0.tar.gz

---
+++ packages/secp256k1-zkp-mw/secp256k1-zkp-mw-0.4.0.tar.gz/secp256k1-zkp-mw-0.4.0/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name="secp256k1-zkp-mw",
+    version="0.4.0",
+
+    description='FFI bindings to libsecp256k1-zkp for Mimblewimble protocol',
+    url='https://github.com/grinventions/secp256k1-zkp-mw',
+    author='Marek Narozniak',
+    author_email='marek.yggdrasil@gmail.com',
+    maintainer='Nicolas Flamel',
+    maintainer_email='nicolasflamel@mwcwallet.com',
+    license='MIT',
+
+    setup_requires=['cffi>=1.3.0'],
+    install_requires=['cffi>=1.3.0'],
+
+    packages=find_packages(),
+    cffi_modules=[
+        'build.py:ffi'
+    ],
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Security :: Cryptography"
+    ]
+)
