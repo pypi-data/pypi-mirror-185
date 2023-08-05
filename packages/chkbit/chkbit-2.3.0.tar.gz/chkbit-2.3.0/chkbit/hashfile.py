af5bfd1b83f9a7a012f76cfe57f1b82850e2e7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package chkbit/2.3.0/chkbit-2.3.0.tar.gz

---
+++ packages/chkbit/chkbit-2.3.0.tar.gz/chkbit-2.3.0/chkbit/hashfile.py
@@ -0,0 +1,28 @@
+import hashlib
+
+
+BLOCKSIZE = 2**10 * 128  # kb
+
+
+def hashfile(path, hash_algo=None):
+
+    if not hash_algo or hash_algo == "md5":
+        h = hashlib.md5()
+    elif hash_algo == "sha512":
+        h = hashlib.sha512()
+    else:
+        raise Exception(f"{hash_algo} is unknown.")
+
+    with open(path, "rb") as f:
+        while True:
+            buf = f.read(BLOCKSIZE)
+            if len(buf) <= 0:
+                break
+            h.update(buf)
+    return h.hexdigest()
+
+
+def hashtext(text):
+    md5 = hashlib.md5()
+    md5.update(text.encode("utf-8"))
+    return md5.hexdigest()
