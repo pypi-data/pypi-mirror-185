fc8cd685b06043e629ab6d7b09d3bf798119e68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:11 +0000

    Add package coiled/0.2.59.post0.dev16/coiled-0.2.59.post0.dev16-py3-none-any.whl

---
+++ packages/coiled/coiled-0.2.59.post0.dev16-py3-none-any.whl/coiled/v2/__init__.py
@@ -0,0 +1,28 @@
+from .._beta.cluster import ClusterBeta as Cluster
+from .._beta.core import AWSOptions, BackendOptions
+from .._beta.core import CloudBeta as Cloud
+from .._beta.core import (
+    FirewallOptions,
+    GCPOptions,
+    cluster_details,
+    cluster_logs,
+    create_cluster,
+    delete_cluster,
+    list_clusters,
+    log_cluster_debug_info,
+    setup_logging,
+)
+
+__all__ = [
+    "Cloud",
+    "Cluster",
+    "AWSOptions",
+    "GCPOptions",
+    "cluster_logs",
+    "BackendOptions",
+    "FirewallOptions",
+    "log_cluster_debug_info",
+    "setup_logging",
+    "cluster_details",
+    "list_clusters",
+]
