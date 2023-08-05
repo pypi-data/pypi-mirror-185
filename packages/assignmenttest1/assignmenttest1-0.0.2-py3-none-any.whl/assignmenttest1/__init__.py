a082920a1fe37ec3b57a20bb99358eb3f37e903e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:11 +0000

    Add package assignmenttest1/0.0.2/assignmenttest1-0.0.2-py3-none-any.whl

---
+++ packages/assignmenttest1/assignmenttest1-0.0.2-py3-none-any.whl/assignmenttest1/__init__.py
@@ -0,0 +1,1 @@
+from assignmenttest1 import mainfile
---
+++ packages/assignmenttest1/assignmenttest1-0.0.2-py3-none-any.whl/assignmenttest1/mainfile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def test():
+    print("Hello friends code is running and package works good")
+
+
+# In[ ]:
+
+
+
+
