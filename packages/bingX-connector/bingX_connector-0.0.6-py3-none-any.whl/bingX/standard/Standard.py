9af06482924df71de1956598adee3f4214710089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:32 +0000

    Add package bingX-connector/0.0.6/bingX_connector-0.0.6-py3-none-any.whl

---
+++ packages/bingX-connector/bingX_connector-0.0.6-py3-none-any.whl/bingX/standard/Standard.py
@@ -0,0 +1,24 @@
+'''
+bingX.standard.Standard
+'''
+
+from bingX import API
+
+class Standard(API):
+    def __init__(self,
+        api_key:    str,
+        api_secret: str,
+        base_url:   str = None
+    ) -> object:
+        super().__init__(
+            api_key    = api_key,
+            api_secret = api_secret,
+            base_url   = base_url or "https://open-api.bingx.com",
+        )
+
+    # ========== STANDARD CONTRACT INTERFACE ==========
+    from bingX.standard.trade import (
+        position,
+        order_history,
+        balance,
+    )
