7398906ea9220d76652b1bf654bc5354e9ca062b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:08 +0000

    Add package cairo-nile/0.13.0/cairo-nile-0.13.0.tar.gz

---
+++ packages/cairo-nile/cairo-nile-0.13.0.tar.gz/cairo-nile-0.13.0/tests/test_nre.py
@@ -0,0 +1,33 @@
+"""
+Tests for nre module.
+
+Only unit tests for now.
+"""
+
+from unittest.mock import patch
+
+import click
+
+from nile.nre import NileRuntimeEnvironment
+
+
+def test_nre_loaded_plugins():
+    @click.command()
+    def dummy():
+        print("dummy_result")
+
+    @click.command()
+    @click.argument("a", type=int)
+    @click.argument("b", type=int)
+    def dummy_params(a, b):
+        return a + b
+
+    with patch(
+        "nile.nre.get_installed_plugins",
+        return_value={"dummy": dummy, "dummy_params": dummy_params},
+    ):
+        nre = NileRuntimeEnvironment()
+        assert callable(nre.dummy)
+
+        nre_result = nre.dummy_params(["1", "2"])
+        assert 3 == nre_result
