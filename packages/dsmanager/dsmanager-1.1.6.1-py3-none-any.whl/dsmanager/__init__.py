b8ef5b0ccd99bcbde40a4028302a1d5bf0c10b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package dsmanager/1.1.6.1/dsmanager-1.1.6.1-py3-none-any.whl

---
+++ packages/dsmanager/dsmanager-1.1.6.1-py3-none-any.whl/dsmanager/__init__.py
@@ -0,0 +1,15 @@
+"""@Author: Rayane AMROUCHE
+
+The source code is composed of three main components:
+
+- A DataManager component
+
+- A Controller component
+
+- A Model component
+
+The View component can also be used if needed.
+"""
+
+from dsmanager.datamanager.datamanager import DataManager
+from dsmanager.model.modelmanager import ModelManager
