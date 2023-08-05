b2c7074350796646f91c2e62410d291b45ac32d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package ConnectionManagementRPAOCI/0.0.2/ConnectionManagementRPAOCI-0.0.2.tar.gz

---
+++ packages/ConnectionManagementRPAOCI/ConnectionManagementRPAOCI-0.0.2.tar.gz/ConnectionManagementRPAOCI-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ConnectionManagementRPAOCI",
+    version="0.0.2",
+    author="Aravind Reddy Ravula",
+    author_email="aravindreddyravula123@gmail.com",
+    description="Test Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aravindreddyravula/ConnectionManagementRPAOCI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["ConnectionManagementRPAOCI"],
+    include_package_data=True,
+)
