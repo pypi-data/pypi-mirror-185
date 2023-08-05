2bf958b8ffd7c0a105aa8a60f287d75cb5483138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package knoxdb/0.4.0/knoxdb-0.4.0.tar.gz

---
+++ packages/knoxdb/knoxdb-0.4.0.tar.gz/knoxdb-0.4.0/src/knoxdb/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='knoxdb',
+    packages=find_packages(include=['knoxdb']),
+    version='0.4.0',
+    description='Quickly create a PostgresSQL DB without the hassle.',
+    author='Knox Dobbins',
+    license='MIT',
+)
