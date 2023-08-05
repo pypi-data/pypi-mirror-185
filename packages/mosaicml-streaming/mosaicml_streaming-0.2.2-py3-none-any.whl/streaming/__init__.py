1e229e90ae66fbe7ed96803a934beabdff9e01ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package mosaicml-streaming/0.2.2/mosaicml_streaming-0.2.2-py3-none-any.whl

---
+++ packages/mosaicml-streaming/mosaicml_streaming-0.2.2-py3-none-any.whl/streaming/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""MosaicML Streaming Datasets for cloud-native model training."""
+
+import streaming.text as text
+import streaming.vision as vision
+from streaming._version import __version__
+from streaming.base import (CSVWriter, JSONWriter, LocalDataset, MDSWriter, StreamingDataLoader,
+                            StreamingDataset, TSVWriter, XSVWriter)
+
+__all__ = [
+    'StreamingDataLoader', 'StreamingDataset', 'CSVWriter', 'JSONWriter', 'MDSWriter', 'TSVWriter',
+    'XSVWriter', 'LocalDataset', 'vision', 'text'
+]
