0e047bcfcf0a292a593d5cdd87cca62955e09b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:49 +0000

    Add package gbs-opt-greeks/0.0/gbs_opt_greeks-0.0.tar.gz

---
+++ packages/gbs-opt-greeks/gbs_opt_greeks-0.0.tar.gz/gbs_opt_greeks-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gbs_opt_greeks',
+      version='0.0',
+      description='Functions for option pricing models',
+      packages=['gbs_opt_greeks'],
+      author_email='yashmahadik96@gmail.com',
+      zip_safe=False)
