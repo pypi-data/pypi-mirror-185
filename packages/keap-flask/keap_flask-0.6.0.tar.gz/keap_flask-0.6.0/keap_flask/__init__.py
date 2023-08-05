f89e664c50a04e417bfd8666ff55d5fb64ef6a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:04 +0000

    Add package keap-flask/0.6.0/keap_flask-0.6.0.tar.gz

---
+++ packages/keap-flask/keap_flask-0.6.0.tar.gz/keap_flask-0.6.0/keap_flask/__init__.py
@@ -0,0 +1,16 @@
+"""
+    keap_flask
+    ~~~~~~~
+
+    The ultimate one stop shop for using the Keap API in a flask integration. Handles setting up auth and creates an
+    easy to use client with three config variables.
+
+    :copyright: (c) 2023 by William Sexton.
+    :license: BSD, see LICENSE for more details.
+"""
+from .consts import version, homepage, author
+__version__ = version
+__homepage__ = homepage
+__author__ = author
+__license__ = 'BSD-3-Clause'
+
