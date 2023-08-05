8c227aaf720a423afd4b98462445ef57f2812459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package pumapy/3.2.1/pumapy-3.2.1.tar.gz

---
+++ packages/pumapy/pumapy-3.2.1.tar.gz/pumapy-3.2.1/python/pumapy/io/input.py
@@ -0,0 +1,252 @@
+from skimage.io import imread
+import numpy as np
+import pickle
+from pumapy.utilities.workspace import Workspace
+from os import path
+from glob import glob
+from vtkmodules.util.numpy_support import vtk_to_numpy
+from vtkmodules.vtkIOXML import vtkXMLImageDataReader, vtkXMLUnstructuredGridReader
+from vtkmodules.vtkIOLegacy import vtkDataSetReader
+
+
+def io_logs(ws, filename, input=True):
+    if input:
+        s = ["Importing", "from "]
+    else:
+        s = ["Exporting", "to "]
+    ws.log.log_section(s[0] + " Domain " + s[1] + filename)
+    ws.log.write_log()
+
+
+def import_3Dtiff(filename, voxel_length=1e-6, import_ws=True):
+    """ Function to io 3D tiff
+
+        :param filename: filepath and name
+        :type filename: string
+        :param voxel_length: size of a voxel side
+        :type voxel_length: float
+        :param import_ws: if True returns a puma.Workspace, otherwise a ndarray
+        :type import_ws: bool
+        :return: domain
+        :rtype: pumapy.Workspace or np.ndarray
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_tiff = puma.import_3Dtiff(puma.path_to_example_file("50_artfibers.tif"), 1.3e-6, import_ws=True)
+        Importing ...
+        >>> ws_tiff.get_shape()
+        (50, 50, 50)
+    """
+    print("Importing " + filename + " ... ", end='')
+
+    if not path.exists(filename):
+        raise Exception("File " + filename + " not found.")
+
+    nparray = imread(filename).astype(np.uint16)
+
+    if nparray.ndim == 2:
+        nparray = nparray[:, :, np.newaxis]
+    else:
+        nparray = nparray.transpose(2, 1, 0)
+    print("Done")
+
+    if import_ws:
+        ws = Workspace.from_array(nparray)
+        ws.set_voxel_length(voxel_length)
+        io_logs(ws, filename)
+        return ws
+    return nparray
+
+
+def import_bin(filename):
+    """ Import a pumapy.Workspace to binary (.pumapy extension)
+
+        :param filename: filepath and name
+        :type filename: string
+        :return: domain
+        :rtype: pumapy.Workspace
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_binary = puma.import_bin(puma.path_to_example_file("fibers_with_orientation.pumapy"))
+        Importing ...
+    """
+    print("Importing " + filename + " ... ", end='')
+
+    if not path.exists(filename):
+        raise Exception("File " + filename + " not found.")
+
+    pumapy_file = open(filename, 'rb')
+    ws = pickle.load(pumapy_file)
+    pumapy_file.close()
+    print("Done")
+    io_logs(ws, filename)
+    return ws
+
+
+def import_vti(filename, voxel_length=None, import_ws=True):
+    """ Function to import either legacy VTK file (.vtk) or vtkImageData (.vti)
+
+        :param filename: filepath and name
+        :type filename: string
+        :param voxel_length: voxel_length. If None, voxel_length from the vtk file is used
+        :type voxel_length: float
+        :param import_ws: True returns a puma.Workspace, otherwise a list of ndarrays
+        :type import_ws: bool
+        :return: if import_ws is True, then it returns a Workspace.
+            if import_ws is False, it returns a dictionary of ndarrays as {"name1": data1, "name2": data2 ...}
+        :rtype: pumapy.Workspace or {str: np.ndarray}
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_vtk = puma.import_vti(puma.path_to_example_file("fibers_with_orientation.vti"))
+        Importing ...
+    """
+    print("Importing " + filename + " ... ", end='')
+
+    if not path.exists(filename):
+        raise Exception("File " + filename + " not found.")
+
+    if filename[-4:] == ".vti":
+        reader = vtkXMLImageDataReader()
+        reader.SetFileName(filename)
+        reader.Update()
+        vtkobject = reader.GetOutput()
+
+    elif filename[-4:] == ".vtk":
+        reader = vtkDataSetReader()
+        reader.SetFileName(filename)
+        reader.ReadAllScalarsOn()
+        reader.ReadAllColorScalarsOn()
+        reader.ReadAllNormalsOn()
+        reader.ReadAllTCoordsOn()
+        reader.ReadAllVectorsOn()
+        reader.Update()  # reading
+        vtkobject = reader.GetOutputDataObject(0)
+
+    else:
+        raise Exception("File of an unrecognized extension, only .vti and .vtk supported.")
+
+    shape = vtkobject.GetDimensions()
+    orientation = None
+
+    if vtkobject.GetPointData().GetNumberOfArrays() == 0 and vtkobject.GetCellData().GetNumberOfArrays() == 0:
+        raise Exception("No CELL_DATA or POINT_DATA arrays detected in file.")
+
+    if import_ws:
+
+        if vtkobject.GetCellData().GetNumberOfArrays() + vtkobject.GetPointData().GetNumberOfArrays() > 2:
+            raise Exception("More than two arrays in file detected: set the import_ws to False to import it.")
+
+        # checking for CELL_DATA
+        if vtkobject.GetCellData().GetNumberOfArrays() > 0:
+            if vtkobject.GetCellData().GetNumberOfArrays() == 1:  # if one array, could either be orientation or matrix
+                nparray = vtk_to_numpy(vtkobject.GetCellData().GetArray(0))
+                if nparray.ndim == 2:  # if orientation
+                    orientation = nparray.copy()
+                    nparray = None
+            else:
+                nparray = vtk_to_numpy(vtkobject.GetCellData().GetArray(0))
+                orientation = vtk_to_numpy(vtkobject.GetCellData().GetArray(1))
+
+        # checking for POINT_DATA
+        else:
+            if vtkobject.GetPointData().GetNumberOfArrays() == 1:  # if one array, could either be orientation or matrix
+                nparray = vtk_to_numpy(vtkobject.GetPointData().GetArray(0))
+                if nparray.ndim == 2:  # if orientation
+                    orientation = nparray.copy()
+                    nparray = None
+            else:
+                nparray = vtk_to_numpy(vtkobject.GetPointData().GetArray(0))
+                orientation = vtk_to_numpy(vtkobject.GetPointData().GetArray(1))
+
+        if nparray is not None:
+            nparray = nparray.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, order="F")
+        if orientation is not None:
+            orientation = orientation.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, 3, order="F")
+        print("Done")
+
+        if nparray is None:
+            ws = Workspace.from_shape((shape[0] - 1, shape[1] - 1, shape[2] - 1))
+        else:
+            ws = Workspace.from_array(nparray)
+        if orientation is not None:
+            ws.set_orientation(orientation)
+        if voxel_length is None:
+            ws.set_voxel_length(vtkobject.GetSpacing()[0])
+        else:
+            ws.set_voxel_length(voxel_length)
+        io_logs(ws, filename)
+        return ws
+
+    else:
+        nparray_list = dict()
+        for i in range(vtkobject.GetCellData().GetNumberOfArrays()):
+            tmp = vtk_to_numpy(vtkobject.GetCellData().GetArray(i))
+            if tmp.ndim == 1:
+                tmp = tmp.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, order="F")
+            else:
+                tmp = tmp.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, 3, order="F")
+            nparray_list[vtkobject.GetCellData().GetArrayName(i)] = tmp
+        for i in range(vtkobject.GetPointData().GetNumberOfArrays()):
+            tmp = vtk_to_numpy(vtkobject.GetPointData().GetArray(i))
+            if tmp.ndim == 1:
+                tmp = tmp.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, order="F")
+            else:
+                tmp = tmp.reshape(shape[0] - 1, shape[1] - 1, shape[2] - 1, 3, order="F")
+            nparray_list[vtkobject.GetPointData().GetArrayName(i)] = tmp
+        print("Done")
+        return nparray_list
+
+
+def import_weave_vtu(filename, from_texgen_gui=False):
+    """ Import TexGen vtu weave in a Workspace
+
+        :param filename: file path and name
+        :type filename: string
+        :param from_texgen_gui: voxel grid exported from the TexGen GUI (Windows) or from TexGen inside PuMA
+        :type from_texgen_gui: bool
+        :return: voxelized weave from TexGen
+        :rtype: pumapy.Workspace
+    """
+
+    if not path.exists(filename):
+        filename = glob(filename + '*.vtu')
+        if len(filename) == 0:
+            raise Exception("File " + filename + " not found.")
+        else:
+            filename = filename[0]
+    print("Importing " + filename + " ... ", end='')
+
+    reader = vtkXMLUnstructuredGridReader()
+    reader.SetFileName(filename)
+    reader.Update()  # reading
+    vtkobject = reader.GetOutputDataObject(0)
+
+    dims = path.split(filename[:-4])[1].split('_')[-3:]
+    yarn_index = vtk_to_numpy(vtkobject.GetCellData().GetArray(0)) + 1
+    ws = Workspace.from_array(yarn_index.reshape(int(dims[0]), int(dims[1]), int(dims[2]), order="F"))
+
+    if vtkobject.GetCellData().GetNumberOfArrays() >= 2:
+        if from_texgen_gui:
+            # ORIGINAL TEXGEN (GUI in Windows)
+            # Number Of Arrays: 6
+            # Array 0 name = YarnIndex  <-- transferring to ws
+            # Array 1 name = YarnTangent (unnecessary)
+            # Array 2 name = Location (unnecessary)
+            # Array 3 name = VolumeFraction (unnecessary)
+            # Array 4 name = SurfaceDistance (unnecessary)
+            # Array 5 name = Orientation  <-- transferring to ws
+            orientation = vtk_to_numpy(vtkobject.GetCellData().GetArray(5))
+        else:
+            # MODIFIED TEXGEN wrapped in PuMA
+            # Number Of Arrays: 6
+            # Array 0 name = YarnIndex  <-- transferring to ws
+            # Array 1 name = Orientation  <-- depends on export_orientation in export_weave_vtu
+            orientation = vtk_to_numpy(vtkobject.GetCellData().GetArray(1))
+        orientation = orientation.reshape(int(dims[0]), int(dims[1]), int(dims[2]), 3, order="F")
+        ws.set_orientation(orientation)
+
+    print("Done")
+    io_logs(ws, filename)
+    return ws
---
+++ packages/pumapy/pumapy-3.2.1.tar.gz/pumapy-3.2.1/python/pumapy/io/output.py
@@ -0,0 +1,245 @@
+from pyevtk.hl import imageToVTK
+import numpy as np
+from skimage.io import imsave
+import pickle
+from pumapy.utilities.workspace import Workspace
+from pumapy.utilities.logger import print_warning
+from pumapy.utilities.isosurface import generate_isosurface
+from pumapy.io.input import io_logs
+from os import path
+
+
+def export_vti(filename, dict_data, voxel_length=None):
+    """ Export either a puma.Workspace or numpy array to vti
+
+        :param filename: filepath and name
+        :type filename: string
+        :param dict_data: dictionary setup as {"name1": data1, "name2": data2 ...} containing either Workspaces or ndarrays
+        :type dict_data: dict or Workspace or np.ndarray
+        :param voxel_length: with voxel length to give to Numpy arrays (if any)
+        :type voxel_length: float
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_vtk = puma.import_vti(puma.path_to_example_file("fibers_with_orientation.vti"))
+        Importing ...
+        >>> puma.export_vti("fibers_with_orientation.vti", ws_vtk)
+        Exporting ...
+    """
+
+    # error path checks
+    if not isinstance(filename, str):
+        raise Exception("Filename has to be a string.")
+    filename_split = path.split(filename)
+    if filename_split[0] != '' and not path.exists(path.split(filename)[0]):
+        raise Exception("Directory " + filename_split[0] + " not found.")
+    if filename[-4:] == ".vti":
+        filename = filename[:-4]
+
+    print("Exporting " + filename + ".vti ... ", end='')
+
+    if isinstance(dict_data, Workspace) or isinstance(dict_data, np.ndarray):
+        dict_data = {"data": dict_data}
+
+    dict_to_export = dict()
+    counter = 0
+
+    for name, data in dict_data.items():
+
+        if isinstance(data, Workspace):
+            if counter == 0:
+                io_logs(data, filename, input=False)
+            mat = data.matrix.copy()  # need to copy to make sure data is "continuous"
+            dict_to_export[name] = mat
+
+            if data.orientation.shape[:3] == data.matrix.shape:
+                orientation = (data.orientation[:, :, :, 0].copy(),
+                               data.orientation[:, :, :, 1].copy(),
+                               data.orientation[:, :, :, 2].copy())
+                dict_to_export[name] = mat
+                dict_to_export[name + "_orient"] = orientation
+
+            if voxel_length is None:
+                voxel_length = data.voxel_length
+            counter += 1
+
+        elif isinstance(data, np.ndarray):
+            if voxel_length is None:
+                voxel_length = 1e-6
+            if data.ndim == 2:  # 2D numpy array to 3D
+                data = np.expand_dims(data.copy(), axis=2)
+            elif data.ndim == 3:
+                data = data.copy()
+            elif data.ndim == 4 and data.shape[3] == 3:
+                data = (data[:, :, :, 0].copy(),
+                        data[:, :, :, 1].copy(),
+                        data[:, :, :, 2].copy())
+            else:
+                raise Exception("Numpy array has to be either 3D for scalars or 4D with shape[3]=3.")
+
+            dict_to_export[name] = data
+        else:
+            raise Exception("Data to export to vti needs to be either a pumapy.Workspace or Numpy array")
+
+    imageToVTK(filename, spacing=[voxel_length] * 3, cellData=dict_to_export)
+
+    print("Done")
+
+
+def export_3Dtiff(filename, ws_or_nparray, to8bit=True):
+    """ Export either a puma.Workspace or numpy array to 3Dtiff
+
+        :param filename: filepath and name
+        :type filename: string
+        :param ws_or_nparray: to be exported
+        :type ws_or_nparray: Workspace or ndarray
+        :param to8bit: if True, it converts the image to 8bit, otherwise 16bit is exported
+        :type to8bit: bool
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_tiff = puma.import_3Dtiff(puma.path_to_example_file("50_artfibers.tif"), 1.3e-6, import_ws=True)
+        Importing ...
+        >>> puma.export_3Dtiff("50_artfibers.tif", ws_tiff)
+        Exporting ...
+    """
+
+    # error checks
+    if filename[-4:] != '.tif' and filename[-5:] != '.tiff':
+        filename += '.tif'
+    if not isinstance(filename, str):
+        raise Exception("Filename has to be a string.")
+    filename_split = path.split(filename)
+    if filename_split[0] != '' and not path.exists(path.split(filename)[0]):
+        raise Exception("Directory " + filename_split[0] + " not found.")
+    if isinstance(ws_or_nparray, Workspace):
+        io_logs(ws_or_nparray, filename, input=False)
+        data = ws_or_nparray.matrix.copy()
+    elif isinstance(ws_or_nparray, np.ndarray):
+        data = ws_or_nparray.copy()
+    else:
+        raise Exception("Data to export to 3Dtiff needs to be either a pumapy.Workspace or Numpy array")
+
+    if to8bit:
+        if data.max() > 255:
+            print_warning("Data max is more than 255, normalizing to range 0-255.")
+            data *= 255.0 / data.max()
+        data = data.astype(np.uint8)
+    else:
+        data = data.astype(np.uint16)
+
+    print("Exporting " + filename + " ... ", end='')
+    if data.ndim == 2:
+        imsave(filename, data.transpose((1, 0)), check_contrast=False)
+    else:
+        imsave(filename, data.transpose((2, 1, 0)), check_contrast=False)
+    print("Done")
+
+
+def export_bin(filename, ws):
+    """ Export a puma.Workspace to binary (.pumapy extension)
+
+        :param filename: filepath and name
+        :type filename: string
+        :param ws: to be exported
+        :type: pumapy.Workspace
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_binary = puma.import_bin(puma.path_to_example_file("fibers_with_orientation.pumapy"))
+        Importing ...
+        >>> puma.export_bin("fibers_with_orientation.vti", ws_binary)
+        Exporting ...
+    """
+
+    # error checks
+    if filename[-7:] != ".pumapy":
+        filename += '.pumapy'
+    if not isinstance(filename, str):
+        raise Exception("Filename has to be a string.")
+    filename_split = path.split(filename)
+    if filename_split[0] != '' and not path.exists(path.split(filename)[0]):
+        raise Exception("Directory " + filename_split[0] + " not found.")
+    if not isinstance(ws, Workspace):
+        raise Exception("Data to export to bin needs to be a pumapy.Workspace")
+
+    print("Exporting " + filename + " ... ", end='')
+    output = open(filename, 'wb')
+    pickle.dump(ws, output)
+    output.close()
+    print("Done")
+    io_logs(ws, filename, input=False)
+
+
+def export_sparta_implicit_surfaces(filename, ws):
+    """ Export a puma.Workspace to binary (.pumapy extension)
+
+        :param filename: filepath and name
+        :type filename: string
+        :param ws: to be exported
+        :type ws: pumapy.Workspace
+    """
+
+    # error checks
+    if filename[-7:] != ".pumapy.isurf":
+        filename += '.pumapy.isurf'
+    if not isinstance(filename, str):
+        raise Exception("Filename has to be a string.")
+    filename_split = path.split(filename)
+    if filename_split[0] != '' and not path.exists(path.split(filename)[0]):
+        raise Exception("Directory " + filename_split[0] + " not found.")
+    if not isinstance(ws, Workspace):
+        raise Exception("Data to export to bin needs to be a pumapy.Workspace")
+
+    print("Exporting " + filename + " ... ", end='')
+
+    if ws.len_z() == 1:
+        f = open(filename, 'w+b')
+        byte_arr = [ws.len_x(), ws.len_y()]
+        binary_format = bytearray(byte_arr)
+        f.write(binary_format)
+        pickle.dump(ws.matrix, f)
+        f.close()
+
+    print("Done")
+    io_logs(ws, filename, input=False)
+
+
+def export_stl(filename, ws, cutoff, flag_closed_edges=True, flag_gaussian=False, binary=True):
+    """ Export a puma.Workspace to STL
+
+        :param filename: filepath and name
+        :type filename: string
+        :param ws: to be exported
+        :type ws: pumapy.Workspace
+        :param cutoff: specify cutoff to binarize material
+        :type cutoff: (int, int)
+        :param flag_closed_edges: close the surface edges on the boundaries
+        :type flag_closed_edges: bool
+        :param flag_gaussian: apply Gaussian filter before creating surface
+        :type flag_gaussian: bool
+
+        :Example:
+        >>> import pumapy as puma
+        >>> ws_imported = puma.import_3Dtiff(puma.path_to_example_file("200_fiberform.tif"), 1.3e-6)
+        Importing ...
+        >>> puma.export_stl('200_fiberform', ws_imported, cutoff=(100, 255), flag_closed_edges=True)
+        Exporting ...
+    """
+
+    # error checks
+    if filename[-4:] != ".stl":
+        filename += '.stl'
+    if not isinstance(filename, str):
+        raise Exception("Filename has to be a string.")
+    filename_split = path.split(filename)
+    if filename_split[0] != '' and not path.exists(path.split(filename)[0]):
+        raise Exception("Directory " + filename_split[0] + " not found.")
+    if not isinstance(ws, Workspace):
+        raise Exception("Data to export to stl needs to be a pumapy.Workspace")
+
+    print("Exporting " + filename + " ... ", end='')
+    mesh = generate_isosurface(ws, cutoff, flag_closed_edges, flag_gaussian)
+    mesh.save(filename, binary)
+    print("Done")
+    io_logs(ws, filename, input=False)
