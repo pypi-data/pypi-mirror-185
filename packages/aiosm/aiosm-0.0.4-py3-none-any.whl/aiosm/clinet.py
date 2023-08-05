704e2183a1adc99e3b49fe047a5108a24f17357b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:16 +0000

    Add package aiosm/0.0.4/aiosm-0.0.4-py3-none-any.whl

---
+++ packages/aiosm/aiosm-0.0.4-py3-none-any.whl/aiosm/clinet.py
@@ -0,0 +1,35 @@
+
+from .responder import Responder
+
+import asyncio
+
+
+class Client(Responder):
+	def __init__(self, name, addr='127.0.0.1', port=8888):
+		super().__init__(name)
+		self.addr = addr
+		self.port = port
+
+		# todo consider adding white_list_functions
+		# self.white_list_functions += []
+
+	async def connect(self) -> None:
+		# todo add exception handling
+		self.reader, self.writer = await asyncio.open_connection(self.addr, self.port)
+		self.ready = True
+
+	async def request(self, func_name: str, *args):
+		await self.send({
+			"type": func_name,
+			"args": args
+		})
+
+	async def broadcast(self, tag: str, func_name: str, *args):  # only Clients subscribed to the tag shall receive
+		# fixme, add key word args
+		#  tdoo, remember what this meant
+		await self.request("broadcast", {
+					"type": func_name,
+					"args": args
+				},
+				tag
+		)
