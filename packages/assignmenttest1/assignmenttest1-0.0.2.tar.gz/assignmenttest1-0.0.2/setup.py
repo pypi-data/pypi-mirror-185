b9a9fdafbef8afc2b73bdefe631b273c18dbef2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package assignmenttest1/0.0.2/assignmenttest1-0.0.2.tar.gz

---
+++ packages/assignmenttest1/assignmenttest1-0.0.2.tar.gz/assignmenttest1-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'test file'
+LONG_DESCRIPTION = 'A package that allows to test.'
+
+# Setting up
+setup(
+        name="assignmenttest1",
+    version=VERSION,
+    author="Aman and Akanksha",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
