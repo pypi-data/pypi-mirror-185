0a8e10eaadafc2e87a0f143bb51a94a1c620052b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package mongo-tooling-metrics/0.0.6/mongo_tooling_metrics-0.0.6.tar.gz

---
+++ packages/mongo-tooling-metrics/mongo_tooling_metrics-0.0.6.tar.gz/mongo_tooling_metrics-0.0.6/src/mongo_tooling_metrics/errors.py
@@ -0,0 +1,18 @@
+class InvalidMetricsObject(Exception):
+    """Custom exception class for invalid metrics."""
+    pass
+
+
+class MetricsCollectionFailure(Exception):
+    """Custom exception class for metrics collection failure."""
+    pass
+
+
+class InvalidMetricsSetup(Exception):
+    """Custom exception class for metrics collection failure."""
+    pass
+
+
+class InvalidHookAccess(Exception):
+    """Custom exception class for invalid metrics."""
+    pass
