2e2031ed74e3960045cc1120f9bff726db7e621d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:18 +0000

    Add package keap-flask/0.1.0/keap_flask-0.1.0.tar.gz

---
+++ packages/keap-flask/keap_flask-0.1.0.tar.gz/keap_flask-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='keap_flask',
+    version='0.1.0',
+    description='A Rest Client For Flask applications',
+    author='will sexton',
+    author_email='will@theapiguys.com',
+    url='https://github.com/codinlikewilly/keap_flask',
+    license='BSD 2-clause',
+    packages=['keap_flask'],
+    install_requires=['Flask',
+                      'AuthLib',
+                      'python-dateutil'
+                      ],
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License'
+    ],
+)
