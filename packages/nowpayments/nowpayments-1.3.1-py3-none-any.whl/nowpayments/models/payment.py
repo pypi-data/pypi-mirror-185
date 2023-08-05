aa3c249b03856205219689639783993f7825a0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package nowpayments/1.3.1/nowpayments-1.3.1-py3-none-any.whl

---
+++ packages/nowpayments/nowpayments-1.3.1-py3-none-any.whl/nowpayments/models/payment.py
@@ -0,0 +1,41 @@
+"""
+Dataclasses for the NowPayments API.
+"""
+from dataclasses import dataclass
+from inspect import signature
+from typing import Dict, Union
+
+
+@dataclass
+class PaymentData:  # pylint: disable=too-many-instance-attributes
+    """
+    The PaymentData class is a container for the data that is used to make a payment.
+    """
+
+    price_amount: float
+    price_currency: str
+    pay_currency: str
+    pay_amount: float = None
+    ipn_callback_url: str = None
+    order_id: str = None
+    order_description: str = None
+    purchase_id: int = None
+    payout_address: str = None
+    payout_currency: str = None
+    payout_extra_id: str = None
+    fixed_rate: bool = None
+    case: str = None
+
+    def clean_data_to_dict(
+        self, is_sandbox: bool = False
+    ) -> Dict[str, Union[str, float, int]]:
+        """
+        Delete None types and return dictionary
+        """
+        data = {}
+        for field in signature(self.__class__).parameters:
+            if getattr(self, field):
+                data[field] = getattr(self, field)
+        if is_sandbox and self.case is None:
+            data["case"] = "success"
+        return data
