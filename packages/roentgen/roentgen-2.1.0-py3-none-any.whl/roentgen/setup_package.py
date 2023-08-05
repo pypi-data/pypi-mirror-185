8749656d41c712440996b2cc6e7009d53f92af24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:40 +0000

    Add package roentgen/2.1.0/roentgen-2.1.0-py3-none-any.whl

---
+++ packages/roentgen/roentgen-2.1.0-py3-none-any.whl/roentgen/setup_package.py
@@ -0,0 +1,9 @@
+def get_package_data():
+    return {
+        "roentgen": [
+            "data/*.json",
+            "data/*.csv",
+            "data/elements/*.csv",
+            "data/compounds_mixtures/*.csv",
+        ]
+    }
