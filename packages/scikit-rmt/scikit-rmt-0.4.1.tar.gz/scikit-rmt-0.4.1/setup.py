f055ced297d9c93ecfb77fb6a9487e6030bc8638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package scikit-rmt/0.4.1/scikit-rmt-0.4.1.tar.gz

---
+++ packages/scikit-rmt/scikit-rmt-0.4.1.tar.gz/scikit-rmt-0.4.1/setup.py
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+import os
+from setuptools import find_packages, setup
+
+with open(
+    os.path.join(os.path.dirname(__file__), "VERSION"),
+    "r",
+) as version_file:
+    VERSION = version_file.read().strip()
+
+with open(
+    os.path.join(os.path.dirname(__file__), "README.md"),
+    "r",
+) as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
+
+with open(
+    os.path.join(os.path.dirname(__file__), "requirements.txt"),
+    "r",
+) as req_file:
+    REQUIREMENTS = req_file.read().split('\n')
+
+DOWNLOAD_URL = f'https://github.com/AlejandroSantorum/scikit-rmt/archive/refs/tags/v{VERSION}.tar.gz'
+
+setup(
+    name='scikit-rmt',
+    author='Alejandro Santorum Varela',
+    author_email='alejandro.santorum@gmail.com',
+    #packages = ['scikit-rmt'],
+    version = VERSION,
+    license='BSD',
+    description='Random Matrix Theory Python package',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/AlejandroSantorum/scikit-rmt',
+    download_url=DOWNLOAD_URL,
+    include_package_data=True,
+    packages=find_packages(),
+    keywords=['RMT', 'Random Matrix Theory', 'Ensemble', 'Covariance matrices'],
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
