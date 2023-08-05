783a1fbe90a8f70e70bcee1cf5f16ca793e321cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:51 +0000

    Add package bluebonnet/0.1.0/bluebonnet-0.1.0-py3-none-any.whl

---
+++ packages/bluebonnet/bluebonnet-0.1.0-py3-none-any.whl/bluebonnet/__init__.py
@@ -0,0 +1,7 @@
+"""Scaling solutions for production analysis from unconventional oil and gas wells."""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version("bluebonnet")
