00314715f26f44e2560089a6f40498d30e63fd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:22 +0000

    Add package Xiaoimage/1.0.7/Xiaoimage-1.0.7.tar.gz

---
+++ packages/Xiaoimage/Xiaoimage-1.0.7.tar.gz/Xiaoimage-1.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'Xiaoimage',
+	version = '1.0.7',
+	packages = find_packages(),
+	include_package_data = True,
+	install_requires=[
+		'Flask',
+                'requests',
+                'BeautifulSoup4',
+	],
+        entry_points="""
+            [consle_scripts]
+            xiaoimage = Xiaoimage:main
+        """,
+)
