2bc1b1428e08769bc929e5480f47a9c741a692c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:45 +0000

    Add package adwite/0.3/adwite-0.3.tar.gz

---
+++ packages/adwite/adwite-0.3.tar.gz/adwite-0.3/README.md
@@ -0,0 +1,20 @@
+# Adwite
+
+使用tkinter与web组件进行开发
+
+---
+
+
+基础示例
+```bash
+python -m adwite
+```
+
+> 库信息
+>> _版本_ `0.3`
+> 
+>> _作者_ `向秦希`
+> 
+>> _许可证_ `麻省理工许可证`
+> 
+>> _文档_ `https://adwite.netlify.app/zh/`
---
+++ packages/adwite/adwite-0.3.tar.gz/adwite-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="adwite",
+    version="0.3",
+    author="XiangQinxi",
+    url="https://adwite.netlify.app",
+    author_email="XiangQinxi@outlook.com",
+    description="GUI extension library",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    packages=find_packages(
+        where=".",
+        exclude=["doc"]
+    ),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
