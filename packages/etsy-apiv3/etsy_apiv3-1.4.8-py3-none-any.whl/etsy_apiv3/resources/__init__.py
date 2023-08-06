3f0fdd38358848418daa0aa9da334791adfec337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:01 +0000

    Add package etsy-apiv3/1.4.8/etsy_apiv3-1.4.8-py3-none-any.whl

---
+++ packages/etsy-apiv3/etsy_apiv3-1.4.8-py3-none-any.whl/etsy_apiv3/resources/__init__.py
@@ -0,0 +1,6 @@
+from .ListingResource import ListingResource
+from .ReceiptResource import ReceiptResource
+from .ReviewResource import ReviewResource
+from .ShopResource import ShopResource
+from .TransactionResource import TransactionResource
+from ..models import Listing
