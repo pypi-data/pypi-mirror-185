d5a0c21e1719a08cecabf0c83e0be0e46b41feea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:46 +0000

    Add package liana/0.1.5/liana-0.1.5-py3-none-any.whl

---
+++ packages/liana/liana-0.1.5-py3-none-any.whl/liana/method/__init__.py
@@ -0,0 +1,13 @@
+from ._liana_pipe import liana_pipe
+from ._Method import MethodMeta, _show_methods
+from .sc._rank_aggregate import AggregateClass, _rank_aggregate_meta
+from .sc import cellphonedb, connectome, logfc, natmi, singlecellsignalr, geometric_mean, cellchat
+
+# callable consensus instance
+_methods = [cellphonedb, connectome, logfc, natmi, singlecellsignalr, cellchat]
+rank_aggregate = AggregateClass(_rank_aggregate_meta, methods=_methods)
+
+
+def show_methods():
+    """Shows methods available in LIANA"""
+    return _show_methods(_methods + [rank_aggregate, geometric_mean])
