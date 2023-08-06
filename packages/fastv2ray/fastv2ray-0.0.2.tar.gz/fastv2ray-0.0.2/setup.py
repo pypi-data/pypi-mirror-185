8155806c28212e767c258dd15240117479d2ba80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package fastv2ray/0.0.2/fastv2ray-0.0.2.tar.gz

---
+++ packages/fastv2ray/fastv2ray-0.0.2.tar.gz/fastv2ray-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import codefast as cf
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = setuptools.find_packages()
+data = [f.lstrip('fastv2ray/') for f in cf.io.walk('fastv2ray/bins/')]
+data.extend([f.lstrip('fastv2ray/') for f in cf.io.walk('fastv2ray/configs/')])
+
+setuptools.setup(
+    name="fastv2ray",
+    version="0.0.2",
+    author="slipper",
+    author_email="r2fscg@gmail.com",
+    description="NULL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://google.com",
+    packages=setuptools.find_packages(),
+    package_data={'fastv2ray': data},
+    install_requires=['codefast', 'fire', 'simauth', 'asyncio'],
+    entry_points={'console_scripts': [
+        'fastv2ray=fastv2ray.app:main',
+    ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
