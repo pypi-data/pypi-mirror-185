8d13ccf229dd491a5f630d0f242b0ba3d80eaa7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:21 +0000

    Add package test0000001/3.0.0/test0000001-3.0.0.tar.gz

---
+++ packages/test0000001/test0000001-3.0.0.tar.gz/test0000001-3.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test0000001",
+    version="3.0.0",
+    package_dir={'test0000001': 'test0000001'},
+    packages=find_packages(),
+    install_requires=[
+        'pyrebase',
+        'uuid',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'test0000001 = test0000001.steps:run_steps'
+        ]
+    }
+)
+
