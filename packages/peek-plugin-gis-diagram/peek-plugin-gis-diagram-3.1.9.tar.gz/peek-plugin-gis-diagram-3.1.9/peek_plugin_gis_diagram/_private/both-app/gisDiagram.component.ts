3d0fb064265a9fa34b801a8756b1a14aff55101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:06 +0000

    Add package peek-plugin-gis-diagram/3.1.9/peek-plugin-gis-diagram-3.1.9.tar.gz

---
+++ packages/peek-plugin-gis-diagram/peek-plugin-gis-diagram-3.1.9.tar.gz/peek-plugin-gis-diagram-3.1.9/peek_plugin_gis_diagram/_private/both-app/gisDiagram.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core"
+import { HeaderService } from "@synerty/peek-plugin-base-js"
+import { NgLifeCycleEvents } from "@synerty/vortexjs"
+
+@Component({
+    selector: "plugin-gis-diagram",
+    templateUrl: "gisDiagram.component.web.html"
+})
+export class GisDiagramComponent extends NgLifeCycleEvents {
+    
+    constructor(private headerService: HeaderService) {
+        super()
+        
+        this.headerService.setTitle("GIS Diagram")
+    }
+}
