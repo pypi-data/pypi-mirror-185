53820ba9c7a00ff832c12c58985edd11928ba643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:44 +0000

    Add package fennel-dataset/0.1.0/fennel_dataset-0.1.0-py3-none-any.whl

---
+++ packages/fennel-dataset/fennel_dataset-0.1.0-py3-none-any.whl/fennel_dataset/lib.py
@@ -0,0 +1,9 @@
+def init(*args, **kwargs): # real signature unknown
+    pass
+
+def lookup(*args, **kwargs): # real signature unknown
+    """
+    lookup queries a dataset matching given metadata (especially name and version)
+    with the given keys
+    """
+    pass
