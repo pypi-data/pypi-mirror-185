a11f381a814ba40d317bfd4d02c651b64841af10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:57 +0000

    Add package alaspo/0.1.4/alaspo-0.1.4-py3-none-any.whl

---
+++ packages/alaspo/alaspo-0.1.4-py3-none-any.whl/alaspo/initial.py
@@ -0,0 +1,18 @@
+
+from .__init__ import logger
+
+
+class ClingoInitialOperator:
+
+    def __init__(self, internal_solver, global_timeout, pre_opt_time=0):
+        self.__timeout = global_timeout
+        self.__internal_solver = internal_solver
+        self.__pre_opt_time = pre_opt_time
+
+    def construct(self):
+        logger.debug(f'default initial operator executing for {self.__timeout} seconds')
+        if self.__pre_opt_time > 0:
+            return self.__internal_solver.solve(timelimit=min(self.__pre_opt_time, self.__timeout))
+        else:
+            return self.__internal_solver.solve(timelimit=self.__timeout, modellimit=1)
+      
