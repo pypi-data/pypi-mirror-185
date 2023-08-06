db818d183de2dbdf9c93ebb206964629872d6540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:32 +0000

    Add package invenio-pure/0.1.1/invenio-pure-0.1.1.tar.gz

---
+++ packages/invenio-pure/invenio-pure-0.1.1.tar.gz/invenio-pure-0.1.1/invenio_pure/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Technische Universität Graz.
+#
+# invenio-pure is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module that adds pure."""
+
+from .ext import InvenioPure
+
+__version__ = "0.1.1"
+
+__all__ = ("__version__", "InvenioPure")
