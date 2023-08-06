f32863f7b579a6eaabfb37cbe816ac2e7136be67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:46 +0000

    Add package drf-extra-utils/1.0.0/drf-extra-utils-1.0.0.tar.gz

---
+++ packages/drf-extra-utils/drf-extra-utils-1.0.0.tar.gz/drf-extra-utils-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md') as readme:
+    README = readme.read()
+
+with open('requirements.txt') as requirements_txt:
+    requirements = requirements_txt.read().strip().splitlines()
+
+setup(
+    name='drf-extra-utils',
+    version='1.0.0',
+    license='MIT License',
+    author='Gabriel Lustosa',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    packages=['drf_extra_utils', 'drf_extra_utils.annotations', 'drf_extra_utils.related_object'],
+    author_email='lustosaki2@gmail.com',
+    description='Utils for django rest',
+    python_requires=">=3.8",
+)
