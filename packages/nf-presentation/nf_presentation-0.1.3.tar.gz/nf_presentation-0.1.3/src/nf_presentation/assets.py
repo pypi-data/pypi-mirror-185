da4110405ae43e59228b6ab8de5cd83415797268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:20 +0000

    Add package nf-presentation/0.1.3/nf_presentation-0.1.3.tar.gz

---
+++ packages/nf-presentation/nf_presentation-0.1.3.tar.gz/nf_presentation-0.1.3/src/nf_presentation/assets.py
@@ -0,0 +1,10 @@
+import importlib.resources as pkg_resources
+from .settings import RFS_LOGO_FILE_NAME
+
+def get_rfs_logo():
+    return pkg_resources.open_binary(__package__, RFS_LOGO_FILE_NAME)
+
+def get_test_data(short=True) -> dict:
+    resource_path= 'test_data_short.json' if short else 'test_data.json'
+    return pkg_resources.open_text(__package__, resource_path,encoding='utf8')
+
