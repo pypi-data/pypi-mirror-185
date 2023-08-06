25eb30d189a39c4a116a708951f5b330be6a6cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package dataclass-to-diagram/0.0.2/dataclass_to_diagram-0.0.2-py3-none-any.whl

---
+++ packages/dataclass-to-diagram/dataclass_to_diagram-0.0.2-py3-none-any.whl/dataclass_to_diagram/models/c4/rel/__init__.py
@@ -0,0 +1,25 @@
+from .rel import (
+    BiRel,
+    BiRelDown,
+    BiRelLeft,
+    BiRelRight,
+    BiRelUp,
+    Rel,
+    RelDown,
+    RelLeft,
+    RelRight,
+    RelUp,
+)
+
+__all__ = [
+    "BiRel",
+    "BiRelDown",
+    "BiRelLeft",
+    "BiRelRight",
+    "BiRelUp",
+    "Rel",
+    "RelDown",
+    "RelLeft",
+    "RelRight",
+    "RelUp",
+]
