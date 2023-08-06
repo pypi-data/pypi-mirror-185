e7625ccdd05f284c63cdd9133bc0690ed56f65f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package aiotieba/2.10.3/aiotieba-2.10.3.tar.gz

---
+++ packages/aiotieba/aiotieba-2.10.3.tar.gz/aiotieba-2.10.3/aiotieba/__init__.py
@@ -0,0 +1,45 @@
+"""
+Asynchronous I/O Client/Reviewer for Baidu Tieba
+
+@Author: starry.qvq@gmail.com
+@License: Unlicense
+@Documentation: https://v-8.top/
+"""
+
+import os
+
+from . import _logging as LOG
+from .__version__ import __version__
+from .client import Client
+from .client._classdef.enums import ReqUInfo
+from .client._classdef.user import UserInfo
+from .client._exception import ContentTypeError, HTTPStatusError, TiebaServerError
+from .client._typing import (
+    Appeal,
+    Comment,
+    Comments,
+    Post,
+    Posts,
+    ShareThread,
+    Thread,
+    Threads,
+    TypeFragAt,
+    TypeFragEmoji,
+    TypeFragImage,
+    TypeFragItem,
+    TypeFragLink,
+    TypeFragmentUnknown,
+    TypeFragText,
+    TypeFragTiebaPlus,
+    UserInfo_home,
+)
+from .database import MySQLDB, SQLiteDB
+from .reviewer import BaseReviewer, Ops, Punish, Reviewer
+
+if os.name == 'posix':
+    import signal
+
+    def terminate(signal_number, frame):
+        raise KeyboardInterrupt
+
+    signal.signal(signal.SIGTERM, terminate)
