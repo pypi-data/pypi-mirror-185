1820e798215ee75d22d487b67dffe6d9240bd309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package osw/0.1.1/osw-0.1.1.tar.gz

---
+++ packages/osw/osw-0.1.1.tar.gz/osw-0.1.1/docs/osl.md
@@ -0,0 +1,7 @@
+OpenSematicLab Class
+
+# OSL
+::: osl.OSL
+
+# OSL.SchemaRegistration
+::: osl.OSL.SchemaRegistration
---
+++ packages/osw/osw-0.1.1.tar.gz/osw-0.1.1/tests/test_wiki_tools.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+import osw.wiki_tools as wt 
+
+
+def test_create_flat_content_structure_from_wikitext():
+    wikitext = (
+        "{{Template"
+        "|param=value"
+        "}}"
+    )
+    expected = [
+        {"Template": {"param": ["value"]}}
+    ]
+    result = wt.create_flat_content_structure_from_wikitext(wikitext)
+    assert result == expected
