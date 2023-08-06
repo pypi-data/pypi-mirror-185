0e29463061497a18254b3ae8a418c6487f1095c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:11 +0000

    Add package spidermon/1.17.1/spidermon-1.17.1-py2.py3-none-any.whl

---
+++ packages/spidermon/spidermon-1.17.1-py2.py3-none-any.whl/tests/fixtures/items.py
@@ -0,0 +1,14 @@
+from scrapy import Item, Field
+
+
+class TreeItem(Item):
+    child = Field()
+
+
+class TestItem(Item):
+
+    __test__ = False
+
+    url = Field()
+    title = Field()
+    error_test = Field()
