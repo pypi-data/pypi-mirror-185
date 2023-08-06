bde4a27da2061824a4aa02cf59a3c8fe9cc3c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:09 +0000

    Add package kswutils/0.1.3/kswutils-0.1.3.tar.gz

---
+++ packages/kswutils/kswutils-0.1.3.tar.gz/kswutils-0.1.3/kswutils/accelerometer.py
@@ -0,0 +1,57 @@
+import os
+
+import numpy as np
+
+from .calculator import isfloat
+from .fileio import get_subdirectories, write_pickle
+
+
+class Accelerometer:
+    def __init__(self, filepath) -> None:
+        self.filepath = filepath
+
+    def get_A(self):
+        acc = []
+        with open(self.filepath, 'r') as f:
+            for i, line in enumerate(f):
+                if isfloat(line.strip('\n').split('\t')[0]):
+                    acc.append(float(line.strip('\n').split('\t')[1]))
+        return np.array(acc)
+
+    def get_sample_rate(self):
+        c = 0
+        ts = []
+        with open(self.filepath, 'r') as f:
+            for i, line in enumerate(f):
+                if isfloat(line.strip('\n').split('\t')[0]):
+                    ts.append(float(line.strip('\n').split('\t')[0]))
+                    c += 1
+                if c == 2:
+                    sr = 1 / (ts[1] - ts[0])
+                    break
+        return sr
+
+
+def combine_data(folder):
+    files = get_subdirectories(folder)
+
+    acc_ls = []
+
+    print('Concatenate files...')
+    for f in files:
+        if f.endswith('.lvm'):
+            print(os.path.basename(f))
+
+            accelerometer = Accelerometer(f)
+            acc_ls.append(accelerometer.get_A())
+
+    return np.concatenate(acc_ls)
+
+
+def save_pkl(inpdir, outdir, name):
+    print('Input:', inpdir)
+    data = combine_data(inpdir)
+
+    write_pickle(outdir, data, obj_name=name)
+    print('Saved to:', outdir)
+    return None
