96c749ceb3684c2635a1aba3fc7e0daf9979289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:54 +0000

    Add package peek-admin-app/3.1.8/peek-admin-app-3.1.8.tar.gz

---
+++ packages/peek-admin-app/peek-admin-app-3.1.8.tar.gz/peek-admin-app-3.1.8/peek_admin_app/src/main.ts
@@ -0,0 +1,18 @@
+import { enableProdMode } from "@angular/core"
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
+
+import { AppModule } from "./app.module"
+import { environment } from "./environments/environment"
+
+if (environment.production) {
+    enableProdMode()
+}
+
+import { VortexService } from "@synerty/vortexjs"
+
+const protocol = location.protocol.toLowerCase() == "https:" ? "wss" : "ws"
+VortexService.setVortexUrl(`${protocol}://${location.hostname}:${location.port}/vortexws`)
+VortexService.setVortexClientName("peek-admin-app")
+
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
