a011b6f7c58c935c311fd25dc94db80b5c932c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:50 +0000

    Add package test0000001/1.9/test0000001-1.9.tar.gz

---
+++ packages/test0000001/test0000001-1.9.tar.gz/test0000001-1.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test0000001",
+    version="1.9",
+    package_dir={'test0000001': 'test0000001'},
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'test0000001 = test0000001.steps:run_steps'
+        ]
+    }
+)
