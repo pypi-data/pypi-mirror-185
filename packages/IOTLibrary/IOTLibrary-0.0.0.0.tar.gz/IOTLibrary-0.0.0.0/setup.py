76ba509c729d1c717e46663c5db58cbd8a2d8424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:20 +0000

    Add package IOTLibrary/0.0.0.0/IOTLibrary-0.0.0.0.tar.gz

---
+++ packages/IOTLibrary/IOTLibrary-0.0.0.0.tar.gz/IOTLibrary-0.0.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+    
+setuptools.setup(
+    name = "IOTLibrary",
+    version = "0.0.0.0",
+    author = "TimmyHuang",
+    author_email="timmy.huang.iot@gmail.com",
+    description="IOTAILibrary",
+    long_description="IOT Library For AI Training",
+    long_description_content_type="text/markdown",
+    url="https://github.com/HuangTeEn",                                         packages=setuptools.find_packages(),     
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+    )
