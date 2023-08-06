d7227acc11e68d7911adab10eec54616ba007ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:39 +0000

    Add package generic-output-manager/0.0.3/generic-output-manager-0.0.3.tar.gz

---
+++ packages/generic-output-manager/generic-output-manager-0.0.3.tar.gz/generic-output-manager-0.0.3/genericoutput/storages/storage.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+import logging
+
+class Storage(ABC):
+
+    def __new__(cls, *args, **kw):
+        
+        
+        if 'storage_type' in kw and kw['storage_type'] is not None:
+            storage_type = kw['storage_type'].lower()
+        else:
+            storage_type = "filesystem"
+
+        # Create a map of all subclasses based on storage_type property (present on each subclass)
+        subclass_map = {subclass.storage_type: subclass for subclass in cls.__subclasses__()}
+
+
+        # Select the proper subclass based on
+        subclass = subclass_map[storage_type]
+        instance = super(Storage, subclass).__new__(subclass)
+        return instance
+    
+    def __init__(self, base_path, storage_type = None):
+        self.base_path = base_path
+        super().__init__()
+
+    @abstractmethod
+    def list_files(self):
+        pass
+
+    @abstractmethod
+    def save(self, name, path, metadata):
+        pass
---
+++ packages/generic-output-manager/generic-output-manager-0.0.3.tar.gz/generic-output-manager-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="generic-output-manager",
+    version="0.0.3",
+    author="Alida research team",
+    author_email="engineering-alida-lab@eng.it",
+    description="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = [
+        #"hdfs>=2.0.0",
+        "bda-service-utils",
+        #"s3fs==2022.1.0",
+        #"minio==7.1.8",
+        "alida-arg-parser>=0.0.35"
+        ],
+)
