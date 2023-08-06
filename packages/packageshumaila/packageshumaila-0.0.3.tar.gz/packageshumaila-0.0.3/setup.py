2a4eaddb85ce03d116efdc1adf31c8ef01d4f616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:45 +0000

    Add package packageshumaila/0.0.3/packageshumaila-0.0.3.tar.gz

---
+++ packages/packageshumaila/packageshumaila-0.0.3.tar.gz/packageshumaila-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name="packageshumaila",
+version="0.0.3",
+description= "a simple calculator..",
+Long_description="this is a calculator in which you can sum, subtract 2 numbers",
+author="shumaila",
+packages=['packageshumaila'],
+install_requires = [])
