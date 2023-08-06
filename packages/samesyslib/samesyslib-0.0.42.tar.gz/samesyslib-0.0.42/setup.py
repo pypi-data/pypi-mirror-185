8ef4b2029c6f7bb44d2690bbdc6cbe7f3c18eb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:26 +0000

    Add package samesyslib/0.0.42/samesyslib-0.0.42.tar.gz

---
+++ packages/samesyslib/samesyslib-0.0.42.tar.gz/samesyslib-0.0.42/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="samesyslib",
+    version=open("samesyslib/_version.py").readlines()[-1].split()[-1].strip("\"'"),
+    author="SameSystem",
+    author_email="linas.ziedas@samesystem.com",
+    description="Common libs used by SameSystem Data Science team.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samesystem-ds/samesyslib",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=["ruamel.yaml", "sqlalchemy", "pandas", "pymysql", "pydantic"],
+    python_requires=">=3.7",
+)
