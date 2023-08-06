0b72d726cc0a536b2272a73565fb5d8f45633d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package stonpy/0.2.1/stonpy-0.2.1.tar.gz

---
+++ packages/stonpy/stonpy-0.2.1.tar.gz/stonpy-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="stonpy",
+    version="0.2.1",
+    description="SBGN to Neo4j database",
+    author="Adrien Rougny",
+    author_email="adrienrougny@gmail.com",
+    packages=["stonpy"],
+    include_package_data=True,
+    install_requires=[
+        "libsbgnpy",
+        "py2neo",
+        "rdflib",
+        "bs4",
+        "python-magic",
+        "pandas",
+        "python-magic-bin; sys_platform == 'win32'",
+    ],
+    entry_points={
+        "console_scripts": [
+            "stonpy = stonpy.cli:main",
+        ],
+    },
+)
