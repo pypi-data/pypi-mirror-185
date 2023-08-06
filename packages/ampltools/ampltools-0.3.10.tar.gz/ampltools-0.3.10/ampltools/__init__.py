671dfc42d00c4e8eaf5e00ef493fe7af9f02e271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:02 +0000

    Add package ampltools/0.3.10/ampltools-0.3.10.tar.gz

---
+++ packages/ampltools/ampltools-0.3.10.tar.gz/ampltools-0.3.10/ampltools/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.3.10"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .pymodules import (
+    install_modules,
+    load_modules,
+)
+
+from .licenses import (
+    activate_license,
+)
+
+from .installer import (
+    ampl_installer,
+)
+
+from .utils import (
+    cloud_platform_name,
+)
