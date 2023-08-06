9cd98427571cf761db93eae9d5ef5b77744a06f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:33 +0000

    Add package ayaka/0.0.0.0b2/ayaka-0.0.0.0b2.tar.gz

---
+++ packages/ayaka/ayaka-0.0.0.0b2.tar.gz/ayaka-0.0.0.0b2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ayaka', 'ayaka.adapters']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ayaka',
+    'version': '0.0.0.0b2',
+    'description': '猫猫，猫猫！',
+    'long_description': '<div align="center">\n\n# Ayaka - 猫猫，猫猫！ - 0.0.0.0b2\n\n</div>\n',
+    'author': 'Su',
+    'author_email': 'wxlxy316@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://bridgel.github.io/ayaka/',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
