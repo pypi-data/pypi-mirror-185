15d1f21d7ab8871119e8beaccb3adbcebd078f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package elflepton/1.0.0/elflepton-1.0.0.tar.gz

---
+++ packages/elflepton/elflepton-1.0.0.tar.gz/elflepton-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="elflepton",
+    version="1.0.0",
+    license="MIT",
+    description="Lightweight ELF Parsing Tool",
+    author="Nikhil Ashok Hegde",
+    author_email="nikhilhegde20@gmail.com",
+    url="https://github.com/nikhilh-20/lepton",
+    packages=find_packages()
+)
