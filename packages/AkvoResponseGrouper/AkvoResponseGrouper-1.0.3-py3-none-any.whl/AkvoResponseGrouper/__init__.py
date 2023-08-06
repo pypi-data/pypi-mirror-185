4351a431476c849470bc6e642229ac91085c1292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:24 +0000

    Add package AkvoResponseGrouper/1.0.3/AkvoResponseGrouper-1.0.3-py3-none-any.whl

---
+++ packages/AkvoResponseGrouper/AkvoResponseGrouper-1.0.3-py3-none-any.whl/AkvoResponseGrouper/__init__.py
@@ -0,0 +1,4 @@
+from .response_grouper import response_grouper
+
+__version__ = "1.0.3"
+__all__ = ["response_grouper"]
---
+++ packages/AkvoResponseGrouper/AkvoResponseGrouper-1.0.3-py3-none-any.whl/AkvoResponseGrouper/routes/collection.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+from typing import Optional
+from ..db.connection import get_session
+from ..views import categories
+
+collection_route = APIRouter(
+    prefix="/collection",
+    tags=["AkvoResponseGrouper - Collection"],
+)
+
+
+@collection_route.get(
+    "/categories",
+    name="collection:get_index_category",
+    summary="get all category items",
+)
+async def get_index_category(
+    id: Optional[int] = Query(default=None),
+    data: Optional[int] = Query(default=None),
+    name: Optional[str] = Query(default=None),
+    category: Optional[str] = Query(default=None),
+    session: Session = Depends(get_session),
+):
+    res = categories.get(
+        id=id, data=data, name=name, category=category, session=session
+    )
+    return res
