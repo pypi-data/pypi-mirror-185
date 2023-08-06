a46750e48bb2f40f214a112eafb06d9a0ee4e0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:40 +0000

    Add package petals/1.1.0/petals-1.1.0.tar.gz

---
+++ packages/petals/petals-1.1.0.tar.gz/petals-1.1.0/src/petals/constants.py
@@ -0,0 +1,6 @@
+PUBLIC_INITIAL_PEERS = [
+    "/dns/bootstrap1.petals.ml/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
+    "/dns6/bootstrap1.petals.ml/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
+    "/dns/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
+    "/dns6/bootstrap2.petals.ml/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
+]
