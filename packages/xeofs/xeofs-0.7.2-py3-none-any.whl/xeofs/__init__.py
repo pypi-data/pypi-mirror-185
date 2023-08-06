b0379f099e34b4c55c99ff29965f984100a3f4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:57 +0000

    Add package xeofs/0.7.2/xeofs-0.7.2-py3-none-any.whl

---
+++ packages/xeofs/xeofs-0.7.2-py3-none-any.whl/xeofs/__init__.py
@@ -0,0 +1,4 @@
+from importlib.metadata import version
+from xeofs import models, pandas, xarray
+
+__version__ = version('xeofs')
---
+++ packages/xeofs/xeofs-0.7.2-py3-none-any.whl/xeofs/xarray/__init__.py
@@ -0,0 +1,6 @@
+from .eof import EOF
+from .rotator import Rotator
+from .bootstrapper import Bootstrapper
+from .mca import MCA
+from .mca_rotator import MCA_Rotator
+from .rock_pca import ROCK_PCA
