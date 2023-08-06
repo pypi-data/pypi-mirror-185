ca79259f157fbc4faa6f21a0d66e5fae77f485a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:13 +0000

    Add package anshils/0.1.5/anshils-0.1.5.tar.gz

---
+++ packages/anshils/anshils-0.1.5.tar.gz/anshils-0.1.5/anshils/__init__.py
@@ -0,0 +1,13 @@
+from .Gaussiandistribution import work_dy
+
+
+def helps():  # поддержка
+    print('get_vowels - Получаем гласные')
+    print('capitalize - Первая буква в верхнем регистре')
+    print('check_duplicate - Проверка дубликатов ')
+    print('Filtering - Фильтрация значений ')
+    print('ByteSize - Размер в байтах ')
+    print('anagrams - Анаграммы ')
+    print('palindrome - Проверка палиндромов ')
+    print('chunk - Разбиение на фрагменты ')
+    print('merge - Объединяем два словаря ')
---
+++ packages/anshils/anshils-0.1.5.tar.gz/anshils-0.1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anshils',
+      version='0.1.5',
+      description='pip install anshils',
+      packages=['anshils'],
+      author_email='i89041056398@gmail.com',
+      zip_safe=False)
