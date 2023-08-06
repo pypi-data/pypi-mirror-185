fb1d8a2278a579a7fe9886e1b0c22805307ea28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:30 +0000

    Add package nb-cli/1.0.0/nb-cli-1.0.0.tar.gz

---
+++ packages/nb-cli/nb-cli-1.0.0.tar.gz/nb-cli-1.0.0/nb_cli/handlers/venv.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Optional
+
+import virtualenv
+
+from .meta import get_default_python
+
+
+async def create_virtualenv(
+    venv_dir: Path,
+    prompt: Optional[str] = None,
+    python_interpreter: Optional[str] = None,
+):
+    if python_interpreter is None:
+        python_interpreter = await get_default_python()
+
+    args = ["--no-download", "--no-periodic-update", "--python", python_interpreter]
+
+    if prompt is not None:
+        args.extend(["--prompt", prompt])
+
+    args.append(str(venv_dir))
+
+    return virtualenv.cli_run(args)
