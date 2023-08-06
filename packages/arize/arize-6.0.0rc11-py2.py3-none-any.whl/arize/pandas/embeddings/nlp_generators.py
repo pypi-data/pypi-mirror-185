9e582fad465f7a265fb2507a41b6843886e7169f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package arize/6.0.0rc11/arize-6.0.0rc11-py2.py3-none-any.whl

---
+++ packages/arize/arize-6.0.0rc11-py2.py3-none-any.whl/arize/pandas/embeddings/nlp_generators.py
@@ -0,0 +1,56 @@
+from functools import partial
+from typing import Dict, cast
+
+import numpy as np
+import pandas as pd
+
+from .base_generators import NLPEmbeddingGenerator
+from .usecases import UseCases
+from .utils import logger
+
+try:
+    import torch
+    from datasets import Dataset
+except ImportError:
+    raise ImportError(
+        "To enable embedding generation, "
+        "the arize module must be installed with the EmbeddingGeneration option: "
+        "pip install 'arize[EmbeddingGeneration]'."
+    )
+
+
+class EmbeddingGeneratorForNLPSequenceClassification(NLPEmbeddingGenerator):
+    def __init__(self, model_name: str = "xlm-roberta-large", **kwargs):
+        super(EmbeddingGeneratorForNLPSequenceClassification, self).__init__(
+            use_case=UseCases.NLP.SEQUENCE_CLASSIFICATION,
+            model_name=model_name,
+            **kwargs
+        )
+
+    def generate_embeddings(self, df: pd.DataFrame, text_col: str) -> pd.Series:
+        if type(df) != pd.DataFrame:
+            raise TypeError("df must be a pandas DataFrame")
+        if type(text_col) != str:
+            raise TypeError("text_col must be a string")
+        if not text_col or text_col not in df.columns:
+            raise ValueError("text_col must be a column of the dataframe")
+
+        ds = Dataset.from_pandas(df[[text_col]])
+        ds.set_transform(partial(self.tokenize, text_col=text_col))
+        logger.info("Generating embedding vectors")
+        ds = ds.map(
+            lambda batch: self.__get_embedding_vector(batch),
+            batched=True,
+            batch_size=self.batch_size,
+        )
+        return cast(pd.DataFrame, ds.to_pandas())["embedding_vector"]
+
+    def __get_embedding_vector(
+        self, batch: Dict[str, torch.Tensor]
+    ) -> Dict[str, np.ndarray]:
+        with torch.no_grad():
+            outputs = self.model(**batch)
+        # (batch_size, seq_length/or/num_tokens, hidden_size)
+        # Select CLS token vector
+        embeddings = outputs.last_hidden_state[:, 0, :]
+        return {"embedding_vector": embeddings.cpu().numpy()}
