44eae61937397ef8f0c278203e11104d27a08d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:18 +0000

    Add package commonsharedclasses/0.0.17/commonsharedclasses-0.0.17.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.17.tar.gz/commonsharedclasses-0.0.17/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.17',
+    description='This package will contain all common classes, version 0.0.17 updates:- Added code to close SQL warehouse connection',
+    author='Abhilash Sharma',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
