cc1761bc476079eb33ce551ea412d878ebbb3f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:07 +0000

    Add package utils-api-pipefy/0.3.5/utils_api_pipefy-0.3.5.tar.gz

---
+++ packages/utils-api-pipefy/utils_api_pipefy-0.3.5.tar.gz/utils_api_pipefy-0.3.5/utils_api_pipefy/libs/excepts.py
@@ -0,0 +1,25 @@
+import json
+import sys
+import logging
+from traceback import (StackSummary, TracebackException, walk_tb)
+
+class exceptions(Exception):
+    
+    def __init__(self, *args: object) -> None:
+        self.args = args
+        self.stack_summary = StackSummary()
+        self.exc_info = sys.exc_info()
+        self.traceback_exception = TracebackException(*self.exc_info)
+        self.str_error = self.traceback_exception._str
+        self.object_error = walk_tb(self.exc_info[2])
+        self.frame_summary = self.stack_summary.extract(self.object_error)
+        
+        self.traceback_cause()
+
+    def traceback_cause(self):
+        try:
+            if self.traceback_exception.stack:
+                logging.error(json.dumps({"error":self.str_error, "path":self.frame_summary[0].filename, "line":self.frame_summary[0].lineno, "code":self.frame_summary[0]._line},indent=2))
+            logging.error(self.args)
+        except Exception as err:
+            print(err)
