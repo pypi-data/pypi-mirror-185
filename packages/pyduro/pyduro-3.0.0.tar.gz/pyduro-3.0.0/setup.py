a491fb15a2373e5d82f7ab17f005ccc004040fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:33 +0000

    Add package pyduro/3.0.0/pyduro-3.0.0.tar.gz

---
+++ packages/pyduro/pyduro-3.0.0.tar.gz/pyduro-3.0.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+
+# --------------------------------------------------------------------------------------------------
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Clément PRÉVOT",
+    author_email="clementprevot+pypi@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Home Automation",
+        "Natural Language :: English",
+        "Natural Language :: French",
+        "Development Status :: 4 - Beta",
+    ],
+    description="A Pypi library to communicate with Aduro (H1) wood/pellet burner via NBE communication",
+    install_requires=[],
+    keywords="aduro h1 wood pellet burner nbe",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="pyduro",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[],
+    python_requires=">=3.6",
+    url="https://github.com/clementprevot/pyduro",
+    version="3.0.0",
+)
---
+++ packages/pyduro/pyduro-3.0.0.tar.gz/pyduro-3.0.0/src/pyduro/actions/set.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------------------------------
+from pyduro.actions import SETTINGS
+from pyduro.protocol import (
+    FUNCTIONS,
+    FunctionNotFoundException,
+    PayloadToLargeException,
+)
+from pyduro.protocol.frame import Frame
+
+# --------------------------------------------------------------------------------------------------
+
+
+def run(burner_address, serial, pin_code, path, value, verbose=False):
+    """
+    Get information from the given burner.
+
+    Args:
+        burner_address (str): The IP address of the burner.
+        serial (str): The serial number of the burner (this can often be found on a sticker somewhere on the burner).
+            Note that this should be a 6 characters string. Any longer string will be truncated and any shorter string
+            will be left padded with 0s.
+        pin_code (str): The secret pincode of the burner (this can often be found on a sticker somewhere on the burner).
+            Note that this should be a 10 characters string. Any longer string will be truncated and any shorter string
+            will be right padded with 0s.
+        path (str): The path of the payload to modify on the burner.
+        value (str): The new value to set the payload on the burner.
+        verbose (bool): Indicates if we want the frame to be printed before sending it.
+            Default: False
+    """
+
+    try:
+        function_id = FUNCTIONS.set.value
+
+        if path is None or len(path) == 0:
+            print("You must pass one of the following as path: {}".format(SETTINGS))
+
+            return
+
+        payload = "{}={}".format(path, value)
+
+        frame = Frame(serial, pin_code, function_id, payload)
+
+        response = frame.send(burner_address, verbose=verbose)
+
+        return response
+    except FunctionNotFoundException as e:
+        print(e.message)
+    except PayloadToLargeException as e:
+        print(e.message)
