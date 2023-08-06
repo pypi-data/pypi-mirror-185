3f4d0c11b21dcec4ec6ba61ab012853c52092c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:44 +0000

    Add package pymeshfix/0.16.2/pymeshfix-0.16.2.tar.gz

---
+++ packages/pymeshfix/pymeshfix-0.16.2.tar.gz/pymeshfix-0.16.2/pymeshfix/__init__.py
@@ -0,0 +1,4 @@
+from pymeshfix import _meshfix
+from pymeshfix._meshfix import PyTMesh, clean_from_arrays, clean_from_file
+from pymeshfix._version import __version__
+from pymeshfix.meshfix import *
