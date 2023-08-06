e2945be6b68a66d738fc8a289e947a95f2abd04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:25 +0000

    Add package flake8-mock-spec/1.2.0/flake8_mock_spec-1.2.0-py3-none-any.whl

---
+++ packages/flake8-mock-spec/flake8_mock_spec-1.2.0-py3-none-any.whl/flake8_mock_spec.py
@@ -0,0 +1,151 @@
+"""A linter that checks mocks are constructed with the spec argument."""
+
+from __future__ import annotations
+
+import ast
+from typing import Iterator, NamedTuple
+from unittest import mock
+
+MOCK_CLASS: str = mock.Mock.__name__
+MAGIC_MOCK_CLASS: str = mock.MagicMock.__name__
+NON_CALLABLE_MOCK_CLASS: str = mock.NonCallableMock.__name__
+ASYNC_MOCK_CLASS: str = mock.AsyncMock.__name__
+MOCK_CLASSES = frozenset((MOCK_CLASS, MAGIC_MOCK_CLASS, NON_CALLABLE_MOCK_CLASS, ASYNC_MOCK_CLASS))
+SPEC_ARGS = frozenset(("spec", "spec_set"))
+
+ERROR_CODE_PREFIX = "TMS"
+MORE_INFO_BASE = "more information: https://github.com/jdkandersson/flake8-mock-spec"
+MOCK_SPEC_MSG_BASE = (
+    f"%s unittest.mock.%s instances should be constructed with the {' or '.join(SPEC_ARGS)} "
+    f"argument, {MORE_INFO_BASE}#fix-%s"
+)
+MOCK_SPEC_CODE = f"{ERROR_CODE_PREFIX}010"
+MOCK_SPEC_MSG = MOCK_SPEC_MSG_BASE % (MOCK_SPEC_CODE, MOCK_CLASS, MOCK_SPEC_CODE.lower())
+MAGIC_MOCK_SPEC_CODE = f"{ERROR_CODE_PREFIX}011"
+MAGIC_MOCK_SPEC_MSG = MOCK_SPEC_MSG_BASE % (
+    MAGIC_MOCK_SPEC_CODE,
+    MAGIC_MOCK_CLASS,
+    MAGIC_MOCK_SPEC_CODE.lower(),
+)
+NON_CALLABLE_MOCK_SPEC_CODE = f"{ERROR_CODE_PREFIX}012"
+NON_CALLABLE_MOCK_SPEC_MSG = MOCK_SPEC_MSG_BASE % (
+    NON_CALLABLE_MOCK_SPEC_CODE,
+    NON_CALLABLE_MOCK_CLASS,
+    NON_CALLABLE_MOCK_SPEC_CODE.lower(),
+)
+ASYNC_MOCK_SPEC_CODE = f"{ERROR_CODE_PREFIX}013"
+ASYNC_MOCK_SPEC_MSG = MOCK_SPEC_MSG_BASE % (
+    ASYNC_MOCK_SPEC_CODE,
+    ASYNC_MOCK_CLASS,
+    ASYNC_MOCK_SPEC_CODE.lower(),
+)
+MOCK_MSG_LOOKUP = {
+    MOCK_CLASS: MOCK_SPEC_MSG,
+    MAGIC_MOCK_CLASS: MAGIC_MOCK_SPEC_MSG,
+    NON_CALLABLE_MOCK_CLASS: NON_CALLABLE_MOCK_SPEC_MSG,
+    ASYNC_MOCK_CLASS: ASYNC_MOCK_SPEC_MSG,
+}
+
+# The attribute actually does exist, mypy reports that it doesn't
+PATCH_FUNCTION: str = mock.patch.__name__  # type: ignore
+PATCH_ARGS = frozenset(("new", "spec", "spec_set", "autospec", "new_callable"))
+PATCH_CODE = f"{ERROR_CODE_PREFIX}020"
+PATCH_MSG = (
+    f"{PATCH_CODE} unittest.mock.{PATCH_FUNCTION} should be called with any of the "
+    f"{', '.join(PATCH_ARGS)} arguments, {MORE_INFO_BASE}#fix-{PATCH_CODE.lower()}"
+)
+
+
+class Problem(NamedTuple):
+    """Represents a problem within the code.
+
+    Attrs:
+        lineno: The line number the problem occurred on
+        col_offset: The column the problem occurred on
+        msg: The message explaining the problem
+    """
+
+    lineno: int
+    col_offset: int
+    msg: str
+
+
+class Visitor(ast.NodeVisitor):
+    """Visits AST nodes and check mock construction calls.
+
+    Attrs:
+        problems: All the problems that were encountered.
+    """
+
+    problems: list[Problem]
+
+    def __init__(self) -> None:
+        """Construct."""
+        self.problems = []
+
+    # The function must be called the same as the name of the node
+    def visit_Call(self, node: ast.Call) -> None:  # pylint: disable=invalid-name
+        """Visit all Call nodes.
+
+        Args:
+            node: The Call node.
+        """
+        # Get the name of the node that has the call
+        name: str | None = None
+        if isinstance(node.func, ast.Name):
+            name = node.func.id
+        if isinstance(node.func, ast.Attribute):
+            name = node.func.attr
+
+        if name is not None and name in MOCK_CLASSES:
+            if not any(keyword.arg in SPEC_ARGS for keyword in node.keywords):
+                self.problems.append(
+                    Problem(
+                        lineno=node.lineno,
+                        col_offset=node.col_offset,
+                        msg=MOCK_MSG_LOOKUP[name],
+                    )
+                )
+
+        if name is not None and name == PATCH_FUNCTION:
+            if not any(keyword.arg in PATCH_ARGS for keyword in node.keywords):
+                self.problems.append(
+                    Problem(lineno=node.lineno, col_offset=node.col_offset, msg=PATCH_MSG)
+                )
+
+        # Ensure recursion continues
+        self.generic_visit(node)
+
+
+class Plugin:
+    """Checks mocks are constructed with the spec argument.
+
+    Attrs:
+        name: The name of the plugin.
+    """
+
+    # flake8 requires this class to exist
+    # pylint: disable=too-few-public-methods
+
+    name = __name__
+
+    def __init__(self, tree: ast.AST) -> None:
+        """Construct.
+
+        Args:
+            tree: The AST syntax tree for a file.
+        """
+        self._tree = tree
+
+    def run(self) -> Iterator[tuple[int, int, str, type["Plugin"]]]:
+        """Lint a file.
+
+        Yields:
+            All the issues that were found.
+        """
+        visitor = Visitor()
+        visitor.visit(self._tree)
+        yield from (
+            (problem.lineno, problem.col_offset, problem.msg, type(self))
+            for problem in visitor.problems
+        )
