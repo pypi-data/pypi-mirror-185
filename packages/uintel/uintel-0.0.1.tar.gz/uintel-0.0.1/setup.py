6a9f229f8accf9349bce6ad3ed6cfc23b9f7fbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:53 +0000

    Add package uintel/0.0.1/uintel-0.0.1.tar.gz

---
+++ packages/uintel/uintel-0.0.1.tar.gz/uintel-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools, os
+setuptools.setup(
+    # State the generic information about the package
+    name='uintel',
+    version='0.0.1',
+    author="Sam Archie",
+    author_email="sam.archie@urbanintelligence.co.nz",
+    description="Urban Intelligence's collection of Python code for everyday use",
+    long_description=open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    url="https://github.com/uintel/pyui/",
+    # State the dependecies of the package
+    install_requires=['matplotlib', 'colour', 'slack_sdk', 'sqlalchemy', 'sqlalchemy_utils', 'psycopg2', 'pyyaml', 'tqdm', 'paramiko', 'boto3', 'geopandas', 'esridump', 'requests'],
+    # State the files required for the package
+    package_data={"ui": ["styles/*.mplstyle", "fonts/*.ttf"],},
+)
---
+++ packages/uintel/uintel-0.0.1.tar.gz/uintel-0.0.1/src/uintel/__init__.py
@@ -0,0 +1,30 @@
+import os, glob, sys, json, uintel.install as install
+_config_path = os.path.expanduser("~/.ui/config")
+
+def reset(force=False):
+    """
+    Complete all necessary installations, including matplotlib stylesheets and Rubik fonts, onto this machine. To revert stylesheets back to default (in case they were manually edited) and revert fonts, use force=True. To re-create the configuration file, please delete the configuration file and run 'import ui'.
+    """
+    
+    if not os.path.exists(_config_path):
+        install._create_config_file()
+
+    install._install_styles(force)
+
+    fonts = {os.path.basename(filepath): filepath for filepath in glob.glob(os.path.dirname(__file__) + "/fonts/*.ttf")}
+    if not force:
+        fonts = install._get_uninstalled_fonts(fonts)
+
+    if sys.platform == "win32":
+        install._install_windows_fonts(fonts)
+    elif sys.platform == "linux":
+        install._install_linux_fonts(fonts)
+    else:
+        print(f"{install._printColours.RED}Unfortunately, the automatic downloading and installing Urban Intelligence's fonts has only been set up to be used for Windows or Linux distributions (because Sam doesn't have a Mac to test this code on). If the Rubik font is not installed on your device, please do so manually. Apolgies for the inconvience.{install._printColours.END}")
+
+reset()
+
+config = json.load(open(_config_path, "r"))
+
+import uintel.aws, uintel.colours, uintel.db, uintel.esri, uintel.server, uintel.slack
+__all__ = ["aws", "colours", "db", "esri", "server", "slack"]
