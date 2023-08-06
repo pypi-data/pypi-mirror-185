d70e3dff86f6985e6c3a92c17fd4fb31cf8af02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:08 +0000

    Add package xknx/2.3.0/xknx-2.3.0.tar.gz

---
+++ packages/xknx/xknx-2.3.0.tar.gz/xknx-2.3.0/xknx/telegram/telegram.py
@@ -0,0 +1,107 @@
+"""
+Module for KNX Telegrams.
+
+The telegram class is the leightweight interaction object between
+
+* business logic (Lights, Covers, etc) and
+* underlaying KNX/IP abstraction (KNX-Routing/KNX-Tunneling).
+
+It contains
+
+* the direction (incoming or outgoing)
+* the group address (e.g. 1/2/3)
+* and the payload (e.g. GroupValueWrite("12%")).
+
+"""
+from __future__ import annotations
+
+from datetime import datetime
+from enum import Enum
+
+from .address import GroupAddress, IndividualAddress, InternalGroupAddress
+from .apci import APCI
+from .tpci import TPCI, TDataBroadcast, TDataGroup, TDataIndividual
+
+
+class TelegramDirection(Enum):
+    """Enum class for the communication direction of a telegram (from KNX bus or to KNX bus)."""
+
+    INCOMING = "Incoming"
+    OUTGOING = "Outgoing"
+
+
+class Telegram:
+    """Class for KNX telegrams."""
+
+    def __init__(
+        self,
+        destination_address: GroupAddress
+        | IndividualAddress
+        | InternalGroupAddress
+        | None = None,
+        direction: TelegramDirection = TelegramDirection.OUTGOING,
+        payload: APCI | None = None,
+        source_address: IndividualAddress | None = None,
+        tpci: TPCI | None = None,
+    ) -> None:
+        """Initialize Telegram class."""
+        self.destination_address = destination_address or GroupAddress(0)
+        self.direction = direction
+        self.payload = payload
+        self.source_address = source_address or IndividualAddress(0)
+        self.timestamp = datetime.now()
+        self.tpci: TPCI
+        if tpci is None:
+            if isinstance(destination_address, GroupAddress):
+                if destination_address.raw == 0:
+                    self.tpci = TDataBroadcast()
+                else:
+                    self.tpci = TDataGroup()
+            elif isinstance(destination_address, IndividualAddress):
+                self.tpci = TDataIndividual()
+            else:  # InternalGroupAddress
+                self.tpci = TDataGroup()
+        else:
+            self.tpci = tpci
+
+    def __str__(self) -> str:
+        """Return object as readable string."""
+        data = f'payload="{self.payload}"' if self.payload else f'tpci="{self.tpci}"'
+        return (
+            "<Telegram "
+            f'direction="{self.direction.value}" '
+            f'source_address="{self.source_address}" '
+            f'destination_address="{self.destination_address}" '
+            f"{data} />"
+        )
+
+    def __repr__(self) -> str:
+        """Return object as string representation."""
+        return (
+            "Telegram("
+            f"destination_address={self.destination_address}, "
+            f"direction={self.direction}, "
+            f"payload={self.payload}, "
+            f"source_address={self.source_address}, "
+            f"tpci={self.tpci}"
+            ")"
+        )
+
+    def __eq__(self, other: object) -> bool:
+        """Equal operator."""
+        for key, value in self.__dict__.items():
+            if key == "timestamp":
+                continue
+            if key not in other.__dict__:
+                return False
+            if other.__dict__[key] != value:
+                return False
+        for key, value in other.__dict__.items():
+            if key not in self.__dict__:
+                return False
+        return True
+
+    def __hash__(self) -> int:
+        """Hash function."""
+        # used to turn lists of Telegram into sets in unittests
+        return hash(repr(self))
