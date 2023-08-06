8fb8724f2a4b2bd01bab9befc3cffdf5cf67dc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:30 +0000

    Add package pairwisedist/1.3.1/pairwisedist-1.3.1.tar.gz

---
+++ packages/pairwisedist/pairwisedist-1.3.1.tar.gz/pairwisedist-1.3.1/HISTORY.rst
@@ -0,0 +1,56 @@
+=======
+History
+=======
+
+1.3.1 (2023-01-13)
+------------------
+
+Changed
+********
+* Added support for Python 3.11.
+* Updated required versions for numpy and scipy.
+
+Fixed
+******
+* Fixed bug where setup.py would install a directory named tests into site-packages folder (thanks to `Bipin Kumar <https://github.com/kbipinkumar>`_)
+
+New Contributors
+*****************
+* `Bipin Kumar`_
+
+1.3.0 (2022-12-27)
+------------------
+
+Added
+******
+* Added sharpened_cosine_distance().
+
+Changed
+********
+* Added support for Python 3.8-3.10, and relinquished support for Python <=3.7.
+* Updated versions of requirements and developer requirements.
+
+1.2.0 (2020-06-21)
+------------------
+
+Added
+******
+* Updated API to make imports easier (for example: 'from pairwisedist import jackknife_distance' instead of 'from pairwisedist.pairwisedist import jackknife_distance').
+* Added pearson_distance() and spearman_distance().
+
+1.1.0 (2020-06-21)
+------------------
+
+Added
+******
+* Added jackknife_distance().
+
+
+1.0.0 (2020-06-18)
+------------------
+
+* First release on PyPI.
+
+Added
+******
+* Added ys1_distance() and yr1_distance().
---
+++ packages/pairwisedist/pairwisedist-1.3.1.tar.gz/pairwisedist-1.3.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().split('\n')
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Guy Teichman",
+    author_email='guyteichman@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    description='Calculate the pairwise-distance matrix for an array of *n* samples by *p* features, '
+                'sing a selection of distance metrics.',
+    install_requires=requirements,
+    python_requires='>=3.7',
+    license="License :: OSI Approved :: Apache Software License",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords=['distance', 'pairwise distance', 'YS1', 'YR1', 'pairwise-distance matrix', 'Son and Baek dissimilarities',
+              'Son and Baek'],
+    name='pairwisedist',
+    packages=find_packages(include=['pairwisedist']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/GuyTeichman/pairwisedist',
+    version='1.3.1',
+    zip_safe=False,
+)
