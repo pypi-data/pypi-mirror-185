8aa2da0d63b4c1a37b5d5b8bbdf27b262785cc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:14 +0000

    Add package corpus-base/0.1.6/corpus_base-0.1.6.tar.gz

---
+++ packages/corpus-base/corpus_base-0.1.6.tar.gz/corpus_base-0.1.6/corpus_base/utils/__init__.py
@@ -0,0 +1,16 @@
+from .convertor import DecisionHTMLConvertMarkdown, add_markdown_file
+from .ponente import RawPonente
+from .resources import (
+    CHIEF_DATES_VIEW,
+    DECISION_PATH,
+    JUSTICE_LOCAL,
+    MAX_JUSTICE_AGE,
+    CourtComposition,
+    DecisionCategory,
+    DecisionSource,
+    get_justices_from_api,
+    sc_jinja_env,
+)
+from .segmentize import clean_segment, validate_segment
+from .tags import tags_from_title
+from .voteline import extract_votelines, voteline_clean
