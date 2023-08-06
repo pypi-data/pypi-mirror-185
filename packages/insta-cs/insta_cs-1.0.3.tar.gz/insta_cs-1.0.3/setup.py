282f397d85a6862ba08b278ac2da3868c00aaf74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:00 +0000

    Add package insta-cs/1.0.3/insta_cs-1.0.3.tar.gz

---
+++ packages/insta-cs/insta_cs-1.0.3.tar.gz/insta_cs-1.0.3/setup.py
@@ -0,0 +1,51 @@
+from os import path
+import io
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    import unittest.mock
+    has_mock = True
+except ImportError:
+    has_mock = False
+
+__author__ = 'Charan <firstmodified@gmail.com>'
+__version__ = '1.0.3'
+
+packages = [
+    'insta_cs',
+    'insta_cs.endpoints',
+    'insta_web_cs'
+]
+test_reqs = [] if has_mock else ['mock']
+
+with io.open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='insta_cs',
+    version=__version__,
+    author='Charan',
+    author_email='firstmodified@gmail.com',
+    license='MIT',
+    # url='https://github.com/',
+    install_requires=[],
+    test_requires=test_reqs,
+    keywords='insta_cs instagram',
+    description='A client interface for the Instagram automation.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=packages,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
