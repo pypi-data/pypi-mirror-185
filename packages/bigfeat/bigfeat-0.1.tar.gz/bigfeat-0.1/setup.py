0812a6f62a12180297d26dad463211fa155bcff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:26 +0000

    Add package bigfeat/0.1/bigfeat-0.1.tar.gz

---
+++ packages/bigfeat/bigfeat-0.1.tar.gz/bigfeat-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name = "bigfeat",
+    version = "0.1",
+    #author = "Hassan Eldeeb, Shota Amashukeli, Radwa Elshawi",
+    #author_email = "firstName.lastName@ut.ee",
+    description = ("Automated feature engineering library"),
+    license = "MIT",
+    keywords = ["feature engineering", "machine learning", "automl", "feature extraction", "feature selection"],
+    url = "https://github.com/DataSystemsGroupUT/BigFeat",
+    packages=['bigfeat'],
+    long_description=("Automated feature engineering library"),
+          install_requires=[
+          'pandas',
+          'numpy',
+          'scikit-learn',
+          'lightgbm',
+      ],
+)
