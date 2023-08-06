884cd573cd64ae90d9ad5ab2dfc259037b714337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:43 +0000

    Add package python-lectio/1.15.0/python-lectio-1.15.0.tar.gz

---
+++ packages/python-lectio/python-lectio-1.15.0.tar.gz/python-lectio-1.15.0/lectio/_lektier.py
@@ -0,0 +1,34 @@
+from .imports import *
+from . import _utils
+
+def lektier(self):
+    url = f"https://www.lectio.dk/lectio/{self.skoleId}/material_lektieoversigt.aspx?elevid={self.elevId}"
+    resp = self.session.get(url)
+    if resp.url != url:
+        raise Exception("lectio-cookie udløbet")
+
+    soup = BeautifulSoup(resp.text, "html.parser")
+
+    lektier = []
+
+    renameDictionary = {
+        "Lærere": "Lærer",
+        "Lokaler": "Lokale"
+    }
+
+    for tr in soup.find_all("tr"):
+        modul = tr.find("a", class_="s2skemabrik")
+        modulDict = _utils.skemaBrikExtract(modul)
+        lektie = {
+            "dato": tr.find("th").get("b"),
+            "aktivitet": modulDict,
+            "note": tr.find_all("td")[1].text,
+            "lektier": {
+                "beskrivelse": tr.find("td", {"class": "ls-homework"}).find("a").text,
+                "link": tr.find("td", {"class": "ls-homework"}).find("a").get("href")
+            }
+        }
+
+        lektier.append(lektie)
+
+    return lektier
