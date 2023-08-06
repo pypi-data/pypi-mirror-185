be93b1be6e5938017923a3595bdb00df12f8db9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package pynguin/0.28.0/pynguin-0.28.0-py3-none-any.whl

---
+++ packages/pynguin/pynguin-0.28.0-py3-none-any.whl/pynguin/__version__.py
@@ -0,0 +1,8 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2023 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+"""Specifies the version of Pynguin."""
+__version__ = "0.28.0"
