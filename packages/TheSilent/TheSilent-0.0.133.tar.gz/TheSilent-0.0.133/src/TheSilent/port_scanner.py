335253d317b41fbc30030e610a3fcd93edc055c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:17 +0000

    Add package TheSilent/0.0.133/TheSilent-0.0.133.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.133.tar.gz/TheSilent-0.0.133/src/TheSilent/port_scanner.py
@@ -0,0 +1,43 @@
+from TheSilent.clear import *
+
+import socket
+import threading
+import time
+
+cyan = "\033[1;36m"
+
+my_socket = socket.socket()
+my_socket.settimeout(1)
+
+#scans for open ports on a server
+def port_scanner(url):
+    clear()
+
+    global open_port_list
+    open_port_list = []
+    
+    print(cyan + "scanning")
+    
+    for i in range(1, 1025):
+        my_thread = threading.Thread(target = port_scanner_thread, args = (url, i,))
+        my_thread.start()
+        my_thread.join()
+
+    open_port_list = list(dict.fromkeys(open_port_list))
+    open_port_list.sort()
+
+    clear()
+    
+    return open_port_list
+
+def port_scanner_thread(url, port):
+    global my_port_list
+
+    my_list = []
+    
+    try:
+        my_socket.connect((url, port))
+        open_port_list.append(port)
+
+    except:
+        pass
