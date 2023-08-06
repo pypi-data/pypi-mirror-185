8fbc484798945eb1bd8e1b7c68abc4695bb971a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:22 +0000

    Add package CNLTK-beta-version/0.113/CNLTK-Beta-version-0.113.tar.gz

---
+++ packages/CNLTK-beta-version/CNLTK-Beta-version-0.113.tar.gz/CNLTK-Beta-version-0.113/CNLTK/ceb_corpus.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import configparser
+from pathlib import Path
+import os
+
+# GET CORPUS
+def cebuano_corpus():
+    # ceb_corpus = pd.read_csv(corpus_csv_filepath)
+    
+    # file = Path('config.ini')
+    # config = configparser.ConfigParser()
+    # config.read(file)
+
+    # corpus_csv_filepath = config['PATHS']['corpus_csv_filepath']
+    # corpus_csv_filepath = config.get('PATHS', 'corpus_csv_filepath')
+    # corpus_csv_filepath = Path('Datasets/annotatedtestxt.csv')
+    corpus_csv_filepath = os.path.join(os.path.dirname(__file__), 'Datasets', 'annotatedtestxt.csv')
+
+    corpus_csv_filepath = pd.read_csv(corpus_csv_filepath)
+    
+    return corpus_csv_filepath
+
+cebuano_corpus()
