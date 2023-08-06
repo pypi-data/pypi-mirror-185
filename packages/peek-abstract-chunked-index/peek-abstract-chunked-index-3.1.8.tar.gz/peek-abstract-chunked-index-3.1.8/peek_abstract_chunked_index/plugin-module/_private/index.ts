f8999658951e635dd941b8a5f0e5262c47c4b80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:53 +0000

    Add package peek-abstract-chunked-index/3.1.8/peek-abstract-chunked-index-3.1.8.tar.gz

---
+++ packages/peek-abstract-chunked-index/peek-abstract-chunked-index-3.1.8.tar.gz/peek-abstract-chunked-index-3.1.8/peek_abstract_chunked_index/plugin-module/_private/index.ts
@@ -0,0 +1,12 @@
+export {
+    ChunkedIndexResultI
+} from "./chunked-index-loader/ChunkedIndexLoaderService"
+
+export {
+    ChunkedIndexServerStatusTuple
+} from "./admin/ChunkedIndexServerStatusTuple"
+
+export { OfflineConfigTuple } from "./tuples/OfflineConfigTuple"
+
+export { ChunkedIndexTupleService } from "./index-blueprint-tuple.service"
+
---
+++ packages/peek-abstract-chunked-index/peek-abstract-chunked-index-3.1.8.tar.gz/peek-abstract-chunked-index-3.1.8/peek_abstract_chunked_index/plugin-module/_private/tuples/OfflineConfigTuple.ts
@@ -0,0 +1,12 @@
+import { addTupleType, Tuple } from "@synerty/vortexjs"
+
+@addTupleType
+export class OfflineConfigTuple extends Tuple {
+    public static readonly tupleName = chunkedIndexTuplePrefix + "OfflineConfigTuple"
+    
+    cacheChunksForOffline: boolean = false
+    
+    constructor() {
+        super(OfflineConfigTuple.tupleName)
+    }
+}
