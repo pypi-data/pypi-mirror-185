fb02a255da59699264785b8b0117127d172191e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:19 +0000

    Add package ez-toolkits/2.6.2/ez_toolkits-2.6.2-py3-none-any.whl

---
+++ packages/ez-toolkits/ez_toolkits-2.6.2-py3-none-any.whl/toolkits/database.py
@@ -0,0 +1,102 @@
+import csv
+
+from loguru import logger
+from sqlalchemy import create_engine, text
+
+from toolkits import utils
+
+
+class Engine(object):
+
+    _engine = None
+
+    def __init__(self, *args, **kwargs):
+        ''' Initiation '''
+
+        self._engine = create_engine(*args, **kwargs)
+
+    def initializer(self):
+        """ensure the parent proc's database connections are not touched in the new connection pool"""
+        self._engine.dispose(close=False)
+
+    def execute(self, sql=None, sql_file=None, csv_file=None):
+
+        _sql = None
+
+        # 提取 SQL
+        if sql != None and sql != '':
+
+            _sql = sql
+
+        elif sql_file != None and sql_file != '':
+
+            if not utils.stat(sql_file, 'file'):
+                logger.error(f'No such file: {sql_file}')
+                return False
+
+            with open(sql_file, 'r') as _file:
+                _sql = _file.read()
+
+        else:
+
+            logger.error('SQL or SQL File is None')
+            return False
+
+        # ------------------------------------------------------------
+
+        try:
+
+            # 执行 SQL
+            with self._engine.connect() as connect:
+
+                logger.success('database connected')
+
+                try:
+
+                    logger.success('execute sql')
+
+                    _results = connect.execute(text(_sql))
+
+                    if csv_file == None:
+
+                        # 返回数据
+                        logger.success('return results')
+                        return _results
+
+                    else:
+
+                        # 导出数据
+                        with open(csv_file, 'w', encoding='utf-8-sig') as _file:
+                            logger.success(f'writer results to {csv_file}')
+                            outcsv = csv.writer(_file)
+                            outcsv.writerow(_results.keys())
+                            outcsv.writerows(_results)
+                            return True
+
+                except Exception as e:
+                    logger.error(e)
+                    return False
+
+        except Exception as e:
+            logger.error(e)
+            return False
+
+    def insert_one(self, table=None, field=None, data=None):
+        try:
+            logger.info('数据追加 ...')
+            with self._engine.connect() as connection:
+                if table == None:
+                    logger.error('table error')
+                    return False
+                if field == None:
+                    logger.error('field error')
+                    return False
+                if data == None:
+                    logger.error('data error')
+                    return False
+                connection.execute(f"INSERT INTO {table} ({field}) VALUES (%s)", (data))
+                logger.success(f'数据追加成功')
+                return True
+        except Exception as e:
+            logger.error(e)
+            return False
