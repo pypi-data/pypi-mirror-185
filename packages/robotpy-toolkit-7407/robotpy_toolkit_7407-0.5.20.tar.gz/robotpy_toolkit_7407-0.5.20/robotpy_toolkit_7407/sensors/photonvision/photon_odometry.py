cb2b8c92232cad55eddd89e38f77d797cee680d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:47 +0000

    Add package robotpy-toolkit-7407/0.5.20/robotpy_toolkit_7407-0.5.20.tar.gz

---
+++ packages/robotpy-toolkit-7407/robotpy_toolkit_7407-0.5.20.tar.gz/robotpy_toolkit_7407-0.5.20/robotpy_toolkit_7407/sensors/photonvision/photon_odometry.py
@@ -0,0 +1,35 @@
+from robotpy_toolkit_7407.sensors.photonvision.photon_camera import PhotonCamera
+from photonvision import PhotonUtils
+from robotpy_apriltag import AprilTagFieldLayout
+from robotpy_toolkit_7407.sensors.gyro import Gyro
+
+
+def LoadFieldLayout(json_path: str):
+    return AprilTagFieldLayout(json_path)
+
+class PhotonOdometry:
+    def __init__(self, camera: PhotonCamera, field_layout: AprilTagFieldLayout, gyro: Gyro):
+        self.camera = camera
+        self.field_layout = field_layout
+        self.gyro = gyro
+
+    def refresh(self):
+        self.camera.refresh()
+
+    def getRobotPose(self):
+        target = self.camera.latest_target
+
+        if target.relative_pose is None:
+            return None
+
+        return PhotonUtils.estimateFieldToRobot(
+            # cameraHeight=self.camera.height,
+            # targetHeight=self.field_layout.getTagPose(target.id).Y,
+            # cameraPitch=self.camera.pitch,
+            # targetPitch=target.pitch,
+            # targetYaw=target.yaw,
+            # gyroAngle=self.gyro.get_robot_heading(),
+            cameraToTarget=target.relative_pose,
+            fieldToTarget=self.field_layout.getTagPose(target.id),
+            cameraToRobot=self.camera.camera_to_robot_pose
+        )
---
+++ packages/robotpy-toolkit-7407/robotpy_toolkit_7407-0.5.20.tar.gz/robotpy_toolkit_7407-0.5.20/robotpy_toolkit_7407/sensors/photonvision/photon_target.py
@@ -0,0 +1,9 @@
+import photonvision
+
+class PhotonTarget:
+    def __init__(self, target: photonvision.PhotonTrackedTarget):
+        self.raw_target = target
+        self.relative_pose = self.raw_target.getBestCameraToTarget()
+        self.id = self.raw_target.getFiducialId()
+        self.pitch = self.raw_target.getPitch()
+        self.yaw = self.raw_target.getYaw()
