e27bcd9e4c80859874d52dfbc59b47ab4b3d314a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:16 +0000

    Add package ipywebcam/0.1.1.dev0/ipywebcam-0.1.1.dev0.tar.gz

---
+++ packages/ipywebcam/ipywebcam-0.1.1.dev0.tar.gz/ipywebcam-0.1.1.dev0/docs/source/_static/embed-bundle.js
@@ -0,0 +1,27 @@
+define("ipywebcam", [], () => { return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for D:\\project\\github\\ipywebcam\\src\\index.ts.\n    at makeSourceMapAndFinish (D:\\project\\github\\ipywebcam\\node_modules\\ts-loader\\dist\\index.js:53:18)\n    at successLoader (D:\\project\\github\\ipywebcam\\node_modules\\ts-loader\\dist\\index.js:40:5)\n    at Object.loader (D:\\project\\github\\ipywebcam\\node_modules\\ts-loader\\dist\\index.js:23:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});;
+//# sourceMappingURL=embed-bundle.js.map
