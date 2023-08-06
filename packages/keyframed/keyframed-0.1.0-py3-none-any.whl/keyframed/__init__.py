6fcbb02f589e790972262930ed070eadc3578e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:02 +0000

    Add package keyframed/0.1.0/keyframed-0.1.0-py3-none-any.whl

---
+++ packages/keyframed/keyframed-0.1.0-py3-none-any.whl/keyframed/__init__.py
@@ -0,0 +1,3 @@
+from .curve import Keyframe, Curve, PromptState, Prompt, ParameterGroup, register_interpolation_method
+
+__all__ = ['Keyframe','Curve','PromptState','Prompt','ParameterGroup', 'register_interpolation_method']
