c480b51ceed36cc771dc71bef9e3a07706ad522a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:10 +0000

    Add package pymars/0.10.0/pymars-0.10.0-cp37-cp37m-win_amd64.whl

---
+++ packages/pymars/pymars-0.10.0-cp37-cp37m-win_amd64.whl/mars/optimization/logical/core.py
@@ -0,0 +1,275 @@
+# Copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import functools
+import weakref
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from dataclasses import dataclass
+from enum import Enum
+from typing import Dict, List, Optional, Type
+
+from ...core import OperandType, EntityType, enter_mode
+from ...core.graph import EntityGraph
+from ...core.operand import Operand
+
+
+class OptimizationRecordType(Enum):
+    replace = 0
+    new = 1
+    delete = 2
+
+
+@dataclass
+class OptimizationRecord:
+    original_entity: EntityType = None
+    new_entity: EntityType = None
+    record_type: OptimizationRecordType = None
+
+
+class OptimizationRecords:
+    _records: List[OptimizationRecord]
+    _original_entity_to_records: Dict[EntityType, OptimizationRecord]
+
+    def __init__(self):
+        self._records = list()
+        self._original_entity_to_records = dict()
+        self._optimized_entity_to_records = dict()
+
+    def append_record(self, record: OptimizationRecord):
+        self._records.append(record)
+        if record.record_type in (
+            OptimizationRecordType.replace,
+            OptimizationRecordType.delete,
+        ):
+            self._original_entity_to_records[record.original_entity] = record
+        if record.record_type in (
+            OptimizationRecordType.new,
+            OptimizationRecordType.replace,
+        ):
+            self._optimized_entity_to_records[record.new_entity] = record
+
+    def get_optimization_result(
+        self, original_entity: EntityType, default: Optional[EntityType] = None
+    ) -> EntityType:
+        entity = original_entity
+        if entity not in self._original_entity_to_records:
+            return default
+        while entity in self._original_entity_to_records:
+            record = self._original_entity_to_records[entity]
+            if record.record_type == OptimizationRecordType.replace:
+                entity = record.new_entity
+            else:
+                assert record.record_type == OptimizationRecordType.delete
+                return None
+        return entity
+
+    def get_original_entity(
+        self, optimized_entity: EntityType, default: Optional[EntityType] = None
+    ) -> EntityType:
+        entity = optimized_entity
+        if entity not in self._optimized_entity_to_records:
+            return default
+        while entity in self._optimized_entity_to_records:
+            record = self._optimized_entity_to_records[entity]
+            if record.record_type == OptimizationRecordType.replace:
+                entity = record.original_entity
+            else:
+                assert record.record_type == OptimizationRecordType.new
+                return None
+        return entity
+
+
+class OptimizationRule(ABC):
+    _preds_to_remove = weakref.WeakKeyDictionary()
+
+    def __init__(
+        self,
+        graph: EntityGraph,
+        records: OptimizationRecords,
+        optimizer_cls: Type["Optimizer"],
+    ):
+        self._graph = graph
+        self._records = records
+        self._optimizer_cls = optimizer_cls
+        self._cached_rule = functools.lru_cache(maxsize=None)(
+            lambda _rule_type: _rule_type(
+                self._graph, self._records, self._optimizer_cls
+            )
+        )
+
+    @abstractmethod
+    def match(self, op: OperandType) -> bool:
+        """
+        If this operand matches this rule.
+
+        Parameters
+        ----------
+        op : OperandType
+            Operand.
+
+        Returns
+        -------
+        matched : bool
+            Matched rule or not.
+        """
+
+    @abstractmethod
+    def apply(self, op: OperandType):
+        """
+        Apply rule to an operand.
+
+        Parameters
+        ----------
+        op : OperandType
+            Operand
+        """
+
+    def _replace_node(self, original_node: EntityType, new_node: EntityType):
+        predecessors = self._graph.predecessors(original_node)
+        successors = self._graph.successors(original_node)
+        self._graph.remove_node(original_node)
+        self._graph.add_node(new_node)
+        for pred in predecessors:
+            self._graph.add_edge(pred, new_node)
+        for succ in successors:
+            self._graph.add_edge(new_node, succ)
+
+    def _add_collapsable_predecessor(self, node: EntityType, predecessor: EntityType):
+        pred_original = self._records.get_original_entity(predecessor, predecessor)
+        if predecessor not in self._preds_to_remove:
+            self._preds_to_remove[pred_original] = {node}
+        else:
+            self._preds_to_remove[pred_original].add(node)
+
+    def _remove_collapsable_predecessors(self, node: EntityType):
+        node = self._records.get_optimization_result(node) or node
+        preds_opt_to_remove = []
+        for pred in self._graph.predecessors(node):
+            pred_original = self._records.get_original_entity(pred, pred)
+            pred_opt = self._records.get_optimization_result(pred, pred)
+
+            if pred_opt in self._graph.results or pred_original in self._graph.results:
+                continue
+            affect_succ = self._preds_to_remove.get(pred_original) or []
+            affect_succ_opt = [
+                self._records.get_optimization_result(s, s) for s in affect_succ
+            ]
+            if all(s in affect_succ_opt for s in self._graph.successors(pred)):
+                preds_opt_to_remove.append((pred_original, pred_opt))
+
+        for pred_original, pred_opt in preds_opt_to_remove:
+            self._graph.remove_node(pred_opt)
+            self._records.append_record(
+                OptimizationRecord(pred_original, None, OptimizationRecordType.delete)
+            )
+
+
+class Optimizer(ABC):
+    _rules: List[Type[OptimizationRule]]
+    _op_to_rules: Dict[Type[OperandType], List[Type[OptimizationRule]]]
+
+    @classmethod
+    def register_rule(
+        cls, operand_types: List[Type[OperandType]], rule: Type[OptimizationRule]
+    ):
+        if not hasattr(cls, "_rules"):
+            cls._rules = []
+        cls._rules.append(rule)
+
+        if not hasattr(cls, "_op_to_rules"):
+            cls._op_to_rules = defaultdict(list)
+        for operand_type in operand_types:
+            cls._op_to_rules[operand_type].append(rule)
+
+    @classmethod
+    def get_rule_types(
+        cls, operand_type: Type[OperandType]
+    ) -> List[Type[OptimizationRule]]:
+        rule_types = cls._op_to_rules.get(operand_type, None)
+        if rule_types is None:
+            for op_cls in operand_type.__mro__:
+                if op_cls is Operand:
+                    break
+                rule_types = cls._op_to_rules.get(op_cls)
+                if rule_types is not None:
+                    break
+            cls._op_to_rules[operand_type] = rule_types or []
+        return rule_types
+
+    @classmethod
+    def _replace_inputs(cls, graph: EntityGraph, records: OptimizationRecords):
+        for node in graph:
+            for succ in graph.successors(node):
+                input_optimized = False
+                new_inputs = []
+                for inp in succ.inputs:
+                    optimized = records.get_optimization_result(inp)
+                    if optimized is None:
+                        optimized = inp
+                    if optimized is not inp:
+                        input_optimized = True
+                    new_inputs.append(optimized)
+                if input_optimized:
+                    succ.inputs = new_inputs
+
+    @classmethod
+    @enter_mode(build=True)
+    def optimize(cls, graph: EntityGraph) -> OptimizationRecords:
+        """
+        Optimize a graph.
+
+        Parameters
+        ----------
+        graph : EntityGraph
+            Tileable or chunk graph.
+
+        Returns
+        -------
+        optimization_records : OptimizationRecords
+            Optimization records.
+        """
+        records = OptimizationRecords()
+        optimized = False
+        cached_rule = functools.lru_cache(maxsize=None)(
+            lambda _rule_type: _rule_type(graph, records, cls)
+        )
+
+        for rule_type in cls._rules:
+            visited = set()
+            for entity in list(graph.topological_iter()):
+                op = entity.op
+                if op in visited:
+                    continue
+                visited.add(op)
+
+                rule_types = cls.get_rule_types(type(op)) or []
+                if rule_type not in rule_types:
+                    continue
+
+                rule = cached_rule(rule_type)
+                if entity not in graph:  # pragma: no cover
+                    # maybe removed during optimization
+                    continue
+                if rule.match(op):
+                    optimized = True
+                    rule.apply(op)
+        if optimized:
+            cls._replace_inputs(graph, records)
+            new_results = []
+            for result in graph.results:
+                new_results.append(
+                    records.get_optimization_result(result, default=result)
+                )
+            graph.results = new_results
+        return records
---
+++ packages/pymars/pymars-0.10.0-cp37-cp37m-win_amd64.whl/mars/serialization/aio.py
@@ -0,0 +1,150 @@
+# Copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import struct
+from io import BytesIO
+from typing import Any
+
+import cloudpickle
+import numpy as np
+
+from ..utils import lazy_import
+from .core import serialize_with_spawn, deserialize
+
+cupy = lazy_import("cupy")
+cudf = lazy_import("cudf")
+
+DEFAULT_SERIALIZATION_VERSION = 1
+DEFAULT_SPAWN_THRESHOLD = 100
+BUFFER_SIZES_NAME = "buf_sizes"
+
+
+class AioSerializer:
+    def __init__(self, obj: Any, compress=0):
+        self._obj = obj
+        self._compress = compress
+
+    async def _get_buffers(self):
+        headers, buffers = await serialize_with_spawn(
+            self._obj, spawn_threshold=DEFAULT_SPAWN_THRESHOLD
+        )
+
+        def _is_cuda_buffer(buf):  # pragma: no cover
+            if cupy is not None and cudf is not None:
+                from cudf.core.buffer import Buffer as CPBuffer
+                from cupy import ndarray as cp_ndarray
+            else:
+                CPBuffer = cp_ndarray = None
+
+            if CPBuffer is not None and isinstance(buf, CPBuffer):
+                return True
+            elif cp_ndarray is not None and isinstance(buf, cp_ndarray):
+                return True
+            else:
+                return False
+
+        is_cuda_buffers = [_is_cuda_buffer(buf) for buf in buffers]
+        headers[0]["is_cuda_buffers"] = np.array(is_cuda_buffers)
+
+        # add buffer lengths into headers
+        headers[0][BUFFER_SIZES_NAME] = [
+            getattr(buf, "nbytes", len(buf)) for buf in buffers
+        ]
+        header = cloudpickle.dumps(headers)
+
+        # gen header buffer
+        header_bio = BytesIO()
+        # write version first
+        header_bio.write(struct.pack("B", DEFAULT_SERIALIZATION_VERSION))
+        # write header length
+        header_bio.write(struct.pack("<Q", len(header)))
+        # write compression
+        header_bio.write(struct.pack("<H", self._compress))
+        # write header
+        header_bio.write(header)
+
+        out_buffers = list()
+        out_buffers.append(header_bio.getbuffer())
+        out_buffers.extend(buffers)
+
+        return out_buffers
+
+    async def run(self):
+        return await self._get_buffers()
+
+
+MALFORMED_MSG = """\
+Received malformed data, please check Mars version on both side,
+if error occurs when using `mars.new_session('http://web_ip:web_port'),
+please check if web port is right."""
+
+
+class AioDeserializer:
+    def __init__(self, file):
+        self._file = file
+
+    def _readexactly(self, n: int):
+        # asyncio StreamReader may not guarantee to read n bytes
+        # for it we need to call `readexactly` instead
+        read = (
+            self._file.readexactly
+            if hasattr(self._file, "readexactly")
+            else self._file.read
+        )
+        return read(n)
+
+    async def _get_obj_header_bytes(self):
+        try:
+            header_bytes = bytes(await self._file.read(11))
+        except ConnectionResetError:
+            raise EOFError("Server may be closed")
+        if len(header_bytes) == 0:
+            raise EOFError("Received empty bytes")
+        version = struct.unpack("B", header_bytes[:1])[0]
+        # now we only have default version
+        assert version == DEFAULT_SERIALIZATION_VERSION, MALFORMED_MSG
+        # header length
+        header_length = struct.unpack("<Q", header_bytes[1:9])[0]
+        # compress
+        _ = struct.unpack("<H", header_bytes[9:])[0]
+        return await self._readexactly(header_length)
+
+    async def _get_obj(self):
+        header = cloudpickle.loads(await self._get_obj_header_bytes())
+        # get buffer size
+        buffer_sizes = header[0].pop(BUFFER_SIZES_NAME)
+        # get buffers
+        buffers = [await self._readexactly(size) for size in buffer_sizes]
+        # get num of objs
+        num_objs = header[0].get("_N", 0)
+
+        if num_objs <= DEFAULT_SPAWN_THRESHOLD:
+            return deserialize(header, buffers)
+        else:
+            return await asyncio.to_thread(deserialize, header, buffers)
+
+    async def run(self):
+        return await self._get_obj()
+
+    async def get_size(self):
+        # extract header
+        header_bytes = await self._get_obj_header_bytes()
+        header = cloudpickle.loads(header_bytes)
+        # get buffer size
+        buffer_sizes = header[0].pop(BUFFER_SIZES_NAME)
+        return 11 + len(header_bytes) + sum(buffer_sizes)
+
+    async def get_header(self):
+        return cloudpickle.loads(await self._get_obj_header_bytes())
