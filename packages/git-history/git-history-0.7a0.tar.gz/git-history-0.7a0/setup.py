3d63ac7f34c4d48095530b297cc26f9562fe5d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:38 +0000

    Add package git-history/0.7a0/git-history-0.7a0.tar.gz

---
+++ packages/git-history/git-history-0.7a0.tar.gz/git-history-0.7a0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+
+VERSION = "0.7a0"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="git-history",
+    description="Tools for analyzing Git history using SQLite",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/git-history",
+    project_urls={
+        "Issues": "https://github.com/simonw/git-history/issues",
+        "CI": "https://github.com/simonw/git-history/actions",
+        "Changelog": "https://github.com/simonw/git-history/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["git_history"],
+    entry_points="""
+        [console_scripts]
+        git-history=git_history.cli:cli
+    """,
+    install_requires=["click", "GitPython", "sqlite-utils>=3.19"],
+    extras_require={"test": ["pytest", "cogapp"]},
+    python_requires=">=3.7",
+)
