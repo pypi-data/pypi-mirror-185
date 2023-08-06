41e9df153227cc2cc306e1d42182628702f43811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:30 +0000

    Add package PyThat/0.1.0/PyThat-0.1.0-py3-none-any.whl

---
+++ packages/PyThat/PyThat-0.1.0-py3-none-any.whl/PyThat/helpers/BLS.py
@@ -0,0 +1,15 @@
+import xarray
+
+
+def norm_on_FO(x, name='Frequency' ,f_max=2):
+    """
+    Normalize a given dataset x on the average of the FO signal around zero frequency.
+    :param x: xarray object
+    :param name: Name of the Frequency coordinate axis along which the frequency is to be selected
+    :param f_max: specifies the width of the frequency area over which is to be normalized
+    :return: normalized xarray object
+    """
+    return x/x.where(abs(x[name]) < f_max).mean(name).squeeze()
+
+xarray.DataArray.norm_on_FO = norm_on_FO
+xarray.Dataset.norm_on_FO = norm_on_FO
