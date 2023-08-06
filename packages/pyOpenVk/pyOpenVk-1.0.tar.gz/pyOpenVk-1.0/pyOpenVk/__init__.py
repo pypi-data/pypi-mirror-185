76b347899be881fa1975d0878f583031d7dfcdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:18 +0000

    Add package pyOpenVk/1.0/pyOpenVk-1.0.tar.gz

---
+++ packages/pyOpenVk/pyOpenVk-1.0.tar.gz/pyOpenVk-1.0/pyOpenVk/__init__.py
@@ -0,0 +1,24 @@
+"""
+:authors: Ptushkea
+:license: MIT License, see LICENSE file
+
+:copyright: (c) 2023 Ptushkea
+"""
+
+
+from .api import *
+from .account import *
+from .friends import *
+from .groups import *
+from .likes import *
+from .messages import *
+from .news_feed import *
+from .ovk import *
+from .users import *
+from .utils import *
+from .wall import *
+
+
+__author__ = 'Ptushkea'
+__version__ = '1.0'
+__email__ = 'alyonalisicyna@gmail.com'
---
+++ packages/pyOpenVk/pyOpenVk-1.0.tar.gz/pyOpenVk-1.0/pyOpenVk/utils.py
@@ -0,0 +1,5 @@
+class utils:
+
+    @staticmethod
+    def get_server_time(client):
+        return http.get(f'https://{client["instance"]}/method/Utils.getServerTime?access_token={client["token"]}').json()['response']
