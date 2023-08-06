09eb5c6735ce16de5d60c970556b4c39049a3b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package lalitsc-distributions/0.1/lalitsc_distributions-0.1.tar.gz

---
+++ packages/lalitsc-distributions/lalitsc_distributions-0.1.tar.gz/lalitsc_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lalitsc_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lalitsc_distributions'],
+      author = "Lalit Shankar Chowdhury",
+      author_email = "lalitshankarch@gmail.com",
+      license = "MIT",
+      long_description="Test package to upload to PyPI for the \"AI Programming with Python Nanodegree\" course on Udacity. Contains classes to construct Gaussian and Binomial distributions.",
+      zip_safe=False)
