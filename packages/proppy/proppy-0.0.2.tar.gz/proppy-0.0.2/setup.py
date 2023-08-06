09adbe06996a58ea4a49896f12fb136bc8164fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:24 +0000

    Add package proppy/0.0.2/proppy-0.0.2.tar.gz

---
+++ packages/proppy/proppy-0.0.2.tar.gz/proppy-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='proppy',
+    version='0.0.2',
+    description='',
+    long_description='',
+    url='https://www.see-through.com',
+    author='izaid',
+    author_email='irwin.zaid@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
