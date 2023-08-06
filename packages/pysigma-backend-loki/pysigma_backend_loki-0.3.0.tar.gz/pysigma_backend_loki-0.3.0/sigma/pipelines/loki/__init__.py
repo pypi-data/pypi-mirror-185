a672479f1774557dc9caa6bd7b80ba1dfa88cc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:41 +0000

    Add package pysigma-backend-loki/0.3.0/pysigma_backend_loki-0.3.0.tar.gz

---
+++ packages/pysigma-backend-loki/pysigma_backend_loki-0.3.0.tar.gz/pysigma_backend_loki-0.3.0/sigma/pipelines/loki/__init__.py
@@ -0,0 +1,16 @@
+from .loki import (
+    LokiCustomAttrs,
+    SetLokiParserTransformation,
+    SetLokiStreamSelectionTransform,
+    loki_grafana_logfmt,
+    loki_promtail_sysmon_message,
+)
+
+
+__all__ = (
+    "LokiCustomAttrs",
+    "SetLokiParserTransformation",
+    "SetLokiStreamSelectionTransform",
+    "loki_grafana_logfmt",
+    "loki_promtail_sysmon_message",
+)
