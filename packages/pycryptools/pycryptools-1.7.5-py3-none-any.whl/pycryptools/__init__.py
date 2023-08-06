ba32a29ea1932b29c1cf66db6b65ab0dc5fba5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:28 +0000

    Add package pycryptools/1.7.5/pycryptools-1.7.5-py3-none-any.whl

---
+++ packages/pycryptools/pycryptools-1.7.5-py3-none-any.whl/pycryptools/__init__.py
@@ -0,0 +1,29 @@
+"""
+██╗    ██╗██╗   ██╗ █████╗ ██╗     
+██║    ██║██║   ██║██╔══██╗██║     
+██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+██║███╗██║██║   ██║██╔══██║██║     
+╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+PyCrypTools:
+    It is currently under early development, but little by little new algorithms will be added.
+    PyCrypTools is a python library that brings us a series of algorithms to encrypt and decrypt inputs.
+    See algorithms >> https://github.com/14wual/pycryptools/tree/main/about
+
+See proyect >> https://github.com/14wual/pycryptools
+Follow me >> https://twitter.com/codewual
+"""
+
+# --------------------Intern Imports--------------------
+from pycryptools import atbash
+from pycryptools import scytale
+from pycryptools import polybios
+from pycryptools import caesar
+from pycryptools import alberti
+from pycryptools import jefferson
+from pycryptools import morse
+
+# No avilable
+#from testing import trithemius
+
