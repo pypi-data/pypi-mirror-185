ea86529a0eb075b8d87d1f44f724bf2aa4684a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package arcane-google-analytics/1.1.10/arcane_google_analytics-1.1.10.tar.gz

---
+++ packages/arcane-google-analytics/arcane_google_analytics-1.1.10.tar.gz/arcane_google_analytics-1.1.10/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arcane', 'arcane.google_analytics']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arcane-core>=1.7,<2.0',
+ 'arcane-credentials==0.1.0',
+ 'arcane-datastore>=1.1.0,<2.0.0',
+ 'arcane-requests>=0,<1',
+ 'google-api-python-client>=1.7.8',
+ 'google-auth-httplib2==0.1.0']
+
+setup_kwargs = {
+    'name': 'arcane-google-analytics',
+    'version': '1.1.10',
+    'description': 'A package to use Google Analytics API',
+    'long_description': '# Arcane google-analytics\n\nThis package helps us to interact with Google Analytics API\n',
+    'author': 'Arcane',
+    'author_email': 'product@arcane.run',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
