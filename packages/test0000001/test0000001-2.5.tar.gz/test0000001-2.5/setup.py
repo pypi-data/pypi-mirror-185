80a68609ec6044d4d9b2cd4aa8e4d65364486ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:34 +0000

    Add package test0000001/2.5/test0000001-2.5.tar.gz

---
+++ packages/test0000001/test0000001-2.5.tar.gz/test0000001-2.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test0000001",
+    version="2.5",
+    package_dir={'test0000001': 'test0000001'},
+    packages=find_packages(),
+    install_requires=[
+        'pyrebase',
+        'uuid',
+        'python-dotenv',
+        'logging',
+    ],
+    entry_points={
+        'console_scripts': [
+            'test0000001 = test0000001.steps:run_steps'
+        ]
+    }
+)
