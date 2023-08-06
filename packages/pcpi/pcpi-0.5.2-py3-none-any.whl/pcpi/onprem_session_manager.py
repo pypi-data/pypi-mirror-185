ba900b1f79e1ad75182f9928961ca4ea7c3f2913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:46 +0000

    Add package pcpi/0.5.2/pcpi-0.5.2-py3-none-any.whl

---
+++ packages/pcpi/pcpi-0.5.2-py3-none-any.whl/pcpi/onprem_session_manager.py
@@ -0,0 +1,31 @@
+#Local
+from ._session_types import CWPSession
+
+class CWPSessionManager:
+    def __init__(self, tenant_name: str, api_url: str, uname: str, passwd: str, verify, logger: object):
+        """
+        Initializes a Prisma Cloud API Session Manager.
+
+        Keyword Arguments:
+        tenant_name -- Name of tenant associated with session
+        a_key -- Tenant Access Key
+        s_key -- Tenant Secret Key
+        api_url -- API URL Tenant is hosted on 
+        """
+        self.logger = logger
+
+        self.tenant = tenant_name
+        self.uname = uname
+        self.passwd = passwd
+        self.api_url = api_url
+
+        self.verify = verify
+
+        self.cwp_session = {}        
+
+
+#==============================================================================
+    def create_cwp_session(self):
+        session = CWPSession(self.tenant, self.api_url, self.uname, self.passwd, self.verify, self.logger)
+        self.cwp_session = session
+        return session
