eaf58c084cb50ac1f520be55d4991742677024e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:03 +0000

    Add package TheSilent/0.0.147/TheSilent-0.0.147-py3-none-any.whl

---
+++ packages/TheSilent/TheSilent-0.0.147-py3-none-any.whl/TheSilent/nmap.py
@@ -0,0 +1,85 @@
+from TheSilent.clear import *
+from ipaddress import *
+
+import socket
+import threading
+import time
+
+cyan = "\033[1;36m"
+
+#global variables mainly used for multi-threading
+host_up_list = []
+nmap_list = []
+open_port_list = []
+
+my_socket = socket.socket()
+my_socket.settimeout(1)
+
+def host_up(port, my_ip):
+    global host_up_list
+
+    my_socket = socket.socket()
+    my_socket.settimeout(1)
+
+    try:
+        my_socket.connect((my_ip, port))
+        host_up_list.append(my_ip)
+
+    except ConnectionRefusedError:
+        host_up_list.append(my_ip)
+        
+    except OSError:
+        pass
+
+    except TimeoutError:
+        host_up_list.append(my_ip)
+
+#scan entire network
+def nmap(ip, subnet):
+    global host_up_list
+    global nmap_list
+
+    host_up_list = []
+    nmap_list = []
+    
+    my_list = []
+
+    port_list = [20, 21, 22, 23, 25, 53, 67, 67, 80, 443]
+
+    clear()
+
+    ip_list = list(ip_network(str(ip) + "/" + str(subnet)).hosts())
+
+    for i in ip_list:
+        print(cyan + "scanning: " + str(i))
+
+        my_thread = threading.Thread(target = host_up, args = (80, str(i),))
+        my_thread.start()
+
+    clear()
+
+    print(str(len(host_up_list)) + " potential hosts up")
+    
+    for my_ip in host_up_list:
+        print(cyan + "scanning: " + str(my_ip))
+
+        for port in port_list:
+            my_thread = threading.Thread(target = nmap_thread, args = (str(my_ip), port,))
+            my_thread.start()
+
+    nmap_list = list(dict.fromkeys(nmap_list))
+    nmap_list.sort()
+
+    clear()
+
+    return nmap_list
+        
+def nmap_thread(my_ip, port):
+    global nmap_list
+
+    try:
+        my_socket.connect((my_ip, port))
+        nmap_list.append(my_ip + ": " + str(port))
+
+    except:
+        pass
