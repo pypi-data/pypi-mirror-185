97cb3202dcd50439508802021efa7767f441a110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:32 +0000

    Add package pydra-freesurfer/0.0.7/pydra_freesurfer-0.0.7.tar.gz

---
+++ packages/pydra-freesurfer/pydra_freesurfer-0.0.7.tar.gz/pydra_freesurfer-0.0.7/README.md
@@ -0,0 +1,82 @@
+# pydra-freesurfer
+
+[![PyPI - Version](https://img.shields.io/pypi/v/pydra-freesurfer.svg)](https://pypi.org/project/pydra-freesurfer)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pydra-freesurfer.svg)](https://pypi.org/project/pydra-freesurfer)
+
+---
+
+Pydra tasks for FreeSurfer.
+
+[Pydra][pydra] is a dataflow engine
+which provides a set of lightweight abstractions
+for DAG construction, manipulation, and distributed execution.
+
+[FreeSurfer][freesurfer] is a neuroimaging toolkit
+for processing, analyzing, and visualizing human brain MR images.
+
+This project exposes some of FreeSurfer's utilities as Pydra tasks
+to facilitate their incorporation into more advanced processing workflows.
+
+**Table of contents**
+
+- [Installation](#installation)
+- [Available interfaces](#available-interfaces)
+- [Development](#development)
+- [Licensing](#licensing)
+
+## Installation
+
+```console
+pip install pydra-freesurfer
+```
+
+## Available interfaces
+
+- gtmseg
+- mri_convert
+- mri_surf2surf
+- mri_vol2vol
+- mris_anatomical_stats
+- mris_ca_label
+- mris_ca_train
+- mris_expand
+- mris_preproc
+- recon-all
+- tkregister2
+
+## Development
+
+This project is managed using [Poetry].
+
+To install, check and test the code:
+
+```console
+make
+```
+
+To run the test suite when hacking:
+
+```console
+make test
+```
+
+To format the code before review:
+
+```console
+make format
+```
+
+To build the project's documentation:
+
+```console
+make docs
+```
+
+## Licensing
+
+This project is released under the terms of the [Apache License, Version 2.0][license].
+
+[pydra]: https://nipype.github.io/pydra
+[freesurfer]: https://surfer.nmr.mgh.harvard.edu
+[poetry]: https://python-poetry.org
+[license]: https://opensource.org/licenses/Apache-2.0
---
+++ packages/pydra-freesurfer/pydra_freesurfer-0.0.7.tar.gz/pydra_freesurfer-0.0.7/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydra',
+ 'pydra.tasks.freesurfer',
+ 'pydra.tasks.freesurfer.recon_all',
+ 'pydra.tasks.freesurfer.recon_all.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydra>=0.21,<0.22']
+
+setup_kwargs = {
+    'name': 'pydra-freesurfer',
+    'version': '0.0.7',
+    'description': 'Pydra tasks for FreeSurfer',
+    'long_description': "# pydra-freesurfer\n\n[![PyPI - Version](https://img.shields.io/pypi/v/pydra-freesurfer.svg)](https://pypi.org/project/pydra-freesurfer)\n[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pydra-freesurfer.svg)](https://pypi.org/project/pydra-freesurfer)\n\n---\n\nPydra tasks for FreeSurfer.\n\n[Pydra][pydra] is a dataflow engine\nwhich provides a set of lightweight abstractions\nfor DAG construction, manipulation, and distributed execution.\n\n[FreeSurfer][freesurfer] is a neuroimaging toolkit\nfor processing, analyzing, and visualizing human brain MR images.\n\nThis project exposes some of FreeSurfer's utilities as Pydra tasks\nto facilitate their incorporation into more advanced processing workflows.\n\n**Table of contents**\n\n- [Installation](#installation)\n- [Available interfaces](#available-interfaces)\n- [Development](#development)\n- [Licensing](#licensing)\n\n## Installation\n\n```console\npip install pydra-freesurfer\n```\n\n## Available interfaces\n\n- gtmseg\n- mri_convert\n- mri_surf2surf\n- mri_vol2vol\n- mris_anatomical_stats\n- mris_ca_label\n- mris_ca_train\n- mris_expand\n- mris_preproc\n- recon-all\n- tkregister2\n\n## Development\n\nThis project is managed using [Poetry].\n\nTo install, check and test the code:\n\n```console\nmake\n```\n\nTo run the test suite when hacking:\n\n```console\nmake test\n```\n\nTo format the code before review:\n\n```console\nmake format\n```\n\nTo build the project's documentation:\n\n```console\nmake docs\n```\n\n## Licensing\n\nThis project is released under the terms of the [Apache License, Version 2.0][license].\n\n[pydra]: https://nipype.github.io/pydra\n[freesurfer]: https://surfer.nmr.mgh.harvard.edu\n[poetry]: https://python-poetry.org\n[license]: https://opensource.org/licenses/Apache-2.0\n",
+    'author': 'The Aramis Lab',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/aramis-lab/pydra-freesurfer',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
