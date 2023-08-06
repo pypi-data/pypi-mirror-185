811a505bcbe79752965cc37c73a49041f6a15c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:10 +0000

    Add package moduextension/0.1.0/moduextension-0.1.0-py3-none-any.whl

---
+++ packages/moduextension/moduextension-0.1.0-py3-none-any.whl/moduextension-0.1.0.data/data/share/jupyter/labextensions/moduextension/static/_0275.9190323a55d1088fa6f3.js
@@ -0,0 +1,14 @@
+(self["webpackChunkmoduextension"] = self["webpackChunkmoduextension"] || []).push([["_0275"],{
+
+/***/ "?0275":
+/*!******************************!*\
+  !*** min-document (ignored) ***!
+  \******************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=_0275.9190323a55d1088fa6f3.js.map
