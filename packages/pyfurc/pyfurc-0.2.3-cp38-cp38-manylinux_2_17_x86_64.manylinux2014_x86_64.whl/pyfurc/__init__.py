9cc5f1b15e73b72882fc1b00b2fb946e3d31a8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:10 +0000

    Add package pyfurc/0.2.3/pyfurc-0.2.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyfurc/pyfurc-0.2.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyfurc/__init__.py
@@ -0,0 +1,25 @@
+__author__ = "ak"
+__version__ = "0.2.3"
+import configparser
+import os
+import warnings
+
+from pyfurc.core import (
+    BifurcationProblem,
+    BifurcationProblemSolution,
+    BifurcationProblemSolver,
+    Dof,
+    Energy,
+    Load,
+    Parameter,
+    PhysicalQuantity,
+)
+from pyfurc.tools import setup_auto_exec_env
+from pyfurc.util import (
+    AutoCodePrinter,
+    AutoOutputReader,
+    AutoParameters,
+    DataDir,
+    HiddenAutoParameters,
+    ParamDict,
+)
