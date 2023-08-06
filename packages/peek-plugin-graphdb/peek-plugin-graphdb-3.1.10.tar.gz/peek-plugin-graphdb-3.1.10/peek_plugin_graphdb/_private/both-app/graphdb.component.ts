ea963fa7f9c67dc4e91c1018dc0b761b6dec01ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package peek-plugin-graphdb/3.1.10/peek-plugin-graphdb-3.1.10.tar.gz

---
+++ packages/peek-plugin-graphdb/peek-plugin-graphdb-3.1.10.tar.gz/peek-plugin-graphdb-3.1.10/peek_plugin_graphdb/_private/both-app/graphdb.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "plugin-graphdb",
+    templateUrl: "graphdb.component.web.html"
+})
+export class DocdbComponent {
+    
+    constructor() {
+    
+    }
+    
+}
---
+++ packages/peek-plugin-graphdb/peek-plugin-graphdb-3.1.10.tar.gz/peek-plugin-graphdb-3.1.10/peek_plugin_graphdb/plugin-module/_private/tuples/OfflineConfigTuple.ts
@@ -0,0 +1,13 @@
+import { addTupleType, Tuple } from "@synerty/vortexjs"
+import { graphDbTuplePrefix } from "../PluginNames"
+
+@addTupleType
+export class OfflineConfigTuple extends Tuple {
+    public static readonly tupleName = graphDbTuplePrefix + "OfflineConfigTuple"
+    
+    cacheChunksForOffline: boolean = false
+    
+    constructor() {
+        super(OfflineConfigTuple.tupleName)
+    }
+}
