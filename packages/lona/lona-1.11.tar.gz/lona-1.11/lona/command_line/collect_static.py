9562b79db51ff5e6131dca0cbe555fa691965a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:50 +0000

    Add package lona/1.11/lona-1.11.tar.gz

---
+++ packages/lona/lona-1.11.tar.gz/lona-1.11/lona/command_line/collect_static.py
@@ -0,0 +1,73 @@
+import shutil
+import os
+
+from lona.logging import setup_logging
+from lona.server import Server
+
+
+def collect_static(args):
+    def _print(*print_args, **print_kwargs):
+        if args.silent:
+            return
+
+        print(*print_args, **print_kwargs)
+
+    def _rm(path):
+        _print(f'rm -rf {path}')
+
+        if args.dry_run:
+            return
+
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+
+        else:
+            os.unlink(path)
+
+    def _cp(source, destination):
+        source_is_dir = os.path.isdir(source)
+
+        if source_is_dir:
+            _print(f'cp -r {source} {destination}')
+
+        else:
+            _print(f'cp {source} {destination}')
+
+        if args.dry_run:
+            return
+
+        if source_is_dir:
+            shutil.copytree(source, destination)
+
+        else:
+            shutil.copy(source, destination)
+
+    # setup logging
+    setup_logging(args)
+
+    # setup server
+    server = Server(
+        project_root=args.project_root,
+        settings_paths=args.settings,
+        settings_pre_overrides=args.settings_pre_overrides,
+        settings_post_overrides=args.settings_post_overrides,
+    )
+
+    # check destination
+    if not os.path.exists(args.destination):
+        exit(f"'{args.destination}' does not exist")
+
+    if not os.path.isdir(args.destination):
+        exit(f"'{args.destination}' is no directory")
+
+    # clean
+    if args.clean:
+        for name in os.listdir(args.destination):
+            path = os.path.join(args.destination, name)
+            _rm(path)
+
+    # copy files
+    for path, url in server._static_file_loader.get_paths():
+        destination = os.path.join(args.destination, url)
+
+        _cp(path, destination)
---
+++ packages/lona/lona-1.11.tar.gz/lona-1.11/lona/html/parsing.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+from html.parser import HTMLParser
+import logging
+
+from lona.html.abstract_node import AbstractNode
+from lona.html.text_node import TextNode
+from lona.html.node import Node
+
+logger = logging.getLogger('lona')
+
+NODE_CLASSES: dict[str, type[Node]] = {}
+INPUT_NODE_CLASSES: dict[str, type[Node]] = {}
+
+SELF_CLOSING_TAGS = [
+    'area',
+    'base',
+    'br',
+    'col',
+    'embed',
+    'hr',
+    'img',
+    'input',
+    'link',
+    'meta',
+    'param',
+    'source',
+    'track',
+    'wbr',
+]
+
+
+def _setup_node_classes_cache():
+    for node_class in AbstractNode.get_all_node_classes():
+        if not issubclass(node_class, Node):
+            continue
+
+        # input nodes
+        if node_class.TAG_NAME == 'input':
+            if 'type' not in node_class.ATTRIBUTES:
+                logger.warning(
+                    'WARNING: input node %r has no type set',
+                    node_class,
+                )
+
+                continue
+
+            if node_class.ATTRIBUTES['type'] in INPUT_NODE_CLASSES:
+                logger.warning(
+                    "WARNING: Two input Node classes with type '%s' were found: %r and %r",
+                    node_class.ATTRIBUTES['type'],
+                    INPUT_NODE_CLASSES[node_class.ATTRIBUTES['type']],
+                    node_class,
+                )
+
+            INPUT_NODE_CLASSES[node_class.ATTRIBUTES['type']] = node_class
+
+        # nodes
+        else:
+            if node_class.TAG_NAME in NODE_CLASSES:
+                logger.warning(
+                    "WARNING: Two Node classes with tag name '%s' were found: %r and %r",
+                    node_class.TAG_NAME,
+                    NODE_CLASSES[node_class.TAG_NAME],
+                    node_class,
+                )
+
+            NODE_CLASSES[node_class.TAG_NAME] = node_class
+
+
+class NodeHTMLParser(HTMLParser):
+    CDATA_CONTENT_ELEMENTS = HTMLParser.CDATA_CONTENT_ELEMENTS + ('textarea',)
+
+    def __init__(self, *args, use_high_level_nodes=True, node_classes=None,
+                 **kwargs):
+
+        self.use_high_level_nodes = use_high_level_nodes
+        self.node_classes = node_classes or {}
+
+        super().__init__(*args, **kwargs)
+
+    def set_current_node(self, node):
+        self._node = node
+
+    def get_node_class(self, tag_name, attributes):
+        if not self.use_high_level_nodes:
+            return Node
+
+        # inputs
+        if tag_name == 'input':
+            input_type = attributes.get('type', 'text')
+
+            if input_type not in INPUT_NODE_CLASSES:
+                return Node
+
+            return INPUT_NODE_CLASSES[input_type]
+
+        # custom node classes
+        if tag_name in self.node_classes:
+            return self.node_classes[tag_name]
+
+        # nodes from the standard library
+        if tag_name in NODE_CLASSES:
+            return NODE_CLASSES[tag_name]
+
+        # generic nodes
+        return Node
+
+    def handle_startendtag(self, tag, attrs):
+        self.handle_starttag(tag, attrs, self_closing=True)
+
+    def handle_starttag(self, tag, attrs, self_closing=False):
+        self_closing = self_closing or tag in SELF_CLOSING_TAGS
+
+        # node attributes
+        node_attributes = {}
+
+        for name, value in attrs:
+            if value is None:
+                value = ''
+
+            if name == 'data-lona-node-id':
+                continue
+
+            node_attributes[name] = value
+
+        # tag overrides
+        node_kwargs = {}
+
+        node_kwargs['tag_name'] = tag
+        node_kwargs['self_closing_tag'] = self_closing
+
+        # setup node
+        for key in ('id', 'class', 'style'):
+            if key in node_attributes:
+                node_kwargs[key] = node_attributes.pop(key)
+
+        node_class = self.get_node_class(tag, node_attributes)
+        node = node_class(**node_kwargs)
+
+        # set attributes
+        node.attributes.update(node_attributes)
+
+        # setup node
+        self._node.append(node)
+        if not self_closing:
+            self.set_current_node(node)
+
+    def handle_data(self, data):
+        text = data
+
+        if self._node.tag_name not in ('pre', 'textarea'):
+            text = text.strip()
+
+            if not text:
+                return
+
+        # textareas
+        if self._node.tag_name == 'textarea':
+            self._node.value = data
+
+        # normal nodes
+        else:
+            self._node.append(TextNode(text))
+
+    def handle_endtag(self, tag):
+        if self._node.parent is None:
+            raise ValueError(
+                f'Invalid html: missing start tag for </{tag}>',
+            )
+        if tag != self._node.tag_name:
+            raise ValueError(
+                f'Invalid html: </{self._node.tag_name}> expected, </{tag}> received',
+            )
+
+        self.set_current_node(self._node.parent)
+
+
+def html_string_to_node_list(html_string, use_high_level_nodes=True,
+                             node_classes=None):
+
+    root_node = Node()
+
+    html_parser = NodeHTMLParser(
+        use_high_level_nodes=use_high_level_nodes,
+        node_classes=node_classes or {},
+    )
+
+    html_parser.set_current_node(root_node)
+    html_parser.feed(html_string)
+
+    if html_parser._node is not root_node:
+        raise ValueError(
+            f'Invalid html: missing end tag </{html_parser._node.tag_name}>',
+        )
+
+    return list(root_node.nodes)
