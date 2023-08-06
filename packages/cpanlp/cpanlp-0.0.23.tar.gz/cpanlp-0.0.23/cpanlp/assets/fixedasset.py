0ecee919982925cc4abeef1992eb07ae98a5c150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:09 +0000

    Add package cpanlp/0.0.23/cpanlp-0.0.23.tar.gz

---
+++ packages/cpanlp/cpanlp-0.0.23.tar.gz/cpanlp-0.0.23/cpanlp/assets/fixedasset.py
@@ -0,0 +1,23 @@
+from cpanlp.assets.asset import *
+class FixedAsset(Asset):
+    def __init__(self, account="建筑",debit=100000, date="2021-05-20",location="beijing"):
+        super().__init__(account, debit, date)
+        self.location = location
+        
+    def __str__(self):
+        return f"{self.account} ({self.debit}), Location: {self.location}"
+    
+    def depreciate(self, rate):
+        if rate < 0 or rate > 1:
+            raise ValueError("Value must be between 0 and 1")
+        self.debit -= rate*self.debit
+        
+    def get_location(self):
+        return self.location
+class RealState(FixedAsset):
+    pass
+
+if __name__ == '__main__':
+    a=FixedAsset()
+    print(a.location)
+    print(a.likely_economic_benefit)
