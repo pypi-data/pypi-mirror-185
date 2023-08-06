3df33198de7d7d5dd9c5279476dfa407874ca9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package whatsonpypi/0.3.4/whatsonpypi-0.3.4-py3-none-any.whl

---
+++ packages/whatsonpypi/whatsonpypi-0.3.4-py3-none-any.whl/whatsonpypi/constants.py
@@ -0,0 +1,7 @@
+"""
+Package level constants
+"""
+PYPI_BASE_URL = "https://pypi.org/pypi"
+REQUIREMENTS_REPLACE_COMMENT = "#wopp"
+ALL_OPTION = "ALL"
+REQ_LINE_REGEX = r"([A-Za-z0-9\[\]\-\_\.]+)((==)|(~=)|(>=)|(<=))([A-Za-z0-9\-\_\.]+)"
