cb6ddf8733491a7a555f4335109a1e930fe4abcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:36 +0000

    Add package bento-lib/5.1.0/bento_lib-5.1.0.tar.gz

---
+++ packages/bento-lib/bento_lib-5.1.0.tar.gz/bento_lib-5.1.0/bento_lib/types.py
@@ -0,0 +1,50 @@
+from typing import TypedDict
+
+__all__ = [
+    "GA4GHServiceType",
+    "GA4GHServiceOrganization",
+    "GA4GHServiceInfo",
+]
+
+
+class GA4GHServiceType(TypedDict):
+    group: str
+    artifact: str
+    version: str
+
+
+class GA4GHServiceOrganization(TypedDict):
+    name: str
+    url: str
+
+
+# TODO: py3.11: Required[] instead of base class
+
+
+class BentoExtraServiceInfo(TypedDict, total=False):
+    service_kind: str  # One service_kind per Bento service/instance
+
+    git_tag: str
+    git_branch: str
+
+
+class _GA4GHServiceInfoBase(TypedDict):
+    id: str
+    name: str
+    type: GA4GHServiceType
+    organization: GA4GHServiceOrganization
+    version: str
+
+
+class GA4GHServiceInfo(_GA4GHServiceInfoBase, total=False):
+    description: str
+    contactUrl: str
+    documentationUrl: str
+    url: str  # Technically not part of spec; comes from service-registry
+    environment: str  # TODO: Literal["dev", "prod"] if JetBrains fixes their inspection...
+
+    # TODO: deprecate in favour of bento object
+    git_tag: str
+    git_branch: str
+
+    bento: BentoExtraServiceInfo
