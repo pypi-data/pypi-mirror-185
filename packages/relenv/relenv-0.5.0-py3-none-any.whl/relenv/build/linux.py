cbd9dd900a5a862077aed1fcd76d07fb685fd41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:30 +0000

    Add package relenv/0.5.0/relenv-0.5.0-py3-none-any.whl

---
+++ packages/relenv/relenv-0.5.0-py3-none-any.whl/relenv/build/linux.py
@@ -0,0 +1,499 @@
+# Copyright 2022-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2
+"""
+The linux build process.
+"""
+from .common import *
+from ..common import arches, LINUX
+
+ARCHES = arches[LINUX]
+
+# Patch for Python's setup.py
+PATCH = """--- ./setup.py
++++ ./setup.py
+@@ -664,6 +664,7 @@
+             self.failed.append(ext.name)
+
+     def add_multiarch_paths(self):
++        return
+         # Debian/Ubuntu multiarch support.
+         # https://wiki.ubuntu.com/MultiarchSpec
+         tmpfile = os.path.join(self.build_temp, 'multiarch')
+"""
+
+
+def populate_env(env, dirs):
+    """
+    Make sure we have the correct environment variables set.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    """
+    # CC and CXX need to be to have the full path to the executable
+    env["CC"] = "{}/bin/{}-gcc -no-pie".format(dirs.toolchain, env["RELENV_HOST"])
+    env["CXX"] = "{}/bin/{}-g++ -no-pie".format(dirs.toolchain, env["RELENV_HOST"])
+    env["PATH"] = "{}/bin/:{PATH}".format(dirs.toolchain, **env)
+    ldflags = [
+        "-Wl,--rpath={prefix}/lib",
+        "-L{prefix}/lib",
+        "-L{}/{RELENV_HOST}/sysroot/lib".format(dirs.toolchain, **env),
+        "-static-libstdc++",
+    ]
+    env["LDFLAGS"] = " ".join(ldflags).format(prefix=dirs.prefix)
+    cflags = [
+        "-L{prefix}/lib",
+        "-L{}/{RELENV_HOST}/sysroot/lib".format(dirs.toolchain, **env),
+        "-I{prefix}/include",
+        "-I{prefix}/include/readline",
+        "-I{prefix}/include/ncursesw",
+        "-I{}/{RELENV_HOST}/sysroot/usr/include".format(dirs.toolchain, **env),
+    ]
+    env["CFLAGS"] = " ".join(cflags).format(prefix=dirs.prefix)
+    # CPPFLAGS are needed for Python's setup.py to find the 'nessicery bits'
+    # for things like zlib and sqlite.
+    cpplags = [
+        "-L{prefix}/lib",
+        "-L{}/{RELENV_HOST}/sysroot/lib".format(dirs.toolchain, **env),
+        "-I{prefix}/include",
+        "-I{prefix}/include/readline",
+        "-I{prefix}/include/ncursesw",
+        "-I{}/{RELENV_HOST}/sysroot/usr/include".format(dirs.toolchain, **env),
+    ]
+    env["CPPFLAGS"] = " ".join(cpplags).format(prefix=dirs.prefix)
+    env["CXXFLAGS"] = " ".join(cpplags).format(prefix=dirs.prefix)
+
+
+def build_bzip2(env, dirs, logfp):
+    """
+    Build bzip2.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    runcmd(
+        [
+            "make",
+            "-j8",
+            "PREFIX={}".format(dirs.prefix),
+            "LDFLAGS={}".format(env["LDFLAGS"]),
+            "CFLAGS=-fPIC",
+            "CC={}".format(env["CC"]),
+            "BUILD={}".format("x86_64-linux-gnu"),
+            "HOST={}".format(env["RELENV_HOST"]),
+            "install",
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    runcmd(
+        [
+            "make",
+            "-f",
+            "Makefile-libbz2_so",
+            "CC={}".format(env["CC"]),
+            "LDFLAGS={}".format(env["LDFLAGS"]),
+            "BUILD={}".format("x86_64-linux-gnu"),
+            "HOST={}".format(env["RELENV_HOST"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    shutil.copy2("libbz2.so.1.0.8", os.path.join(dirs.prefix, "lib"))
+
+
+def build_gdbm(env, dirs, logfp):
+    """
+    Build gdbm.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    runcmd(
+        [
+            "./configure",
+            "--prefix={}".format(dirs.prefix),
+            "--enable-libgdbm-compat",
+            "--build={}".format(env["RELENV_BUILD"]),
+            "--host={}".format(env["RELENV_HOST"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+
+def build_ncurses(env, dirs, logfp):
+    """
+    Build ncurses.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    configure = pathlib.Path(dirs.source) / "configure"
+    if env["RELENV_BUILD_ARCH"] == "aarch64" or env["RELENV_HOST_ARCH"] == "aarch64":
+        os.chdir(dirs.tmpbuild)
+        runcmd([str(configure)], stderr=logfp, stdout=logfp)
+        runcmd(["make", "-C", "include"], stderr=logfp, stdout=logfp)
+        runcmd(["make", "-C", "progs", "tic"], stderr=logfp, stdout=logfp)
+    os.chdir(dirs.source)
+    runcmd(
+        [
+            str(configure),
+            "--prefix=/",
+            "--with-shared",
+            "--without-cxx-shared",
+            "--without-static",
+            "--without-cxx",
+            "--enable-widec",
+            "--without-normal",
+            "--disable-stripping",
+            "--build={}".format(env["RELENV_BUILD"]),
+            "--host={}".format(env["RELENV_HOST"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(
+        [
+            "make",
+            "DESTDIR={}".format(dirs.prefix),
+            "TIC_PATH={}".format(str(pathlib.Path(dirs.tmpbuild) / "progs" / "tic")),
+            "install",
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+
+
+def build_libffi(env, dirs, logfp):
+    """
+    Build libffi.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    runcmd(
+        [
+            "./configure",
+            "--prefix={}".format(dirs.prefix),
+            "--disable-multi-os-directory",
+            "--build={}".format(env["RELENV_BUILD"]),
+            "--host={}".format(env["RELENV_HOST"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    # libffi doens't want to honor libdir, force install to lib instead of lib64
+    runcmd(
+        ["sed", "-i", "s/lib64/lib/g", "Makefile"], env=env, stderr=logfp, stdout=logfp
+    )
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+
+def build_zlib(env, dirs, logfp):
+    """
+    Build zlib.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    env["CFLAGS"] = "-fPIC {}".format(env["CFLAGS"])
+    runcmd(
+        [
+            "./configure",
+            "--prefix={}".format(dirs.prefix),
+            "--libdir={}/lib".format(dirs.prefix),
+            "--shared",
+            '--archs="-arch {}"'.format(env["RELENV_BUILD_ARCH"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    runcmd(["make", "-no-pie", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+
+def build_krb(env, dirs, logfp):
+    """
+    Build kerberos.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    if env["RELENV_BUILD_ARCH"] != env["RELENV_HOST_ARCH"]:
+        env["krb5_cv_attr_constructor_destructor"] = "yes,yes"
+        env["ac_cv_func_regcomp"] = "yes"
+        env["ac_cv_printf_positional"] = "yes"
+    os.chdir(dirs.source / "src")
+    runcmd(
+        [
+            "./configure",
+            "--prefix={}".format(dirs.prefix),
+            "--without-system-verto",
+            "--without-libedit",
+            "--build={}".format(env["RELENV_BUILD"]),
+            "--host={}".format(env["RELENV_HOST"]),
+        ],
+        env=env,
+        stderr=logfp,
+        stdout=logfp,
+    )
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+
+def build_python(env, dirs, logfp):
+    """
+    Run the commands to build Python.
+
+    :param env: The environment dictionary
+    :type env: dict
+    :param dirs: The working directories
+    :type dirs: ``relenv.build.common.Dirs``
+    :param logfp: A handle for the log file
+    :type logfp: file
+    """
+    env["LDFLAGS"] = "-Wl,--rpath={prefix}/lib {ldflags}".format(
+        prefix=dirs.prefix, ldflags=env["LDFLAGS"]
+    )
+
+    # Needed when using a toolchain even if build and host match.
+    runcmd(
+        [
+            "sed",
+            "-i",
+            "s/ac_cv_buggy_getaddrinfo=yes/ac_cv_buggy_getaddrinfo=no/g",
+            "configure",
+        ]
+    )
+    runcmd(
+        [
+            "sed",
+            "-i",
+            (
+                "s/ac_cv_enable_implicit_function_declaration_error=yes/"
+                "ac_cv_enable_implicit_function_declaration_error=no/g"
+            ),
+            "configure",
+        ]
+    )
+
+    with open("/tmp/patch", "w") as fp:
+        fp.write(PATCH)
+    runcmd(["patch", "-p0", "-i", "/tmp/patch"], env=env, stderr=logfp, stdout=logfp)
+
+    cmd = [
+        "./configure",
+        "-v",
+        "--prefix={}".format(dirs.prefix),
+        "--with-openssl={}".format(dirs.prefix),
+        "--enable-optimizations",
+        "--with-ensurepip=no",
+        "--build={}".format(env["RELENV_BUILD"]),
+        "--host={}".format(env["RELENV_HOST"]),
+    ]
+
+    # Needed when using a toolchain even if build and host match.
+    cmd += [
+        "ac_cv_file__dev_ptmx=yes",
+        "ac_cv_file__dev_ptc=no",
+    ]
+
+    runcmd(cmd, env=env, stderr=logfp, stdout=logfp)
+    with io.open("Modules/Setup", "a+") as fp:
+        fp.seek(0, io.SEEK_END)
+        fp.write("*disabled*\n" "_tkinter\n" "nsl\n" "nis\n")
+    runcmd(["make", "-j8"], env=env, stderr=logfp, stdout=logfp)
+    runcmd(["make", "install"], env=env, stderr=logfp, stdout=logfp)
+
+    # RELENVCROSS=relenv/_build/aarch64-linux-gnu  relenv/_build/x86_64-linux-gnu/bin/python3 -m ensurepip
+    # python = dirs.prefix / "bin" / "python3"
+    # if env["RELENV_BUILD_ARCH"] == "aarch64":
+    #    python = env["RELENV_NATIVE_PY"]
+    # env["PYTHONUSERBASE"] = dirs.prefix
+    # runcmd([str(python), "-m", "ensurepip", "-U"], env=env, stderr=logfp, stdout=logfp)
+
+
+build = Builder(populate_env=populate_env)
+build.add(
+    "OpenSSL",
+    build_func=build_openssl,
+    download={
+        "url": "https://www.openssl.org/source/openssl-{version}.tar.gz",
+        "version": "1.1.1q",
+        # "md5sum": "2aad5635f9bb338bc2c6b7d19cbc9676",
+    },
+)
+
+build.add(
+    "XZ",
+    download={
+        "url": "http://tukaani.org/xz/xz-{version}.tar.gz",
+        "version": "5.2.3",
+        "md5sum": "ef68674fb47a8b8e741b34e429d86e9d",
+    },
+)
+
+build.add(
+    name="SQLite",
+    build_func=build_sqlite,
+    download={
+        "url": "https://sqlite.org/2022/sqlite-autoconf-{version}.tar.gz",
+        "version": "3390300",
+    },
+)
+
+build.add(
+    name="bzip2",
+    build_func=build_bzip2,
+    download={
+        "url": "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz",
+        "version": "1.0.8",
+        "md5sum": "67e051268d0c475ea773822f7500d0e5",
+    },
+)
+
+build.add(
+    name="gdbm",
+    build_func=build_gdbm,
+    download={
+        "url": "https://ftp.gnu.org/gnu/gdbm/gdbm-{version}.tar.gz",
+        "version": "1.21",
+        "md5sum": "a285c6e2dfed78668664c0555a7d202b",
+    },
+)
+
+build.add(
+    name="ncurses",
+    build_func=build_ncurses,
+    wait_on=["readline"],
+    download={
+        "url": "https://ftp.gnu.org/pub/gnu/ncurses/ncurses-{version}.tar.gz",
+        "version": "6.3",
+    },
+)
+
+build.add(
+    "libffi",
+    build_libffi,
+    download={
+        "url": "https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz",
+        "version": "3.3",
+        "md5sum": "6313289e32f1d38a9df4770b014a2ca7",
+    },
+)
+
+build.add(
+    "zlib",
+    build_zlib,
+    download={
+        "url": "https://zlib.net/fossils/zlib-{version}.tar.gz",
+        "version": "1.2.12",
+        "md5sum": "5fc414a9726be31427b440b434d05f78",
+    },
+)
+
+build.add(
+    "uuid",
+    download={
+        "url": "https://sourceforge.net/projects/libuuid/files/libuuid-{version}.tar.gz",
+        "version": "1.0.3",
+        "md5sum": "d44d866d06286c08ba0846aba1086d68",
+    },
+)
+
+build.add(
+    "krb5",
+    build_func=build_krb,
+    wait_on=["OpenSSL"],
+    download={
+        "url": "https://kerberos.org/dist/krb5/{version}/krb5-{version}.tar.gz",
+        "version": "1.20",
+    },
+)
+
+build.add(
+    "readline",
+    download={
+        "url": "https://ftp.gnu.org/gnu/readline/readline-{version}.tar.gz",
+        "version": "8.1.2",
+        "md5sum": "12819fa739a78a6172400f399ab34f81",
+    },
+)
+
+build.add(
+    "python",
+    build_func=build_python,
+    wait_on=[
+        "OpenSSL",
+        "XZ",
+        "SQLite",
+        "bzip2",
+        "gdbm",
+        "ncurses",
+        "libffi",
+        "zlib",
+        "uuid",
+        "krb5",
+        "readline",
+    ],
+    download={
+        "url": "https://www.python.org/ftp/python/{version}/Python-{version}.tar.xz",
+        "version": "3.10.9",
+    },
+)
+
+
+build.add(
+    "relenv-finalize",
+    build_func=finalize,
+    wait_on=[
+        "python",
+    ],
+)
+
+
+def main(args):
+    """
+    The entrypoint into the linux build.
+
+    :param args: The arguments for the build
+    :type args: argparse.Namespace
+    """
+    run_build(build, args)
---
+++ packages/relenv/relenv-0.5.0-py3-none-any.whl/relenv/create.py
@@ -0,0 +1,129 @@
+# Copyright 2022-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2
+"""
+The ``relenv create`` command.
+"""
+
+import contextlib
+import os
+import pathlib
+import sys
+import tarfile
+
+from .common import RelenvException, arches, archived_build, build_arch
+
+
+@contextlib.contextmanager
+def chdir(path):
+    """
+    Context manager that changes to the specified directory and back.
+
+    :param path: The path to temporarily change to
+    :type path: str
+    """
+    cwd = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+class CreateException(RelenvException):
+    """
+    Raised when there is an issue creating a new relenv environment.
+    """
+
+
+def setup_parser(subparsers):
+    """
+    Setup the subparser for the ``create`` command.
+
+    :param subparsers: The subparsers object returned from ``add_subparsers``
+    :type subparsers: argparse._SubParsersAction
+    """
+    create_subparser = subparsers.add_parser(
+        "create",
+        description=(
+            "Create a Relenv environment. This will create a directory of the given "
+            "name with newly created Relenv environment.",
+        ),
+    )
+    create_subparser.set_defaults(func=main)
+    create_subparser.add_argument("name", help="The name of the directory to create")
+    create_subparser.add_argument(
+        "--arch",
+        default=build_arch(),
+        choices=arches[sys.platform],
+        type=str,
+        help="The host architecture [default: %(default)s]",
+    )
+
+
+def create(name, dest=None, arch=None):
+    """
+    Create a relenv environment.
+
+    :param name: The name of the environment
+    :type name: str
+    :param dest: The path the environment should be created under
+    :type dest: str
+    :param arch: The architecture to create the environment for
+    :type arch: str
+
+    :raises CreateException: If there is a problem in creating the relenv environment
+    """
+    if arch is None:
+        arch = build_arch()
+    if dest:
+        writeto = pathlib.Path(dest) / name
+    else:
+        writeto = pathlib.Path(name).resolve()
+
+    if pathlib.Path(writeto).exists():
+        raise CreateException("The requested path already exists.")
+
+    plat = sys.platform
+
+    if plat == "linux":
+        if arch in arches[plat]:
+            triplet = "{}-{}-gnu".format(arch, plat)
+        else:
+            raise CreateException("Unknown arch")
+    elif plat == "darwin":
+        if arch in arches[plat]:
+            triplet = "{}-macos".format(arch)
+        else:
+            raise CreateException("Unknown arch")
+    elif plat == "win32":
+        if arch in arches[plat]:
+            triplet = "{}-win".format(arch)
+        else:
+            raise CreateException("Unknown arch")
+    else:
+        raise CreateException("Unknown platform")
+
+    tar = archived_build(triplet)
+    if not tar.exists():
+        raise CreateException(
+            f"Error, build archive for {arch} doesn't exist: {tar}\n"
+            "You might try relenv fetch to resolve this."
+        )
+    with tarfile.open(tar, "r:xz") as fp:
+        for f in fp:
+            fp.extract(f, writeto)
+
+
+def main(args):
+    """
+    The entrypoint into the ``relenv create`` command.
+
+    :param args: The args passed to the command
+    :type args: argparse.Namespace
+    """
+    name = args.name
+    try:
+        create(name, arch=args.arch)
+    except CreateException as exc:
+        print(exc)
+        sys.exit(1)
