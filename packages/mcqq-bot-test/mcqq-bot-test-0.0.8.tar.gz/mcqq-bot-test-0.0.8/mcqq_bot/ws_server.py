ecfbbfcf8e5dda1ce4668867a09876429ac4081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:03 +0000

    Add package mcqq-bot-test/0.0.8/mcqq-bot-test-0.0.8.tar.gz

---
+++ packages/mcqq-bot-test/mcqq-bot-test-0.0.8.tar.gz/mcqq-bot-test-0.0.8/mcqq_bot/ws_server.py
@@ -0,0 +1,42 @@
+import aiomcrcon
+import websockets
+from nonebot import logger
+
+from .database.models import Server
+from .plugins.on_msg.data_source import ws_client, RCON_CLIENTS
+from .utils import config
+
+
+async def start_ws_server():
+    """启动 WebSocket 服务器"""
+    global ws
+    ws = await websockets.serve(ws_client, config.get_mc_qq_ws_ip(), config.get_mc_qq_ws_port())
+    logger.success("[MC_QQ]丨WebSocket 服务器已开启")
+
+
+async def stop_ws_server():
+    """关闭 WebSocket 服务器"""
+    global ws
+    ws.close()
+    logger.success("[MC_QQ]丨WebSocket 服务器已关闭")
+
+
+async def start_rcon_connect():
+    servers = Server.all()
+    async for server in servers:
+        if server.rcon_msg | server.rcon_cmd:
+            rcon_client = aiomcrcon.Client(server.rcon_ip, server.rcon_port, server.rcon_password)
+
+            try:
+                await rcon_client.connect()
+                rcon_client_status = True
+                RCON_CLIENTS.append({
+                    "server_name": server.server_name,
+                    "rcon": rcon_client,
+                    "is_open": rcon_client_status
+                })
+                logger.success(f"[MC_QQ]丨[Server:{server.server_name}] 的Rcon连接成功")
+            except aiomcrcon.RCONConnectionError:
+                logger.error(f"[MC_QQ]丨[Server:{server.server_name}] 的Rcon连接失败")
+            except aiomcrcon.IncorrectPasswordError:
+                logger.error(f"[MC_QQ]丨[Server:{server.server_name}] 的Rcon密码错误")
