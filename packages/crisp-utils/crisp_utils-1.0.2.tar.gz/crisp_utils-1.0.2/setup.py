9a16a30e6689c75b23b0383c369df95692e5622f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:38 +0000

    Add package crisp-utils/1.0.2/crisp_utils-1.0.2.tar.gz

---
+++ packages/crisp-utils/crisp_utils-1.0.2.tar.gz/crisp_utils-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+import platform
+from urllib import request, parse
+
+setup(
+    name='crisp_utils',
+    version='1.0.2',
+    license='MIT',
+    author="",
+    author_email='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='crisp utils',
+    install_requires=[
+          'scikit-learn',
+          'requests',
+      ],
+)
