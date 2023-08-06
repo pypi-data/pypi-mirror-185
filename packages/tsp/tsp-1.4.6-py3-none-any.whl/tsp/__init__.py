8832359e7f3c23a2ed8b22e84c84a54c6891c94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:12 +0000

    Add package tsp/1.4.6/tsp-1.4.6-py3-none-any.whl

---
+++ packages/tsp/tsp-1.4.6-py3-none-any.whl/tsp/__init__.py
@@ -0,0 +1,8 @@
+from tsp.core import TSP, IndexedTSP
+from tsp.misc import _is_depth_column
+from tsp.plots.static import trumpet_curve, time_series, colour_contour
+from tsp.readers import read_gtnp, read_geotop, read_geoprecision, read_hoboware, read_ntgs, read_logr, read_csv
+
+#TSP.__module__ = "teaspoon"
+
+__all__ = ["TSP", "IndexedTSP"]
