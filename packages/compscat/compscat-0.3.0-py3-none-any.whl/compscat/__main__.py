f296e32426dc7542c01f83555931204aae8fa5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:28 +0000

    Add package compscat/0.3.0/compscat-0.3.0-py3-none-any.whl

---
+++ packages/compscat/compscat-0.3.0-py3-none-any.whl/compscat/__main__.py
@@ -0,0 +1,39 @@
+"""
+Copyright (c) 2022-2023 Aman Desai. All rights reserved.
+
+"""
+
+import argparse
+
+from cross_section import CrossSection
+from save_events import SaveEvent
+
+import compscat
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog="compscat",
+        description="Monte Carlo Simulation of Compton Scattering",
+    )
+    parser.add_argument(
+        "--energy",
+        type=float,
+        required=True,
+        help="Energy of the Photon in MeV",
+    )
+    arguments = parser.parse_args()
+
+    if arguments.energy <= 0.1:
+        raise Exception("Energy Cannot be less than 1 MeV")
+
+    E = arguments.energy * 1e-3
+
+    w_sum, w_square, w_max = CrossSection(E / compscat.constants.m).integrate_xsec()
+
+    SaveEvent(10000, w_max, E).to_root()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/compscat/compscat-0.3.0-py3-none-any.whl/compscat/cross_section.py
@@ -0,0 +1,44 @@
+"""
+Copyright (c) 2022-2023 Aman Desai. All rights reserved.
+
+"""
+
+import random
+
+import compscat.constants
+
+
+class CrossSection:
+    def __init__(self, w):
+        self.pi = compscat.constants.pi
+        self.alpha = compscat.constants.alpha
+        self.delta = compscat.constants.delta
+        self.m = compscat.constants.m
+        self.w = w
+
+    def dsigma(self, costh):
+        pre_fact = 1
+        sum_1 = 0
+        pre_fact *= self.pi * (self.alpha / self.m) ** 2
+        pre_fact *= 1 / (1 + self.w * (1 - costh)) ** 2
+        sum_1 += 1 / (1 + self.w * (1 - costh))
+        sum_1 += self.w * (1 - costh)
+        sum_1 += costh**2
+        return pre_fact * sum_1
+
+    def xsection(self, w_max):
+        costh = -1 + random.random() * self.delta
+        w_i = CrossSection(self.w).dsigma(costh) * self.delta
+        if w_max < w_i:
+            w_max = w_i
+        return w_i, w_max
+
+    def integrate_xsec(self, N=10000):
+        w_sum = 0
+        w_max = 0
+        w_square = 0
+        for _i in range(N):
+            w_i, w_max = CrossSection(self.w).xsection(w_max)
+            w_sum += w_i
+            w_square += w_i * w_i
+        return w_sum, w_square, w_max
