739fd54d6d62cc87fc536af5f50d4231cb91e682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:09 +0000

    Add package gators/0.3.2/gators-0.3.2-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/gators/gators-0.3.2-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl/gators/__init__.py
@@ -0,0 +1,8 @@
+import os
+from typing import TypeVar
+
+__version__ = "0.3.2"
+os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
+
+DataFrame = TypeVar("Union[pd.DataFrame, ks.DataFrame, dd.DataFrame]")
+Series = TypeVar("Union[pd.Series, ks.Series, dd.Series]")
