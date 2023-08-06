3bdbfc9ba285b3696d69bb8821e543806b6c9eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:41 +0000

    Add package gemeente-lwd1/0.1/gemeente_lwd1-0.1-py3-none-any.whl

---
+++ packages/gemeente-lwd1/gemeente_lwd1-0.1-py3-none-any.whl/gemeente_lwd1/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 15:22:14 2023
+
+@author: hzheng
+"""
+
+def add_numbers(a, b):
+    return a + b
+
+
+def mul_numbers(a, b):
+    return a * b
+
+
+def div_numbers(a, b):
+    return a / b
+
+
+def sub_numbers(a, b):
+    return a - b
+
