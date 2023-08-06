23be78963513265292c733efcaeb2fa88b769b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:18 +0000

    Add package nflscraPy/0.1.3/nflscraPy-0.1.3.tar.gz

---
+++ packages/nflscraPy/nflscraPy-0.1.3.tar.gz/nflscraPy-0.1.3/nflscraPy/__init__.py
@@ -0,0 +1,10 @@
+from .season import _gamelogs
+from .metadata import _gamelog_metadata
+from .stats import _gamelog_statistics
+from .expected_points import _gamelog_expected_points
+from .scoring import _gamelog_scoring
+from .roster import _gamelog_roster
+from .snap_counts import _gamelog_snap_counts
+from .splits import _season_splits
+from .five_thirty_eight import _five_thirty_eight
+from .teams import _tms, _splits_tms
