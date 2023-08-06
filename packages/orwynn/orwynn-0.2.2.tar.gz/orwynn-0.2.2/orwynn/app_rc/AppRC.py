6a2e1258a629c5fb072fcbab73bfd54fac6427de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:29 +0000

    Add package orwynn/0.2.2/orwynn-0.2.2.tar.gz

---
+++ packages/orwynn/orwynn-0.2.2.tar.gz/orwynn-0.2.2/orwynn/app_rc/AppRC.py
@@ -0,0 +1,3 @@
+from typing import Any
+
+AppRC = dict[str, Any]
---
+++ packages/orwynn/orwynn-0.2.2.tar.gz/orwynn-0.2.2/orwynn/cls/__init__.py
@@ -0,0 +1,45 @@
+from orwynn.error.Error import Error
+from orwynn.types import Class
+
+
+class ClassNotFoundError(Error):
+    pass
+
+
+def find_subclass_by_name(name: str, BaseClass: Class) -> Class:
+    """Searches given base class for subclass with given name.
+
+    Args:
+        name:
+            Name of the target class.
+        BaseClass:
+            Where to search for subclasses.
+
+    Returns:
+        Class found.
+    """
+    # Maybe given class has correct name
+    if BaseClass.__name__ == name:
+        return BaseClass
+
+    out: Class | None = __traverse_subclasses_checking_name(name, BaseClass)
+
+    if out is None:
+        raise ClassNotFoundError(
+            f"class of supertype {BaseClass} with name {name} is not found"
+        )
+    else:
+        return out
+
+
+def __traverse_subclasses_checking_name(name: str, C: Class) -> Class | None:
+    for SubClass in C.__subclasses__():
+        if SubClass.__name__ == name:
+            return SubClass
+        else:
+            result: Class | None = \
+                __traverse_subclasses_checking_name(name, SubClass)
+            if result is not None:
+                return result
+
+    return None
