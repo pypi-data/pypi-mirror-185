ed5a373a7482117b61d3ac8b2f8731ec6389e836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:17 +0000

    Add package oracle-of-ammon/0.1.6/oracle_of_ammon-0.1.6-py3-none-any.whl

---
+++ packages/oracle-of-ammon/oracle_of_ammon-0.1.6-py3-none-any.whl/oracle_of_ammon/tests/oracle_test.py
@@ -0,0 +1,25 @@
+import os
+import pathlib
+
+from oracle_of_ammon.api.oracle import Oracle
+
+oracle = Oracle()
+
+
+def test_oracle_init():
+    assert hasattr(oracle, "document_store")
+    assert hasattr(oracle, "retriever")
+    assert hasattr(oracle, "pipeline")
+
+
+def test_index_documents():
+    file = open(
+        file=pathlib.Path(os.getcwd(), "oracle_of_ammon", "data", "test.csv"), mode="br"
+    )
+    oracle.index_documents(data=file)
+    assert oracle.document_store.get_document_count() != 0
+    file.close()
+
+
+def test_search():
+    assert oracle.search(query="This is a question.")
