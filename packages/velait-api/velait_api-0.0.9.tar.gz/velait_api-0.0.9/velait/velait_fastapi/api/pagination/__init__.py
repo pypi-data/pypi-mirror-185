ca6d6c4627a729dec6206df9f4fb435ccd68354e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:41 +0000

    Add package velait-api/0.0.9/velait_api-0.0.9.tar.gz

---
+++ packages/velait-api/velait_api-0.0.9.tar.gz/velait_api-0.0.9/velait/velait_fastapi/api/pagination/__init__.py
@@ -0,0 +1,3 @@
+from velait.velait_fastapi.api.pagination.schemas import Page
+from velait.velait_fastapi.api.pagination.pagination import paginate,\
+    get_prev_page, get_next_page, get_page_limits
