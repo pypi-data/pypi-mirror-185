380dc6e057868224243d42ad25ebafb38379eb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package netbox-disk/0.0.7.6.5.40/netbox_disk-0.0.7.6.5.40-py3-none-any.whl

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.40-py3-none-any.whl/netbox_disk/tables/physicalvolume.py
@@ -0,0 +1,53 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ChoiceFieldColumn,
+    ToggleColumn,
+    TagColumn,
+    ActionsColumn,
+)
+
+from netbox_disk.models import PhysicalVolume
+
+
+class PhysicalVolumeBaseTable(NetBoxTable):
+    """Base class for tables displaying PhysicalVolume"""
+
+    size = tables.Column(
+        linkify=True,
+    )
+
+
+class PhysicalVolumeTable(PhysicalVolumeBaseTable):
+    """Table for displaying PhysicalVolume objects."""
+
+    pk = ToggleColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = PhysicalVolume
+        fields = (
+            "pk",
+            "size",
+            "description",
+        )
+        default_columns = (
+            "size",
+            "description"
+        )
+
+
+class RelatedPhysicalVolumeTable(PhysicalVolumeBaseTable):
+    actions = ActionsColumn(actions=())
+
+    class Meta(NetBoxTable.Meta):
+        model = PhysicalVolume
+        fields = (
+            "pk",
+            "size",
+            "description",
+        )
+        default_columns = (
+            "size",
+            "description"
+        )
