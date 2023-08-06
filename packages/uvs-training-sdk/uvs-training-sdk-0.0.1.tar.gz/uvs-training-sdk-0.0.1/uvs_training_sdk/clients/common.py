31014e81a51b7974c5c0d48c65687273219961ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:31 +0000

    Add package uvs-training-sdk/0.0.1/uvs-training-sdk-0.0.1.tar.gz

---
+++ packages/uvs-training-sdk/uvs-training-sdk-0.0.1.tar.gz/uvs-training-sdk-0.0.1/uvs_training_sdk/clients/common.py
@@ -0,0 +1,7 @@
+class ResourceType:
+    """
+    https://learn.microsoft.com/en-us/azure/cognitive-services/cognitive-services-apis-create-account?tabs=multiservice%2Canomaly-detector%2Clanguage-service%2Ccomputer-vision%2Cwindows
+    """
+
+    MultiServiceResource = "multi_service_account"
+    SingleServiceResource = "single_service_account"
