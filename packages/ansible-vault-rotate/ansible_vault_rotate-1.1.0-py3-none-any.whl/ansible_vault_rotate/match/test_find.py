bbb1189ecdd0973a2c36bbee512588a880ffb6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:52 +0000

    Add package ansible-vault-rotate/1.1.0/ansible_vault_rotate-1.1.0-py3-none-any.whl

---
+++ packages/ansible-vault-rotate/ansible_vault_rotate-1.1.0-py3-none-any.whl/ansible_vault_rotate/match/test_find.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+from .find import find_vault_strings, FindVaultStringResult
+
+
+class MatchFindTest(unittest.TestCase):
+
+    def load_results(self, file_name: str) -> list[FindVaultStringResult]:
+        return [item for item in
+                find_vault_strings(f"{os.path.dirname(os.path.abspath(__file__))}/__testdata__/{file_name}.yml")]
+
+    def test_find_single(self):
+        results = self.load_results("single_vaulted")
+        self.assertEqual(len(results), 1)
+
+        result = results[0]
+        self.assertIsNone(result['label'])
+        self.assertEqual(result['indent'], '  ')
+
+    def test_find_labeled(self):
+        results = self.load_results("label_vaulted")
+        self.assertEqual(len(results), 1)
+
+        result = results[0]
+        self.assertEqual(result['label'], "dev")
+        self.assertEqual(result['indent'], '  ')
+
+    def test_find_nested(self):
+        results = self.load_results("nested_vaulted")
+        self.assertEqual(len(results), 1)
+
+        result = results[0]
+        self.assertEqual(result['label'], "dev")
+        self.assertEqual(result['indent'], '    ')
