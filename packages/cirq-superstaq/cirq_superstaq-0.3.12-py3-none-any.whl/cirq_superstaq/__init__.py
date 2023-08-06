1b7e9d292552ff4487d3c07b58d2af9b40a52eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:48 +0000

    Add package cirq-superstaq/0.3.12/cirq_superstaq-0.3.12-py3-none-any.whl

---
+++ packages/cirq-superstaq/cirq_superstaq-0.3.12-py3-none-any.whl/cirq_superstaq/__init__.py
@@ -0,0 +1,62 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cirq_superstaq import compiler_output, serialization
+from cirq_superstaq._version import __version__
+from cirq_superstaq.compiler_output import active_qubit_indices
+from cirq_superstaq.custom_gates import (
+    AQTICCX,
+    AQTITOFFOLI,
+    CR,
+    ZX,
+    AceCR,
+    AceCRMinusPlus,
+    AceCRPlusMinus,
+    Barrier,
+    ParallelGates,
+    ParallelRGate,
+    RGate,
+    ZXPowGate,
+    ZZSwapGate,
+    barrier,
+    parallel_gates_operation,
+)
+from cirq_superstaq.job import Job
+from cirq_superstaq.sampler import Sampler
+from cirq_superstaq.service import Service
+
+__all__ = [
+    "__version__",
+    "active_qubit_indices",
+    "AceCR",
+    "AceCRMinusPlus",
+    "AceCRPlusMinus",
+    "barrier",
+    "Barrier",
+    "compiler_output",
+    "CR",
+    "AQTICCX",
+    "AQTITOFFOLI",
+    "Job",
+    "ParallelGates",
+    "ParallelRGate",
+    "parallel_gates_operation",
+    "serialization",
+    "RGate",
+    "Sampler",
+    "Service",
+    "ZX",
+    "ZXPowGate",
+    "ZZSwapGate",
+]
