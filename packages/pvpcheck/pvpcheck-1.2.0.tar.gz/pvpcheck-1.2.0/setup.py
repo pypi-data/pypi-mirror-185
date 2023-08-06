202011f5ee288e4d2ba01c90bd044f23f1bd11c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:52 +0000

    Add package pvpcheck/1.2.0/pvpcheck-1.2.0.tar.gz

---
+++ packages/pvpcheck/pvpcheck-1.2.0.tar.gz/pvpcheck-1.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+  
+long_description = 'Package to check if private repository libraries have a public doppelgaenger with the same name. The goal is to prevent a dependency confusion attack on the PyPi ecosystem.'
+  
+setup(
+        name ='pvpcheck',
+        version ='1.2.0',
+        author ='Robert Cooper Buer McKee',
+        author_email ='RCBM@duck.com',
+        url ='https://github.com/rcmckee/pvpcheck',
+        description ='Package to check if private repository libraries have a public doppelgaenger with the same name. The goal is to prevent a dependency confusion attack on the PyPi ecosystem.',
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = ["pvpcheck"],
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='pypi dependency confusion attack python package pvpcheck public private library index comparison ',
+        install_requires = [],
+        zip_safe = False
+)
