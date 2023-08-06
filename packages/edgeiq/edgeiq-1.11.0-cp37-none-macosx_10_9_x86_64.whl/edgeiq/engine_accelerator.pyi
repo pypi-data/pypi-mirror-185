af1f78def2e3930b6b0e4743cd222de90cb8b7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:13 +0000

    Add package edgeiq/1.11.0/edgeiq-1.11.0-cp37-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-1.11.0-cp37-none-macosx_10_9_x86_64.whl/edgeiq/engine_accelerator.pyi
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Engine(Enum):
+    DNN: str
+    DNN_OPENVINO: str
+    DNN_CUDA: str
+    TENSOR_RT: str
+    HAILO_RT: str
+
+class Accelerator(Enum):
+    DEFAULT: str
+    CPU: str
+    GPU: str
+    MYRIAD: str
+    NVIDIA: str
+    NVIDIA_FP16: str
+    HAILO: str
