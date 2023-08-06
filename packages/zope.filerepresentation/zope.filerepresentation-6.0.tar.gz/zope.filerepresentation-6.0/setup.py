4a533dc60cbe4ddfc634c63e8ed8e7621a231636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:22 +0000

    Add package zope.filerepresentation/6.0/zope.filerepresentation-6.0.tar.gz

---
+++ packages/zope.filerepresentation/zope.filerepresentation-6.0.tar.gz/zope.filerepresentation-6.0/setup.py
@@ -0,0 +1,87 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.filerepresentation package
+"""
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(
+    name='zope.filerepresentation',
+    version='6.0',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='File-system Representation Interfaces',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    keywords="zope3 filesystem representation",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    url='http://zopefilerepresentation.readthedocs.io',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.schema',
+    ],
+    extras_require={
+        'test': [
+            'zope.testrunner',
+        ],
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    include_package_data=True,
+    test_suite='zope.filerepresentation.tests.test_suite',
+    zip_safe=True,
+    python_requires='>=3.7',
+)
