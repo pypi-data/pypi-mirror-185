9496009df8f881fa2f781bb2d25eb23b245a99bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package anshils/0.1.7/anshils-0.1.7.tar.gz

---
+++ packages/anshils/anshils-0.1.7.tar.gz/anshils-0.1.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anshils',
+      version='0.1.7',
+      description='pip install anshils',
+      packages=['anshils'],
+      author_email='i89041056398@gmail.com',
+      zip_safe=False)
