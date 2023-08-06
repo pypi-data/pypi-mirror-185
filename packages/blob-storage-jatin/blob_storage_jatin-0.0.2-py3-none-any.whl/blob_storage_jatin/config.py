aaa593a40dc00777d017cb317e00fe8073c83baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:45 +0000

    Add package blob-storage-jatin/0.0.2/blob_storage_jatin-0.0.2-py3-none-any.whl

---
+++ packages/blob-storage-jatin/blob_storage_jatin-0.0.2-py3-none-any.whl/blob_storage_jatin/config.py
@@ -0,0 +1,6 @@
+BLOB_CONN_STR = "DefaultEndpointsProtocol=https;AccountName=hhnextlogicappnonprod;AccountKey=1G" \
+                "+d3qcQon9ycahlMdn0tcuUiQvljkIoK1yL5ELQx3jjZJkCfhxJNfEPmOF0k00DLwUuuPj6q+97+AStrTdQ4Q" \
+                "==;EndpointSuffix=core.windows.net"
+CONTAINER_NAME = "fast-api-container"
+BLOB_NAME = "temp_req.txt"
+LOGS_BLOB_NAME = "logs.txt"
