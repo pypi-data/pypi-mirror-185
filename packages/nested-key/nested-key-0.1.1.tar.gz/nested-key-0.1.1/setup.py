534ab0c44be55eb5d9f025b955a348f68735fc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:11 +0000

    Add package nested-key/0.1.1/nested-key-0.1.1.tar.gz

---
+++ packages/nested-key/nested-key-0.1.1.tar.gz/nested-key-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+NAME = "nested-key"
+VERSION = "0.1.1"
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="nested-key",
+    author="BayooG",
+    author_email="obaydaba96@gmail.com",
+    url="https://github.com/bayoog/nested-key",
+    keywords=["Nested dict"],
+    python_requires=">=3.6",
+    include_package_data=True,
+    long_description="""\
+    get nested-key
+    """,
+)
