4c0d296691860376c3a0abd68789288cc0c36aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:02 +0000

    Add package lama-cleaner/0.31.1/lama_cleaner-0.31.1-py3-none-any.whl

---
+++ packages/lama-cleaner/lama_cleaner-0.31.1-py3-none-any.whl/lama_cleaner/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+from lama_cleaner.parse_args import parse_args
+from lama_cleaner.server import main
+
+def entry_point():
+    args = parse_args()
+    main(args)
