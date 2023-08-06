4615970ed13f7d1e1ca6b6efe8013479631e2b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:55 +0000

    Add package betwatch/0.7.6/betwatch-0.7.6.tar.gz

---
+++ packages/betwatch/betwatch-0.7.6.tar.gz/betwatch-0.7.6/betwatch/types/__init__.py
@@ -0,0 +1,20 @@
+from .bookmakers import Bookmaker
+from .filters import RaceProjection
+from .markets import (
+    BetfairMarket,
+    BetfairSide,
+    BetfairTick,
+    BookmakerMarket,
+    Fluc,
+    MarketPriceType,
+    Price,
+)
+from .race import (
+    Meeting,
+    MeetingType,
+    Race,
+    RaceLink,
+    RaceUpdate,
+    Runner,
+    SubscriptionUpdate,
+)
