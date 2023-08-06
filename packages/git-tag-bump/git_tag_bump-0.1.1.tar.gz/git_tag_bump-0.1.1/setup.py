61cda23ab11131a77d166f3109965560bd0495fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package git-tag-bump/0.1.1/git_tag_bump-0.1.1.tar.gz

---
+++ packages/git-tag-bump/git_tag_bump-0.1.1.tar.gz/git_tag_bump-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['git_tag_bump']
+install_requires = \
+['typer[all]>=0.7']
+
+entry_points = \
+{'console_scripts': ['git-bump = git_tag_bump:app']}
+
+setup_kwargs = {
+    'name': 'git-tag-bump',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Dmitry Voronin',
+    'author_email': 'dimka665@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
