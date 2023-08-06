09d1ecdcb5a186b1b39d2657fc0d47b19228ab32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package uintel/0.0.4/uintel-0.0.4.tar.gz

---
+++ packages/uintel/uintel-0.0.4.tar.gz/uintel-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools, os
+setuptools.setup(
+    # State the generic information about the package
+    name='uintel',
+    version='0.0.4',
+    author="Sam Archie",
+    author_email="sam.archie@urbanintelligence.co.nz",
+    description="Urban Intelligence's collection of Python code for everyday use",
+    long_description=open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    url="https://github.com/uintel/pyui/",
+    # State the dependecies of the package
+    install_requires=['matplotlib', 'colour', 'slack_sdk', 'sqlalchemy', 'sqlalchemy_utils', 'psycopg2', 'pyyaml', 'tqdm', 'paramiko', 'boto3', 'geopandas', 'esridump', 'requests'],
+    # Include all package files when installing (hence moving over the mplstyle and ttf files)
+    packages=setuptools.find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "uintel.fonts": ["*.ttf"],
+        "uintel.styles": ["*.mplstyle"]
+    }
+)
