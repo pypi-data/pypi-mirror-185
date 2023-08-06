dd8c56e051dd09ba53006020c3d17ee3cd781ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:55 +0000

    Add package transphone/1.3.1/transphone-1.3.1-py3-none-any.whl

---
+++ packages/transphone/transphone-1.3.1-py3-none-any.whl/transphone/tokenizer.py
@@ -0,0 +1,25 @@
+from phonepiece.iso import normalize_lang_id
+from phonepiece.lexicon import read_lexicon
+from transphone.lang.base_tokenizer import BaseTokenizer
+from transphone.lang.eng.tokenizer import ENGTokenizer
+from transphone.lang.cmn.tokenizer import CMNTokenizer
+from transphone.lang.jpn.tokenizer import JPNTokenizer
+from transphone.lang.g2p_tokenizer import G2PTokenizer
+from transphone.lang.epitran_tokenizer import read_epitran_tokenzier
+
+def read_tokenizer(lang_id, g2p_model='latest', device=None):
+
+    lang_id = normalize_lang_id(lang_id)
+
+    if lang_id == 'eng':
+        tokenizer = ENGTokenizer(lang_id, g2p_model, device)
+    elif lang_id == 'cmn':
+        tokenizer = CMNTokenizer(lang_id, g2p_model, device)
+    elif lang_id == 'jpn':
+        tokenizer = JPNTokenizer(lang_id, g2p_model, device)
+    elif lang_id == 'spa':
+        tokenizer = read_epitran_tokenzier('spa-Latn')
+    else:
+        tokenizer = G2PTokenizer(lang_id, g2p_model, device)
+
+    return tokenizer
