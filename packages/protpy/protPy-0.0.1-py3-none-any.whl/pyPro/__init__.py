87268d2ddccc2eeb4024bd9627a1ecce64cffbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:08 +0000

    Add package protpy/0.0.1/protPy-0.0.1-py3-none-any.whl

---
+++ packages/protpy/protPy-0.0.1-py3-none-any.whl/pyPro/__init__.py
@@ -0,0 +1,13 @@
+""" pyPro """
+__name__ = 'pyPro'
+__version__ = "0.0.1"
+__description__ = ""
+__author__ = 'AJ McKenna, https://github.com/amckenna41'
+__authorEmail__ = 'amckenna41@qub.ac.uk'
+__maintainer__ = "AJ McKenna"
+__license__ = 'MIT'
+__url__ = 'https://github.com/amckenna41/pyPro'
+__download_url__ = "https://github.com/amckenna41/pyPro/archive/refs/heads/main.zip"
+__status__ = "Production"
+__keywords__ = ["bioinformatics", "protein engineering", "python", "pypi", "machine learning", \
+        "directed evolution", "sequence activity relationships", "SAR", "aaindex", "protein descriptors"]
