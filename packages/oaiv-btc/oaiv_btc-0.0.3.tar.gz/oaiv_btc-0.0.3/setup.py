2c07816e00d7930c0187f056dfb4a49553ad698d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:44 +0000

    Add package oaiv-btc/0.0.3/oaiv_btc-0.0.3.tar.gz

---
+++ packages/oaiv-btc/oaiv_btc-0.0.3.tar.gz/oaiv_btc-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#
+import setuptools
+from setuptools import setup
+
+
+metadata = {'name': 'oaiv_btc',
+            'maintainer': 'Edward Azizov',
+            'maintainer_email': 'edazizovv@gmail.com',
+            'description': 'BTC Blockchain Interaction Library',
+            'license': 'MIT',
+            'url': 'https://github.com/edazizovv/oaiv_btc',
+            'download_url': 'https://github.com/edazizovv/oaiv_btc',
+            'packages': setuptools.find_packages(),
+            'include_package_data': True,
+            'version': '0.0.3',
+            'long_description': '',
+            'python_requires': '>=3.10',
+            'install_requires': []}
+
+setup(**metadata)
