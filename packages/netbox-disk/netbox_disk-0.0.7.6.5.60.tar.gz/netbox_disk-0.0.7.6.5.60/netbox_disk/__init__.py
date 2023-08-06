1c454f4efd79e6213348f5463a6d88c56fd2913f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:36 +0000

    Add package netbox-disk/0.0.7.6.5.60/netbox_disk-0.0.7.6.5.60.tar.gz

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.60.tar.gz/netbox_disk-0.0.7.6.5.60/netbox_disk/__init__.py
@@ -0,0 +1,18 @@
+from extras.plugins import PluginConfig
+
+__version__ = "0.0.7.6.5.60"
+
+
+class DiskConfig(PluginConfig):
+    name = "netbox_disk"
+    verbose_name = "Netbox Storage"
+    description = "Netbox Storage"
+    min_version = "3.4.0"
+    version = __version__
+    author = "Tim Rhomberg"
+    author_email = "timrhomberg@hotmail.cm.com"
+    required_settings = []
+    base_url = "netbox-disk"
+
+
+config = DiskConfig
---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.60.tar.gz/netbox_disk-0.0.7.6.5.60/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netbox_disk',
+ 'netbox_disk.api',
+ 'netbox_disk.filters',
+ 'netbox_disk.forms',
+ 'netbox_disk.migrations',
+ 'netbox_disk.tables',
+ 'netbox_disk.views']
+
+package_data = \
+{'': ['*'],
+ 'netbox_disk': ['templates/netbox_disk/*', 'templates/netbox_disk/disk/*']}
+
+setup_kwargs = {
+    'name': 'netbox-disk',
+    'version': '0.0.7.6.5.60',
+    'description': 'Netbox Disk Plugin',
+    'long_description': '### Directory structure\n\n```\n+- api - The API type\n+- filters - Filters of the models\n+- forms - The ModelForm, ModelFilterForm, ModelImportForm, ModelBulkEditForm\n+- migrations - DB Django Migration\n+- tables - The ModelBaseTable, ModelTable, RelatedModelTable\n+- templates\n  +- netbox_disk - The detail view of each model\n    +- disk - The template content box in the Virtual Machine Model\n+- views - PhysicalvolumeListView, PhysicalvolumeView, PhysicalvolumeEditView, PhysicalvolumeDeleteView, \n           PhysicalvolumeBulkImportView, PhysicalvolumeBulkEditView, PhysicalvolumeBulkDeleteView\n```\n### Models\n#### ERM\n\n\n#### Drive\nThe drive has 4 parameter:\n\n| Name           |           Example Value           |\n|:---------------|:---------------------------------:|\n| Virtualmachine | test-vm (Link zu virtual machine) |\n| Identifer      |           Festplatte 1            |\n| Cluster        |   STOR2000000 (Link zu cluster)   |\n| Size           |               50GB                |\n| System         |                No                 |\n\n#### Filesystem\nThe filesystem has 1 parameter:\n\n| Name | Example Value |\n|:-----|:-------------:|\n| fs   |     EXT4      |\n\n#### Linux Volume\nThe linux volume has 1 parameter:\n\n| Name            |       Example Value       |\n|:----------------|:-------------------------:|\n| vg_name         |          docker           |\n| lv_name         |          docker           |\n| Path            |      /var/lib/docker      |\n| Filesystem      | NTFS (Link zu Filesystem) |\n\n\n#### Windows Volume\nThe linux volume has 1 parameter:\n\n| Name       |        Example Value        |\n|:-----------|:---------------------------:|\n| drive_name |          (D, E, F)          |\n| Filesystem |  NTFS (Link zu Filesystem)  |\n\n\n\nBasis:\n- pv:\n  - size\n  - Storage Cluster\n  - virtual_machine\n\nWindows Form:\n- drive_name (D, E, F)\n- filesystem (ntfs)\n\nLinux Form:\n- vg name\n- lv name\n- path\n- filesystem\n\n\nExtra Filesystem Model & als ChoiceField ausgeben\n\n# Build\npoetry publish --build\n\n\n\ngit add . && git commit -m "0.0.7.6.5.60" && git push',
+    'author': 'Tim Rhomberg',
+    'author_email': 'timrhomberg@hotmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
