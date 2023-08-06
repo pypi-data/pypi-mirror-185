ac22083b060a79ac40b019eb8fa0689a4bfaaac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package ayugespidertools/1.0.14/ayugespidertools-1.0.14.tar.gz

---
+++ packages/ayugespidertools/ayugespidertools-1.0.14.tar.gz/ayugespidertools-1.0.14/ayugespidertools/templates/project/module/common/DataEnum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from enum import Enum
+
+
+class TableEnum(Enum):
+    """
+    数据库表枚举信息示例
+    """
+
+    article_list_table = {"value": "article_info_list", "notes": "项目列表信息", "demand_code": "DemoSpider_article_list_table_demand_code"}
