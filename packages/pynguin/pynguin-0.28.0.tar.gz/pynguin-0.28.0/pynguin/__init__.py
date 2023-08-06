85be999df0b05ae5a6cb4c71b2234aca42a58f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package pynguin/0.28.0/pynguin-0.28.0.tar.gz

---
+++ packages/pynguin/pynguin-0.28.0.tar.gz/pynguin-0.28.0/pynguin/__init__.py
@@ -0,0 +1,28 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2023 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+"""Pynguin is an automated unit test generation framework for Python."""
+import pynguin.configuration as config
+import pynguin.generator as gen
+
+set_configuration = gen.set_configuration
+run_pynguin = gen.run_pynguin
+Configuration = config.Configuration
+Algorithm = config.Algorithm
+ExportStrategy = config.ExportStrategy
+StatisticsBackend = config.StatisticsBackend
+TypeInferenceStrategy = config.TypeInferenceStrategy
+
+__all__ = [
+    "set_configuration",
+    "run_pynguin",
+    "Configuration",
+    "__version__",
+    "Algorithm",
+    "ExportStrategy",
+    "StatisticsBackend",
+    "TypeInferenceStrategy",
+]
