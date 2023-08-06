05d6d2f70eb513c7c91c60cacd5dfeb29fa84e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:06 +0000

    Add package lantoolkit/0.3.1/lantoolkit-0.3.1.tar.gz

---
+++ packages/lantoolkit/lantoolkit-0.3.1.tar.gz/lantoolkit-0.3.1/lantoolkit/__init__.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 16:52:17 2023
+
+@author: Administrator
+"""
+__all__ = ['7z.py', 'file.py', 'sudo.py', 'pip.py']
+import platform
+import os
+import sudo
+import archive
+import piptools
+# logog=''''''
+# logoe=''''''
+
+
+def __runfst__():
+    __fc = 'Python Version:'+' '+platform.python_version()
+    __et = 'PKG Version:'+'0.3.1'
+    print(__et)
+    print(__fc)
+
+
+# %%Gets the operating system type
+if __name__ == '__main__':
+    # %%Logo
+
+    logo = '''
+    ╔════╦═══╦═══╦╗  ╔╗╔═╦══╦════╗
+    ║╔╗╔╗║╔═╗║╔═╗║║  ║║║╔╩╣╠╣╔╗╔╗║
+    ╚╝║║╚╣║ ║║║ ║║║  ║╚╝║ ║║╚╝║║╚╝
+      ║║ ║║ ║║║ ║║║ ╔╣╔╗║ ║║  ║║
+      ║║ ║╚═╝║╚═╝║╚═╝║║║╚╦╣╠╗ ║║
+      ╚╝ ╚═══╩═══╩═══╩╝╚═╩══╝ ╚╝'''
+
+    # %%Logo Operate
+
+    # Do not run.
+    # Debugging~
+    # def __debuging__setlogo__(logof):
+    #     global logoe
+    #     logoe=logof
+    # if logoe=='':
+    #     pass
+    # else:
+    #     logo==logoe
+    # def _nologo():
+    #     global logog
+    #     logog=''''''
+    # if not logog=='':
+    #     pass
+    # if logog=='':
+    #     logo==logog
+    print(logo)
+    __runfst__()
+
+
+def pshell(shell):
+    os.system("powershell " + shell)
+
+
+class clear:
+    def __init__(self):
+        os.system("cls")
+
+
+sudo = sudo.sudo
+unarchive = archive.unfile
+make_archive = archive.addfile
+pip_install = piptools.install
+pip_uninstall = piptools.uninstall
+pip_reinstall = piptools.reinstall
---
+++ packages/lantoolkit/lantoolkit-0.3.1.tar.gz/lantoolkit-0.3.1/lantoolkit/sudo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 18:46:11 2023
+
+@author: Administrator
+"""
+import os,platform
+def sudopy(*shell,password='',timeout=1800000):
+    if platform.system()=='Windows':
+        with open("temp.py",'a') as f:
+            f.write(shell)
+        os.system('sudo py temp.py')
+    elif platform.system()=='Linux':
+        with open("temp.py",'a') as f:
+            f.write(shell)
+        if not password:
+            os.system('sudo python temp.py')
+        else:
+            os.system('sudo -S '+password+' python temp.py')
+def sudo(shell,password='',timeout=1800000):
+    if platform.system()=='Windows':
+        os.system('sudo '+shell)
+    elif platform.system()=='Linux':
+        if not password:
+            os.system('sudo python temp.py')
+        else:
+            os.system('sudo -S '+password+' python temp.py')
