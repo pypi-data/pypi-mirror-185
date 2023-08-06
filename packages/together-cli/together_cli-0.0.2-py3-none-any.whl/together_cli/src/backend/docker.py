928b6e32ccfcefdb2992b96617d7a5e86ef48d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package together-cli/0.0.2/together_cli-0.0.2-py3-none-any.whl

---
+++ packages/together-cli/together_cli-0.0.2-py3-none-any.whl/together_cli/src/backend/docker.py
@@ -0,0 +1,50 @@
+import os
+from together_cli.src.core.render import render
+from together_cli.src.constants import MODEL_CONFIG
+from together_cli.src.utility import id_generator
+
+DOCKER_TEMPLATE="""
+docker run --rm --gpus device=$CUDA_VISIBLE_DEVICES --ipc=host \
+-e NUM_WORKERS=auto \
+-v {{TOGETHER_DATA_DIR}}:/home/user/.together \
+-v {{TOGETHER_HOME_DIR}}:/host_together_home \
+-v {{TOGETHER_DATA_DIR}}/weights/{{MODEL_NAME}}:/home/user/.together/models/ \
+-v {{TOGETHER_HOME_DIR}}/hf:/hf \
+-v {{TOGETHER_DATA_DIR}}/scratch:/scratch \
+{{CONTAINER_ID}} /usr/local/bin/together start --name {{NODE_NAME}}  --worker.model {{WORKER_MODEL_NAME}} --datadir /host_together_home --worker.model_dir /home/user/.together/models/ --worker.env "HF_HOME=/hf" --worker.mode local-service --worker.group.alloc each --worker.command {{STARTUP_COMMAND}} {{TAGS}} {{MODEL_TYPE}} {{HTTP_PORT}} {{WS_PORT}} --computer.api {{MATCHMAKER_ADDR}}
+"""
+
+def generate_docker_script(
+    home_dir:str,
+    data_dir:str,
+    model_name:str,
+    tags:str,
+    matchmaker_addr:str,
+    port:int
+):
+    node_name = id_generator(size=10)
+    worker_model_name = MODEL_CONFIG[model_name]['worker_model']
+    model_type=""
+    if 'model_type' in MODEL_CONFIG[model_name]:
+        model_type = f"--worker.model_type {MODEL_CONFIG[model_name]['model_type']}"
+    if tags!="":
+        tags = "--worker.tags " + tags
+    http_port = f"--jsonrpc.http.port {port}"
+    ws_port = f"--jsonrpc.ws.port {port+1}"
+    startup_command = MODEL_CONFIG[model_name]['startup_command']
+    container_id = MODEL_CONFIG[model_name]['docker_id']
+    return render(
+        DOCKER_TEMPLATE,
+        together_home_dir=home_dir,
+        together_data_dir=data_dir,
+        model_name=model_name,
+        worker_model_name=worker_model_name,
+        model_type=model_type,
+        startup_command=startup_command,
+        container_id=container_id,
+        tags = tags,
+        matchmaker_addr = matchmaker_addr,
+        node_name = node_name,
+        http_port = http_port,
+        ws_port = ws_port,
+    )
