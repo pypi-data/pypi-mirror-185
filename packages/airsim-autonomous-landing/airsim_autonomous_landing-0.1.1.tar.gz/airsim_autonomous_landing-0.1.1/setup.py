cbe93aba7a67114444e795abc68ab1574472c03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:47:17 +0000

    Add package airsim-autonomous-landing/0.1.1/airsim_autonomous_landing-0.1.1.tar.gz

---
+++ packages/airsim-autonomous-landing/airsim_autonomous_landing-0.1.1.tar.gz/airsim_autonomous_landing-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+def parse_requirements_file(filename="requirements.txt"):
+    with open(filename) as fid:
+        requires = [lin.strip() for lin in fid.readlines()]
+    return requires
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE/"README.md").read_text()
+REQUIRES = parse_requirements_file()
+
+setup(
+    name="airsim_autonomous_landing", 
+    version="0.1.1", 
+    description="easy to use autonomous landing package for airsim",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RazGavrieli/Autonomous-Drone-Landing-On-Moving-Object-AirSim",
+    author="Raz Gavrieli", 
+    author_email="razgavrieli@gmail.com",
+    ackages=find_packages(exclude=("tests", "close-target-long-leash-time", "far-target(long-catching)")),
+    include_package_data=True,
+    install_requires = REQUIRES,
+    license="MIT"
+    )
+
+
