486678fe58682b23caa3dc887283b776d34f54d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:16 +0000

    Add package munitests/0.2.4/munitests-0.2.4.tar.gz

---
+++ packages/munitests/munitests-0.2.4.tar.gz/munitests-0.2.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['munitests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ibm-watson>=6.1.0,<7.0.0',
+ 'jupyter>=1.0.0,<2.0.0',
+ 'matplotlib>=3.6.3,<4.0.0',
+ 'notebook>=6.5.2,<7.0.0',
+ 'pandas>=1.5.2,<2.0.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'scikit-learn>=1.2.0,<2.0.0',
+ 'seaborn>=0.12.2,<0.13.0',
+ 'tqdm>=4.64.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'munitests',
+    'version': '0.2.4',
+    'description': 'A test suite for Chatbot Muni',
+    'long_description': '# Munitests\n\nA test suite for the municipal chatbot Muni.\n\nThis package is made with love to support the ongoing development of digitalization in the Danish municipalities. The package contain core functionality\n',
+    'author': 'Jens Peder Meldgaard',
+    'author_email': 'jenspederm@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
