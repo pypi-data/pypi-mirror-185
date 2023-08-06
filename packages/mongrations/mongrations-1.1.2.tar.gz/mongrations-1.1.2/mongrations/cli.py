4d7ad732163a021e57754e7cd9169bfeb0eefcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:56 +0000

    Add package mongrations/1.1.2/mongrations-1.1.2.tar.gz

---
+++ packages/mongrations/mongrations-1.1.2.tar.gz/mongrations-1.1.2/mongrations/cli.py
@@ -0,0 +1,114 @@
+import click
+import sys
+import json
+import os
+# check python version
+try:
+    sys_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+    min_required_version = "3.6"
+    if int(sys_version[sys_version.index('.')+1:]) < int(min_required_version[min_required_version.index('.')+1:]):
+        click.echo(f"Python version 3.6 or greater is required to run mongrations. Your system version: {sys_version}")
+        sys.exit(98)
+except Exception:
+    pass
+
+try:
+    from mongrations.main import MongrationsCli
+    from mongrations.version import __version__
+except Exception:
+    from .main import MongrationsCli
+    from .version import __version__
+
+main = MongrationsCli()
+
+def add_credentials_to_application_environment(service: str, credentials: dict) -> None:
+    service_prefix = {
+        "mongodb": "MONGO_",
+        "mysql": "MYSQL_",
+        "postgres": "POSTGRES_"
+    }.get(service, None)
+    for key in credentials:
+        os.environ[f'{service_prefix}{key}'] = str(credentials[key])
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    """Mongrations; a database independent migration and seeding tool for python. Compatible with MySQL, PostgreSQL and MongoDB."""
+    pass
+
+
+@cli.command()
+@click.option("--file", required=False, help="Pass a migration file with database credentials.")
+@click.option("--env", default="development", required=False, help="Application environment. This is used with --file.")
+@click.option("--service", required=False, help="The database service to use. Options: mongodb, mysql or postgres.")
+def migrate(file, env, service):
+    """Run migrations. The up() method will run on all migration files."""
+    if file is not None and service is None:
+        click.echo("You must provide the service name.")
+        sys.exit(86)
+    if file is not None:
+        try:
+            with open(os.path.join(os.getcwd(), file)) as mf:
+                mongration_file = json.load(mf)
+        except FileNotFoundError:
+            click.echo(f"The migration file, {file}, is not a file or is a directory.")
+            sys.exit(86)
+        try:
+            credentials = mongration_file[env][service]
+        except KeyError:
+            click.echo("KeyError: Confirm the service name and env are valid.")
+            sys.exit(86)
+        add_credentials_to_application_environment(service, credentials)
+    main.migrate()
+
+
+@cli.command()
+@click.argument('name', nargs=1)
+@click.argument('directory', nargs=-1)
+def create(name, directory):
+    """Create a new migration file. You can specify a name after the create command. You may also specify a directory name
+    after the name argument."""
+    if len(directory) > 0:
+        directory = directory[0]
+    else:
+        directory = 'migrations'
+    if len(name) == 0:
+        name = 'no_name_migration'
+    main.create(directory=directory, name=name)
+
+
+@cli.command()
+def undo():
+    """Undo the last migration. Undo will run the down() method on the last migration
+    file created and delete the migration file."""
+    value = click.prompt("Are you sure you want to undo? (Y/n) ")
+    if value.lower() == 'y':
+        main.undo()
+
+
+@cli.command()
+def inspect():
+    """Display the config file for mongrations. This will print a list of all migrations,
+    the last migration file created/ran, etc."""
+    main.inspector()
+
+
+@cli.command()
+@click.option("--all", default=False, help="Run down() method all migration files. This will not delete the migration files.")
+def rollback(all):
+    """Run the down() method on the last migration file. If --all is True the down()
+    method will run on all migration files."""
+    if all:
+        main.down()
+    else:
+        main.down(last_migration_only=True)
+
+@cli.command()
+def file():
+    """Generate a mongrationFile.json at the root of the project directory."""
+    main.create_mongration_file()
+
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/mongrations/mongrations-1.1.2.tar.gz/mongrations-1.1.2/setup.py
@@ -0,0 +1,51 @@
+import os.path as path
+from setuptools import setup, find_packages
+
+try:
+    from mongrations.version import __version__
+except ImportError:
+    from mongrations import __version__
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
+setup(
+    name="mongrations",
+    version=__version__,
+    author="AbleInc - Jaylen Douglas",
+    author_email="douglas.jaylen@gmail.com",
+    description="Mongrations; a database independent migration and seeding tool for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ableinc/mongrations",
+    keywords=['migrations', 'python3', 'automation', 'database', 'json', 'nosql', 'python', 'database tool',
+              'automation tool', 'open source', 'mongodb', 'mysql', 'postgres', 'sql'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+      'mongrations': ['mongrations/data/template.txt', 'mongrations/data/mongrationFile.json']
+    },
+    data_files=[
+        ('/mongrations/data', [path.join('mongrations/data', 'template.txt'), path.join('mongrations/data', 'mongrationFile.json')])
+    ],
+    entry_points='''
+        [console_scripts]
+        mongrations=mongrations.cli:cli
+    ''',
+    install_requires=['Click', 'motor', 'pydotenvs', 'pymongo', 'PyMySQL', 'requests', 'psycopg[binary,pool]'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+)
