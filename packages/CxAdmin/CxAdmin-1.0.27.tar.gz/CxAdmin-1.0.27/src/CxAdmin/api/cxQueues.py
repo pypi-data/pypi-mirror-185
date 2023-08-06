ea19d49080f75fb445c789eab42e208c7caa40fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:53 +0000

    Add package CxAdmin/1.0.27/CxAdmin-1.0.27.tar.gz

---
+++ packages/CxAdmin/CxAdmin-1.0.27.tar.gz/CxAdmin-1.0.27/src/CxAdmin/api/cxQueues.py
@@ -0,0 +1,13 @@
+import json
+from CxAdmin.api.cxItem import CxItem
+from CxAdmin.objects.cxQueue import CxQueue
+
+
+class CxQueues(CxItem):
+    def getQueues(self) -> list[CxQueue]:
+        queuesJson: str = self._httpClient.get(self._path).json()
+        queues = [
+            CxQueue.from_json(json.loads(queueJson)["result"])
+            for queueJson in queuesJson
+        ]
+        return queues
