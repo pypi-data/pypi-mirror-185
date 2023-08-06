fc468209c5ead07d5d3c6f898fe12283a33c478c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:03 +0000

    Add package netbox-disk/0.0.7.6.5.65/netbox_disk-0.0.7.6.5.65.tar.gz

---
+++ packages/netbox-disk/netbox_disk-0.0.7.6.5.65.tar.gz/netbox_disk-0.0.7.6.5.65/netbox_disk/template_content.py
@@ -0,0 +1,28 @@
+from extras.plugins import PluginTemplateExtension
+
+from netbox_disk.models import Drive
+from netbox_disk.tables import RelatedDriveTable
+
+
+class RelatedDrives(PluginTemplateExtension):
+    model = "virtualization.virtualmachine"
+
+    def right_page(self):
+        obj = self.context.get("object")
+
+        drives = Drive.objects.filter(
+            virtual_machine=obj
+        )
+        drive_table = RelatedDriveTable(
+            data=drives
+        )
+
+        return self.render(
+            "netbox_disk/disk/disk_box.html",
+            extra_context={
+                "related_drives": drive_table,
+            },
+        )
+
+
+template_extensions = [RelatedDrives]
