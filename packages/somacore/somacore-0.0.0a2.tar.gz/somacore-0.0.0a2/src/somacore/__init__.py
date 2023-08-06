bd7d202a7354dc86200aabb48e7c73c3bc8f4a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package somacore/0.0.0a2/somacore-0.0.0a2.tar.gz

---
+++ packages/somacore/somacore-0.0.0a2.tar.gz/somacore-0.0.0a2/src/somacore/__init__.py
@@ -0,0 +1,39 @@
+"""Module base for the Python reference specification of SOMA.
+
+Types will be defined in their own modules and then imported here for a single
+unified namespace.
+"""
+
+from somacore import _version
+from somacore import base
+from somacore import data
+from somacore import options
+
+__version__ = _version.version
+__version_tuple__ = _version.version_tuple
+
+SOMAObject = base.SOMAObject
+Collection = base.Collection
+
+DataFrame = data.DataFrame
+NDArray = data.NDArray
+DenseNDArray = data.DenseNDArray
+SparseNDArray = data.SparseNDArray
+ReadIter = data.ReadIter
+SparseRead = data.SparseRead
+
+IOfN = options.IOfN
+BatchSize = options.BatchSize
+
+__all__ = (
+    "SOMAObject",
+    "Collection",
+    "DataFrame",
+    "NDArray",
+    "DenseNDArray",
+    "SparseNDArray",
+    "ReadIter",
+    "SparseRead",
+    "IOfN",
+    "BatchSize",
+)
