00b3b0b44a4017fa487184d2f8e859e2f9cd928c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:06 +0000

    Add package video2audio/2023.1.13/video2audio-2023.1.13.tar.gz

---
+++ packages/video2audio/video2audio-2023.1.13.tar.gz/video2audio-2023.1.13/src/video2audio/cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def main():
+    pass
+    # parser = argparse.ArgumentParser( ... )
+    # TODO: print result(s) / exit (code?)
+
+
+if __name__ == '__main__':
+    main()
