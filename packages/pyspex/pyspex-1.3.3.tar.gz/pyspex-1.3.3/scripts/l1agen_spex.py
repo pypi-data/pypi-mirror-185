a61084126b1ba5b7d175d9348e1f8131879ac139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:01 +0000

    Add package pyspex/1.3.3/pyspex-1.3.3.tar.gz

---
+++ packages/pyspex/pyspex-1.3.3.tar.gz/pyspex-1.3.3/scripts/l1agen_spex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+# This file is part of pyspex
+#
+# https://github.com/rmvanhees/pyspex.git
+#
+# Copyright (c) 2022-2023 SRON - Netherlands Institute for Space Research
+#    All Rights Reserved
+#
+# License:  BSD-3-Clause
+"""Python script to store SPEXone Level-0 data in a Level-1A product."""
+
+import sys
+
+from pyspex import version
+from pyspex.gen_l1a.cli import main
+
+if __name__ == '__main__':
+    print(f'l1agen_spex.py {version.get()}\n')
+    sys.exit(main())
