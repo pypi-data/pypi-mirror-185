4123d6809fe7e34720693a8aceb4812af526d12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:51 +0000

    Add package geci-pipa/0.1.1/geci_pipa-0.1.1-py3-none-any.whl

---
+++ packages/geci-pipa/geci_pipa-0.1.1-py3-none-any.whl/geci_pipa/__init__.py
@@ -0,0 +1,4 @@
+"""Internal platforms for analysts module"""
+
+__version__ = "0.1.1"
+from .repo_init import *  # noqa
---
+++ packages/geci-pipa/geci_pipa-0.1.1-py3-none-any.whl/geci_pipa/repo_init.py
@@ -0,0 +1,25 @@
+import os
+
+
+def repo_init() -> None:
+    os.system("template in GitHub")
+
+
+def change_module_name_in_makefile() -> None:
+    change_module_name_in_file("Makefile")
+
+
+def change_module_name_in_pyproject() -> None:
+    change_module_name_in_file("pyproject.toml")
+
+
+def change_module_name_in_tests() -> None:
+    change_module_name_in_file("tests/test_transformations.py")
+
+
+def change_module_name_in_actions() -> None:
+    change_module_name_in_file(".github/workflows/develop.yml")
+
+
+def change_module_name_in_file(filename: str) -> None:
+    os.system(f"sed --in-place 's/dummy_transformations/pipa/' {filename}")
