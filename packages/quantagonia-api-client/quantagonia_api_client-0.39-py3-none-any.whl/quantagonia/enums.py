67cb955c799e00a884ad94412ebbcf673c1b3a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:24 +0000

    Add package quantagonia-api-client/0.39/quantagonia_api_client-0.39-py3-none-any.whl

---
+++ packages/quantagonia-api-client/quantagonia_api_client-0.39-py3-none-any.whl/quantagonia/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class HybridSolverConnectionType(Enum):
+	CLOUD = 0
+	LOCAL = 1
+
+class HybridSolverServers(Enum):
+    PROD = "https://api.quantagonia.com"
+    STAGING = "https://staging.quantagonia.com"
+    DEV = "https://dev.quantagonia.com"
+    DEV3 = "https://dev3.quantagonia.com"
+    LOCAL = "http://localhost:8088"
