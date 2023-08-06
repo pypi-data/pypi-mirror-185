75f7054d48ad4f1d7d381467b37f1fbb0aff9d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:47 +0000

    Add package crocodile/9.0/crocodile-9.0.tar.gz

---
+++ packages/crocodile/crocodile-9.0.tar.gz/crocodile-9.0/myresources/crocodile/cluster/distribute.py
@@ -0,0 +1,156 @@
+
+import numpy as np
+# import psutil
+import crocodile.toolbox as tb
+from math import ceil
+from crocodile.cluster.remote_machine import RemoteMachine
+from rich.console import Console
+# from platform import system
+
+
+console = Console()
+
+
+class Cluster:
+    @staticmethod
+    def get_cluster_path(job_id): return tb.P.home().joinpath(rf"tmp_results/remote_machines/job_id__{job_id}")
+
+    def __init__(self, machine_specs_list: list[dict], ditch_unavailable_machines=False, max_num: int = 100, criterion_name: str = ["cpu", "ram", "both"][-1], func_kwargs_list=None, load_ratios=None, load_ratios_repr="", open_console=False, description="", **kwargs, ):
+        self.job_id = tb.randstr(length=10)
+        self.results_path = self.get_cluster_path(self.job_id)
+        self.results_downloaded = False
+        self.load_ratios = load_ratios
+        self.load_ratios_repr = load_ratios_repr
+        self.max_num = max_num
+        self.criterion_name = criterion_name
+
+        sshz = []
+        for machine_specs in machine_specs_list:
+            try:
+                tmp = tb.SSH(**machine_specs)
+                sshz.append(tmp)
+            except Exception:
+                print(f"Couldn't connect to {machine_specs}")
+                if ditch_unavailable_machines: continue
+                else: raise Exception(f"Couldn't connect to {machine_specs}")
+
+        self.sshz: list[tb.SSH] = sshz
+        self.machines: list[RemoteMachine] = []
+        self.rams, self.cpus = None, None
+
+        self.description = description
+        self.open_console = open_console
+        self.kwargs = kwargs
+        if func_kwargs_list is None:
+            self.generate_standard_kwargs()
+            self.viz_load_ratios()
+        else: self.func_kwargs_list = func_kwargs_list
+
+    def __repr__(self): return f"Cluster with following machines:\n" + "\n".join([repr(item) for item in (self.machines if self.machines else self.sshz)])
+
+    def generate_standard_kwargs(self):
+        cpus = []
+        for an_ssh in self.sshz:
+            a_cpu = an_ssh.run_py("import psutil; print(psutil.cpu_count())", verbose=False).capture().op
+            a_cpu = int(a_cpu)
+            cpus.append(a_cpu)
+        self.cpus = np.array(cpus)
+        cpu_ratios = self.cpus / self.cpus.sum()
+
+        self.rams = np.array([ceil(int(an_ssh.run_py("import psutil; print(psutil.virtual_memory().total)", verbose=False).capture().op) / 2**30) for an_ssh in self.sshz])
+        ram_ratios = self.rams / self.rams.sum()
+
+        both_ratios = (cpus * self.rams) / (cpus * self.rams).sum()
+        self.load_ratios = self.load_ratios if self.load_ratios is not None else {"cpu": cpu_ratios, "ram": ram_ratios, "both": both_ratios}[self.criterion_name]
+
+        func_kwargs_list = []
+        for a_ratio, a_cpu, a_ram in zip(self.load_ratios, cpus, self.rams):
+            idx1 = 0 if len(func_kwargs_list) == 0 else func_kwargs_list[-1]["end_idx"]
+            idx2 = self.max_num if len(func_kwargs_list) == len(cpu_ratios) - 1 else (ceil(a_ratio * self.max_num) + idx1)
+            func_kwargs_list.append(dict(start_idx=idx1, end_idx=idx2, num_threads=a_cpu, ram_gb=a_ram))
+        self.func_kwargs_list = func_kwargs_list
+
+    def viz_load_ratios(self):
+        plt = tb.install_n_import("plotext")
+        names = tb.L(self.sshz).get_repr('remote', add_machine=True).list
+
+        plt.simple_multiple_bar(names, [list(self.cpus), list(self.rams)], title=f"Resources per machine", labels=["#cpu threads", "memory size"])
+        plt.show()
+        print("")
+        plt.simple_bar(names, self.load_ratios, width=100, title=f"Load distribution for machines using criterion `{self.criterion_name}`")
+        plt.show()
+
+        self.load_ratios_repr = tb.S(dict(zip(names, tb.L((self.load_ratios * 100).round(1)).apply(lambda x: f"{int(x)}%")))).print(as_config=True, justify=75, return_str=True)
+        print(self.load_ratios_repr)
+
+    def submit(self):
+        for idx, (a_kwargs, an_ssh) in enumerate(zip(self.func_kwargs_list, self.sshz)):
+            m = RemoteMachine(kwargs=a_kwargs, ssh=an_ssh, open_console=self.open_console, description=self.description + f"\nLoad Ratios on machines:\n{self.load_ratios_repr}", job_id=self.job_id + f"_{idx}", **self.kwargs)
+            m.run()
+            self.machines.append(m)
+        try:
+            tb.Save.pickle(obj=self, path=self.results_path.joinpath("cluster.Cluster.pkl"))
+        except TypeError:
+            print("Couldn't pickle cluster object")
+
+    def mux_consoles(self):
+        cmd = ""
+        for idx, m in enumerate(self.machines):
+            if idx == 0: cmd += f""" wt pwsh -Command "{m.ssh.get_ssh_conn_str()}" `; """
+            else: cmd += f""" split-pane --horizontal --size 0.8 pwsh -Command "{m.ssh.get_ssh_conn_str()}" `; """
+
+        tb.Terminal().run_async(*cmd.split(" "))
+
+    def check_job_status(self): tb.L(self.machines).apply(lambda machine: machine.check_job_status())
+    def download_results(self):
+        if self.results_path is None: self.check_job_status()
+        if self.results_path is None: print(f"Results are not ready yet. Try later"); return None
+        if self.results_downloaded:
+            print(f"All results downloaded to {self.results_path} 🤗")
+            return True
+        for idx, a_m in enumerate(self.machines):
+            results_folder = tb.P(a_m.results_path).expanduser()
+            if results_folder is not None and a_m.results_downloaded is False:
+                print("\n")
+                console.rule(f"Downloading results from {a_m}")
+                print("\n")
+                a_m.download_results(target=self.results_path.joinpath(results_folder.name), r=True, zip_first=False)
+        if tb.L(self.machines).results_downloaded.to_numpy().sum() == len(self.machines):
+            print(f"All results downloaded to {self.results_path} 🤗")
+            self.results_downloaded = True
+
+    @staticmethod
+    def load(job_id) -> 'Cluster': return Cluster.get_cluster_path(job_id=job_id).joinpath("cluster.Cluster.pkl").readit()
+    def run(self):
+        self.generate_standard_kwargs()
+        self.submit()
+        self.mux_consoles()
+        return self
+
+
+def try_it():
+    from crocodile.cluster.trial_file import expensive_function_parallel
+    machine_specs_list = [dict(host="thinkpad"), dict(host="p51s")]  # , dict(host="surface_wsl"), dict(port=2224)
+    c = Cluster(func=expensive_function_parallel, machine_specs_list=machine_specs_list, max_num=1000, install_repo=False)
+    print(c)
+    c.generate_standard_kwargs()
+    c.submit()
+    c.mux_consoles()
+
+    # later ...
+    c = Cluster.load("cluster")
+    c.check_job_status()
+    c.download_results()
+    tb.L(c.machines).delete_remote_results()
+    return c
+
+
+class Zellij:
+    def __init__(self, ssh):
+        self.ssh = ssh
+    def get_session(self):
+        return self.ssh.run("zellij ls").op.split("\n")[0]
+
+
+if __name__ == '__main__':
+    pass
---
+++ packages/crocodile/crocodile-9.0.tar.gz/crocodile-9.0/myresources/crocodile/cluster/remote_machine.py
@@ -0,0 +1,300 @@
+
+import crocodile.toolbox as tb
+from crocodile.cluster import meta_handling as meta
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich import inspect
+from rich.console import Console
+import time
+import os
+import pandas as pd
+
+
+class ResourceManager:
+    lock_path = tb.P(f"~/tmp_results/remote_machines/lock.Struct.pkl")
+
+    def __init__(self, job_id, remote_machine_type):
+        """Log files to track execution process:
+        * A text file that cluster deletes at the begining then write to at the end of each job.
+        * pickle of Machine and clusters objects.
+        """
+        # EVERYTHING MUST REMAIN IN RELATIVE PATHS
+        self.remote_machine_type = remote_machine_type
+        self.job_id = job_id
+
+        self.submission_time = pd.Timestamp.now()
+
+        self.root_dir = tb.P(f"~/tmp_results/remote_machines/job_id__{self.job_id}")
+        self.machine_obj_path = self.root_dir.joinpath(f"machine.Machine.pkl")
+        # tb.P(self.func_relative_file).stem}__{self.func.__name__ if self.func is not None else ''}
+        self.py_script_path = self.root_dir.joinpath(f"python/cluster_wrap.py").create(parents_only=True)
+        self.cloud_download_py_script_path = self.root_dir.joinpath(f"python/download_data.py").create(parents_only=True)
+        self.shell_script_path = self.root_dir.joinpath(f"shell/cluster_script" + {"Windows": ".ps1", "Linux": ".sh"}[self.remote_machine_type]).create(parents_only=True)
+        self.kwargs_path = self.root_dir.joinpath(f"data/cluster_kwargs.Struct.pkl").create(parents_only=True)
+        self.execution_log_dir = self.root_dir.joinpath(f"logs").create()
+
+    shell_script_path_log = rf"~/tmp_results/cluster/last_cluster_script.txt"
+    # simple text file referring to shell script path
+
+    def get_resources_unlocking(self):
+        return f"""
+rm {self.lock_path.collapseuser()}
+echo "Unlocked resources"
+"""
+    def secure_resources(self):
+        console = Console()
+        sleep_time_mins = 10
+        lock_path = self.lock_path.expanduser()
+        print(f"Inspecting Lock file @ {lock_path}")
+        if not lock_path.exists(): print(f"Lock file was not found, creating it...")
+        else:
+            lock_file = lock_path.readit()
+            lock_status = lock_file['status']
+            print(f"Found lock file with status = `{lock_status}`.")
+            if lock_status == 'locked':
+                while lock_status == 'locked':
+                    import psutil
+                    try: proc = psutil.Process(lock_file['pid'])
+                    except psutil.NoSuchProcess:
+                        print(f"Locking process with pid {lock_file['pid']} is dead. Ignoring this lock file.")
+                        break
+                    attrs_txt = ['status', 'memory_percent', 'exe', 'num_ctx_switches',
+                                 'ppid', 'num_threads', 'pid', 'cpu_percent', 'create_time', 'nice',
+                                 'name', 'cpu_affinity', 'cmdline', 'username', 'cwd']
+                    if self.remote_machine_type == 'Windows': attrs_txt += ['num_handles']
+                    # environ, memory_maps
+                    attrs_objs = ['memory_info', 'memory_full_info', 'cpu_times', 'ionice', 'threads', 'io_counters', 'open_files', 'connections']
+                    inspect(tb.Struct(proc.as_dict(attrs=attrs_objs)), value=False, title=f"Process holding the Lock (pid = {lock_file['pid']})", docs=False, sort=False)
+                    inspect(tb.Struct(proc.as_dict(attrs=attrs_txt)), value=False, title=f"Process holding the Lock (pid = {lock_file['pid']})", docs=False, sort=False)
+
+                    print(f"Submission time: {self.submission_time}")
+                    print(f"Time now: {pd.Timestamp.now()}")
+                    print(f"Time spent waiting in the queue so far = {pd.Timestamp.now() - self.submission_time} 🛌")
+                    print(f"Time consumed by locking job (job_id = {lock_file['job_id']}) so far = {pd.Timestamp.now() - lock_file['start_time']} ⏰")
+                    console.rule(title=f"Resources are locked by another job `{lock_file['job_id']}`. Sleeping for {sleep_time_mins} minutes. 😴", style="bold red", characters="-")
+                    print("\n")
+                    time.sleep(sleep_time_mins * 60)
+                    lock_status = lock_path.readit()['status']
+        self.lock_resources()
+        console.print(f"Resources are locked by this job `{self.job_id}`. Process pid ={os.getpid()}.", highlight=True)
+
+    def lock_resources(self):
+        tb.Struct(status="locked", pid=os.getpid(), job_id=self.job_id, start_time=pd.Timestamp.now(), submission_time=self.submission_time).save(path=self.lock_path.expanduser())
+    def unlock_resources(self):
+        dat = self.lock_path.expanduser().readit()
+        dat['status'] = 'unlocked'
+        dat.save(path=ResourceManager.lock_path.expanduser())
+        console = Console()
+        console.print(f"Resources have been released by this job `{self.job_id}`.")
+        # this is further handled by the calling script in case this function failed.
+
+
+class RemoteMachine:
+    def __init__(self, func, kwargs: dict or None = None, description="",
+                 copy_repo: bool = False, update_repo: bool = False, update_essential_repos: bool = True,
+                 data: list or None = None, open_console: bool = True, transfer_method="sftp", job_id=None,
+                 notify_upon_completion=False, to_email=None, email_config_name=None,
+                 machine_specs=None, ssh=None, install_repo=None,
+                 ipython=False, interactive=False, pdb=False, wrap_in_try_except=False,
+                 lock_resources=False):
+
+        # function and its data
+        if type(func) is str or type(func) is tb.P: self.func_file, self.func = tb.P(func), None
+        elif "<class 'module'" in str(type(func)): self.func_file, self.func = tb.P(func.__file__), None
+        else: self.func_file, self.func = tb.P(func.__code__.co_filename), func
+        try:
+            self.repo_path = tb.P(tb.install_n_import("git", "gitpython").Repo(self.func_file, search_parent_directories=True).working_dir)
+            self.func_relative_file = self.func_file.relative_to(self.repo_path)
+        except: self.repo_path, self.func_relative_file = self.func_file.parent, self.func_file.name
+        self.kwargs = kwargs or tb.S()
+        self.data = data if data is not None else []
+        self.description = description
+        self.copy_repo = copy_repo
+        self.update_repo = update_repo
+        self.install_repo = install_repo if install_repo is not None else (True if "setup.py" in self.repo_path.listdir().apply(str) else False)
+        self.update_essential_repos = update_essential_repos
+
+        # execution behaviour
+        self.wrap_in_try_except = wrap_in_try_except
+        self.ipython = ipython
+        self.interactive = interactive
+        self.pdb = pdb
+        self.execution_command = None
+
+        # remote machine behaviour
+        self.open_console = open_console
+        self.notify_upon_completion = notify_upon_completion
+        self.to_email = to_email
+        self.email_config_name = email_config_name
+        self.lock_resources = lock_resources
+
+        # conn
+        self.machine_specs = machine_specs
+        self.transfer_method = transfer_method
+        self.ssh = ssh or tb.SSH(**machine_specs)
+
+        # scripts
+        self.job_id = job_id or tb.randstr(length=10)
+        self.path_dict = ResourceManager(self.job_id, self.ssh.get_remote_machine())
+
+        # flags
+        self.submitted = False
+        self.results_downloaded = False
+        self.results_path = None
+
+    def __repr__(self): return f"Compute Machine {self.ssh.get_repr('remote', add_machine=True)}"
+    def execution_command_to_clip_memory(self): print(self.execution_command); tb.install_n_import("clipboard").copy(self.execution_command)
+    def fire_execution_script_on_remote(self):
+        # self.ssh.run(f"zellij --session cluster action new-tab; zellij --session cluster action write-chars {self.execution_command}")
+        sep = "\n"
+        self.ssh.run(f"zellij --session {self.ssh.run('zellij ls').op.split(sep)[0]} action write-chars '{self.execution_command}'")
+
+    # m.ssh.run(f"zellij --session {m.ssh.run('zellij ls').op.split(sep)[0]} run -- '{m.execution_command}'")
+
+    def check_job_status(self) -> tb.P or None:
+        if not self.submitted:
+            print("Job even not submitted yet. 🤔")
+            return None
+        elif self.results_downloaded:
+            print("Job already completed. 🤔")
+            return None
+
+        base = self.path_dict.execution_log_dir.expanduser().create()
+        try: self.ssh.copy_to_here(self.path_dict.execution_log_dir, zip_first=True)
+        except: pass  # the directory doesn't exist yet at the remote.
+        end_time_file = base.joinpath("end_time.txt")
+
+        if not end_time_file.exists():
+
+            start_time_file = base.joinpath("start_time.txt")
+
+            if not start_time_file.exists():
+                print(f"Job {self.job_id} is still in the queue. 🤯")
+            else:
+                start_time = start_time_file.read_text()
+                print(f"Machine {self.ssh.get_repr('remote', add_machine=True)} has not yet finished job `{self.job_id}`. 😟")
+                print(f"It started at {start_time}. 🕒, and is still running. 🏃‍♂️")
+                import pandas as pd
+                print(f"Execution time so far: {pd.Timestamp.now() - pd.to_datetime(start_time)}. 🕒")
+        else:
+
+            results_folder_file = base.joinpath("results_folder_path.txt")  # it could be one returned by function executed or one made up by the running context.
+            results_folder = results_folder_file.read_text()
+            print(f"""Machine {self.ssh.get_repr('remote', add_machine=True)} has finished job `{self.job_id}`. 😁
+📁 results_folder_path: {results_folder} """)
+            try:
+                print(inspect(base.joinpath("execution_times.Struct.pkl").readit(), value=False, title="Execution Times", docs=False, sort=False))
+            except Exception as err: print(f"Could not read execution times files. 🤷‍, here is the error:\n {err}️")
+
+            self.results_path = results_folder
+            return results_folder
+
+    def download_results(self, target=None, r=True, zip_first=False):
+        if self.results_path is not None:
+            self.ssh.copy_to_here(source=self.results_path, target=target, r=r, zip_first=zip_first)
+            self.results_downloaded = True
+        else:
+            print("Results path is unknown until job execution is finalized. 🤔\nTry checking the job status first.")
+        return self
+    def delete_remote_results(self): self.ssh.run_py(f"tb.P(r'{self.results_path.as_posix()}').delete(sure=True)", verbose=False); return self
+
+    def run(self):
+        self.generate_scripts()
+        self.show_scripts()
+        self.submit()
+        if self.interactive and self.lock_resources:
+            print(f"If interactive is on along with lock_resources, the job might never end.")
+
+    def submit(self):
+        from crocodile.cluster.data_transfer import Submission
+        self.submitted = True  # before sending `self` to the remote.
+        tb.Save.pickle(obj=self, path=self.path_dict.machine_obj_path.expanduser())
+        if self.transfer_method == "transfer_sh":
+            Submission.transfer_sh(machine=self)
+        elif self.transfer_method == "gdrive":
+            Submission.gdrive(machine=self)
+        elif self.transfer_method == "sftp":
+            Submission.sftp(self)
+        else: raise ValueError(f"Transfer method {self.transfer_method} not recognized. 🤷‍")
+        self.execution_command_to_clip_memory()
+
+    def generate_scripts(self):
+
+        func_name = self.func.__name__ if self.func is not None else None
+        func_module = self.func.__module__ if self.func is not None else None
+        rel_full_path = self.repo_path.rel2home().joinpath(self.func_relative_file).as_posix()
+
+        meta_kwargs = dict(ssh_repr=repr(self.ssh),
+                           ssh_repr_remote=self.ssh.get_repr("remote"),
+                           repo_path=self.repo_path.collapseuser().as_posix(),
+                           func_name=func_name, func_module=func_module, rel_full_path=rel_full_path, description=self.description,
+                           job_id=self.job_id, lock_resources=self.lock_resources)
+        py_script = meta.get_py_script(kwargs=meta_kwargs, wrap_in_try_except=self.wrap_in_try_except, func_name=func_name, rel_full_path=rel_full_path)
+
+        if self.notify_upon_completion:
+            if self.func is not None: executed_obj = f"""**{self.func.__name__}** from *{tb.P(self.func.__code__.co_filename).collapseuser().as_posix()}*"""  # for email.
+            else: executed_obj = f"""File *{tb.P(self.repo_path).joinpath(self.func_relative_file).collapseuser().as_posix()}*"""  # for email.
+            meta_kwargs = dict(addressee=self.ssh.get_repr("local", add_machine=True),
+                               speaker=self.ssh.get_repr('remote', add_machine=True),
+                               ssh_conn_string=self.ssh.get_repr('remote', add_machine=False),
+                               executed_obj=executed_obj,
+                               job_id=self.job_id,
+                               to_email=self.to_email, email_config_name=self.email_config_name)
+            py_script += meta.get_script(name="script_notify_upon_completion", kwargs=meta_kwargs)
+
+        shell_script = f"""
+    
+# EXTRA-PLACEHOLDER-PRE
+
+echo "~~~~~~~~~~~~~~~~SHELL~~~~~~~~~~~~~~~"
+{self.ssh.remote_env_cmd}
+{self.ssh.run_py("import machineconfig.scripts.python.devops_update_repos as x; print(x.main(verbose=False))").op if self.update_essential_repos else ''}
+{f'cd {tb.P(self.repo_path).collapseuser().as_posix()}'}
+{'git pull' if self.update_repo else ''}
+{'pip install -e .' if self.install_repo else ''}
+echo "~~~~~~~~~~~~~~~~SHELL~~~~~~~~~~~~~~~"
+
+# EXTRA-PLACEHOLDER-POST
+
+cd ~
+{'python' if (not self.ipython and not self.pdb) else 'ipython'} {'--pdb' if self.pdb else ''} {'-i' if self.interactive else ''} ./{self.path_dict.py_script_path.rel2home().as_posix()}
+
+{self.path_dict.get_resources_unlocking() if self.lock_resources else ''}
+deactivate
+
+"""
+
+        # only available in py 3.10:
+        # shell_script_path.write_text(shell_script, encoding='utf-8', newline={"Windows": None, "Linux": "\n"}[ssh.get_remote_machine()])  # LF vs CRLF requires py3.10
+        with open(file=self.path_dict.shell_script_path.expanduser().create(parents_only=True), mode='w', newline={"Windows": None, "Linux": "\n"}[self.ssh.get_remote_machine()]) as file: file.write(shell_script)
+        tb.Save.pickle(obj=self.kwargs, path=self.path_dict.kwargs_path.expanduser(), verbose=False)
+        self.path_dict.py_script_path.expanduser().create(parents_only=True).write_text(py_script, encoding='utf-8')  # py_version = sys.version.split(".")[1]
+
+    def show_scripts(self) -> None:
+        Console().print(Panel(Syntax(self.path_dict.shell_script_path.expanduser().read_text(), lexer="ps1" if self.ssh.get_remote_machine() == "Windows" else "sh", theme="monokai", line_numbers=True), title="prepared shell script"))
+        Console().print(inspect(tb.Struct(shell_script=repr(tb.P(self.path_dict.shell_script_path).expanduser()), python_script=repr(tb.P(self.path_dict.py_script_path).expanduser()), kwargs_file=repr(tb.P(self.path_dict.kwargs_path).expanduser())), title="Prepared scripts and files.", value=False, docs=False, sort=False))
+
+
+def try_main():
+    # noinspection PyUnresolvedReferences
+    from crocodile.cluster.remote_machine import RemoteMachine  # importing the function is critical for the pickle to work.
+    st = tb.P.home().joinpath("dotfiles/creds/msc/source_of_truth.py").readit()
+    from crocodile.cluster import trial_file
+    m = RemoteMachine(func=trial_file.expensive_function, machine_specs=dict(host="p51s"), update_essential_repos=True,
+                      notify_upon_completion=True, to_email=st.EMAIL['enaut']['email_add'], email_config_name='enaut',
+                      copy_repo=False, update_repo=False, wrap_in_try_except=True, install_repo=False,
+                      ipython=True, interactive=True, lock_resources=True,
+                      transfer_method="sftp")
+    m.generate_scripts()
+    m.show_scripts()
+    m.submit()
+    m.check_job_status()
+
+    m.download_results(r=True)
+    m.delete_remote_results()
+    return m
+
+
+if __name__ == '__main__':
+    # try_main()
+    pass
