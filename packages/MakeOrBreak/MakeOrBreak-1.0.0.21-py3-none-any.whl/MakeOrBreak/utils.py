ac7bb32773d620122c6ea725d74f9fe3ed0fa6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:43 +0000

    Add package MakeOrBreak/1.0.0.21/MakeOrBreak-1.0.0.21-py3-none-any.whl

---
+++ packages/MakeOrBreak/MakeOrBreak-1.0.0.21-py3-none-any.whl/MakeOrBreak/utils.py
@@ -0,0 +1,20 @@
+from http.server import executable
+from subprocess import Popen
+from random import randint
+from sys import argv
+
+def runAsBackgroundProcess(choice, interval):
+   controllerFileLocation = executable + "/controller.py" 
+   print(controllerFileLocation)
+   # Popen(['python3', 'controller.py', str(choice), str(interval)])
+
+def generateRdmNumber(start, end):
+   return randint(start, end)
+
+def getSysArgs():
+   return argv
+
+
+if __name__ == "__main__":
+   runAsBackgroundProcess()
+   print(generateRdmNumber(0, 10))
