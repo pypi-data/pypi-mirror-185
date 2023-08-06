3a72c49ec67d3eb2df60f62499074ac8fcace4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:39 +0000

    Add package django-extra-settings/0.8.0/django_extra_settings-0.8.0-py3-none-any.whl

---
+++ packages/django-extra-settings/django_extra_settings-0.8.0-py3-none-any.whl/tests/test_metadata.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from extra_settings.version import __version__
+
+
+class ExtraSettingsMetadataTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
