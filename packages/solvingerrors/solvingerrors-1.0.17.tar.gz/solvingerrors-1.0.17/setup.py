f6c7aa50d30a4df4562dccd80466245c059b2551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:48 +0000

    Add package solvingerrors/1.0.17/solvingerrors-1.0.17.tar.gz

---
+++ packages/solvingerrors/solvingerrors-1.0.17.tar.gz/solvingerrors-1.0.17/setup.py
@@ -0,0 +1,25 @@
+
+
+###parameter_setup
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="UTF-8") as f:
+    README = f.read()
+setup(
+    name="solvingerrors",
+    version='1.0.17',
+    url="https://github.com/pypa/pip",
+    author="Mani@pythonLovers",
+    author_email="maniscrow@proton.me",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    description="An automated package that solves basic python errors",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
