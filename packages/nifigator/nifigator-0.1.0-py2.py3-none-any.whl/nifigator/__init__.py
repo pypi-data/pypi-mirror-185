a5dd3478a7c45a94120e02e0809bf18c5b747e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:44 +0000

    Add package nifigator/0.1.0/nifigator-0.1.0-py2.py3-none-any.whl

---
+++ packages/nifigator/nifigator-0.1.0-py2.py3-none-any.whl/nifigator/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Nifigator."""
+
+__author__ = """Willem Jan Willemse"""
+__email__ = "w.j.willemse@dnb.nl"
+__version__ = "0.1.0"
+
+from .nifgraph import *
+from .nifobjects import *
+from .utils import *
+from .pdfparser import *
+from .converters import *
