b756be468909eb60855dc119e6b0969a03cfad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:24 +0000

    Add package py-heideltime/1.0.0/py_heideltime-1.0.0.tar.gz

---
+++ packages/py-heideltime/py_heideltime-1.0.0.tar.gz/py_heideltime-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="py_heideltime",
+    version="1.0.0",
+    description="Python wrapper for HeidelTime temporal tagger.",
+    author='Jorge Mendes, Ricardo Campos, and Hugo Sousa',
+    author_email='hmosousa@gmail.com',
+    url='https://github.com/hmosousa/py_heideltime.git',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "emoji==2.0.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'py_heideltime=py_heideltime.cli:cli'
+        ]
+    },
+)
