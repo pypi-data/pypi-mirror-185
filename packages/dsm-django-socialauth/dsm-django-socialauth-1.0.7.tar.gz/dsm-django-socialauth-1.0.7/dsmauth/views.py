23b985c29eda77e7b327fee71c3e8bfabebdf414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:30 +0000

    Add package dsm-django-socialauth/1.0.7/dsm-django-socialauth-1.0.7.tar.gz

---
+++ packages/dsm-django-socialauth/dsm-django-socialauth-1.0.7.tar.gz/dsm-django-socialauth-1.0.7/dsmauth/views.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib.auth import logout as logout_view
+from django.http import HttpResponseRedirect
+
+_REDIRECT_BACKEND = getattr(settings, 'BASE_BACKEND_URL', None)
+_REDIRECT_FRONTEND = getattr(settings, 'BASE_FRONTEND_URL', None)
+_uri = getattr(settings, 'OAUTH_DSM_SERVER_BASEURL', None)
+_scheme = getattr(settings, 'OAUTH_DSM_SCHEME', None)
+if _uri is None:
+    raise Exception("Please input `OAUTH_DSM_SERVER_BASEURL` in settings.py")
+
+def logout(request):
+    logout_view(request)
+    if _REDIRECT_FRONTEND is not None:
+        return HttpResponseRedirect(f"{_scheme}://{_uri}/logout?next={_REDIRECT_FRONTEND}")
+    return HttpResponseRedirect(f"{_scheme}://{_uri}/logout?next={_REDIRECT_BACKEND}")
+    
