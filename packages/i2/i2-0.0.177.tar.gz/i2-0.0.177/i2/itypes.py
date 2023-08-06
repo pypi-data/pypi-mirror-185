e8f6467c6a09f4e4072f455961a75a43a326c728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:53 +0000

    Add package i2/0.0.177/i2-0.0.177.tar.gz

---
+++ packages/i2/i2-0.0.177.tar.gz/i2-0.0.177/i2/itypes.py
@@ -0,0 +1,199 @@
+"""Types"""
+
+from typing import (
+    NewType,
+    Optional,
+    Iterable,
+    Protocol,
+    Any,
+    runtime_checkable,
+    get_args,
+)
+
+
+def new_type(
+    name,
+    tp,
+    doc: Optional[str] = None,
+    aka: Optional[Iterable] = None,
+    assign_to_globals=False,
+):
+    """
+    Make a new type with (optional) doc and (optional) aka, set of var names it often
+    appears as
+
+    Args:
+        name: Name to give the variable
+        tp: type (see typing module)
+        doc: Optional string to put in __doc__ attribute
+        aka: Optional set (or any iterable) to put in _aka attribute,
+            meant to list names the variables of this type often appear as.
+
+    Returns: None
+
+    >>> from typing import Any, Union, List
+    >>> MyType = new_type('MyType', int)
+    >>> # TODO: Skipping the next part because outputs <class 'typing.NewType'> in 3.10
+    >>> type(MyType)  # doctest: +SKIP
+    <class 'function'>
+    >>> Key = new_type('Key', Any, aka=['key', 'k'])
+    >>> sorted(Key._aka)
+    ['k', 'key']
+    >>> Val = new_type(
+    ... 'Val', Union[int, float, List[Union[int, float]]],
+    ... doc="A number or list of numbers.")
+    >>> Val.__doc__
+    'A number or list of numbers.'
+    """
+    new_tp = NewType(name, tp)
+    if doc is not None:
+        setattr(new_tp, '__doc__', doc)
+    if aka is not None:
+        setattr(new_tp, '_aka', set(aka))
+    if assign_to_globals:
+        globals()[
+            name
+        ] = new_tp  # not sure how kosher this is... Should only use at top level of module, for sure!
+    return new_tp
+
+
+class HasAttrs:
+    """
+    Make a protocol to express the existence of specific attributes.
+
+    >>> SizedAndAppendable = HasAttrs["__len__", "append"]
+    >>> assert isinstance([1, 2, 3], SizedAndAppendable)  # lists have both a length and an append
+    >>> assert not isinstance((1, 2, 3), SizedAndAppendable)  # tuples don't have an append
+
+    [Python Protocols](https://www.python.org/dev/peps/pep-0544/) are a way to be able to do
+    "behavior typing" (my bad terminology).
+    Basically, if you want your static analyzer
+    (the swingles in your IDE, or linter validation process...)
+    to check if you're manipulating the expected types, except the types
+    (classes, subclasses, ABCs, abstract classes...) are too restrictive (they are!),
+    you can use Protocols to fill the gap.
+
+    Except writing them can sometimes be verbose.
+
+    With HasAttrs you can have the basic "does it have these attributes" cases covered.
+
+    >>> assert isinstance(dict(), HasAttrs["items"])
+    >>> assert not isinstance(list(), HasAttrs["items"])
+    >>> assert not isinstance(dict(), HasAttrs["append"])
+    >>>
+    >>> class A:
+    ...     prop = 2
+    ...
+    ...     def method(self):
+    ...         pass
+    >>>
+    >>> a = A()
+    >>> assert isinstance(a, HasAttrs["method"])
+    >>> assert isinstance(a, HasAttrs["method", "prop"])
+    >>> assert not isinstance(a, HasAttrs["method", "prop", "this_attr_does_not_exist"])
+
+    """
+
+    def __class_getitem__(self, attr_names):
+        if isinstance(attr_names, str):
+            attr_names = [attr_names]
+        assert all(map(str.isidentifier, attr_names)), (
+            f"The following are not valid python 'identifiers' "
+            f"{', '.join(a for a in attr_names if not a.isidentifier())}"
+        )
+
+        annotations = {attr: Any for attr in attr_names}
+
+        @runtime_checkable
+        class HasAttrs(Protocol):
+            __annotations__ = annotations
+
+            # def __repr__(self):  # TODO: This is for the instance, need it for the class
+            #     return "HasAttrs[{', '.join(annotations)}]"
+
+        return HasAttrs
+
+        # return type(
+        #     "HasAttrs",
+        #     (Protocol,),
+        #     {"__annotations__": {attr: Any for attr in attr_names}},
+        # )
+
+
+# TODO: Complete scary hack: Find another way (see uses)
+def is_a_new_type(typ):
+    return (
+        callable(typ)
+        and getattr(typ, '__qualname__', '').startswith('NewType')
+        and hasattr(typ, '__supertype__')
+    )
+
+
+def typ_name(typ):
+    if is_a_new_type(typ):
+        return typ.__name__
+    else:
+        return typ._name
+
+
+def is_callable_kind(typ):
+    """
+    >>> from typing import Callable, Tuple
+    >>> is_callable_kind(Callable)
+    True
+    >>> is_callable_kind(Callable[[int, float], str])
+    True
+    >>> is_callable_kind(Tuple[int, float, str])
+    False
+    """
+    if is_a_new_type(typ):
+        return is_callable_kind(typ.__supertype__)
+    return typ_name(typ) == 'Callable'
+    # Also possible: typ.mro()[0] == __import__('collections.abc').Callable
+
+
+def input_and_output_types(typ: type):
+    """Returns the input and output types
+
+    >>> from typing import Callable, Tuple
+    >>> input_types, output_type = input_and_output_types(Callable[[float, int], str])
+    >>> assert input_types == [float, int] and output_type == str
+    >>> input_types, output_type = input_and_output_types(Callable[[], str])
+    >>> assert input_types == [] and output_type == str
+
+    But will fail if `typ` isn't a `Callable`:
+
+    >>> input_and_output_types(Tuple[float, int, str])  # doctest: +NORMALIZE_WHITESPACE
+    Traceback (most recent call last):
+      ...
+    AssertionError: Is not a typing.Callable kind: typing.Tuple[float, int, str]
+
+    Will also fail if `typ` is a Callable but not "parametrized".
+
+    >>> input_and_output_types(Callable)  # doctest: +NORMALIZE_WHITESPACE
+    Traceback (most recent call last):
+      ...
+    AssertionError: Can only be used on a Callable[[...],...] kind: typing.Callable
+
+    """
+    if is_a_new_type(typ):
+        return input_and_output_types(typ.__supertype__)
+    assert is_callable_kind(typ), f'Is not a typing.Callable kind: {typ}'
+    typ_args = get_args(typ)
+    assert len(typ_args) > 0, f'Can only be used on a Callable[[...],...] kind: {typ}'
+    return typ_args[0], typ_args[1]
+
+
+def dot_string_of_callable_typ(typ):
+    input_types, output_type = input_and_output_types(typ)
+    return (
+        ','.join(map(typ_name, input_types))
+        + f' -> {typ_name(typ)} -> '
+        + typ_name(output_type)
+    )
+
+
+def dot_strings_of_callable_types(*typs, func_shape='box'):
+    for typ in typs:
+        yield dot_string_of_callable_typ(typ)
+        yield f'{typ_name(typ)} [shape="{func_shape}"]'
---
+++ packages/i2/i2-0.0.177.tar.gz/i2-0.0.177/i2/routing_forest.py
@@ -0,0 +1,486 @@
+"""
+Tools to specify functions through trees and forests.
+
+Whaaa?!?
+
+Well, you see, often -- especially when writing transformers -- you have a series of
+if/then conditions nested into eachother, in code, where it gets ugly and un-reusable.
+
+This module explores ways to objectivy this: That is, to give us the means to create
+such nested conditions in a way that we can define the parts as reusable operable
+components.
+
+Think of the relationship between the for loop (code) and the iterator (object), along
+with iterator tools (itertools).
+This is what we're trying to explore, but for if/then conditions.
+
+I said explore. Some more work is needed here to make it easily usable.
+
+Let's look at an example involving the three main actors of our play.
+Each of these are ``Iterable`` and ``Callable`` (``Generator`` to be precise).
+
+- ``CondNode``: implements the if/then (no else) logic
+- ``FinalNode``: Final -- yields (both with call and iter) it's single `.val` attribute.
+- ``RoutingForest``: An Iterable of ``CondNode``
+
+>>> import inspect
+>>>
+>>> def could_be_int(obj):
+...     if isinstance(obj, int):
+...         b = True
+...     else:
+...         try:
+...             int(obj)
+...             b = True
+...         except ValueError:
+...             b = False
+...     if b:
+...         print(f'{inspect.currentframe().f_code.co_name}')
+...     return b
+...
+>>> def could_be_float(obj):
+...     if isinstance(obj, float):
+...         b = True
+...     else:
+...         try:
+...             float(obj)
+...             b = True
+...         except ValueError:
+...             b = False
+...     if b:
+...         print(f'{inspect.currentframe().f_code.co_name}')
+...     return b
+...
+>>> print(
+...     could_be_int(30),
+...     could_be_int(30.3),
+...     could_be_int('30.2'),
+...     could_be_int('nope'),
+... )
+could_be_int
+could_be_int
+True True False False
+>>> print(
+...     could_be_float(30),
+...     could_be_float(30.3),
+...     could_be_float('30.2'),
+...     could_be_float('nope'),
+... )
+could_be_float
+could_be_float
+could_be_float
+True True True False
+>>> assert could_be_int('30.2') is False
+>>> assert could_be_float('30.2') is True
+could_be_float
+>>>
+>>> st = RoutingForest(
+...     [
+...         CondNode(
+...             cond=could_be_int,
+...             then=RoutingForest(
+...                 [
+...                     CondNode(
+...                         cond=lambda x: int(x) >= 10,
+...                         then=FinalNode('More than a digit'),
+...                     ),
+...                     CondNode(
+...                         cond=lambda x: (int(x) % 2) == 1,
+...                         then=FinalNode("That's odd!"),
+...                     ),
+...                 ]
+...             ),
+...         ),
+...         CondNode(cond=could_be_float, then=FinalNode('could be seen as a float')),
+...     ]
+... )
+>>> assert list(st('nothing I can do with that')) == []
+>>> assert list(st(8)) == ['could be seen as a float']
+could_be_int
+could_be_float
+>>> assert list(st(9)) == ["That's odd!", 'could be seen as a float']
+could_be_int
+could_be_float
+>>> assert list(st(10)) == ['More than a digit', 'could be seen as a float']
+could_be_int
+could_be_float
+>>> assert list(st(11)) == [
+...     'More than a digit',
+...     "That's odd!",
+...     'could be seen as a float',
+... ]
+could_be_int
+could_be_float
+>>>
+>>> print(
+...     '### RoutingForest ########################################################################################'
+... )
+### RoutingForest ########################################################################################
+>>> rf = RoutingForest(
+...     [
+...         SwitchCaseNode(
+...             switch=lambda x: x % 5,
+...             cases={0: FinalNode('zero_mod_5'), 1: FinalNode('one_mod_5')},
+...             default=FinalNode('default_mod_5'),
+...         ),
+...         SwitchCaseNode(
+...             switch=lambda x: x % 2,
+...             cases={0: FinalNode('even'), 1: FinalNode('odd')},
+...             default=FinalNode('that is not an int'),
+...         ),
+...     ]
+... )
+>>>
+>>> assert list(rf(5)) == ['zero_mod_5', 'odd']
+>>> assert list(rf(6)) == ['one_mod_5', 'even']
+>>> assert list(rf(7)) == ['default_mod_5', 'odd']
+>>> assert list(rf(8)) == ['default_mod_5', 'even']
+>>> assert list(rf(10)) == ['zero_mod_5', 'even']
+>>>
+
+"""
+from itertools import chain
+from dataclasses import dataclass
+from typing import Any, Iterable, Callable, Mapping, Tuple
+from i2.util import LiteralVal
+
+
+# TODO: Think a bit harder about this mini-language
+# TODO: Right now use has to explicitly declare final nodes. Can do better.
+def _default_mini_lang(x):
+    # TODO: One is really tempted to use CondNode here to define this, right?
+    if isinstance(x, (CondNode, RoutingForest, SwitchCaseNode)):
+        return x
+    elif isinstance(x, LiteralVal):
+        return x.get_val()
+    elif isinstance(x, tuple):
+        if len(x) == 2:
+            return CondNode(*map(_default_mini_lang, x))
+        elif len(x) == 3:
+            return SwitchCaseNode(*map(_default_mini_lang, x))
+        else:
+            raise ValueError(
+                f'If a tuple, element must be a `(cond, then)` pair, '
+                f'or a (switch, case, default) triple, or a `Literal`. Was: {x}'
+            )
+    elif isinstance(x, dict):
+        keys = set(x)
+        if 2 <= len(keys) <= 3:
+            if {'cond', 'then'}.issubset(keys):
+                return CondNode(**{k: _default_mini_lang(v) for k, v in x.items()})
+            elif {'switch', 'case'}.issubset(keys) and keys == {
+                'switch',
+                'case',
+                'default',
+            }:
+                return SwitchCaseNode(
+                    **{k: _default_mini_lang(v) for k, v in x.items()}
+                )
+            else:
+                raise ValueError(
+                    "keys should be 'switch', 'case' and optionally 'default'. "
+                    f'Were: {keys}'
+                )
+        else:
+            raise ValueError(
+                f"A non-Literal dict must have keys 'cond' and 'then' "
+                f"or 'switch' and 'case' (and optionally 'default'). Was: {x}"
+            )
+    elif isinstance(x, list):
+        return RoutingForest(cond_nodes=list(map(_default_mini_lang, x)))
+    else:
+        return x
+
+
+class RoutingNode:
+    """A RoutingNode instance needs to be callable on a single object, yielding an iterable or a final value"""
+
+    def __call__(self, obj):
+        raise NotImplementedError('You should implement this.')
+
+    @staticmethod
+    def from_object(x, mini_lang=_default_mini_lang):
+        return mini_lang(x)
+
+
+@dataclass
+class FinalNode(RoutingNode):
+    """A RoutingNode that is final.
+    It yields (both with call and iter) it's single `.val` attribute."""
+
+    val: Any
+
+    def __call__(self, obj=None):
+        yield self.val
+
+    def __iter__(self):
+        yield self.val
+
+    # def __getstate__(self):
+    #     return {'val': self.val}
+
+
+# TODO: Add tooling to merge validation into `then` functions/values
+@dataclass
+class CondNode(RoutingNode):
+    """A RoutingNode that implements the if/then (no else) logic"""
+
+    cond: Callable[[Any], bool]
+    then: Any
+
+    def __call__(self, obj):
+        if self.cond(obj):
+            yield from self.then(obj)
+
+    def __iter__(self):
+        yield from self.then
+
+
+@dataclass
+class RoutingForest(RoutingNode):
+    """
+
+    >>> rf = RoutingForest([
+    ...     CondNode(cond=lambda x: isinstance(x, int),
+    ...              then=RoutingForest([
+    ...                  CondNode(cond=lambda x: int(x) >= 10, then=FinalNode('More than a digit')),
+    ...                  CondNode(cond=lambda x: (int(x) % 2) == 1, then=FinalNode("That's odd!"))])
+    ...             ),
+    ...     CondNode(cond=lambda x: isinstance(x, (int, float)),
+    ...              then=FinalNode('could be seen as a float')),
+    ... ])
+    >>> assert list(rf('nothing I can do with that')) == []
+    >>> assert list(rf(8)) == ['could be seen as a float']
+    >>> assert list(rf(9)) == ["That's odd!", 'could be seen as a float']
+    >>> assert list(rf(10)) == ['More than a digit', 'could be seen as a float']
+    >>> assert list(rf(11)) == ['More than a digit', "That's odd!", 'could be seen as a float']
+    """
+
+    cond_nodes: Iterable
+
+    def __post_init__(self):
+        self.cond_nodes = list(map(self.from_object, self.cond_nodes))
+
+    def __call__(self, obj):
+        yield from chain(*(cond_node(obj) for cond_node in self.cond_nodes))
+        # for cond_node in self.cond_nodes:
+        #     yield from cond_node(obj)
+
+    def __iter__(self):
+        yield from chain(*self.cond_nodes)
+
+
+FeatCondThens = Iterable[Tuple[Callable, Callable]]
+
+
+@dataclass
+class FeatCondNode(RoutingNode):
+    """A RoutingNode that yields multiple routes, one for each of several conditions met,
+    where the condition is computed implements computes a feature of the obj and
+    according to an iterable of conditions on the feature"""
+
+    feat: Callable
+    feat_cond_thens: FeatCondThens
+
+    def __call__(self, obj):
+        feature = self.feat(obj)
+        for cond, then in self.feat_cond_thens:
+            if cond(feature):
+                yield from then(obj)
+
+    def __iter__(self):
+        yield from chain(*self.feat_cond_thens.values())
+
+
+NoDefault = type('NoDefault', (object,), {})
+NO_DFLT = NoDefault()
+
+
+@dataclass
+class SwitchCaseNode(RoutingNode):
+    """A RoutingNode that implements the switch/case/else logic.
+    It's just a specialization (enhanced with a "default" option) of the FeatCondNode class to a situation
+    where the cond function of feat_cond_thens is equality, therefore the routing can be
+    implemented with a {value_to_compare_to_feature: then_node} map.
+    :param switch: A function returning the feature of an object we want to switch on
+    :param cases: The mapping from feature to RoutingNode that should be yield for that feature.
+        Often is a dict, but only requirement is that it implements the cases.get(val, default) method.
+    :param default: Default RoutingNode to yield if no
+
+    >>> rf = RoutingForest([
+    ...     SwitchCaseNode(switch=lambda x: x % 5,
+    ...                    cases={0: FinalNode('zero_mod_5'), 1: FinalNode('one_mod_5')},
+    ...                    default=FinalNode('default_mod_5')),
+    ...     SwitchCaseNode(switch=lambda x: x % 2,
+    ...                    cases={0: FinalNode('even'), 1: FinalNode('odd')},
+    ...                    default=FinalNode('that is not an int')),
+    ... ])
+    >>>
+    >>> assert(list(rf(5)) == ['zero_mod_5', 'odd'])
+    >>> assert(list(rf(6)) == ['one_mod_5', 'even'])
+    >>> assert(list(rf(7)) == ['default_mod_5', 'odd'])
+    >>> assert(list(rf(8)) == ['default_mod_5', 'even'])
+    >>> assert(list(rf(10)) == ['zero_mod_5', 'even'])
+    """
+
+    switch: Callable
+    cases: Mapping
+    default: Any = NO_DFLT
+
+    def __call__(self, obj):
+        feature = self.switch(obj)
+        if self.default is NO_DFLT:
+            yield from self.cases.get(feature)(obj)
+        else:
+            yield from self.cases.get(feature, self.default)(obj)
+
+    def __iter__(self):
+        yield from chain(*self.cases.values())
+        if self.default:
+            yield self.default
+
+
+def wrap_leafs_with_final_node(x):
+    for xx in x:
+        if isinstance(xx, RoutingNode):
+            yield xx
+        else:
+            yield FinalNode(xx)
+
+
+def test_routing_forest():
+    def could_be_int(obj):
+        if isinstance(obj, int):
+            b = True
+        else:
+            try:
+                int(obj)
+                b = True
+            except ValueError:
+                b = False
+        # if b:
+        #     print(f'{inspect.currentframe().f_code.co_name}')
+        return b
+
+    def could_be_float(obj):
+        if isinstance(obj, float):
+            b = True
+        else:
+            try:
+                float(obj)
+                b = True
+            except ValueError:
+                b = False
+        # if b:
+        #     print(f'{inspect.currentframe().f_code.co_name}')
+        return b
+
+    assert could_be_int(30)
+    assert could_be_int(30.3)
+    assert not could_be_int('30.2')
+    assert not could_be_int('nope')
+
+    assert could_be_float(30)
+    assert could_be_float(30.3)
+    assert could_be_float('30.2')
+    assert not could_be_float('nope')
+
+    assert could_be_int('30.2') is False
+    assert could_be_float('30.2') is True
+
+    st = RoutingForest(
+        [
+            CondNode(
+                cond=could_be_int,
+                then=RoutingForest(
+                    [
+                        CondNode(
+                            cond=lambda x: int(x) >= 10,
+                            then=FinalNode('More than a digit'),
+                        ),
+                        CondNode(
+                            cond=lambda x: (int(x) % 2) == 1,
+                            then=FinalNode("That's odd!"),
+                        ),
+                    ]
+                ),
+            ),
+            CondNode(cond=could_be_float, then=FinalNode('could be seen as a float')),
+        ]
+    )
+    assert list(st('nothing I can do with that')) == []
+    assert list(st(8)) == ['could be seen as a float']
+    assert list(st(9)) == ["That's odd!", 'could be seen as a float']
+    assert list(st(10)) == ['More than a digit', 'could be seen as a float']
+    assert list(st(11)) == [
+        'More than a digit',
+        "That's odd!",
+        'could be seen as a float',
+    ]
+
+    rf = RoutingForest(
+        [
+            SwitchCaseNode(
+                switch=lambda x: x % 5,
+                cases={0: FinalNode('zero_mod_5'), 1: FinalNode('one_mod_5')},
+                default=FinalNode('default_mod_5'),
+            ),
+            SwitchCaseNode(
+                switch=lambda x: x % 2,
+                cases={0: FinalNode('even'), 1: FinalNode('odd')},
+                default=FinalNode('that is not an int'),
+            ),
+        ]
+    )
+
+    assert list(rf(5)) == ['zero_mod_5', 'odd']
+    assert list(rf(6)) == ['one_mod_5', 'even']
+    assert list(rf(7)) == ['default_mod_5', 'odd']
+    assert list(rf(8)) == ['default_mod_5', 'even']
+    assert list(rf(10)) == ['zero_mod_5', 'even']
+
+    # testing default mini-language #####################################################
+
+    st2 = RoutingNode.from_object(
+        [  # RoutingForest
+            (  # CondNode
+                could_be_int,
+                [  # RoutingForest
+                    (lambda x: int(x) >= 10, FinalNode('More than a digit')),
+                    (lambda x: (int(x) % 2) == 1, FinalNode("That's odd!")),
+                ],
+            ),
+            (could_be_float, FinalNode('could be seen as a float')),
+        ]
+    )
+
+    assert list(st2('nothing I can do with that')) == []
+    assert list(st2(8)) == ['could be seen as a float']
+    assert list(st2(9)) == ["That's odd!", 'could be seen as a float']
+    assert list(st2(10)) == ['More than a digit', 'could be seen as a float']
+    assert list(st2(11)) == [
+        'More than a digit',
+        "That's odd!",
+        'could be seen as a float',
+    ]
+
+    rf2 = RoutingForest(
+        [
+            (
+                lambda x: x % 5,
+                LiteralVal({0: FinalNode('zero_mod_5'), 1: FinalNode('one_mod_5')}),
+                FinalNode('default_mod_5'),
+            ),
+            (
+                lambda x: x % 2,
+                LiteralVal({0: FinalNode('even'), 1: FinalNode('odd')}),
+                FinalNode('that is not an int'),
+            ),
+        ]
+    )
+
+    assert list(rf2(5)) == ['zero_mod_5', 'odd']
+    assert list(rf2(6)) == ['one_mod_5', 'even']
+    assert list(rf2(7)) == ['default_mod_5', 'odd']
+    assert list(rf2(8)) == ['default_mod_5', 'even']
+    assert list(rf2(10)) == ['zero_mod_5', 'even']
