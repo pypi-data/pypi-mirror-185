dd0bb337f36467b42d5bac93f83091810ed1790b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:50 +0000

    Add package graffe/0.1.0/graffe-0.1.0-py3-none-any.whl

---
+++ packages/graffe/graffe-0.1.0-py3-none-any.whl/graffe/__init__.py
@@ -0,0 +1,7 @@
+"""Graffe
+
+A package to simplify graph creation for scientific, two-variable data.  The
+Graph class is provided to create, show, and save these graphs.
+"""
+
+from .graph import Graph
