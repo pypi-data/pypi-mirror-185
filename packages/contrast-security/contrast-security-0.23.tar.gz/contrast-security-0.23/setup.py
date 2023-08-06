584ce77f955d9d16bceda9f24712e91cef1ecb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:42 +0000

    Add package contrast-security/0.23/contrast-security-0.23.tar.gz

---
+++ packages/contrast-security/contrast-security-0.23.tar.gz/contrast-security-0.23/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name='contrast-security',
+  packages=['contrast_security', 'contrast_security/filters', 'contrast_security/types', 'contrast_security/user_api'],
+  version='0.23',
+  description='Module to easily interact with the Contrast api',
+  author='Contrast Security',
+  author_email='integrations@contrastsecurity.com',
+  url='https://github.com/Contrast-Security-OSS/contrast-sdk-python',
+  download_url='https://github.com/Contrast-Security-OSS/contrast-sdk-python/releases',
+  keywords=['contrast', 'security', 'iast', 'sast', 'rasp'],
+  classifiers=[],
+  install_requires=[
+    'requests',
+    'validators'
+  ],
+)
