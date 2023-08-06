b3c5ebfc9e4bef9f542f1b8ec06eddb198fe72d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:28 +0000

    Add package jwst/1.9.3/jwst-1.9.3-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/jwst/jwst-1.9.3-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl/jwst/regtest/test_nirspec_fs_spec3.py
@@ -0,0 +1,42 @@
+from astropy.io.fits.diff import FITSDiff
+import pytest
+
+from jwst.stpipe import Step
+
+
+@pytest.fixture(scope="module")
+def run_pipeline(jail, rtdata_module):
+    """
+    Run the calwebb_spec3 pipeline on NIRSpec Fixed-Slit exposures.
+    """
+    rtdata = rtdata_module
+
+    # Get the ASN file and input exposures
+    rtdata.get_asn('nirspec/fs/jw93045-o010_20180725t035735_spec3_001_asn.json')
+
+    # Run the calwebb_spec3 pipeline; save results from intermediate steps
+    args = ["calwebb_spec3", rtdata.input,
+            "--steps.outlier_detection.save_results=true",
+            "--steps.resample_spec.save_results=true",
+            "--steps.extract_1d.save_results=true"]
+    Step.from_cmdline(args)
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize("suffix", ["cal", "crf", "s2d", "x1d"])
+def test_nirspec_fs_spec3(run_pipeline, rtdata_module, fitsdiff_default_kwargs, suffix):
+    """Test spec3 pipeline on a set of NIRSpec FS exposures."""
+    rtdata = rtdata_module
+
+    output = f"jw93045-o010_s00003_nirspec_f290lp-g395h-subs400a1_{suffix}.fits"
+    rtdata.output = output
+    rtdata.get_truth(f"truth/test_nirspec_fs_spec3/{output}")
+
+    # Adjust tolerance for machine precision with float32 drizzle code
+    if suffix == "s2d":
+        fitsdiff_default_kwargs["rtol"] = 1e-2
+        fitsdiff_default_kwargs["atol"] = 2e-4
+
+    # Compare the results
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
