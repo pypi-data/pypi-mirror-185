de43b1ab64ebc45559f8a621cd31c9144a2dd0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:31 +0000

    Add package jalla/0.0.3/jalla-0.0.3-py3-none-any.whl

---
+++ packages/jalla/jalla-0.0.3-py3-none-any.whl/jalla/jalla.py
@@ -0,0 +1,14 @@
+def mathias():
+    print("Hei Mathias")
+    
+def brian():
+    print("Hei Brian")
+
+def mango():
+    print("Mangooooooo")
+    
+def mango2():
+    print("Mer mangoooo")
+    
+def mango3():
+    print("Enda mer mangoooo")
