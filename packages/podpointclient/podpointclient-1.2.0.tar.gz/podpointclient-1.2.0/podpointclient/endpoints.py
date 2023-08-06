7719f1c7c8fbca15f5fd3632ff4bd0e153055ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:56 +0000

    Add package podpointclient/1.2.0/podpointclient-1.2.0.tar.gz

---
+++ packages/podpointclient/podpointclient-1.2.0.tar.gz/podpointclient-1.2.0/podpointclient/endpoints.py
@@ -0,0 +1,14 @@
+"""Endpoints from PodPoint"""
+
+AUTH = '/auth'
+SESSIONS = '/sessions'
+USERS = '/users'
+PODS = '/pods'
+UNITS = '/units'
+CHARGE_SCHEDULES = '/charge-schedules'
+CHARGES = '/charges'
+FIRMWARE = '/firmware'
+
+API_BASE = 'api.pod-point.com'
+API_VERSION = 'v4'
+API_BASE_URL = 'https://' + API_BASE + '/' + API_VERSION
