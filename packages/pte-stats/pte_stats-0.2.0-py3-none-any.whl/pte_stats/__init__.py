5f271910b597f57b4245c87f5fcfdda8a887877e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:35 +0000

    Add package pte-stats/0.2.0/pte_stats-0.2.0-py3-none-any.whl

---
+++ packages/pte-stats/pte_stats-0.2.0-py3-none-any.whl/pte_stats/__init__.py
@@ -0,0 +1,29 @@
+"""An open-source software package for statistics with time series. """
+
+__version__ = "0.2.0"
+
+from .cluster import (
+    cluster_analysis_1d,
+    cluster_analysis_2d,
+    cluster_analysis_from_pvals,
+    clusters_from_pvals,
+    get_clusters_1d,
+)
+from .permutation import (
+    permutation_onesample,
+    permutation_twosample,
+    permutation_1d,
+    permutation_1d_onesample,
+    permutation_1d_twosample,
+    permutation_2d,
+    permutation_2d_onesample,
+    permutation_2d_twosample,
+    spearmans_rho_permutation,
+)
+from .timeseries import (
+    timeseries_pvals,
+    correct_pvals,
+    baseline_correct,
+    handle_baseline,
+    handle_baseline_bytimes,
+)
