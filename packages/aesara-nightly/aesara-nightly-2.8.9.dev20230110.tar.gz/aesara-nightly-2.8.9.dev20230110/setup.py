55643136e0f52c9db91413d89cb23bde59c15105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:38 +0000

    Add package aesara-nightly/2.8.9.dev20230110/aesara-nightly-2.8.9.dev20230110.tar.gz

---
+++ packages/aesara-nightly/aesara-nightly-2.8.9.dev20230110.tar.gz/aesara-nightly-2.8.9.dev20230110/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+from setuptools.dist import Distribution
+
+import versioneer
+
+
+dist = Distribution()
+dist.parse_config_files()
+
+# Handle builds of nightly release
+if "BUILD_AESARA_NIGHTLY" in os.environ:
+    from versioneer import get_versions as original_get_versions
+
+    def get_versions():
+        from datetime import datetime, timezone
+
+        suffix = datetime.now(timezone.utc).strftime(r".dev%Y%m%d")
+        versions = original_get_versions()
+        versions["version"] = versions["version"].split("+")[0] + suffix
+        return versions
+
+    versioneer.get_versions = get_versions
+
+
+if __name__ == "__main__":
+    setup(
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+    )
