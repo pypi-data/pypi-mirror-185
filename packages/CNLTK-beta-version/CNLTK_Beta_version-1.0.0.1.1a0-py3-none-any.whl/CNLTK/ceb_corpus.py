fc295293322a5de062e124b5451ae78d305551bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:31 +0000

    Add package CNLTK-beta-version/1.0.0.1.1a0/CNLTK_Beta_version-1.0.0.1.1a0-py3-none-any.whl

---
+++ packages/CNLTK-beta-version/CNLTK_Beta_version-1.0.0.1.1a0-py3-none-any.whl/CNLTK/ceb_corpus.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import configparser
+from config import corpus_csv_filepath
+
+
+# GET CORPUS
+def cebuano_corpus():
+    ceb_corpus = pd.read_csv(corpus_csv_filepath)
+    
+    # file = 'config.ini'
+    # config = configparser.ConfigParser()
+    # config.read(file)
+
+    # corpus_csv_filepath = config['PATHS']['corpus_csv_filepath']
+
+    # corpus_csv_filepath = pd.read_csv(corpus_csv_filepath)
+    
+    return ceb_corpus
+
+cebuano_corpus()
