6e723e2804f4df9bdf915ca51301b0934615a086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:44 +0000

    Add package boxhed/2.45/boxhed-2.45.tar.gz

---
+++ packages/boxhed/boxhed-2.45.tar.gz/boxhed-2.45/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="boxhed",
+    version="2.45",
+    description="BoXHED2.0",
+    author='Arash Pakbin',
+    packages=find_packages(),
+    python_requires=">=3.8",
+    include_package_data=True,
+    install_package_data = True,
+    zip_safe=False,
+    install_requires = ['pandas', 'scikit-learn', 'matplotlib', 'tqdm', 'py3nvml', 'boxhed_kernel', 'boxhed_prep']
+)
