cc24adf5ffbe790030240b41304451e3714c513e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:41 +0000

    Add package phiterm/1.0.0.dev5/phiterm-1.0.0.dev5.tar.gz

---
+++ packages/phiterm/phiterm-1.0.0.dev5.tar.gz/phiterm-1.0.0.dev5/phiterm/workspace/ws_enums.py
@@ -0,0 +1,35 @@
+from phiterm.utils.enums import ExtendedEnum
+
+
+class WorkspaceVisibilityEnum(ExtendedEnum):
+    PRIVATE = "PRIVATE"
+    PUBLIC = "PUBLIC"
+
+
+class WorkspaceStarterTemplate(ExtendedEnum):
+    aws = "aws"
+    aws_snowflake = "aws-snowflake"
+    aws_duckdb = "aws-duckdb"
+    aws_backend = "aws-backend"
+
+
+class WorkspaceEnv(ExtendedEnum):
+    dev = "dev"
+    stg = "stg"
+    prd = "prd"
+    test = "test"
+    all = "all"
+
+
+class WorkspaceConfigType(ExtendedEnum):
+    docker = "docker"
+    k8s = "k8s"
+    gcp = "gcp"
+    aws = "aws"
+
+
+class WorkspaceSetupActions(ExtendedEnum):
+    WS_CONFIG_IS_AVL = "WS_CONFIG_IS_AVL"
+    WS_IS_AUTHENTICATED = "WS_IS_AUTHENTICATED"
+    GCP_SVC_ACCOUNT_IS_AVL = "GCP_SVC_ACCOUNT_IS_AVL"
+    GIT_REMOTE_ORIGIN_IS_AVL = "GIT_REMOTE_ORIGIN_IS_AVL"
