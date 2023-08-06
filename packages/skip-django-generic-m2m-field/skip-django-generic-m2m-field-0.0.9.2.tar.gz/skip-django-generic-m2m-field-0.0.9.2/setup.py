f14ca058c810e9f8ce9cb236faed0cca04bda2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:39 +0000

    Add package skip-django-generic-m2m-field/0.0.9.2/skip-django-generic-m2m-field-0.0.9.2.tar.gz

---
+++ packages/skip-django-generic-m2m-field/skip-django-generic-m2m-field-0.0.9.2.tar.gz/skip-django-generic-m2m-field-0.0.9.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='skip-django-generic-m2m-field',
+    version='0.0.9.2',
+    description="Django generic many to many field library.",
+    keywords='django, fields, generic',
+    author='Lubos Matl',
+    author_email='matllubos@gmail.com',
+    url='https://github.com/skip-pay/django-generic-m2m-field',
+    license='MIT',
+    package_dir={'generic_m2m_field': 'generic_m2m_field'},
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Czech',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    install_requires=[
+        'django>=2.2.9, <4.0',
+        'skip-django-chamber>=0.6.16.2'
+    ],
+    zip_safe=False
+)
