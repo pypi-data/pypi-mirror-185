826f46d783972650651969c94ec065e68871eae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package phc/0.24.0/phc-0.24.0.tar.gz

---
+++ packages/phc/phc-0.24.0.tar.gz/phc-0.24.0/phc/services/__init__.py
@@ -0,0 +1,41 @@
+"""
+Contains services for accessing different parts of the PHC platform.
+"""
+
+from phc.services.accounts import Accounts
+from phc.services.analytics import Analytics
+from phc.services.fhir import Fhir
+from phc.services.projects import Projects
+from phc.services.files import Files
+from phc.services.cohorts import Cohorts
+from phc.services.genomics import Genomics
+from phc.services.tools import Tools
+from phc.services.workflows import Workflows
+from phc.services.genomic_ingestions import GenomicIngestions, IngestionStep
+
+
+__all__ = [
+    "Accounts",
+    "Analytics",
+    "Fhir",
+    "Projects",
+    "Files",
+    "Cohorts",
+    "Genomics",
+    "Tools",
+    "Workflows",
+    "GenomicIngestions",
+]
+
+__pdoc__ = {
+    "accounts": False,
+    "analytics": False,
+    "fhir": False,
+    "projects": False,
+    "files": False,
+    "cohorts": False,
+    "genomics": False,
+    "tools": False,
+    "workflows": False,
+    "genomic_ingestions": False,
+}
