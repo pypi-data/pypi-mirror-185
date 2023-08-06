c26b4ca8eaf496ad96e9e9cdd85b93d938768db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package multibot/1.9.0/multibot-1.9.0.tar.gz

---
+++ packages/multibot/multibot-1.9.0.tar.gz/multibot-1.9.0/multibot/models/registered_callback.py
@@ -0,0 +1,66 @@
+__all__ = ['RegisteredCallback']
+
+from collections.abc import Callable, Iterable
+
+import flanautils
+from flanautils import FlanaBase
+
+from multibot import constants
+
+
+class RegisteredCallback(FlanaBase):
+    callback: Callable
+    keywords: tuple[tuple[str, ...], ...]
+    priority: int | float
+    min_score: float
+    always: bool
+    default: bool
+
+    def __init__(
+        self,
+        callback: Callable,
+        keywords: str | Iterable[str | Iterable[str]] = (),
+        priority: int | float = 1,
+        min_score: float = constants.PARSER_MIN_SCORE_DEFAULT,
+        always=False,
+        default=False
+    ):
+        self.callback = callback
+
+        match keywords:
+            case str(text):
+                text = flanautils.remove_accents(text.strip().lower())
+                self.keywords = (tuple(text.split()),)
+            case [*_, [*_]]:
+                def generator():
+                    for element in keywords:
+                        if isinstance(element, str):
+                            text_ = flanautils.remove_accents(element.strip().lower())
+                            keywords_group = tuple(text_.split())
+                        else:
+                            keywords_group = tuple(flanautils.remove_accents(keyword.strip().lower()) for keyword in element)
+                        yield keywords_group
+
+                self.keywords = tuple(generator())
+            case [*_, str()]:
+                keywords = (flanautils.remove_accents(keyword.strip().lower()).split() for keyword in keywords)
+                self.keywords = (tuple(flanautils.flatten_iterator(keywords)),)
+            case _:
+                self.keywords = tuple(keywords)
+
+        self.priority = priority
+        self.min_score = min_score
+        self.always = always
+        self.default = default
+
+    def __call__(self, *args, **kwargs):
+        return self.callback(*args, **kwargs)
+
+    def __eq__(self, other):
+        if isinstance(other, RegisteredCallback):
+            return self.callback == other.callback
+        else:
+            return self.callback == other
+
+    def __hash__(self):
+        return hash(self.callback)
