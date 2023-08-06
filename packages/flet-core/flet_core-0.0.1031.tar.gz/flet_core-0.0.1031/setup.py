b7baaba9d145aabad97a8c101b0926cf7345931a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:20 +0000

    Add package flet-core/0.0.1031/flet_core-0.0.1031.tar.gz

---
+++ packages/flet-core/flet_core-0.0.1031.tar.gz/flet_core-0.0.1031/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['flet_core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['repath>=0.9.0,<0.10.0']
+
+extras_require = \
+{':python_version < "3.8"': ['typing-extensions>=4.4.0,<5.0.0']}
+
+setup_kwargs = {
+    'name': 'flet-core',
+    'version': '0.0.1031',
+    'description': 'Flet core library',
+    'long_description': '# Flet core library\n\nTBD\n',
+    'author': 'Appveyor Systems Inc.',
+    'author_email': 'hello@flet.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
