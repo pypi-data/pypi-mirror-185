87e1fd315b52fcc193c994ec16c39b79d6774dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:55 +0000

    Add package damv1env/0.0.3/damv1env-0.0.3-py3-none-any.whl

---
+++ packages/damv1env/damv1env-0.0.3-py3-none-any.whl/damv1env/__init__.py
@@ -0,0 +1,2 @@
+from .myenv import sandbox_srv, sandbox_evernote, sandbox_telegram, sandbox_airtable, \
+                   production_srv, production_evernote, production_telegram, production_airtable
