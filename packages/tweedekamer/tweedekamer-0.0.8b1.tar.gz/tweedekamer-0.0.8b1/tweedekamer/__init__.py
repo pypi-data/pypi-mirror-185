1aa48f6c633e3351b92ba59aa8d0cee50c630ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:23 +0000

    Add package tweedekamer/0.0.8b1/tweedekamer-0.0.8b1.tar.gz

---
+++ packages/tweedekamer/tweedekamer-0.0.8b1.tar.gz/tweedekamer-0.0.8b1/tweedekamer/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.8-beta1"
+
+from .debate import Debate
+from .search import Search
+from .enums.debate_type import DebateType
+from .enums.sort_direction import SortDirection
+from .enums.sort_type import SortType
