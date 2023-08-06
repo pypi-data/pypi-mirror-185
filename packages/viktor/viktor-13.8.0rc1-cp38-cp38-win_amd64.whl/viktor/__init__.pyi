d007be8b3bc9215f63a51e77690c25f77dd2df49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:47 +0000

    Add package viktor/13.8.0rc1/viktor-13.8.0rc1-cp38-cp38-win_amd64.whl

---
+++ packages/viktor/viktor-13.8.0rc1-cp38-cp38-win_amd64.whl/viktor/__init__.pyi
@@ -0,0 +1,2 @@
+from .core import Color as Color, File as File, InitialEntity as InitialEntity, ParamsFromFile as ParamsFromFile, UserException as UserException, UserMessage as UserMessage, ViktorController as ViktorController, progress_message as progress_message
+from .errors import UserError as UserError
