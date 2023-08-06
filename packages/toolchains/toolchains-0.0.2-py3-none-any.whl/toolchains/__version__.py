49503b435d59d2ebbbe839c232563d0f12b7d9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:21 +0000

    Add package toolchains/0.0.2/toolchains-0.0.2-py3-none-any.whl

---
+++ packages/toolchains/toolchains-0.0.2-py3-none-any.whl/toolchains/__version__.py
@@ -0,0 +1,9 @@
+####
+__title__ = 'toolchains'
+__description__ = 'toolchains is a collection of libraries for IaC.'
+__url__ = 'https://github.com/jinwoo-j/toolchains'
+__version__ = '0.0.2'
+__author__ = 'Jinwoo Jeong'
+__author_email__ = 'jinwoo@iconloop.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 JINWOO'
