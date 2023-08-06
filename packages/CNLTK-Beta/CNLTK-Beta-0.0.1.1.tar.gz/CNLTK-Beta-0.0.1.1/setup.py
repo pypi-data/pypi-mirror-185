07bc388d15f8be2bf11e5cd4d0064aec1095b5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:10 +0000

    Add package CNLTK-Beta/0.0.1.1/CNLTK-Beta-0.0.1.1.tar.gz

---
+++ packages/CNLTK-Beta/CNLTK-Beta-0.0.1.1.tar.gz/CNLTK-Beta-0.0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    
+]
+
+setup(
+    name='CNLTK-Beta',
+    version='0.0.1.1',
+    description='Beta version of CNLTK library',
+    long_description=open('README.md').read(),
+    url='https://github.com/AG0nzales',
+    author='Joshua Gonzales',
+    author_email='jgonzales_190000001387@uic.edu.ph',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='NLP',
+    packages=find_packages(),
+    install_requires=['']
+)
