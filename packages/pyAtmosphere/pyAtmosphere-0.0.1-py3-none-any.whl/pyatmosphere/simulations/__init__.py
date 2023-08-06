bc9964d30d2eef025e681a8dfdd294950bd3bc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:26 +0000

    Add package pyAtmosphere/0.0.1/pyAtmosphere-0.0.1-py3-none-any.whl

---
+++ packages/pyAtmosphere/pyAtmosphere-0.0.1-py3-none-any.whl/pyatmosphere/simulations/__init__.py
@@ -0,0 +1,9 @@
+from .result import Result
+from .simulation import Simulation
+from .measure import Measure
+
+from .beam import BeamResult, BeamPropagationResult
+from .si import SIResult
+from .pdt import PDTResult, TrackedPDTResult
+from .structure_function import StructureFunctionResult
+from .wind import WindResult, TimeCoherenceResult, TimeBWcorrSimulation
---
+++ packages/pyAtmosphere/pyAtmosphere-0.0.1-py3-none-any.whl/pyatmosphere/theory/phase_screens/sf.py
@@ -0,0 +1,8 @@
+from pyatmosphere.gpu import get_xp
+
+
+def calculate_sf(input):
+    xp = get_xp()
+    a = [xp.sum((input[:, :-r] - input[:, r:])**2, axis=1) /
+        (input.shape[1] - r) for r in range(1, input.shape[1])]
+    return xp.array(a)
