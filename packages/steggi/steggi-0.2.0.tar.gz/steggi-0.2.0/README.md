4f99fb9276145cbfa2f2668bb3edb287b50f7b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:36 +0000

    Add package steggi/0.2.0/steggi-0.2.0.tar.gz

---
+++ packages/steggi/steggi-0.2.0.tar.gz/steggi-0.2.0/README.md
@@ -0,0 +1,3 @@
+# steggi
+
+Stegsolve like GUI program in python with opencv and tkinter.
---
+++ packages/steggi/steggi-0.2.0.tar.gz/steggi-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['steggi', 'steggi.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opencv-python>=4.6.0.66,<5.0.0.0',
+ 'pillow>=9.3.0,<10.0.0',
+ 'tk>=0.1.0,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['steggi = main:main']}
+
+setup_kwargs = {
+    'name': 'steggi',
+    'version': '0.2.0',
+    'description': 'Stegsolve like GUI program for steganographic purposes.',
+    'long_description': '# steggi\n\nStegsolve like GUI program in python with opencv and tkinter.\n',
+    'author': 'Aquib',
+    'author_email': 'aquibjavedt007@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tamton-aquib/steggi',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
