0c56f5a3f1a537b6820c2ede11a3c811f7f27ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:00 +0000

    Add package seebuoy/0.2.2/seebuoy-0.2.2-py3-none-any.whl

---
+++ packages/seebuoy/seebuoy-0.2.2-py3-none-any.whl/seebuoy/ndbc/utils.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = "https://www.ndbc.noaa.gov/data"
+
+
+def get_url(url):
+
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return resp.text
+    elif resp.status_code == 404:
+        print(f"Dataset not available (404 Error) for url: \n {url}")
+        return None
+    else:
+        raise ValueError(f"Error code {resp.status_code} for url: \n {url}")
