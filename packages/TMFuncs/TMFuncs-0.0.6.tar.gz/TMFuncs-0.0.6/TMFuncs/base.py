93ea311be5e3aef19ff355fbb08376d4a73b7d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:54 +0000

    Add package TMFuncs/0.0.6/TMFuncs-0.0.6.tar.gz

---
+++ packages/TMFuncs/TMFuncs-0.0.6.tar.gz/TMFuncs-0.0.6/TMFuncs/base.py
@@ -0,0 +1,33 @@
+# 常用小模块
+import datetime
+import time
+from pytz import timezone
+
+
+def time_taken(end_time, start_time):
+    time_spend = end_time - start_time
+    m, s = divmod(time_spend, 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    return d, h, m, s
+
+
+def get_date(days=1, str_format='%Y-%m-%d', time_zone='Asia/Shanghai'):
+    """
+    基于今天的日期，返回指定间隔的日期字符串，
+    :param days: 距离“今天”的日期，0表示今天，1表示昨天，依次类推
+    :param str_format: 格式化后的字符串
+    :return: 字符串格式的日期，例如 2022-12-20
+    """
+    # 获取当天时间——服务器时间
+    dt_loc_today = datetime.datetime.now()
+    # 获取对应时区的时间——转换为目标时区时间
+    utc8 = timezone('Asia/Shanghai')
+    dt_utc8 = dt_loc_today.astimezone(utc8)
+    # 获取日期间隔的时间——基于目标时区时间
+    the_day = dt_utc8 - datetime.timedelta(days=int(days))
+    return the_day.strftime(str_format)
+
+
+def get_current_timestamp(str_format='%Y%m%d%H%M%S'):
+    return time.strftime(str_format, time.localtime(time.time()))
