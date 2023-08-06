8e2509b45ba6298f618ce4409ed26a54687686e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:10 +0000

    Add package opteryx/0.8.3/opteryx-0.8.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.8.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/managers/planner/temporal.py
@@ -0,0 +1,377 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This compensates for missing temporal table support in the SQL parser (sqlparser-rs).
+This is relatively complex for what it appears to be doing - it needs to account for
+a number of situations whilst being able to reconstruct the SQL query as the parser
+would expect it.
+
+For information on temporal tables see:
+https://blog.devgenius.io/a-query-in-time-introduction-to-sql-server-temporal-tables-145ddb1355d9
+
+This supports the following syntaxes
+
+- FOR TODAY
+- FOR YESTERDAY
+- FOR <timestamp>
+- FOR DATES BETWEEN <timestamp> AND <timestamp>
+- FOR DATES IN <range>
+
+"""
+import datetime
+import re
+
+from opteryx.exceptions import InvalidTemporalRangeFilterError
+from opteryx.exceptions import SqlError
+from opteryx.utils import dates
+
+COLLECT_RELATION = [
+    r"FROM",
+    r"INNER\sJOIN",
+    r"CROSS\sJOIN",
+    r"LEFT\sJOIN",
+    r"LEFT\sOUTER\sJOIN",
+    r"RIGHT\sJOIN",
+    r"RIGHT\sOUTER\sJOIN",
+    r"FULL\sJOIN",
+    r"FULL\sOUTER\sJOIN",
+    r"JOIN",
+    r"CREATE\sTABLE",
+    r"ANALYZE\sTABLE",
+]
+
+COLLECT_TEMPORAL = [r"FOR"]
+
+STOP_COLLECTING = [
+    r"GROUP\sBY",
+    r"HAVING",
+    r"LIKE",
+    r"LIMIT",
+    r"OFFSET",
+    r"ON",
+    r"ORDER\sBY",
+    r"SHOW",
+    r"SELECT",
+    r"WHERE",
+    r"WITH",
+    r"USING",
+    r";",
+]
+
+COLLECT_ALIAS = [r"AS"]
+
+BOUNDARIES = [r"(", r")"]
+
+SQL_PARTS = (
+    COLLECT_RELATION
+    + COLLECT_TEMPORAL
+    + STOP_COLLECTING
+    + COLLECT_ALIAS
+    + [r"DATES\sIN\s\w+", r"DATES\sBETWEEN\s[^\r\n\t\f\v]AND\s[^\r\n\t\f\v]"]
+)
+
+COMBINE_WHITESPACE_REGEX = re.compile(r"\r\n\t\f\v+")
+
+# states for the collection algorithm
+WAITING: int = 1
+RELATION: int = 4
+TEMPORAL: int = 16
+ALIAS: int = 64
+
+
+def clean_statement(string):  # pragma: no cover
+    """
+    Remove carriage returns and all whitespace to single spaces.
+
+    Avoid removing whitespace in quoted strings.
+    """
+    pattern = r"(\"[^\"]\"|\'[^\']\'|\`[^\`]\`)|(\r\n\t\f\v+)"
+    regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+    def _replacer(match):
+        if match.group(2) is not None:
+            return " "
+        return match.group(1)  # captured quoted-string
+
+    return regex.sub(_replacer, string).strip()
+
+
+def sql_parts(string):  # pragma: no cover
+    """
+    Split a SQL statement into clauses
+    """
+    keywords = re.compile(
+        r"(\,|\(|\)|\;|"
+        + r"|".join([r"\b" + i.replace(r" ", r"\s") + r"\b" for i in SQL_PARTS])
+        + r")",
+        re.IGNORECASE,
+    )
+    quoted_strings = re.compile(
+        r'(?:[^"\s]*"(?:\\.|[^"])*")+|(?:[^\'\s]*\'(?:\\.|[^\'])*\')+|[^\s]+'
+    )
+
+    # This probably can be done with regexes, but after trying I've implemented
+    # procedurally. We're capturing strings in quotes and preserving them, strings
+    # not in quotes we're then splitting by keywords. This means that no matter
+    # what is in quotes, it will not be split, even SQL statements and FOR clauses.
+    parts = []
+    accumulator = ""
+    for part in quoted_strings.findall(string):
+        part = part.strip()
+        if part != "":
+            if part[0] in ("\"'`"):
+                parts.extend(keywords.split(accumulator))
+                parts.append(part)
+                accumulator = ""
+            else:
+                accumulator += f" {part}"
+    if accumulator != "":
+        parts.extend(keywords.split(accumulator))
+
+    parts = [i.strip() for i in parts if i.strip() != ""]
+
+    return parts
+
+
+def remove_comments(string):  # pragma: no cover
+    """
+    Remove comments from the string
+    """
+    # first group captures quoted strings (double, single or back tick)
+    # second group captures comments (//single-line or /* multi-line */)
+    pattern = r"(\"[^\"]\"|\'[^\']\'|\`[^\`]\`)|(/\*[^\*/]*\*/|--[^\r\n]*$)"
+    regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return ""  # so we will return empty to remove the comment
+        # otherwise, we will return the 1st group
+        return match.group(1)  # captured quoted-string
+
+    return regex.sub(_replacer, string)
+
+
+def _subtract_one_month(in_date):  # pragma: no cover
+    day = in_date.day
+    end_of_previous_month = in_date.replace(day=1) - datetime.timedelta(days=1)
+    while True:
+        try:
+            return end_of_previous_month.replace(day=day)
+        except:
+            day -= 1
+            if day < 0:
+                raise ValueError("Unable to determine previous month")
+
+
+def parse_range(fixed_range):  # pragma: no cover
+    fixed_range = fixed_range.upper()
+    today = datetime.datetime.utcnow().date()
+
+    if fixed_range in ("PREVIOUS_MONTH", "LAST_MONTH"):
+        # end the day before the first of this month
+        end = today.replace(day=1) - datetime.timedelta(days=1)
+        # start the first day of that month
+        start = end.replace(day=1)
+    elif fixed_range == "THIS_MONTH":
+        # start the first day of this month
+        start = today.replace(day=1)
+        # end today
+        end = today
+    elif fixed_range in ("PREVIOUS_CYCLE", "LAST_CYCLE"):
+        # if we're before the 21st
+        if today.day < 22:
+            # end the 21st of last month
+            end = _subtract_one_month(today).replace(day=21)
+            # start the 22nd of the month before
+            start = _subtract_one_month(end).replace(day=22)
+        else:
+            # end the 21st of this month
+            end = today.replace(day=21)
+            # start the 22nd of the month before
+            start = _subtract_one_month(end).replace(day=22)
+    elif fixed_range == "THIS_CYCLE":
+        # if we're before the 21st
+        if today.day < 22:
+            # end today
+            end = today
+            # start the 22nd of last month
+            start = _subtract_one_month(today).replace(day=22)
+        else:
+            # end the today
+            end = today
+            # start the 22nd of this month
+            start = today.replace(day=22)
+
+    else:
+        if parse_date(fixed_range):
+            raise InvalidTemporalRangeFilterError(
+                f"`THIS_MONTH`, `LAST_MONTH` expected, got `{fixed_range}`"
+            )
+        raise InvalidTemporalRangeFilterError(f"Unknown temporal range `{fixed_range}`")
+
+    return start, end
+
+
+def parse_date(date):  # pragma: no cover
+
+    if not date:
+        return None
+
+    today = datetime.datetime.utcnow().date()
+
+    # the splitter keeps ';' at the end of date literals
+    if date[-1] == ";":
+        date = date[:-1].strip()
+
+    if date == "TODAY":
+        return today
+    if date == "YESTERDAY":
+        return today - datetime.timedelta(days=1)
+
+    parsed_date = dates.parse_iso(date[1:-1])
+    if parsed_date:
+        return parsed_date.date()
+
+
+def _temporal_extration_state_machine(parts):
+    """
+    we use a three state machine to extract the temporal information from the query
+    and maintain the relation to filter information.
+
+    We separate out the two key parts of the algorithm, first we determin the state,
+    then we work out if the state transition means we should do something.
+
+    We're essentially using a bit mask to record state and transitions.
+    """
+
+    state = WAITING
+    relation = ""
+    temporal = ""
+    query_collector = []
+    temporal_range_collector = []
+    for part in parts:
+
+        # record the current state
+        transition = [state]
+        comparable_part = part.upper().replace(" ", r"\s")
+
+        # work out what our current state is
+        if comparable_part in BOUNDARIES:
+            state = WAITING
+        if comparable_part in STOP_COLLECTING:
+            state = WAITING
+        if comparable_part in COLLECT_RELATION:
+            state = RELATION
+        if comparable_part in COLLECT_TEMPORAL:
+            state = TEMPORAL
+        if comparable_part in COLLECT_ALIAS:
+            state = ALIAS
+        transition.append(state)
+
+        # based on what the state was and what it is now, do something
+        if transition == [TEMPORAL, TEMPORAL]:
+            temporal = (temporal + " " + part).strip()
+        elif (
+            transition
+            in (
+                [WAITING, WAITING],
+                [TEMPORAL, RELATION],
+                [RELATION, RELATION],
+                [RELATION, WAITING],
+                [ALIAS, RELATION],
+                [ALIAS, WAITING],  # probably
+            )
+            and relation
+        ):
+            temporal_range_collector.append((relation, temporal))
+            relation = ""
+            temporal = ""
+        elif transition == [RELATION, RELATION]:
+            relation = part
+        elif transition == [WAITING, TEMPORAL]:
+            raise SqlError(
+                "Temporal `FOR` statements must directly follow the dataset they apply to."
+            )
+
+        if state != TEMPORAL:
+            query_collector.append(part)
+
+    # if we're at the end of we have a relation, emit it
+    if relation:
+        temporal_range_collector.append((relation, temporal))
+
+    return temporal_range_collector, " ".join(query_collector)
+
+
+def extract_temporal_filters(sql):  # pragma: no cover
+
+    # prep the statement, by normalizing it
+    clean_sql = remove_comments(sql)
+    clean_sql = clean_statement(clean_sql)
+    parts = sql_parts(clean_sql)
+
+    # define today once
+    today = datetime.datetime.utcnow().date()
+
+    # extract the raw temporal information
+    initial_collector, sql = _temporal_extration_state_machine(parts)
+
+    final_collector = []
+
+    for relation, for_date_string in initial_collector:
+
+        start_date = today
+        end_date = today
+
+        for_date_string = for_date_string.upper()
+        for_date = parse_date(for_date_string)
+
+        if for_date:
+            start_date = for_date
+            end_date = for_date
+
+        elif for_date_string.startswith("DATES BETWEEN "):
+            parts = for_date_string.split(" ")
+            start_date = parse_date(parts[2])
+            end_date = parse_date(parts[4])
+
+            if start_date is None or end_date is None:
+                raise InvalidTemporalRangeFilterError(
+                    "Unrecognized temporal range values."
+                )
+            if start_date > end_date:
+                raise InvalidTemporalRangeFilterError(
+                    "Invalid temporal range, start of range is after end of range."
+                )
+
+        elif for_date_string.startswith("DATES IN "):
+            parts = for_date_string.split(" ")
+            start_date, end_date = parse_range(parts[2])
+
+        elif for_date_string:
+            raise InvalidTemporalRangeFilterError(
+                f"Unable to interpret temporal filter `{for_date_string}`"
+            )
+
+        final_collector.append(
+            (
+                relation,
+                start_date,
+                end_date,
+            )
+        )
+
+    # we've rewritten the sql so make it sqlparser-rs compatible
+    return sql, final_collector
---
+++ packages/opteryx/opteryx-0.8.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/models/execution_tree.py
@@ -0,0 +1,313 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The Execution Tree is the graph which defines a Query Plan.
+
+The execution tree contains functionality to:
+
+- build and define the plan
+- execute the plan
+- manipulate the plan
+
+"""
+
+import pyarrow
+
+
+from opteryx.exceptions import DatabaseError
+
+
+class ExecutionTree:
+    """
+    The execution tree is defined separately to the planner to simplify the
+    complex code which is the planner from the tree that describes the plan.
+    """
+
+    def __init__(self):
+        """create empty plan"""
+        self._nodes: dict = {}
+        self._edges: list = []
+
+    def get_nodes_of_type(self, operator):
+        """
+        Traverse the plan looking for nodes of a given type or set of types.
+
+        Parameters:
+            operator: BaseOperator or Iterable of BaseOperator
+                The operator type(s) to match.
+        Yields:
+            string
+                nids of matching nodes.
+        """
+
+        def _inner(operator):
+            if not isinstance(operator, (list, set)):
+                operator = tuple([operator])
+            if not isinstance(operator, tuple):
+                operator = tuple(operator)
+            for nid, item in list(self._nodes.items()):
+                if isinstance(item, operator):
+                    yield nid
+
+        return list(_inner(operator))
+
+    def nodes(self):
+        """return all of the nodes in the plan"""
+        all_nodes = []
+        for node in self._nodes.values():
+            # [#275] - because the subqueries are attached as query plans, we need to
+            # walk them differently to just walking the tree
+            if hasattr(node, "_dataset") and isinstance(node._dataset, ExecutionTree):
+                all_nodes.extend(node._dataset.nodes())  # pragma: no cover
+            all_nodes.append(node)
+        return all_nodes
+
+    def insert_operator_before(self, nid, operator, before_nid):
+        """rewrite the plan putting the new node before a given node"""
+        # add the new node to the plan
+        self.add_operator(nid, operator)
+        # change all the edges that were going into the old nid to the new one
+        self._edges = [
+            (source, target if target != before_nid else nid, direction)
+            for source, target, direction in self._edges
+        ]
+        # add an edge from the new nid to the old one
+        self.link_operators(nid, before_nid)
+
+    def insert_operator_after(self, nid, operator, after_nid):
+        """rewrite the plan putting the new node after a given node"""
+        # add the new node to the plan
+        self.add_operator(nid, operator)
+        # change all the edges that were coming from the old nid to the new one
+        self._edges = [
+            (source if source != after_nid else nid, target, direction)
+            for source, target, direction in self._edges
+        ]
+        # add an edge from the new nid to the old one
+        self.link_operators(after_nid, nid)
+
+    def remove_operator(self, nid):
+        """rewrite a plan, removing a node"""
+
+        # remove the node
+        self._nodes.pop(nid, None)
+
+        # link the nodes each side of the node being removed
+        out_going = self.get_outgoing_links(nid)
+        in_coming = self.get_incoming_links(nid)
+        for out_nid in out_going:
+            for in_nid in in_coming:
+                self.link_operators(in_nid[0], out_nid, in_nid[1])
+
+        self._edges = [
+            (source, target, direction)
+            for source, target, direction in self._edges
+            if nid not in (source, target)
+        ]
+
+    def add_operator(self, nid, operator):
+        """
+        Add a step to the plan
+
+        Parameters:
+            id: string
+                The id of the step, must be unique
+            Operator: BaseOperator
+                The Operator
+        """
+        self._nodes[nid] = operator
+
+    def link_operators(self, source_operator, target_operator, direction=None):
+        """
+        Link steps in a plan.
+
+        Parameters:
+            source_operator: string
+                The id of the source step
+            target_operator: string
+                The id of the target step
+            direction: string (optional)
+                The name of the connection, for joins this will be Left/Right
+        """
+        edge = (
+            source_operator,
+            target_operator,
+            direction,
+        )
+        if edge not in self._edges:
+            self._edges.append(edge)
+
+    def get_outgoing_links(self, nid):
+        """
+        Get the ids of outgoing nodes from a given step.
+
+        Paramters:
+            name: string
+                The name of the step to search from
+        Returns:
+            Set of nids
+        """
+        retval = {target for source, target, direction in self._edges if source == nid}
+        return sorted(retval)
+
+    def get_incoming_links(self, nid):
+        """
+        Get the ids of incoming nodes for a given step.
+
+        Paramters:
+            nid: string
+                The name of the step to search from
+        Retutns:
+            Set of Tuples, (source nid, connection direction)
+        """
+        retval = {
+            (source, direction)
+            for source, target, direction in self._edges
+            if target == nid
+        }
+        return sorted(retval)
+
+    def get_exit_points(self):
+        """
+        Get steps in the plan with no outgoing steps.
+        """
+        if len(self._nodes) == 1:  # pragma: no cover
+            return list(self._nodes.keys())
+        sources = {source for source, target, direction in self._edges}
+        retval = (
+            target for source, target, direction in self._edges if target not in sources
+        )
+        return sorted(retval)
+
+    def get_entry_points(self):
+        """
+        Get steps in the plan with no incoming steps.
+        """
+        if len(self._nodes) == 1:
+            return list(self._nodes.keys())
+        targets = {target for source, target, direction in self._edges}
+        retval = (
+            source for source, target, direction in self._edges if source not in targets
+        )
+        return sorted(retval)
+
+    def get_operator(self, nid):
+        """
+        Get the Operator class by nid.
+
+        Parameters:
+            nid: string
+                The id of the step
+        """
+        return self._nodes.get(nid)
+
+    def is_acyclic(self):
+        """
+        Test if the plan is acyclic - it should always be
+        """
+        # cycle over the graph removing a layer of exits each cycle
+        # if we have nodes but no exists, we're cyclic
+        my_edges = self._edges.copy()
+
+        while len(my_edges) > 0:
+            # find all of the exits
+            sources = {source for source, target, direction in my_edges}
+            exits = {
+                target
+                for source, target, direction in my_edges
+                if target not in sources
+            }
+
+            if len(exits) == 0:
+                return False
+
+            # remove the exits
+            new_edges = [
+                (source, target, direction)
+                for source, target, direction in my_edges
+                if target not in exits
+            ]
+            my_edges = new_edges
+        return True
+
+    def execute(self):
+        """
+        This implements a 'pull' model execution engine. It finds the last stage in
+        the plan and pulls records from it - this stage then pulls records from earlier
+        stages in the plan as needed, and so on, until we get to a node that creates
+        records to feed into the engine (usually reading a data file).
+        """
+
+        def map_operators(nodes):
+            """
+            We're walking the query plan telling each node where to get the data it
+            needs from.
+            """
+            for node in nodes:
+                producers = self.get_incoming_links(node)
+                operator = self.get_operator(node)
+                if producers:
+                    operator.set_producers([self.get_operator(i[0]) for i in producers])
+                    map_operators(i[0] for i in producers)
+
+        # do some basic validation before we try to execute
+        if not self.is_acyclic():  # pragma: no cover
+            raise DatabaseError("Problem executing the query plan - it is cyclic.")
+
+        # we get the tail of the query - the first steps
+        head = list(dict.fromkeys(self.get_exit_points()))
+        if len(head) != 1:  # pragma: no cover
+            raise DatabaseError(
+                f"Problem executing the query plan - it has {len(head)} heads."
+            )
+
+        map_operators(head)
+
+        operator = self.get_operator(head[0])
+        yield from operator.execute()
+
+    def explain(self):
+
+        from opteryx import operators
+        from opteryx.models import Columns
+
+        def _inner_explain(node, depth):
+            if depth == 1:
+                operator = self.get_operator(node)
+                yield {
+                    "operator": operator.name,
+                    "config": operator.config,
+                    "depth": depth - 1,
+                }
+            incoming_operators = self.get_incoming_links(node)
+            for operator_name in incoming_operators:
+                operator = self.get_operator(operator_name[0])
+                if isinstance(operator, operators.BasePlanNode):
+                    yield {
+                        "operator": operator.name,
+                        "config": operator.config,
+                        "depth": depth,
+                    }
+                yield from _inner_explain(operator_name[0], depth + 1)
+
+        head = list(dict.fromkeys(self.get_exit_points()))
+        # print(head, _edges)
+        if len(head) != 1:  # pragma: no cover
+            raise DatabaseError(f"Problem with the plan - it has {len(head)} heads.")
+        plan = list(_inner_explain(head[0], 1))
+
+        table = pyarrow.Table.from_pylist(plan)
+        table = Columns.create_table_metadata(
+            table, table.num_rows, "plan", None, "calculated", "explain"
+        )
+        yield table
