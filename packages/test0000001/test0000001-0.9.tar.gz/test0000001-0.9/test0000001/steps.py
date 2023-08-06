e0cefb897302a1288c99500549cce7f34b2892da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:18 +0000

    Add package test0000001/0.9/test0000001-0.9.tar.gz

---
+++ packages/test0000001/test0000001-0.9.tar.gz/test0000001-0.9/test0000001/steps.py
@@ -0,0 +1,63 @@
+import pyrebase
+import os
+from dotenv import load_dotenv
+import time
+import logging
+import logger
+
+load_dotenv()
+
+config = {
+  "apiKey": os.environ.get("API_KEY"),
+  "authDomain": os.environ.get("AUTH_DOMAIN"),
+  "databaseURL": "https://test0000001-e47d5-default-rtdb.firebaseio.com",
+  "projectId": os.environ.get("PROJECT_ID"),
+  "storageBucket": os.environ.get("STORAGE_BUCKET"),
+  "messagingSenderId": os.environ.get("MESSAGING_SENDER_ID"),
+  "appId": os.environ.get("APP_ID"),
+  "measurementId": os.environ.get("MEASUREMENT_ID")
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
+def run_steps():
+    start_time = time.time()
+    step = 1
+    total_time = 0
+    while step <= 10:
+        start_step_time = time.time()
+        message = f"Current step: {step}"
+        logging.info(message)
+        db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+        entered_step = input("Please enter the number of the step: ")
+        end_step_time = time.time()
+        step_time = end_step_time - start_step_time
+        total_time += step_time
+        if entered_step != str(step):
+            if entered_step == 'exit':
+                message = f"User decided to exit the script"
+                logging.info(message)
+                db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+            else:
+                message = f"Incorrect step entered. Exiting... Step time taken: {step_time} seconds"
+                logging.info(message)
+                db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+                message = f"User stopped at step {step} because of 'incorrect step entered'"
+                logging.info(message)
+                db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+            break
+        message = f"Proceeding to step {step+1}... Step time taken: {step_time} seconds"
+        logging.info(message)
+        db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+        step += 1
+    else:
+        end_time = time.time()
+        total_time = end_time - start_time
+        message = "All steps completed!"
+        logging.info(message)
+        db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
+        message = f"Total time taken: {total_time} seconds"
+        logging.info(message)
+        db.child("logs").push({"message": message, "level": "info", "timestamp": time.time()})
