f337efa5ad75805cf5ce9eb888bdd86c9e470c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:45 +0000

    Add package trajpy/1.4.1/trajpy-1.4.1.tar.gz

---
+++ packages/trajpy/trajpy-1.4.1.tar.gz/trajpy-1.4.1/setup.py
@@ -0,0 +1,19 @@
+"""
+this setup will check for dependencies and install TrajPy on your computer
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='trajpy',
+    version='1.4.1',
+    url='https://github.com/ocbe-uio/trajpy.git',
+    author='Mauricio Moreira and Eduardo Mossmann',
+    author_email='trajpy@protonmail.com',
+    description='Trajectory classifier for cells, nanoparticles & whatelse.',
+    keywords=['trajectory quantification', 'feature engineering', 'diffusion classification'],
+    license='GNU GPLv3',
+    platform='Python 3.7',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.14.3',
+                      'scipy >= 1.7.1'],
+)
