c8ba420913428fb63f21ee65acd7f954c10f4dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:35 +0000

    Add package swaps-gateway/0.1.1/swaps-gateway-0.1.1.tar.gz

---
+++ packages/swaps-gateway/swaps-gateway-0.1.1.tar.gz/swaps-gateway-0.1.1/README.md
@@ -0,0 +1,7 @@
+# swaps-gateway-py
+A python wrapper for easier interactions with swap-based DEXs.
+
+## Based on
+* Web3
+* uniswap-python
+
---
+++ packages/swaps-gateway/swaps-gateway-0.1.1.tar.gz/swaps-gateway-0.1.1/src/swaps_gateway/__init__.py
@@ -0,0 +1,9 @@
+from .uniswap_ecosystem import GeneralSwapV2, BinanceSmartChainSwap, BinanceSmartChainTestnetSwap,\
+    EthGoerliTestnetSwap, GeneralSwapV3
+__all__ = [
+    "GeneralSwapV2",
+    "BinanceSmartChainSwap",
+    "BinanceSmartChainTestnetSwap",
+    "EthGoerliTestnetSwap",
+    "GeneralSwapV3",
+]
