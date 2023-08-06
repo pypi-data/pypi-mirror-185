29bb82a877961c4fee85a6bfab84d9e3c2c368cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package fastapi-pagination/0.11.2/fastapi_pagination-0.11.2-py3-none-any.whl

---
+++ packages/fastapi-pagination/fastapi_pagination-0.11.2-py3-none-any.whl/fastapi_pagination/ext/beanie.py
@@ -0,0 +1,31 @@
+from typing import Optional, TypeVar, Union
+
+from beanie import Document
+from beanie.odm.queries.find import FindMany
+
+from ..api import create_page
+from ..bases import AbstractPage, AbstractParams
+from ..types import AdditionalData
+from ..utils import verify_params
+
+TDocument = TypeVar("TDocument", bound=Document)
+
+
+async def paginate(
+    query: Union[TDocument, FindMany[TDocument]],
+    params: Optional[AbstractParams] = None,
+    *,
+    additional_data: AdditionalData = None,
+    fetch_links: bool = False,
+) -> AbstractPage[TDocument]:
+    params, raw_params = verify_params(params, "limit-offset")
+
+    items = await query.find_many(limit=raw_params.limit, skip=raw_params.offset, fetch_links=fetch_links).to_list()
+    total = await query.find({}, fetch_links=fetch_links).count()
+
+    return create_page(items, total, params, **(additional_data or {}))
+
+
+__all__ = [
+    "paginate",
+]
