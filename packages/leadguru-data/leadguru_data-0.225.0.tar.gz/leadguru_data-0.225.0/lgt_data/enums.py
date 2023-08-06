2a331621e2dc001eabc6b7305f149bc0793b8c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:20 +0000

    Add package leadguru-data/0.225.0/leadguru_data-0.225.0.tar.gz

---
+++ packages/leadguru-data/leadguru_data-0.225.0.tar.gz/leadguru_data-0.225.0/lgt_data/enums.py
@@ -0,0 +1,23 @@
+from enum import Enum, IntFlag
+
+
+class DedicatedBotState(IntFlag):
+    Operational = 0
+    Stopped = 1
+    ScheduledForDeletion = 2
+
+
+class UserAccountState(IntFlag):
+    Operational = 0
+    Suspended = 1
+
+
+class UserRole(str, Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+    MODERATOR = 'moderator'
+
+class BotType(str, Enum):
+    DEDICATED_BOT = 'dedicated',
+    USER_BOT = 'user',
+    LEADGURU_BOT = 'leadguru'
