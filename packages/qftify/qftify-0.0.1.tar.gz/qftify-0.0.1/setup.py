f9bc2bb6cc6aab6af880a2aab7aabadd48e05e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:32 +0000

    Add package qftify/0.0.1/qftify-0.0.1.tar.gz

---
+++ packages/qftify/qftify-0.0.1.tar.gz/qftify-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name="qftify",
+  version="0.0.1",
+  description="",
+  long_description_content_type="text/markdown",
+  author="",
+  author_email="",
+  license="MIT",
+  packages=["qftify"],
+  package_dir = {
+    'qftify': 'src/qftify',
+  },
+  zip_safe=False
+)
