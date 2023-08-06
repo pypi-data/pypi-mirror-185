057558aebb3550d0b76f622f8a5636fccbf973c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:13 +0000

    Add package victorforever/1.0.1/victorforever-1.0.1-py3-none-any.whl

---
+++ packages/victorforever/victorforever-1.0.1-py3-none-any.whl/victorforever/__init__.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+
+def main():
+    try:
+        webbrowser.open("https://youtu.be/xvFZjo5PgG0")
+    except:
+        pass
+    print("\n")
+    print("##############################################")
+    print("#                VICTOR FOREVER              #")
+    print("##############################################")
+    print("\n")
+
+
+if __name__ == "__main__":
+    main()
