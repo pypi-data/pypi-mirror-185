3a625ed579e70903a5cc2a453d4d78473a3b1ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:32 +0000

    Add package potyk-doc/0.7.5/potyk-doc-0.7.5.tar.gz

---
+++ packages/potyk-doc/potyk-doc-0.7.5.tar.gz/potyk-doc-0.7.5/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['potyk_doc', 'potyk_doc.translation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyPDF2>=3.0.0,<4.0.0',
+ 'docxtpl>=0.16.4,<0.17.0',
+ 'pdfkit>=1.0.0,<2.0.0',
+ 'potyk-lib>=0.5,<0.6']
+
+setup_kwargs = {
+    'name': 'potyk-doc',
+    'version': '0.7.5',
+    'description': '',
+    'long_description': None,
+    'author': 'potykion',
+    'author_email': 'potykion@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
