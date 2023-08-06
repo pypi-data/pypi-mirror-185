8a50fedd351772cb7969b6c10c5b604be02c8c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package comtradeapicall/1.0.2/comtradeapicall-1.0.2.tar.gz

---
+++ packages/comtradeapicall/comtradeapicall-1.0.2.tar.gz/comtradeapicall-1.0.2/src/comtradeapicall/__init__.py
@@ -0,0 +1,10 @@
+# PreviewGet module
+
+from .PreviewGet import previewFinalData
+from .PreviewGet import previewTarifflineData
+from .PreviewGet import getFinalData
+from .PreviewGet import getTarifflineData
+
+# BulkDownload module
+from .BulkDownload import bulkDownloadFinalFile
+from .BulkDownload import bulkDownloadTarifflineFile
