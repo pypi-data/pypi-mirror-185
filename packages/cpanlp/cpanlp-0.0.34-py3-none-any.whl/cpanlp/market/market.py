0b3b75b95e1417968af837f28b5e7ee09181c8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:18 +0000

    Add package cpanlp/0.0.34/cpanlp-0.0.34-py3-none-any.whl

---
+++ packages/cpanlp/cpanlp-0.0.34-py3-none-any.whl/cpanlp/market/market.py
@@ -0,0 +1,29 @@
+from cpanlp.account.income.income import *
+from cpanlp.entity.entity import *
+class Market:
+    def __init__(self,commodity,participants):
+        self.commodity = commodity
+        self.participants = participants
+class PerfectlyCompetitiveMarket(Market):
+    def __init__(self, commodity, participants):
+        super().__init__(commodity, participants)
+        self.equilibrium_price = None
+        self.equilibrium_quantity = None
+
+    def calculate_equilibrium(self):
+        # Code to calculate equilibrium price and quantity
+        pass
+class MonopolyMarket(Market):
+    def __init__(self, commodity, businessentity):
+        super().__init__(commodity, [])
+        self.businessentity=businessentity
+        self.profit_maximizing_price = None
+        self.profit_maximizing_quantity = None
+        self.market_demand = None
+        self.total_cost = None
+if __name__ == '__main__':
+    commodity=Commodity("苹果",5,30,10)
+    llc=BusinessEntity("科技公司","小企业",3000)
+    llc1=BusinessEntity("科技公司2","小企业2",3000)
+    a= Market(commodity,[llc,llc1])
+    print(a.commodity.demand)
