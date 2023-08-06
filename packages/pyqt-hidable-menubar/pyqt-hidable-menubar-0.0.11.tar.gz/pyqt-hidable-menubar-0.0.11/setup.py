94aaa76451cb370c15b501a1d24e317f47977c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:13 +0000

    Add package pyqt-hidable-menubar/0.0.11/pyqt-hidable-menubar-0.0.11.tar.gz

---
+++ packages/pyqt-hidable-menubar/pyqt-hidable-menubar-0.0.11.tar.gz/pyqt-hidable-menubar-0.0.11/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-hidable-menubar',
+    version='0.0.11',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyqt_hidable_menubar.ico': ['close.svg']},
+    description='PyQt Hidable Menubar (show/hide menu bar with close button at the right corner of it)',
+    url='https://github.com/yjg30737/pyqt-hidable-menubar.git',
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-svg-button>=0.0.1'
+    ]
+)
