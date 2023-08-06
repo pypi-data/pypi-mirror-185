07c0a47f86286ef1db11fb731d82f7fefee070a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:24 +0000

    Add package speeq/0.0.0/speeq-0.0.0.tar.gz

---
+++ packages/speeq/speeq-0.0.0.tar.gz/speeq-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='speeq',
+   version='0.0.0',
+   description='A useful module',
+   author='Mahmoud Salhab',
+   author_email='mahmoud@salhab.work',
+   packages=['speeq'],  #same as name
+   install_requires=[]
+)
