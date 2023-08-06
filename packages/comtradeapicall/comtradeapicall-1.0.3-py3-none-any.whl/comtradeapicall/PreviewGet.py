6158d8efbf16802e26ffb7cf6a45e8cb7b107114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package comtradeapicall/1.0.3/comtradeapicall-1.0.3-py3-none-any.whl

---
+++ packages/comtradeapicall/comtradeapicall-1.0.3-py3-none-any.whl/comtradeapicall/PreviewGet.py
@@ -0,0 +1,109 @@
+import json
+import pandas
+from pandas import json_normalize
+import requests
+
+def getPreviewData(subscription_key, tradeDataType, typeCode, freqCode, clCode, period, reporterCode, cmdCode, flowCode,
+                              partnerCode,
+                              partner2Code, customsCode, motCode, maxRecords, format_output, aggregateBy,
+                              breakdownMode,
+                              countOnly, includeDesc):
+    if subscription_key is not None:
+        if tradeDataType == 'TARIFFLINE':
+            baseURL = 'https://comtradeapi.un.org/data/v1/getTariffline/' + typeCode + '/' + freqCode + '/' + clCode
+        else:
+            baseURL = 'https://comtradeapi.un.org/data/v1/get/' + typeCode + '/' + freqCode + '/' + clCode
+    else:
+        if tradeDataType == 'TARIFFLINE':
+            baseURL = 'https://comtradeapi.un.org/public/v1/previewTariffline/' + typeCode + '/' + freqCode + '/' + clCode
+        else:
+            baseURL = 'https://comtradeapi.un.org/public/v1/preview/' + typeCode + '/' + freqCode + '/' + clCode
+
+    PARAMS = dict(reportercode=reporterCode, flowCode=flowCode,
+                  period=period, cmdCode=cmdCode, partnerCode=partnerCode, partner2Code=partner2Code,
+                  motCode=motCode, customsCode=customsCode,
+                  maxRecords=maxRecords, format=format_output, aggregateBy=aggregateBy, breakdownMode=breakdownMode,
+                  countOnly=countOnly, includeDesc=includeDesc)
+    # add key
+    PARAMS["subscription-key"] = subscription_key
+    # print(PARAMS)
+    # only for JSON format
+    if format_output != 'JSON':
+        print("Only JSON output is supported with this function")
+    else:
+        try:
+            resp = requests.get(baseURL, params=PARAMS, timeout=120)
+            # print(resp.text)
+            # print(resp.url)
+            if resp.status_code != 200:
+                # This means something went wrong.
+                jsonResult = resp.json()
+                print('Error in calling API:', resp.url)
+                print('Error code:', jsonResult['statusCode'])
+                print('Error message:', jsonResult['message'])
+            else:
+                jsonResult = resp.json()
+                if countOnly:
+                    dictCount = dict(count=jsonResult['count'])
+                    df = pandas.DataFrame([dictCount])
+                else:
+                    df = json_normalize(jsonResult['data'])  # Results contain the required data
+                    # print(df.head())
+                    # print(df.describe())
+                return df
+        except requests.exceptions.Timeout:
+            # Maybe set up for a retry, or continue in a retry loop
+            print('Request failed due to timeout')
+        except requests.exceptions.TooManyRedirects:
+            # Tell the user their URL was bad and try a different one
+            print('Request failed due to too many redirects')
+        except requests.exceptions.RequestException as e:
+            # catastrophic error. bail.
+            raise SystemExit(e)
+
+def previewFinalData(typeCode, freqCode, clCode, period, reporterCode, cmdCode, flowCode,
+                              partnerCode,
+                              partner2Code, customsCode, motCode, maxRecords, format_output, aggregateBy,
+                              breakdownMode,
+                              countOnly, includeDesc):
+    return getPreviewData(None, 'FINAL', typeCode, freqCode, clCode, period, reporterCode,
+                                  cmdCode, flowCode,
+                                  partnerCode,
+                                  partner2Code, customsCode, motCode, maxRecords, format_output, aggregateBy,
+                                  breakdownMode,
+                                  countOnly, includeDesc)
+
+def previewTarifflineData(typeCode, freqCode, clCode, period, reporterCode, cmdCode, flowCode,
+                              partnerCode,
+                              partner2Code, customsCode, motCode, maxRecords, format_output,
+                              countOnly, includeDesc):
+    return getPreviewData(None, 'TARIFFLINE', typeCode, freqCode, clCode, period, reporterCode,
+                                  cmdCode, flowCode,
+                                  partnerCode,
+                                  partner2Code, customsCode, motCode, maxRecords, format_output, None,
+                                  None,
+                                  countOnly, includeDesc)
+
+def getFinalData(subscription_key, typeCode, freqCode, clCode, period, reporterCode, cmdCode, flowCode,
+                              partnerCode,
+                              partner2Code, customsCode, motCode, maxRecords, format_output, aggregateBy,
+                              breakdownMode,
+                              countOnly, includeDesc):
+    return getPreviewData(subscription_key, 'FINAL', typeCode, freqCode, clCode, period, reporterCode,
+                                  cmdCode, flowCode,
+                                  partnerCode,
+                                  partner2Code, customsCode, motCode, maxRecords, format_output, aggregateBy,
+                                  breakdownMode,
+                                  countOnly, includeDesc)
+
+def getTarifflineData(subscription_key, typeCode, freqCode, clCode, period, reporterCode, cmdCode, flowCode,
+                              partnerCode,
+                              partner2Code, customsCode, motCode, maxRecords, format_output,
+                              countOnly, includeDesc):
+    return getPreviewData(subscription_key, 'TARIFFLINE', typeCode, freqCode, clCode, period, reporterCode,
+                                  cmdCode, flowCode,
+                                  partnerCode,
+                                  partner2Code, customsCode, motCode, maxRecords, format_output, None,
+                                  None,
+                                  countOnly, includeDesc)
+
---
+++ packages/comtradeapicall/comtradeapicall-1.0.3-py3-none-any.whl/comtradeapicall/__init__.py
@@ -0,0 +1,14 @@
+# PreviewGet module
+
+from .PreviewGet import previewFinalData
+from .PreviewGet import previewTarifflineData
+from .PreviewGet import getFinalData
+from .PreviewGet import getTarifflineData
+
+# BulkDownload module
+from .BulkDownload import bulkDownloadFinalFile
+from .BulkDownload import bulkDownloadTarifflineFile
+
+# DataAvailability module
+from .DataAvailability import getFinalDataAvailability
+from .DataAvailability import getTarifflineDataAvailability
