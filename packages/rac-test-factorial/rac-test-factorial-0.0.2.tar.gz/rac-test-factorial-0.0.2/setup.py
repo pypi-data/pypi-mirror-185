d15717191c2dee73333fe4c9918d873206ce31ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:00 +0000

    Add package rac-test-factorial/0.0.2/rac-test-factorial-0.0.2.tar.gz

---
+++ packages/rac-test-factorial/rac-test-factorial-0.0.2.tar.gz/rac-test-factorial-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name = 'rac-test-factorial',
+    version = '0.0.2',
+    description = "Factorial module.",
+    long_description = "A test module for our book.",
+    platforms = ["Linux"],
+    packages=find_packages()
+    )
