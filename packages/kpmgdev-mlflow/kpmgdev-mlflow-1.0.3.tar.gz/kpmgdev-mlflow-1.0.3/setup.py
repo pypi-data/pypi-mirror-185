51bbaef170b2840afacdb831783f8307c44c7b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:20 +0000

    Add package kpmgdev-mlflow/1.0.3/kpmgdev-mlflow-1.0.3.tar.gz

---
+++ packages/kpmgdev-mlflow/kpmgdev-mlflow-1.0.3.tar.gz/kpmgdev-mlflow-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='kpmgdev-mlflow',
+      version='1.0.3',
+      description='kpmgdev-mlflow util package',
+      url='https://github.com/yourusername/mypackage',
+      author='KPMG Israel',
+      author_email='akadosh@kpmg.com',
+      license='MIT',
+      packages=['kpmgdev-mlflow'],
+      install_requires=['azure-core', 'azure-mlflow', 'openpyxl', 'scikit-learn',
+                        'torch==1.13.1', 'pandas', 'numpy'],
+      zip_safe=False)
