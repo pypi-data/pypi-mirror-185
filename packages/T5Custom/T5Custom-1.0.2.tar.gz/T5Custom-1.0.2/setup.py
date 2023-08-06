3536f804b207e9624781a1c11156da4a1d4be954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:16 +0000

    Add package T5Custom/1.0.2/T5Custom-1.0.2.tar.gz

---
+++ packages/T5Custom/T5Custom-1.0.2.tar.gz/T5Custom-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2'
+DESCRIPTION = 'Custom T5 model creation with own csv training dataset'
+LONG_DESCRIPTION = 'Custom T5 model creation with own csv training dataset'
+
+# Setting up
+setup(
+    name="T5Custom",
+    version=VERSION,
+    author="Sushil Ranjan",
+    author_email="nikuraj006@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["pandas","sklearn","scikit-learn","torch","numpy","transformers","flask","pytorch_lightning","flask-cors","num2words"],
+    keywords=['python', 'T5', 'T5-Base', 'T5-Large', 'T5-Small'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
