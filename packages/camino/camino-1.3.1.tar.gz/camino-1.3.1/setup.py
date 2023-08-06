0c965b0375d0db27fcbbb0ecdb00dc841959b6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:13 +0000

    Add package camino/1.3.1/camino-1.3.1.tar.gz

---
+++ packages/camino/camino-1.3.1.tar.gz/camino-1.3.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="camino",
+    version="1.3.1",
+    author="Nathan Wachholz",
+    author_email="camino@nathanwachholz.com",
+    description="A library for controlling an Arduino from Python over Serial",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/n-wach/camino",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pyserial',
+    ],
+)
