2565193d29538741b8fcce2abb0469db6cb2e8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:35 +0000

    Add package peek-core-device/3.1.10/peek-core-device-3.1.10.tar.gz

---
+++ packages/peek-core-device/peek-core-device-3.1.10.tar.gz/peek-core-device-3.1.10/peek_core_device/_private/server/TupleDataObservable.py
@@ -0,0 +1,73 @@
+from vortex.handler.TupleDataObservableHandler import TupleDataObservableHandler
+
+from peek_core_device._private.PluginNames import deviceFilt
+from peek_core_device._private.PluginNames import deviceObservableName
+from peek_core_device._private.server.tuple_providers.ClientSettingsTupleProvider import (
+    ClientSettingsTupleProvider,
+)
+from peek_core_device._private.server.tuple_providers.DeviceGpsLocationTupleProvider import (
+    DeviceGpsLocationTupleProvider,
+)
+from peek_core_device._private.server.tuple_providers.DeviceInfoTableTupleProvider import (
+    DeviceInfoTableTupleProvider,
+)
+from peek_core_device._private.server.tuple_providers.DeviceInfoTupleProvider import (
+    DeviceInfoTupleProvider,
+)
+from peek_core_device._private.server.tuple_providers.DeviceUpdateTupleProvider import (
+    DeviceUpdateTupleProvider,
+)
+from peek_core_device._private.storage.DeviceInfoTable import DeviceInfoTable
+from peek_core_device._private.storage.DeviceUpdateTuple import (
+    DeviceUpdateTuple,
+)
+from peek_core_device._private.tuples.ClientSettingsTuple import (
+    ClientSettingsTuple,
+)
+from peek_core_device.tuples.DeviceGpsLocationTuple import (
+    DeviceGpsLocationTuple,
+)
+from peek_core_device.tuples.DeviceInfoTuple import DeviceInfoTuple
+
+
+def makeTupleDataObservableHandler(ormSessionCreator):
+    """ " Make Tuple Data Observable Handler
+
+    This method creates the observable object, registers the tuple providers and then
+    returns it.
+
+    :param ormSessionCreator: A function that returns a SQLAlchemy session when called
+
+    :return: An instance of :code:`TupleDataObservableHandler`
+
+    """
+    tupleObservable = TupleDataObservableHandler(
+        observableName=deviceObservableName, additionalFilt=deviceFilt
+    )
+
+    # Register TupleProviders here
+    tupleObservable.addTupleProvider(
+        DeviceUpdateTuple.tupleName(),
+        DeviceUpdateTupleProvider(ormSessionCreator),
+    )
+
+    tupleObservable.addTupleProvider(
+        DeviceInfoTuple.tupleName(), DeviceInfoTupleProvider(ormSessionCreator)
+    )
+
+    tupleObservable.addTupleProvider(
+        DeviceInfoTable.tupleName(),
+        DeviceInfoTableTupleProvider(ormSessionCreator),
+    )
+
+    tupleObservable.addTupleProvider(
+        ClientSettingsTuple.tupleName(),
+        ClientSettingsTupleProvider(ormSessionCreator),
+    )
+
+    tupleObservable.addTupleProvider(
+        DeviceGpsLocationTuple.tupleName(),
+        DeviceGpsLocationTupleProvider(ormSessionCreator),
+    )
+
+    return tupleObservable
