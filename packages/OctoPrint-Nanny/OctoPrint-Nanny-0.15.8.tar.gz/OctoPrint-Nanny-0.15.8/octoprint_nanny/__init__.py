5664ad18dc1a14f070602f8e2bf16bd0f3798d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:32 +0000

    Add package OctoPrint-Nanny/0.15.8/OctoPrint-Nanny-0.15.8.tar.gz

---
+++ packages/OctoPrint-Nanny/OctoPrint-Nanny-0.15.8.tar.gz/OctoPrint-Nanny-0.15.8/octoprint_nanny/__init__.py
@@ -0,0 +1,23 @@
+# type: ignore
+from __future__ import absolute_import
+
+__plugin_name__ = "OctoPrint-Nanny"
+
+__plugin_pythoncompat__ = ">=3,<4"  # only python 3
+
+__plugin_version__ = "0.15.8"
+
+
+def __plugin_load__():
+    from .plugins import OctoPrintNannyPlugin
+
+    global __plugin_implementation__
+    __plugin_implementation__ = OctoPrintNannyPlugin()
+    __plugin_implementation__.version = __plugin_version__
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        # "octoprint.timelapse.capture.post": __plugin_implementation__.on_timelapse_capture,
+        "octoprint.events.register_custom_events": __plugin_implementation__.register_custom_events,
+        # "octoprint.server.http.routes": __plugin_implementation__.register_custom_routes
+    }
