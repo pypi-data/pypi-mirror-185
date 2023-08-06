c74cfda9585811e49c0fe36284af366d43887d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:32 +0000

    Add package rscencryption/0.1.0/rscencryption-0.1.0.tar.gz

---
+++ packages/rscencryption/rscencryption-0.1.0.tar.gz/rscencryption-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rscencryption',
+    version='0.1.0',
+    description='Library for the RCSE',
+    author='Ava Info Conseils',
+    license='AVA IC',
+)
