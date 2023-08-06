929a4f1f8c1e3c5d27554735802b087947dd3e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:04 +0000

    Add package uptick-datawarehouse/0.1.3/uptick_datawarehouse-0.1.3.tar.gz

---
+++ packages/uptick-datawarehouse/uptick_datawarehouse-0.1.3.tar.gz/uptick_datawarehouse-0.1.3/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['app',
+ 'uptick_datawarehouse',
+ 'uptick_datawarehouse.subscriptionmanager',
+ 'uptick_datawarehouse.subscriptionmanager.migrations',
+ 'uptick_datawarehouse.superblocks',
+ 'uptick_datawarehouse.superblocks.migrations',
+ 'uptick_datawarehouse.vitally',
+ 'uptick_datawarehouse.vitally.migrations',
+ 'uptick_datawarehouse.workforce',
+ 'uptick_datawarehouse.workforce.migrations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dj-database-url>=1.2.0,<2.0.0',
+ 'django-extensions>=3.2.1,<4.0.0',
+ 'django<empty>',
+ 'dsnparse>=0.1.15,<0.2.0',
+ 'psycopg2>=2.9.5,<3.0.0']
+
+setup_kwargs = {
+    'name': 'uptick-datawarehouse',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': 'Datawarehousing models.\n',
+    'author': 'william chu',
+    'author_email': 'william.chu@uptickhq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
