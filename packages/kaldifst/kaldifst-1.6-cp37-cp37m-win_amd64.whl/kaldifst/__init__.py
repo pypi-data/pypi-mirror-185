c5c8369c59f2507543397c760b748ca240ea7fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:53 +0000

    Add package kaldifst/1.6/kaldifst-1.6-cp37-cp37m-win_amd64.whl

---
+++ packages/kaldifst/kaldifst-1.6-cp37-cp37m-win_amd64.whl/kaldifst/__init__.py
@@ -0,0 +1,32 @@
+from _kaldifst import (
+    FloatWeight,
+    StdArc,
+    StdConstFst,
+    StdVectorFst,
+    SymbolTable,
+    TropicalWeight,
+    arcsort,
+    compile,
+    compose,
+    connect,
+    determinize,
+    determinize_star,
+    divide,
+    draw,
+    info,
+    invert,
+    minimize,
+    minimize_encoded,
+    plus,
+    reverse,
+    rmepsilon,
+    times,
+)
+
+from .iterator import ArcIterator, StateIterator
+from .table_types import (
+    RandomAccessVectorFstReader,
+    SequentialVectorFstReader,
+    VectorFstWriter,
+)
+__version__ = '1.6'
