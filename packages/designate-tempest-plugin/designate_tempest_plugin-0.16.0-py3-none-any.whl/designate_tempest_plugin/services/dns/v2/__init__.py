038bda669fde5e32d67f9e7464a055a90df4e0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:58 +0000

    Add package designate-tempest-plugin/0.16.0/designate_tempest_plugin-0.16.0-py3-none-any.whl

---
+++ packages/designate-tempest-plugin/designate_tempest_plugin-0.16.0-py3-none-any.whl/designate_tempest_plugin/services/dns/v2/__init__.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2017 Andrea Frittoli
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from designate_tempest_plugin.services.dns.v2.json.blacklists_client import \
+    BlacklistsClient
+from designate_tempest_plugin.services.dns.v2.json.designate_limit_client \
+    import DesignateLimitClient
+from designate_tempest_plugin.services.dns.v2.json.pool_client import \
+    PoolClient
+from designate_tempest_plugin.services.dns.v2.json.ptr_client \
+    import PtrClient
+from designate_tempest_plugin.services.dns.v2.json.quotas_client import \
+    QuotasClient
+from designate_tempest_plugin.services.dns.v2.json.recordset_client import \
+    RecordsetClient
+from designate_tempest_plugin.services.dns.v2.json.service_client import \
+    ServiceClient
+from designate_tempest_plugin.services.dns.v2.json.tld_client import TldClient
+from designate_tempest_plugin.services.dns.v2.json.transfer_accepts_client \
+    import TransferAcceptClient
+from designate_tempest_plugin.services.dns.v2.json.transfer_request_client \
+    import TransferRequestClient
+from designate_tempest_plugin.services.dns.v2.json.tsigkey_client import \
+    TsigkeyClient
+from designate_tempest_plugin.services.dns.v2.json.zones_client import \
+    ZonesClient
+from designate_tempest_plugin.services.dns.v2.json.zone_exports_client import \
+    ZoneExportsClient
+from designate_tempest_plugin.services.dns.v2.json.zone_imports_client import \
+    ZoneImportsClient
+from designate_tempest_plugin.services.dns.v2.json.api_version_client import \
+    ApiVersionClient
+
+__all__ = ['BlacklistsClient', 'DesignateLimitClient', 'PoolClient',
+           'PtrClient', 'QuotasClient', 'RecordsetClient', 'ServiceClient',
+           'TldClient', 'TransferAcceptClient', 'TransferRequestClient',
+           'TsigkeyClient', 'ZonesClient', 'ZoneExportsClient',
+           'ZoneImportsClient', 'ApiVersionClient']
