850180e3245878e18503c378539909bdf85cb46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:07 +0000

    Add package facts-gene/0.0.0/facts_gene-0.0.0.tar.gz

---
+++ packages/facts-gene/facts_gene-0.0.0.tar.gz/facts_gene-0.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="facts_gene",
+    version="0.0.0",
+    author="ALIASGAR - ALI",
+    author_email="aholo2000@gmail.com",
+    description="facts_gene for Networking Geeks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alias1978/facts_gene",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['pandas', 'xlrd', 'openpyxl', 'nettoolkit']
+)
+
