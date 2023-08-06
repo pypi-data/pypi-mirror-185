f77993ca4cb8920fd7e87ee2bfcbd2e7f1731031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:24 +0000

    Add package LibRecommender/0.12.2/LibRecommender-0.12.2-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/LibRecommender/LibRecommender-0.12.2-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/libreco/algorithms/__init__.py
@@ -0,0 +1,53 @@
+from .user_cf import UserCF
+from .item_cf import ItemCF
+from .svd import SVD
+from .svdpp import SVDpp
+from .als import ALS
+from .bpr import BPR
+from .ncf import NCF
+from .youtube_retrieval import YouTubeRetrieval
+from .youtube_ranking import YouTubeRanking
+from .fm import FM
+from .wide_deep import WideDeep
+from .deepfm import DeepFM
+from .autoint import AutoInt
+from .din import DIN
+from .rnn4rec import RNN4Rec
+from .caser import Caser
+from .wave_net import WaveNet
+from .item2vec import Item2Vec
+from .deepwalk import DeepWalk
+from .ngcf import NGCF
+from .lightgcn import LightGCN
+from .pinsage import PinSage
+from .pinsage_dgl import PinSageDGL
+from .graphsage import GraphSage
+from .graphsage_dgl import GraphSageDGL
+
+__all__ = [
+    "UserCF",
+    "ItemCF",
+    "SVD",
+    "SVDpp",
+    "ALS",
+    "BPR",
+    "NCF",
+    "YouTubeRetrieval",
+    "YouTubeRanking",
+    "FM",
+    "WideDeep",
+    "DeepFM",
+    "AutoInt",
+    "DIN",
+    "RNN4Rec",
+    "Caser",
+    "WaveNet",
+    "Item2Vec",
+    "DeepWalk",
+    "NGCF",
+    "LightGCN",
+    "PinSage",
+    "PinSageDGL",
+    "GraphSage",
+    "GraphSageDGL",
+]
