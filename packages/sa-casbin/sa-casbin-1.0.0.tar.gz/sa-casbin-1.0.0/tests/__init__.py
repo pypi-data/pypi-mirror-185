c795bfce53ad1b0c56b4c7c1a1afaa9ec8941349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:34 +0000

    Add package sa-casbin/1.0.0/sa-casbin-1.0.0.tar.gz

---
+++ packages/sa-casbin/sa-casbin-1.0.0.tar.gz/sa-casbin-1.0.0/tests/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 The casbin Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .test_distributed_api import TestDistributedApi
+from .test_enforcer import *
+from .test_filter import TestFilteredAdapter
+from .test_frontend import TestFrontend
+from .test_management_api import TestManagementApi, TestManagementApiSynced
+from .test_rbac_api import TestRbacApi, TestRbacApiSynced
+from . import benchmarks
+from . import config
+from . import model
+from . import rbac
+from . import util
