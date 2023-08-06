455ef760f6a17b328fb22597cc0ee4d32fc4bd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:44 +0000

    Add package django-Tinymce4-lsu/2.1/django_Tinymce4_lsu-2.1-py3-none-any.whl

---
+++ packages/django-Tinymce4-lsu/django_Tinymce4_lsu-2.1-py3-none-any.whl/tinymce/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from .views import spell_check, filebrowser, css, spell_check_callback
+
+urlpatterns = [
+    path('spellchecker/', spell_check, name='tinymce-spellchecker'),
+    path('filebrowser/', filebrowser, name='tinymce-filebrowser'),
+    re_path(r'^tinymce4.css', css, name='tinymce-css'),
+    re_path(r'^spellcheck-callback.js', spell_check_callback, name='tinymce-spellcheck-callback')
+]
