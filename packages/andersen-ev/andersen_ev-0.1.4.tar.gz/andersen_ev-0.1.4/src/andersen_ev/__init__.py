c26fc9b76eb4203c8c78c54813663e088061fb30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:33 +0000

    Add package andersen-ev/0.1.4/andersen_ev-0.1.4.tar.gz

---
+++ packages/andersen-ev/andersen_ev-0.1.4.tar.gz/andersen_ev-0.1.4/src/andersen_ev/__init__.py
@@ -0,0 +1,15 @@
+"""
+andersen.
+Python library for controlling the Andersen A2 EV car charger.
+"""
+
+# For relative imports to work in Python 3.6
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+__version__ = "0.1.4"
+__author__  = 'James Brown'
+__credits__ = 'Catch22'
+
+from .andersen import AndersenA2
+from .auth     import AndersenAuth
+from .mobile   import AndersenMobile
