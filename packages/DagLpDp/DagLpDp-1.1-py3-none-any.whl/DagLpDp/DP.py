8faf784e8c90c0da4f0f0cfe5a1ca800f68aae13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package DagLpDp/1.1/DagLpDp-1.1-py3-none-any.whl

---
+++ packages/DagLpDp/DagLpDp-1.1-py3-none-any.whl/DagLpDp/DP.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+class DAGLP:
+    def __init__(self,graph,sec=60):
+        self.graph = graph #{(from_node,to_node):value,...}
+        self.starts = set()
+        self.ends = set()
+        self.from_nodes={} # from_node as key
+        self.to_nodes={} # to_node as key
+        self.nodePV={} # node:{path,value} 
+        self.sec = sec
+        for edge,value in self.graph.items():
+            self.starts.add(edge[0])
+            self.ends.add(edge[1])
+            parent = self.to_nodes.get(edge[1],None)
+            if parent is None:
+                parent = set()
+            parent.add(edge[0])
+            self.to_nodes[edge[1]] = parent            
+        for edge,value in self.graph.items():
+            self.starts.discard(edge[1])
+            self.ends.discard(edge[0])
+            child = self.from_nodes.get(edge[0],None)
+            if child is None:
+                child = set()
+            child.add(edge[1])
+            self.from_nodes[edge[0]] = child
+            
+        for i in self.starts:
+            self.nodePV[i]=([i],0)
+            
+        self.done = self.calcLP() 
+        
+        if self.done:
+            self.max_value = 0
+            for k,v in self.nodePV.items():
+                if v[1] > self.max_value:
+                    self.max_path = v[0]
+                    self.max_value = v[1] 
+        
+    def calcLP(self):
+        import datetime
+        start_time = datetime.datetime.now()
+        copystarts = self.starts.copy()
+        while len(copystarts)>0:
+            end = datetime.datetime.now()
+            if (end - start_time).seconds > self.sec:
+                return False
+            start = copystarts.pop()
+            spv = self.nodePV[start]
+            if start in self.ends:
+                continue
+            for child in self.from_nodes[start]:
+                self.to_nodes[child].discard(start)
+                if len(self.to_nodes[child])==0:
+                    copystarts.add(child)
+                cpv = self.nodePV.get(child,None) 
+                ev = self.graph[(start,child)] # edge value
+                if (cpv is not None) and (cpv[1] > ev + spv[1]):
+                    continue
+                self.nodePV[child]=(spv[0]+[child],ev + spv[1])       
+        return True
---
+++ packages/DagLpDp/DagLpDp-1.1-py3-none-any.whl/DagLpDp/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*- 
+
+from .DP import DAGLP
+
+__version__ = '0.1.1'
+
+__all__ = (
+    DAGLP
+)
