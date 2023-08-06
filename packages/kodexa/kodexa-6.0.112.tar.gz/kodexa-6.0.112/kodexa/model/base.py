4ab6c19744c743891f0d4682291fe940e1f481d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:16 +0000

    Add package kodexa/6.0.112/kodexa-6.0.112.tar.gz

---
+++ packages/kodexa/kodexa-6.0.112.tar.gz/kodexa-6.0.112/kodexa/model/base.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, Extra
+
+
+def to_camel(string: str) -> str:
+    return ''.join(word.capitalize() for word in string.split('_'))
+
+
+class KodexaBaseModel(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+        use_enum_values = True
+        json_encoders = {
+            # custom output conversion for datetime (yyyy-MM-dd'T'HH:mm:ss.SSS'Z')
+            datetime: lambda v: v.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
+        }
+
+    def to_dict(self):
+        return self.dict(by_alias=True, exclude_none=True)
+
+
+class BaseEntity(KodexaBaseModel):
+    id: Optional[str] = Field(None, description='The ID of the object')
