eb09818e6d698de8a5ab88152552ce73adae0893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:09 +0000

    Add package hspylib-cfman/0.9.76/hspylib-cfman-0.9.76.tar.gz

---
+++ packages/hspylib-cfman/hspylib-cfman-0.9.76.tar.gz/hspylib-cfman-0.9.76/cfman/core/cf.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib-CFMan
+   @package: cfman.core
+      @file: cf.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+from hspylib.core.metaclass.singleton import Singleton
+from hspylib.modules.cli.terminal import Terminal
+from typing import List, Optional
+
+import os
+
+
+class CloudFoundry(metaclass=Singleton):
+    """Cloud Foundry command line tool python wrapper"""
+
+    def __init__(self) -> None:
+        self.connected = False
+        self.targeted = {"org": None, "space": None, "targeted": False}
+        self.last_result = None
+        self.last_exit_code = None
+
+    def is_targeted(self) -> bool:
+        return self.targeted["org"] and self.targeted["space"] and self.targeted["targeted"]
+
+    # Before getting started:
+    def connect(self) -> bool:
+        """Attempt to connect to CloudFoundry"""
+        if not self.connected:
+            result = self._exec("orgs")
+            self.connected = result and "FAILED" not in str(result)
+        return self.connected
+
+    def api(self, api: str | None = None) -> Optional[str]:
+        """Set or view target api url"""
+        result = self._exec(f"api {api or ''}")
+        return result if "FAILED" not in str(result) else None
+
+    def auth(self, username: str, password: str) -> bool:
+        """Authorize a CloudFoundry user"""
+        result = self._exec(f"auth {username} {password}")
+        return result and "FAILED" not in str(result)
+
+    def target(self, **kwargs) -> dict:
+        """Set or view the targeted org or space"""
+        params = ["target"]
+        if not kwargs and (parts := self._exec(" ".join(params)).split(os.linesep)):
+            if len(parts) >= 4:
+                org = parts[2].split(":")[1].strip()
+                space = parts[3].split(":")[1].strip()
+                self.targeted = {"org": org, "space": space, "targeted": True}
+        else:
+            if "org" in kwargs and kwargs["org"]:
+                params.append("-o")
+                params.append(kwargs["org"])
+                self.targeted["org"] = kwargs["org"]
+            if "space" in kwargs and kwargs["space"]:
+                params.append("-s")
+                params.append(kwargs["space"])
+                self.targeted["space"] = kwargs["space"]
+            result = self._exec(" ".join(params))
+            self.targeted["targeted"] = result and "FAILED" not in str(result)
+
+        return self.targeted
+
+    # Space management
+    def spaces(self) -> List[str]:
+        """List all spaces from organization"""
+        all_spaces = self._exec("spaces").split("\n")
+        return all_spaces[3:] if all_spaces and "FAILED" not in str(all_spaces) else None
+
+    # Org management
+    def orgs(self) -> List[str]:
+        """List all organizations"""
+        all_orgs = self._exec("orgs").split("\n")
+        return all_orgs[3:] if all_orgs and "FAILED" not in str(all_orgs) else None
+
+    # Application lifecycle:
+    def apps(self) -> List[str]:
+        """List all applications from targeted space"""
+        all_apps = self._exec("apps").split("\n")
+        return all_apps[4:] if all_apps and "FAILED" not in str(all_apps) else None
+
+    def start(self, **kwargs) -> str:
+        """Start an app"""
+        return self._exec(f"start {kwargs['app']}")
+
+    def stop(self, **kwargs) -> str:
+        """Stop an app"""
+        return self._exec(f"stop {kwargs['app']}")
+
+    def restart(self, **kwargs) -> str:
+        """Stop all instances of the app, then start them again. This causes downtime."""
+        return self._exec(f"restart {kwargs['app']}")
+
+    def restage(self, **kwargs) -> str:
+        """Recreate the app's executable artifact using the latest pushed app files and the latest environment
+        (variables, service bindings, buildpack, stack, etc.). This action will cause app downtime."""
+        return self._exec(f"restage {kwargs['app']}")
+
+    def logs(self, **kwargs) -> None:
+        """Tail or show recent logs for an app"""
+        return self._exec(f"logs {kwargs['app']} {'--recent' if 'recent' in kwargs else ''}")
+
+    def _exec(self, cmd_line: str, pool: bool = False) -> Optional[str]:
+        """TODO"""
+        if pool:
+            Terminal.shell_poll(f"cf {cmd_line}")
+            return None
+        self.last_result, self.last_exit_code = Terminal.shell_exec(f"cf {cmd_line}")
+        return self.last_result
---
+++ packages/hspylib-cfman/hspylib-cfman-0.9.76.tar.gz/hspylib-cfman-0.9.76/cfman/core/cf_application.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib-CFMan
+   @package: cfman.core
+      @file: cf_application.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+
+from hspylib.core.exception.exceptions import InvalidArgumentError
+from hspylib.core.tools.commons import sysout
+from typing import List
+
+import re
+
+
+class CFApplication:
+    max_name_length = 70
+
+    @classmethod
+    def of(cls, app_line: str):
+        """TODO"""
+        parts = re.split(r" {2,}", app_line)
+        # format: name | state | instances | memory | disk | urls
+        if len(parts) == 6:
+            return CFApplication(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5].split(", "))
+        # format: name | state | type:i/o | urls
+        if len(parts) == 4:
+            mat = re.search(r"(\w+):(\d+/\d+)", parts[2])
+            if not mat:
+                raise InvalidArgumentError(f"Invalid application line: {app_line}")
+            instances = mat.group(2)
+            memory = "-"
+            disk = "-"
+            return CFApplication(parts[0], parts[1], instances, memory, disk, parts[3].split(", "))
+
+        raise InvalidArgumentError(f"Invalid application line: {app_line}")
+
+    def __init__(self, name: str, state: str, instances: str, memory: str, disk: str, urls: List[str]) -> None:
+        self.name = name
+        self.state = state
+        self.instances = instances
+        self.memory = memory
+        self.disk = disk
+        self.urls = urls
+        self.max_name_length = max(self.max_name_length, len(self.name))
+
+    def __str__(self) -> str:
+        return f"[{self.colored_state}] {self.name}"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    # pylint: disable=consider-using-f-string
+    def print_status(self) -> None:
+        """TODO"""
+        sysout(
+            "%CYAN%{}  {:5}  %NC%{:10}  {:4}  {:4}  {}".format(
+                self.name.ljust(self.max_name_length),
+                self.colored_state,
+                self.instances,
+                self.memory,
+                self.disk,
+                self.urls,
+            )
+        )
+
+    @property
+    def colored_state(self) -> str:
+        """TODO"""
+        state = self.state.upper()
+        return f"{'%GREEN%' if state == 'STARTED' else '%RED%':5}{state}%NC%"
