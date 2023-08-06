92c7cc4a73231736dda383f0ab848230852bfeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:19 +0000

    Add package ClassSave/1.2/ClassSave-1.2.tar.gz

---
+++ packages/ClassSave/ClassSave-1.2.tar.gz/ClassSave-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from ClassSave import __version__
+
+setup(
+    name='ClassSave',
+    version=__version__,
+
+    url='https://github.com/OpNoob/ClassSave',
+    author='Jonathan Attard',
+    author_email='jonattard1@gmail.com',
+
+    py_modules=['ClassSave'],
+)
