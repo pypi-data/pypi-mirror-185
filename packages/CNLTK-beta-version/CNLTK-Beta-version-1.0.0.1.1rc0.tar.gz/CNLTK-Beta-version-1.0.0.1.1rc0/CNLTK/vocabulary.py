078d52f57cfc2dc5d4609e13ed8427acc2ff3300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:02 +0000

    Add package CNLTK-beta-version/1.0.0.1.1rc0/CNLTK-Beta-version-1.0.0.1.1rc0.tar.gz

---
+++ packages/CNLTK-beta-version/CNLTK-Beta-version-1.0.0.1.1rc0.tar.gz/CNLTK-Beta-version-1.0.0.1.1rc0/CNLTK/vocabulary.py
@@ -0,0 +1,22 @@
+import pickle
+import configparser
+
+
+# VOCABULARY OF THE MODEL
+def get_VOCAB_model():
+    # with open(vocabs_pkl_filepath, "rb") as f:
+    #     vocab_model = pickle.load(f)
+    
+    file = 'config.ini'
+    config = configparser.ConfigParser()
+    config.read(file)
+
+    # vocabs_pkl_filepath = config['PATHS']['vocabs_pkl_filepath']
+    vocabs_pkl_filepath = config.get('PATHS', 'vocabs_pkl_filepath')
+
+    with open(vocabs_pkl_filepath, "rb") as f:
+        vocab_model = pickle.load(f)
+    
+    return vocab_model
+
+get_VOCAB_model()
