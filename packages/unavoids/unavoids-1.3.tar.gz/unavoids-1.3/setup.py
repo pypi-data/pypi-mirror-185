b2507117c76ab5e1ed5b2cb2b6213d043cdcaabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package unavoids/1.3/unavoids-1.3.tar.gz

---
+++ packages/unavoids/unavoids-1.3.tar.gz/unavoids-1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from pathlib import Path
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.txt").read_text()
+
+setup(
+    name='unavoids',
+    version='1.3',    
+    short_description='UNAVOIDS: Unsupervised and Nonparametric Approach for Visualizing Outliers and Invariant Detection Scoring',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/isotlaboratory/UNAVOIDS-Code',
+    author='Yousef, Waleed A. and Traoré, Issa and Briguglio, William',
+    author_email='wyousef@uvic.ca',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['unavoids'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
