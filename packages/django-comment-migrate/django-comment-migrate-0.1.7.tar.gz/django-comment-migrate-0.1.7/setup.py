1babd47a1e62f3a2a401fa917e564b2772c9acf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:25 +0000

    Add package django-comment-migrate/0.1.7/django-comment-migrate-0.1.7.tar.gz

---
+++ packages/django-comment-migrate/django-comment-migrate-0.1.7.tar.gz/django-comment-migrate-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+readme = open('README.rst', encoding='utf-8').read()
+
+setup(
+    name='django-comment-migrate',
+    version='0.1.7',
+    description="""An app that provides Django model comment migration """,
+    long_description=readme,
+    author='starryrbs',
+    author_email='1322096624@qq.com',
+    url='https://github.com/starryrbs/django-comment-migrate.git',
+    keywords='django-comment-migrate',
+    packages=['django_comment_migrate'],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    install_requires=['django>=2.2'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
