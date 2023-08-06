8cad5792145b80f5f51c24806b013ac4912bc415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package peek-plugin-livedb/3.1.10/peek-plugin-livedb-3.1.10.tar.gz

---
+++ packages/peek-plugin-livedb/peek-plugin-livedb-3.1.10.tar.gz/peek-plugin-livedb-3.1.10/peek_plugin_livedb/_private/admin-app/status/status.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core"
+import { TupleDataObserverService, TupleSelector } from "@synerty/vortexjs"
+import {
+    BalloonMsgService,
+} from "@synerty/peek-plugin-base-js"
+import { NgLifeCycleEvents } from "@synerty/vortexjs"
+import { AdminStatusTuple } from "../tuples/AdminStatusTuple"
+
+@Component({
+    selector: "pl-livedb-status",
+    templateUrl: "./status.component.html"
+})
+export class StatusComponent extends NgLifeCycleEvents {
+    
+    item: AdminStatusTuple = new AdminStatusTuple()
+    
+    constructor(
+        private balloonMsg: BalloonMsgService,
+        private tupleObserver: TupleDataObserverService
+    ) {
+        super()
+        
+        let ts = new TupleSelector(AdminStatusTuple.tupleName, {})
+        this.tupleObserver.subscribeToTupleSelector(ts)
+            .takeUntil(this.onDestroyEvent)
+            .subscribe((tuples: AdminStatusTuple[]) => this.item = tuples[0])
+        
+    }
+    
+}
---
+++ packages/peek-plugin-livedb/peek-plugin-livedb-3.1.10.tar.gz/peek-plugin-livedb-3.1.10/peek_plugin_livedb/plugin-module/_private/index.ts
@@ -0,0 +1,8 @@
+export * from "./PluginNames"
+export { LiveDBTuple } from "./tuples/LiveDBTuple"
+export { StringIntTuple } from "./tuples/StringIntTuple"
+export { SettingPropertyTuple } from "./tuples/SettingPropertyTuple"
+export { AddIntValueActionTuple } from "./tuples/AddIntValueActionTuple"
+export {
+    StringCapToggleActionTuple
+} from "./tuples/StringCapToggleActionTuple"
