2ea020ae92a0658584f3c688b9727c06047dd610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package three-in-a-row/0.1/three_in_a_row-0.1.tar.gz

---
+++ packages/three-in-a-row/three_in_a_row-0.1.tar.gz/three_in_a_row-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='three_in_a_row',
+      version='0.1',
+      description='Game of three in a row',
+      packages=['three_in_a_row'],
+      zip_safe=False)
