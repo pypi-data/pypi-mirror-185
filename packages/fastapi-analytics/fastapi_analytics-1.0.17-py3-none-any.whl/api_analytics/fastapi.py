5b0676076e30ab8bcdfa06b3ccfafcd89189ed93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:54 +0000

    Add package fastapi-analytics/1.0.17/fastapi_analytics-1.0.17-py3-none-any.whl

---
+++ packages/fastapi-analytics/fastapi_analytics-1.0.17-py3-none-any.whl/api_analytics/fastapi.py
@@ -0,0 +1,34 @@
+import threading
+from time import time
+
+from api_analytics.core import log_request
+from starlette.middleware.base import (BaseHTTPMiddleware,
+                                       RequestResponseEndpoint)
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+
+class Analytics(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, api_key: str):
+        super().__init__(app)
+        self.api_key = api_key
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        start = time()
+        response = await call_next(request)
+                
+        data = {
+            'api_key': self.api_key,
+            'hostname': request.url.hostname,
+            'ip_address': request.client.host,
+            'path': request.url.path,
+            'user_agent': request.headers['user-agent'],
+            'method': request.method,
+            'status': response.status_code,
+            'framework': 'FastAPI',
+            'response_time': int((time() - start) * 1000),
+        }
+        
+        threading.Thread(target=log_request, args=(data,)).start()
+        return response
