d4c587899f538b9050b3b228ac9aa0e022f3c808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:24 +0000

    Add package betterpipe/0.1.2/betterpipe-0.1.2.tar.gz

---
+++ packages/betterpipe/betterpipe-0.1.2.tar.gz/betterpipe-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['betterpipe']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'betterpipe',
+    'version': '0.1.2',
+    'description': 'Betterpipe: Break free from limited observability in DevOps/MLOps platforms. Pipe .py file data to external data warehouse for complete control over model performance. Easily troubleshoot and make informed decisions. ',
+    'long_description': None,
+    'author': 'Clerkie AI',
+    'author_email': 'clerkieai@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
