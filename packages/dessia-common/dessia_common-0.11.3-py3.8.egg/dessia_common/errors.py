6bc2a086657e53dcd9a55728003cae5147944ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:07 +0000

    Add package dessia-common/0.11.3/dessia_common-0.11.3-py3.8.egg

---
+++ packages/dessia-common/dessia_common-0.11.3-py3.8.egg/dessia_common/errors.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for errors in dessia_common
+"""
+
+
+class ExceptionWithTraceback(Exception):
+    """
+    Base class with a message and a traceback
+    """
+
+    def __init__(self, message, traceback_=''):
+        super().__init__(message)
+        self.message = message
+        self.traceback = traceback_
+
+    def __str__(self):
+        return f'{self.message}\nTraceback:\n{self.traceback}'
+
+
+class DeepAttributeError(ExceptionWithTraceback, AttributeError):
+    pass
+
+
+class ModelError(Exception):
+    pass
+
+
+class ConsistencyError(Exception):
+    pass
+
+
+class SerializationError(Exception):
+    pass
+
+
+class DeserializationError(Exception):
+    pass
+
+
+class CopyError(Exception):
+    pass
+
+
+class UntypedArgumentError(Exception):
+    pass
