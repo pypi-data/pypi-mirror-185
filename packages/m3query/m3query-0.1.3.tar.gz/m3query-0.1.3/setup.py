6271c07a41efb674dc9765311096329864bc8343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:15 +0000

    Add package m3query/0.1.3/m3query-0.1.3.tar.gz

---
+++ packages/m3query/m3query-0.1.3.tar.gz/m3query-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['m3query']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'm3query',
+    'version': '0.1.3',
+    'description': 'A wrapper around the JayDeBeApi library to connect to M3. Should not be distributed due to the ownership of the driver.',
+    'long_description': None,
+    'author': 'Kim Timothy Engh',
+    'author_email': 'kim.timothy.engh@epiroc.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
