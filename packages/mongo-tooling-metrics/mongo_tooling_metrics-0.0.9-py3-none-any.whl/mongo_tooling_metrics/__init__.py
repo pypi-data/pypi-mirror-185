96c7a535f6aa3deec0b8f178fb913bcaca161525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:54 +0000

    Add package mongo-tooling-metrics/0.0.9/mongo_tooling_metrics-0.0.9-py3-none-any.whl

---
+++ packages/mongo-tooling-metrics/mongo_tooling_metrics-0.0.9-py3-none-any.whl/mongo_tooling_metrics/__init__.py
@@ -0,0 +1,18 @@
+from typing import Type
+from mongo_tooling_metrics.base_hook import BaseHook
+from mongo_tooling_metrics.registry import _HookRegistry
+
+HOOK_REGISTRY = _HookRegistry()
+
+
+def register_hook(hook_instance: BaseHook) -> BaseHook:
+    """Add the hook to the registry if it doesn't exist & return the singleton hook instance."""
+    return HOOK_REGISTRY._safe_set_hook(hook_instance)
+
+
+def get_hook(hook_class: Type[BaseHook]) -> BaseHook:
+    """Get the hook if it exists -- else fail."""
+    return HOOK_REGISTRY._safe_get_hook(hook_class)
+
+
+__all__ = ["base_models", "client", "errors", "hooks"]
