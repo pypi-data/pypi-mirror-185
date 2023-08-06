00360c2aa443c767bfc02f8c8012b766f3f5e522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:28 +0000

    Add package cs-models/0.0.525/cs-models-0.0.525.tar.gz

---
+++ packages/cs-models/cs-models-0.0.525.tar.gz/cs-models-0.0.525/src/cs_models/resources/NewswireIPOOutbox/schemas.py
@@ -0,0 +1,14 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class NewswireIPOOutboxResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    news_id = fields.Integer(required=True)
+    type = fields.String(allow_none=True)
+    updated_at = fields.DateTime()
