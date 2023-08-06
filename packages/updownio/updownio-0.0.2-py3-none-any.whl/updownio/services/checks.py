53e7e36fa6a05da2894a281ab7c7e3dc5da6895c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:53 +0000

    Add package updownio/0.0.2/updownio-0.0.2-py3-none-any.whl

---
+++ packages/updownio/updownio-0.0.2-py3-none-any.whl/updownio/services/checks.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Adrien Delle Cave
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""updownio.services.checks"""
+
+
+import logging
+
+from six import ensure_text
+from updownio.service import UpDownIoServiceBase, SERVICES
+
+
+_DEFAULT_API_PATH = "api/checks"
+
+LOG               = logging.getLogger('updownio.checks')
+
+
+class UpDownIoChecks(UpDownIoServiceBase):
+    SERVICE_NAME = 'checks'
+
+    @staticmethod
+    def get_default_api_path():
+        return _DEFAULT_API_PATH
+
+    @staticmethod
+    def _build_disabled_locations(disabled_locations):
+        r = []
+
+        if not isinstance(disabled_locations, (list, tuple)):
+            return r
+
+        for i, x in enumerate(disabled_locations):
+            if isinstance(x, str):
+                r.append(('disabled_locations[]', x))
+
+        return r
+
+    @staticmethod
+    def _build_recipients(recipients):
+        r = []
+
+        if not isinstance(recipients, (list, tuple)):
+            return r
+
+        for i, x in enumerate(recipients):
+            if isinstance(x, str):
+                r.append(('recipients[]', x))
+
+        return r
+
+    def list(self):
+        return self.mk_api_call()
+
+    def show(self, token, params = None):
+        return self.mk_api_call(token, params)
+
+    def downtimes(self, token, page = 1, results = False):
+        return self.mk_api_call("%s/downtimes" % token,
+                                {'page': int(page),
+                                 'results': bool(results)})
+
+    def metrics(self, token, xfrom = None, to = None, group = None):
+        return self.mk_api_call("%s/metrics" % token,
+                                {'from': xfrom,
+                                 'to': to,
+                                 'group': group})
+
+    def add(self, url, data = None):
+        if not isinstance(data, dict):
+            data = {}
+
+        data['url']        = url
+        disabled_locations = data.pop('disabled_locations', None)
+        recipients         = data.pop('recipients', None)
+        data               = list(data.items())
+
+        if disabled_locations:
+            data.extend(self._build_disabled_locations(disabled_locations))
+
+        if recipients:
+            data.extend(self._build_recipients(recipients))
+
+        return self.mk_api_call(method = 'POST', data = data)
+
+    def update(self, token, data = None):
+        if not isinstance(data, dict):
+            data = {}
+
+        disabled_locations = data.pop('disabled_locations', None)
+        recipients         = data.pop('recipients', None)
+        data               = list(data.items())
+
+        if disabled_locations:
+            data.extend(self._build_disabled_locations(disabled_locations))
+
+        if recipients:
+            data.extend(self._build_recipients(recipients))
+
+        return self.mk_api_call("%s" % token,
+                                method = 'PUT',
+                                data = data)
+
+    def delete(self, token):
+        return self.mk_api_call("%s" % token,
+                                method = 'DELETE')
+
+if __name__ != "__main__":
+    def _start():
+        SERVICES.register(UpDownIoChecks())
+    _start()
