39f10b8ca1ecb4bc98e93f2f585c3327de181821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:00 +0000

    Add package udcd-verteilung/0.1/udcd_verteilung-0.1.tar.gz

---
+++ packages/udcd-verteilung/udcd_verteilung-0.1.tar.gz/udcd_verteilung-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udcd_verteilung',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udcd_verteilung'],
+      zip_safe=False)
