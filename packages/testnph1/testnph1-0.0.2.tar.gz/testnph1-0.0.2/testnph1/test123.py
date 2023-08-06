2c9f5d1ce1ed070df11e70a7fbe989706fd90525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package testnph1/0.0.2/testnph1-0.0.2.tar.gz

---
+++ packages/testnph1/testnph1-0.0.2.tar.gz/testnph1-0.0.2/testnph1/test123.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import threading
+import websocket
+import logging
+import enum
+import datetime
+import hashlib
+import time
+import urllib
+from time import sleep
+from datetime import datetime as dt
+
+def add(a, b):
+    """
+    This function adds two numbers and returns the result.
+    """
+    return a + b
