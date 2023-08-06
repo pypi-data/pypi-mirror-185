24d9ef5006dca55df70369d7088bf3eeb642fbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:11 +0000

    Add package data-check/0.14.0/data_check-0.14.0.tar.gz

---
+++ packages/data-check/data_check-0.14.0.tar.gz/data_check-0.14.0/data_check/checks/generator.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from ..io import get_expect_file, read_sql_file, rel_path, write_csv
+from ..result import DataCheckResult
+from .base_check import BaseCheck
+
+
+class DataCheckGenerator(BaseCheck):
+    """Special type of a "check" to generate the expectation files."""
+
+    def gen_expectation(
+        self, sql_file: Path, force: bool = False, template_data: Dict[str, Any] = {}
+    ) -> DataCheckResult:
+        """
+        Executes the query for a data_check test
+        and stores the result in the csv file.
+        """
+        expect_result = get_expect_file(sql_file)
+        _rel_path = rel_path(expect_result)
+        if not expect_result.exists() or force:
+            result = self.data_check.sql.run_query(
+                read_sql_file(sql_file=sql_file, template_data=template_data),
+                params=self.data_check.sql_params,
+            )
+            write_csv(result, expect_result)
+            output = f"expectation written to {_rel_path}"
+        else:
+            output = f"expectation skipped for {_rel_path}"
+        return DataCheckResult(passed=True, source=sql_file, result=output)
+
+    def run_test(self) -> DataCheckResult:
+        return self.gen_expectation(
+            sql_file=self.check_path,
+            force=self.data_check.config.force,
+            template_data=self.data_check.template_data,
+        )
---
+++ packages/data-check/data_check-0.14.0.tar.gz/data_check-0.14.0/data_check/runner.py
@@ -0,0 +1,76 @@
+import multiprocessing as mp
+import sys
+from concurrent.futures import Executor, Future, ProcessPoolExecutor, as_completed
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+from .checks.base_check import BaseCheck
+from .output import DataCheckOutput
+from .result import DataCheckResult
+
+if sys.version_info >= (3, 9):
+    SubmitResult = Future[Any]
+else:
+    SubmitResult = Future
+
+
+class NoPoolExecutor(Executor):
+    def submit(  # type: ignore
+        self, fn: Callable[..., Any], *args: Tuple[Any], **kwargs: Dict[str, Any]
+    ) -> SubmitResult:
+        f: SubmitResult = Future()
+        try:
+            result = fn(*args, **kwargs)
+        except BaseException as e:
+            f.set_exception(e)
+        else:
+            f.set_result(result)
+        return f
+
+
+class DataCheckRunner:
+    def __init__(self, workers: int, output: Optional[DataCheckOutput] = None) -> None:
+        self.workers = workers
+        if output is None:
+            output = DataCheckOutput()
+        self.output = output
+
+    def executor(self, task_list: List[Any]) -> Executor:
+        max_new_workers = self.workers - len(mp.get_context().active_children())
+        max_new_workers = min(max_new_workers, len(task_list))
+
+        # Makes no sense to create a single worker
+        # if we can process the work in this process:
+        if max_new_workers > 1:
+            return ProcessPoolExecutor(max_workers=max_new_workers)
+        return NoPoolExecutor()
+
+    def run_checks(
+        self,
+        run_method: Callable[[BaseCheck], DataCheckResult],
+        all_checks: List[BaseCheck],
+    ) -> List[DataCheckResult]:
+        """
+        Runs all tests.
+        Returns a list of the results
+        """
+        result_futures = [
+            self.executor(all_checks).submit(run_method, f) for f in all_checks
+        ]
+        results: List[DataCheckResult] = []
+        for future in as_completed(result_futures):
+            dc_result = future.result()
+            results.append(dc_result)
+            self.output.print(dc_result)
+        return results
+
+    def run_any(
+        self, run_method: Callable[..., Any], parameters: List[Dict[str, Any]]
+    ) -> List[Any]:
+        result_futures = [
+            self.executor(parameters).submit(run_method, **p) for p in parameters
+        ]
+        results: List[Any] = []
+        for future in as_completed(result_futures):
+            dc_result = future.result()
+            results.append(dc_result)
+        return results
