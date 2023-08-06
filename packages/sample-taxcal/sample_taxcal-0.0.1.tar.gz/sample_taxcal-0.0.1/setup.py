df7cc331d011e1f68fc33d19d3f499cf3f1efac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:02 +0000

    Add package sample-taxcal/0.0.1/sample_taxcal-0.0.1.tar.gz

---
+++ packages/sample-taxcal/sample_taxcal-0.0.1.tar.gz/sample_taxcal-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="sample_taxcal", 
+        version=VERSION,
+        author="MounicaAnju",
+        author_email="mounicapothureddy555@gmail.com",
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
