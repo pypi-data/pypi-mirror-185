3d18f1b073bed65434e81674d640fd0c10032963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package hspylib-vault/0.9.77/hspylib-vault-0.9.77.tar.gz

---
+++ packages/hspylib-vault/hspylib-vault-0.9.77.tar.gz/hspylib-vault-0.9.77/vault/entity/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-vault v0.9.77
+#
+# Package: main.vault.entity
+"""Package initialization."""
+
+__all__ = [
+  'validator', 
+  'vault_entry'
+]
+__version__ = '0.9.77'
