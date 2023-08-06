7cb82c1979b3f214b69f3ef5d094db1f574f0811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:17 +0000

    Add package spopt/0.5.0rc1/spopt-0.5.0rc1-py3-none-any.whl

---
+++ packages/spopt/spopt-0.5.0rc1-py3-none-any.whl/spopt/__init__.py
@@ -0,0 +1,6 @@
+from . import locate
+from . import region
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
---
+++ packages/spopt/spopt-0.5.0rc1-py3-none-any.whl/spopt/region/__init__.py
@@ -0,0 +1,8 @@
+from .maxp import MaxPHeuristic
+from .region_k_means import RegionKMeansHeuristic
+from .base import w_to_g
+from .skater import Skater
+from .ward import WardSpatial
+from .spenc import Spenc
+from .azp import AZP
+from .random_region import RandomRegion, RandomRegions
