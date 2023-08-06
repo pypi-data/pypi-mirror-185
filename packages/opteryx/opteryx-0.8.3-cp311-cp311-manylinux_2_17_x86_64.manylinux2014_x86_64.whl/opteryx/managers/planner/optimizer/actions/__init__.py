fc4c0961b5c3aadfda671c87cfc88602fa98fca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:08 +0000

    Add package opteryx/0.8.3/opteryx-0.8.3-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.8.3-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/managers/planner/optimizer/actions/__init__.py
@@ -0,0 +1,23 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# fmt: off
+from .action_apply_demorgans_law import apply_demorgans_law
+from .action_constant_evaluations import eliminate_constant_evaluations
+from .action_function_evaluations import eliminate_fixed_function_evaluations
+from .action_defragment_pages import defragment_pages
+from .action_eliminate_negations import eliminate_negations
+from .action_selection_pushdown import selection_pushdown
+from .action_split_conjunctive_predicates import split_conjunctive_predicates
+from .action_use_heap_sort import use_heap_sort
+
+# fmt:on
