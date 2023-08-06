6f2f9179fe71b806ccea2b6f461c5b707fd65a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:04 +0000

    Add package selcol/0.0.3/selcol-0.0.3.tar.gz

---
+++ packages/selcol/selcol-0.0.3.tar.gz/selcol-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+install_requires = ["numpy", "onnxruntime"]
+dev_requires = ["torch", "scikit-robot", "tqdm"]
+
+setup(
+    name="selcol",
+    version="0.0.3",
+    description="self collision network",
+    author="Hirokazu Ishida",
+    author_email="h-ishida@jsk.imi.i.u-tokyo.ac.jp",
+    license="MIT",
+    install_requires=install_requires,
+    extras_require={"dev": dev_requires},
+    packages=find_packages(exclude=("tests", "docs")),
+    package_data={"selcol": ["py.typed"]},
+)
