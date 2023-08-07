973f790e6947a6936505b1e5e0fbc3c12b2a4957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:46 +0000

    Add package farama/0.0.0/farama-0.0.0.tar.gz

---
+++ packages/farama/farama-0.0.0.tar.gz/farama-0.0.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='farama',
+      version='0.0.0',
+      author='Farama Foundation',
+      author_email='contact@farama.org',
+     )
