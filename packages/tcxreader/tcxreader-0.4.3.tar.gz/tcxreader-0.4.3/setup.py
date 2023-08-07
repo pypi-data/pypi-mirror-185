f784dba864d3213ffa4e158adeee2606ef984bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:48:05 +0000

    Add package tcxreader/0.4.3/tcxreader-0.4.3.tar.gz

---
+++ packages/tcxreader/tcxreader-0.4.3.tar.gz/tcxreader-0.4.3/setup.py
@@ -0,0 +1,27 @@
+# - *- coding: utf- 8 - *-
+import setuptools
+
+with open("README.md", encoding='UTF-8', mode="r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tcxreader",
+    version="0.4.3",
+    author="Alen Rajšp",
+    author_email="alen.rajsp@gmail.com",
+    description="tcxreader is a reader for Garmin’s TCX file format. It also works well with missing data!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alenrajsp/tcxreader",
+    packages=setuptools.find_packages(exclude=("*.tests", "*.tests.*", "tests.*", "tests",
+                                               "*.examples","*.examples.*", "examples.*", "examples",
+                                               "*.example_data", "*.example_data.*", "example_data.*", "example_data")),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.6',
+    test_suite="tests"
+)
