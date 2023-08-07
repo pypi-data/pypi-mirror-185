219b58d59f326153d846d3e2d2b876256105004a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:47:29 +0000

    Add package vistools-for-screening666/0.3.0/vistools_for_screening666-0.3.0-py3-none-any.whl

---
+++ packages/vistools-for-screening666/vistools_for_screening666-0.3.0-py3-none-any.whl/visualisation_package/vis_tools/baseline.py
@@ -0,0 +1,262 @@
+'''
+Tools for simple descriptive visualisations of screening data.
+These tools might be useful to gain a better understanding of the statistical
+properties of the dataset at hand. 
+Analysis_Plot:
+    Provides customisability to the graphs.
+histogram():
+    Plots a histogram of the chosen float column.
+area_analysis():
+    Allows the user to choose an area and graph its proportion of patients
+    screened over time.
+linear_comp:
+    Allows the user to choose areas to compare in an over the time analysis.
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Analysis_Plot:
+    '''
+    Creates an empty matplotlib.pyplot figure as an object.
+    Atributes:
+    ----------
+    fig
+    ax
+    Methods:
+    --------
+    title(title, fontsize)
+    x_label(x_label)
+    y_label(y_label)
+    fontsize(fontsize)
+    legend(include_leg)
+    figure_size(figsize)
+    '''
+    def __init__(self):
+        '''
+        Constructs an empty matplotlib.pyplot figure.
+        Parameters:
+        ----------
+        fig: object
+            figure
+        ax: object
+            ax
+        '''
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot()
+        self.ax.tick_params(axis='both', labelsize=12)
+        self.ax.grid(linestyle='--')
+    def title(self, title, fontsize=12):
+        '''
+        Adds title and changes its fontsize.
+        Parameters:
+        ----------
+        title: str
+            title of the graph
+        fontsize: int
+            size of font for title
+        '''
+        self.ax.set_title(title).set_fontsize(fontsize)
+    def x_label(self, x_label):
+        '''
+        Adds a chosen label to the x axis.
+        Parameters:
+        ----------
+        x_label: str
+            chosen label for x axis
+        '''
+        self.ax.set_xlabel(x_label, fontsize=12)
+    
+    def y_label(self, y_label):
+        '''
+        Adds a chosen label to the y axis.
+        Parameters:
+        ----------
+        y_label: str
+            chosen label for x axis
+        '''
+        self.ax.set_ylabel(y_label, fontsize=12)
+
+    def fontsize(self, fontsize):
+        '''
+        Changes the fontsize of the axis labels after the figure is already
+        created.
+        Parameters:
+        ----------
+        fontsize:
+            size of the font for the axis labels
+        '''
+        self.ax.xaxis.label.set_size(fontsize)
+        self.ax.yaxis.label.set_size(fontsize)
+    
+    def legend(self, include_leg):
+        '''
+        Adds a legend to the graph.
+        Parameters:
+        ----------
+        include_leg: bool
+            If True, includes the legend
+        '''
+        if include_leg:
+            self.ax.legend()
+    
+    def figure_size(self, figsize):
+        '''
+        Allows the user to choose figure size.
+        Parameters:
+        ----------
+        figsize: touple
+            size of the figure in inches (width, height)
+        '''
+        self.fig.set_size_inches(figsize)
+    
+    def adjust_fig(self, title="Plot", x_label="X", y_label="Y", fontsize=12,
+                  include_leg=True, figsize=(8,5)):
+        '''
+        Combines other figure customisation functions to ajust figure
+        parameters at once.
+        Parameters:
+        ----------
+        title: str
+            the title of the plot, default="Plot"
+        x_label: str
+            name of X axis on the plot, default="X"
+        y_label: str
+            name of the Y axis on the plot, default="Y"
+        fontsize: int
+            size of the font, default=12
+        include_leg: bool
+            if True includes legend, default=True
+        figsize: touple
+            size of the figure in inches (width, height), default=(8,5)
+        '''
+        self.title(title, fontsize)
+        self.x_label(x_label)
+        self.y_label(y_label)
+        self.fontsize(fontsize)
+        self.legend(include_leg)
+        self.figure_size(figsize)
+
+# Histogram of any float values:
+def histogram(df, col, title="Plot", x_label="X", y_label="Y", fontsize=12,
+        include_leg=False, figsize=(8,5)):
+    '''
+    Plots an customised histogram.
+    Parameters:
+    ----------
+    df: pandas DataFrame
+        dataframe containing the column user wishes to plot
+    col: str
+        name of the column to be plotted
+    title: str
+        the title of the plot, default="Plot"
+    x_label: str
+        name of X axis on the plot, default="X"
+    y_label: str
+        name of the Y axis on the plot, default="Y"
+    fontsize: int
+        size of the font, default=12
+    include_leg: bool
+        if True includes legend, default=True
+    figsize: touple
+        size of the figure in inches (width, height), default=(8,5)
+    '''
+    plot_o = Analysis_Plot()
+    plot_o.adjust_fig(title=title, x_label=x_label, y_label=y_label,
+                     fontsize=fontsize, include_leg=include_leg,
+                     figsize=figsize)
+    _=plot_o.ax.hist(df[col])
+    plt.show()
+
+def clean_data_for_area_analysis(df, area_name):
+    '''
+    Cleans dataframe only leaving data about the chosen area.
+    Parameters:
+    ----------
+    area_name: str
+        chosen area name
+    Returns:
+    --------
+    df: pandas DataFrame
+        cleaned dataframe
+    '''
+    df = df[df['Area Name'] == area_name]
+    keep = ['Time period', 'Value']
+    df = df[keep]
+    df.rename(columns={'Time period':'year'}, inplace=True)
+    # set index to year
+    df.set_index('year', inplace=True)
+    return df
+
+# can plot area, or region data over time. 
+def area_analysis(in_df, area_name, fontsize=12, include_leg=True,
+                 figsize=(8,5)):
+    '''
+    Plots the change of the proportion of screened patients over time.
+    In the chosen dataframe column
+    Parameters:
+    ----------
+    df: pandas DataFrame
+        dataframe
+    area_name: str
+        name of the area to be plotted over time
+    fontsize: int
+        size of the font, default=12
+    include_leg: bool
+        if True includes legend, default=True
+    figsize: touple
+        size of the figure in inches (width, height), default=(8,5)
+    '''
+    in_df = clean_data_for_area_analysis(in_df, area_name)
+    title = f"{area_name} Screening Coverage Over the Years"
+    x_label = 'Year'
+    y_label = 'Value'
+    plot = Analysis_Plot()
+    plot.adjust_fig(title=title, x_label=x_label, y_label=y_label,
+                    fontsize=fontsize, include_leg=include_leg,
+                    figsize=figsize)
+    plot.ax.plot(in_df['Value'], 'co-', label=area_name)
+    plt.show()
+
+def linear_comp(df, area_list, title="Plot", fontsize=12, include_leg=True, 
+                figsize=(8,5)):
+    '''
+    Function takes a list of area names and provides a comparison linear graph
+    of how screening rates changed in the chosen areas.
+    Parameters:
+    ----------
+    df: pandas DataFrame
+        data set
+    area_list: lst of str
+        a list of area names
+    title: str
+        title of the graph
+    fontsize: int
+        size of the axes font
+    include_leg: bool
+        if true includes the legend in the graph
+    figsize: touple
+        size of the graph in inches
+    '''
+    plot_o = Analysis_Plot()
+    y_label = "Proportion Screened, %"
+    x_label="Years"
+    # Checks if more than one area was included to be plotted.
+    if(len(area_list) == 1):
+        area_analysis(df, area_list, title="Plot", fontsize=12, 
+                      include_leg=True, figsize=(8,5))
+    else:
+        # Loops through the areas in the list, plotting them on the same axis.
+        for i in area_list:
+            print(i)
+            df1 = df.loc[df['Area Name']==i]
+            plt.plot(df1['Time period'], df1['Value'], label=i)
+        plot_o.adjust_fig(title=title, x_label=x_label, y_label=y_label,
+                          fontsize=fontsize, include_leg=include_leg,
+                          figsize=figsize)
+        plt.show()
+
+# Some testing, does not work atm, because of imports.
+# hist(df, 'Value', figsize=(8,5))
+# area_analysis(df, 'Exeter')
+# linear_comp(df, ['Exeter', 'Mid Sussex', 'Horsham'])
---
+++ packages/vistools-for-screening666/vistools_for_screening666-0.3.0-py3-none-any.whl/visualisation_package/vis_tools/datasets.py
@@ -0,0 +1,306 @@
+'''
+Contains functions for cleaning external datasets and loading built-in ones. 
+One for cervical, one for bowel, one for breast cancer screening. 
+
+Datasets originate from Health and Social Care Information Centre/Public 
+Health England. 
+Source: https://data.england.nhs.uk/organization/public-health-england?page=3
+
+basic_data_cleaning: shapes the dataframes for visualisation.
+load_cerv: loads data from cervical screening file from 2010 to 2016.
+load_bowel: loads data from bowel screening file from 2015 to 2016.
+load_breast: loads data from breast screening file from 2010 to 2016.
+'''
+import os 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+package_dir = os.path.dirname(data.__file__)
+print(package_dir)
+
+def basic_data_cleaning(df, age=True, sex=True, deprivation=False):
+    """
+    Function for basic data cleaning of an NHS screening uptake dataset.
+    It is possible to choose if to include age, sex and deprivation status. 
+
+    Parameters
+    ----------
+    df: pandas DataFrame
+        DataFrame containing the data to be explored.
+    age: bool
+        If True, then includes age information
+    sex: bool
+        If True, then includes sex information
+    deprivation: bool
+        If True, then only includes rows with deprivation deciles. 
+    Returns
+    -------
+    df: pandas DataFrame
+        data frame with unnecessary data excluded 
+    """
+    # Fill NaNs
+    df['Category'].fillna('NA', inplace=True)
+    
+    # Dropping "Cl_L3_..." values representing types of areas.
+    df = df[df['Area Code'].str.contains('E')]
+    
+    # Columns we want to keep. 
+    keep_col = ['Area Code', 'Area Name', 'Area Type', 'Time period', 'Value', 'Sex', 'Age']
+
+    if deprivation==True:
+        df = df[df['Category'].str.contains('IMD2015')]
+        keep_col.append('Category')
+        keep_col.append('Category Type')
+        df = df[keep_col]
+
+    # else:
+    #     if age == True:
+    #         keep_col.append('Age')
+    #         df = df[keep_col]
+    #     if sex == True:
+    #         keep_col.append('Sex')
+    #         df = df[keep_col]
+    
+    return df
+
+
+def load_cerv(age=True, sex=True, deprivation=False):
+    '''
+    Loads data from local a file on cervical cancer screening. 
+    The file includes data on the percentage of women in the resident
+    population eligible for cervical cancer screening who were screened
+    adequately within the years 2010 and 2016.
+    ----------
+    Parameters
+    ----------
+    age: bool
+        If True, then includes age information
+    sex: bool
+        If True, then includes sex information
+    deprivation: bool
+        If True, then includes "Category type" and "Category columns", which
+        describe deprivation groups.
+    Returns
+    -------
+    cerv_data: pandas DataFrame
+        cleaned dataframe    
+    '''  
+    filepath = os.path.join(package_dir, 'cervical_cancer_data.csv')
+    cerv_data = pd.read_csv(filepath)
+    cerv_data = basic_data_cleaning(cerv_data, age=age, sex=sex, 
+                                     deprivation=deprivation)
+    return cerv_data
+
+def load_bowel(age=True, sex=True, deprivation=False):
+    '''
+    Loads data from local file on bowel cancer screening. 
+    The file includes data on the percentage of people in the resident
+    population eligible for bowel screening who were screened adequately
+    within the years 2010 and 2016.
+    ----------
+    Parameters
+    ----------
+    age: bool
+        If True, then includes age information
+    sex: bool
+        If True, then includes sex information
+    deprivation: bool
+        If True, then includes "Category type" and "Category columns", which
+        describe deprivation groups.
+    Returns
+    -------
+    bowel_data: pandas DataFrame
+        cleaned dataframe    
+    '''
+    filepath = os.path.join(package_dir, 'bowel_cancer_data.csv')
+    bowel_data = pd.read_csv(filepath)
+    bowel_data = basic_data_cleaning(bowel_data, age=age, sex=sex, 
+                                     deprivation=deprivation)
+    return bowel_data
+
+def load_breast(age=True, sex=True, deprivation=False):
+    '''
+    Loads data from local file on breast cancer screening. 
+    The file includes data on the percentage of people in the resident
+    population eligible for breast cancer screening who were screened
+    adequately within the years 2010 and 2016.
+    ----------
+    Parameters
+    ----------
+    age: bool
+        If True, then includes age information
+    sex: bool
+        If True, then includes sex information
+    deprivation: bool
+        If True, then includes "Category type" and "Category columns", which
+        describe deprivation groups.
+    Returns
+    -------
+    breast_data: pandas DataFrame
+        cleaned dataframe    
+    '''
+    filepath = os.path.join(package_dir, 'breast_cancer_data.csv')
+    breast_data = pd.read_csv(filepath)
+    breast_data = basic_data_cleaning(breast_data, age=age, sex=sex, 
+                                     deprivation=deprivation)
+    return breast_data
+
+def load_custom(filename=str, age=True, sex=True, deprivation=False):
+    '''
+    Loads data from local custom file
+
+    If you are using your own dataset. Ensure its columns include:
+    * 'Area Code', eg. 'E12000001'
+    * 'Area Name', eg. 'Exeter'
+    * 'Area Type', eg. 'LA'
+    * 'Time period', eg. 2010
+    * 'Value', eg. '77.5379545'
+    * 'Age', eg. '25-64 yrs'
+    * 'Sex', eg. 'Female'
+
+    ----------
+    Parameters
+    ----------
+    filepath: str
+        The relative path for accessing the custom dataset
+    age: bool
+        If True, then includes age information
+    sex: bool
+        If True, then includes sex information
+    deprivation: bool
+        If True, then includes "Category type" and "Category columns", which
+        describe deprivation groups.
+    Returns
+    -------
+    breast_data: pandas DataFrame
+        cleaned dataframe    
+    '''
+    filepath = os.path.join(package_dir, filename)
+    custom_data = pd.read_csv(filepath)
+    custom_data = basic_data_cleaning(custom_data, age=age, sex=sex, 
+                                     deprivation=deprivation)
+    return custom_data
+
+class BasicDataExploration:
+    
+    """
+    A class for basic data exploration.
+    
+    This class provides basic data exploration for a given DataFrame.
+    It provides insights on data types, 
+    null values, duplicated rows, area type frequencies,
+    float column histograms, area name frequencies, 
+    value grouped by area type 
+    boxplots and value grouped by LA, UA and Region boxplots.
+
+    It also provides a breakdown of county and district deprivation deciles
+    when dep_deciles is set to True.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe
+    dep_deciles : bool, optional
+        Boolean to indicate whether or not to generate 
+        plots relating to deprivation deciles (default is False).
+
+    Methods
+    -------
+    explore()
+        Generates basic data exploration plots.
+    """
+
+    def __init__(self, df, dep_deciles = bool):
+        self.df = df
+        self.dep_deciles = dep_deciles
+
+    def explore(self):
+        """
+        Explore and visualize the data using matplotlib plots
+
+        Args:
+            self (obj): Instance of the BasicDataExploration class
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        print(' ')
+        print(f'The datatypes of the columns in the DataFrame are:\
+            \n{self.df.dtypes}')
+        print(' ')
+        print(f'The number of null values in each column are:\
+            \n{self.df.isnull().sum()}')
+
+        # Check for any duplicated rows
+        print(f'The number of duplicated rows are:\
+             {self.df.duplicated().sum()}')
+
+        # Show Area Type Frequency
+        print('Below is a barplot of Area Type frequencies:')
+        self.df['Area Type'].value_counts().plot.bar(figsize=(10,5))
+        plt.show()
+
+        # Generate histogram to identify outliers for the 'Value' Column
+        print('Below is a histogram of the float columns:') 
+        sns.histplot(self.df['Value'])
+        plt.ylabel('Percentage Uptake (%)')
+        plt.show()
+
+        # Count by area name
+        print('Below is a barplot overview of the area names and frequencies:') 
+        area_freq = self.df.groupby(['Area Name'])['Area Name'].count()
+        area_freq.plot(kind='bar', figsize =(100,5))
+        plt.legend(prop={'size': 4})
+        plt.xticks(rotation=90)
+        plt.figure(dpi=300)
+        plt.show()
+        
+        # Add boxplots of the ['Value'] column in the dataframe, grouped by 'Area Type'
+        print('Below are boxplots of the Value column in the dataframe, grouped by Area Type:')
+        sns.boxplot(x='Area Type', y='Value', data=self.df)
+        plt.xticks(rotation=75)
+        plt.show()
+        
+        # Add multiple boxplots of the ['Value'] 
+        # column in the dataframe, grouped by 'Area Type' LA, UA and Region
+        print('Below are multiple boxplots of the Value column \
+        in the dataframe, grouped by Area Type LA, UA and Region:')
+        sns.boxplot(x='Area Type', y='Value', \
+                    data=self.df[self.df['Area Type'].isin(['LA','UA','Region'])])
+        plt.xticks(rotation=75)
+        plt.show()
+
+        if self.dep_deciles == True:
+            county_dd = self.df[self.df['Category Type'].\
+                isin(["County & UA deprivation deciles in England (IMD2010)", \
+                    "County & UA deprivation deciles in England (IMD2015)"])]
+            district_dd = self.df[self.df['Category Type'].\
+                isin(["District & UA deprivation deciles in England (IMD2010)", \
+                    "District & UA deprivation deciles in England (IMD2015)"])]
+
+            print(len(county_dd))
+            print(len(district_dd))
+
+            county_cat_yr_freq = county_dd.groupby(['Category', 'Time period'])\
+            ['Time period'].count()
+            county_cat_yr_freq = county_cat_yr_freq.unstack(level=0)
+            county_cat_yr_freq.plot(kind='bar', figsize =(12,5), legend = 'right')
+            plt.legend(prop={'size': 4})
+            plt.figure(dpi=300)
+            plt.show()
+
+            district_cat_yr_freq = district_dd.groupby(['Category', 'Time period'])\
+            ['Time period'].count()
+            district_cat_yr_freq = district_cat_yr_freq.unstack(level=0)
+            district_cat_yr_freq.plot(kind='bar', figsize =(12,5), legend = 'right')
+            plt.legend(prop={'size': 4})
+            plt.figure(dpi=300)
+            plt.show()
+
