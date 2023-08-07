44a8fac4544993375c7f5a2723345587c6a555d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:48:04 +0000

    Add package PyHasteBin/1.2.5/PyHasteBin-1.2.5.tar.gz

---
+++ packages/PyHasteBin/PyHasteBin-1.2.5.tar.gz/PyHasteBin-1.2.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+try:
+    with open("README.md") as rfile:
+        long_description = rfile.read()
+except:
+    long_description = "https://github.com/ytcalifax/PyHasteBin (description failed to load)"
+
+setup(
+    name='PyHasteBin',
+    version='1.2.5',
+    packages=['postbin', "postbin.v2"],
+    url='https://github.com/ytcalifax/PyHasteBin',
+    python_requires=">=3.6",
+    license='MIT',
+    author='EEKIM10, ytcalifax',
+    author_email='kcustom.businessonly@gmail.com',
+    description='A simple two-function program that POSTs to hastebin',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    extras_require={"cli": "webbrowser"}
+)
+#
