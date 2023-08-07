e0d97d0f70b028a7231f4189f1700eee452da63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:26 +0000

    Add package fairbench/0.0.4/fairbench-0.0.4-py3-none-any.whl

---
+++ packages/fairbench/fairbench-0.0.4-py3-none-any.whl/fairbench/output.py
@@ -0,0 +1,49 @@
+from fairbench.fork import parallel, Fork
+from matplotlib import pyplot as plt
+import json
+
+
+def _is_dict_of_dicts(report):
+    return isinstance(report[next(iter(report))], dict)
+
+
+def tojson(report):
+    assert isinstance(report, Fork)
+    report = report.branches()
+    data = dict()
+    if not _is_dict_of_dicts(report):
+        report = {"": report}
+    data["header"] = ["Metric"] + [key for key in report]
+    for value in report.values():
+        for metric in value:
+            if metric not in data:
+                data[metric] = list()
+            data[metric].append(float(f"{value[metric]}"))
+    return json.dumps(data)
+
+
+def describe(report: Fork, spacing: int = 15):
+    assert isinstance(report, Fork)
+    report = json.loads(tojson(report))
+    ret = ""
+    if report["header"]:
+        ret += " ".join([entry.ljust(spacing) for entry in report["header"]]) + "\n"
+    for metric in report:
+        if metric == "header":
+            continue
+        ret += (
+            " ".join(
+                [metric.ljust(spacing)]
+                + [f"{entry:.3f}".ljust(spacing) for entry in report[metric]]
+            )
+            + "\n"
+        )
+    print(ret)
+
+
+@parallel
+def visualize(report: dict):
+    for i, key in enumerate(report.keys()):
+        plt.subplot(1, len(report), i + 1)
+        plt.bar([key], [report[key]])
+    plt.show()
