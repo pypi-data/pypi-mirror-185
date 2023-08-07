8f34f594ba82261ec2fbd64f9a315e9764eaea5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:00 +0000

    Add package celestis-tutorial/1.1/celestis-tutorial-1.1.tar.gz

---
+++ packages/celestis-tutorial/celestis-tutorial-1.1.tar.gz/celestis-tutorial-1.1/create_files.py
@@ -0,0 +1,29 @@
+import os
+
+views_content = '''
+# views for {} project
+'''
+
+urls_content = '''
+# urls for {} project
+'''
+
+server_code = ''
+
+def create_app(project_name):
+    os.makedirs(project_name, exist_ok=True)
+
+    views_path = os.path.join(project_name, "views.py")
+    with open(views_path, "w") as f:
+        f.write(views_content.format(project_name))
+    
+    urls_path = os.path.join(project_name, "urls.py")
+    with open(urls_path, "w") as f:
+        f.write(urls_content.format(project_name))
+    
+    server_path = os.path.join(project_name, "server.py")
+    with open(server_path) as f:
+        f.write(server_code)
+    
+
+    
