608c5f89236bf22190f503afeb35f922452fb053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:03 +0000

    Add package dumbo-scopus/0.1.0/dumbo_scopus-0.1.0.tar.gz

---
+++ packages/dumbo-scopus/dumbo_scopus-0.1.0.tar.gz/dumbo_scopus-0.1.0/README.md
@@ -0,0 +1,26 @@
+# Dumbo Scopus
+
+Simple CLI to search on Scopus and obtain the results in a XLSX file.
+
+
+# Prerequisites
+
+- Python 3.10
+
+
+# Install
+
+```bash
+$ pip install dumbo-scopus
+```
+
+
+# Usage
+
+Use the following command line:
+```bash
+$ python -m dumbo-scopus "TITLE(magic sets)"
+```
+
+A file `scopus.xlsx` with the results will be produced.
+Add `--help` to see more options.
---
+++ packages/dumbo-scopus/dumbo_scopus-0.1.0.tar.gz/dumbo_scopus-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dumbo_scopus']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.2,<3.0.0',
+ 'rich>=13.1.0,<14.0.0',
+ 'typer>=0.7.0,<0.8.0',
+ 'xlsxwriter>=3.0.7,<4.0.0']
+
+setup_kwargs = {
+    'name': 'dumbo-scopus',
+    'version': '0.1.0',
+    'description': 'Simple CLI to search on Scopus and obtain the results in a XLSX file',
+    'long_description': '# Dumbo Scopus\n\nSimple CLI to search on Scopus and obtain the results in a XLSX file.\n\n\n# Prerequisites\n\n- Python 3.10\n\n\n# Install\n\n```bash\n$ pip install dumbo-scopus\n```\n\n\n# Usage\n\nUse the following command line:\n```bash\n$ python -m dumbo-scopus "TITLE(magic sets)"\n```\n\nA file `scopus.xlsx` with the results will be produced.\nAdd `--help` to see more options.\n',
+    'author': 'Mario Alviano',
+    'author_email': 'mario.alviano@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
