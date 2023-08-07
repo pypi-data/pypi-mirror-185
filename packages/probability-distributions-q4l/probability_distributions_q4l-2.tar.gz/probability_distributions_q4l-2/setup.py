9e93f5cb18cf8ade6d98b8f2de495ade9cad98e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:35 +0000

    Add package probability-distributions-q4l/2/probability_distributions_q4l-2.tar.gz

---
+++ packages/probability-distributions-q4l/probability_distributions_q4l-2.tar.gz/probability_distributions_q4l-2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distributions_q4l',
+      version='2',
+      description='Gaussian distributions',
+      packages=['probability_distributions_q4l'],
+      zip_safe=False)
