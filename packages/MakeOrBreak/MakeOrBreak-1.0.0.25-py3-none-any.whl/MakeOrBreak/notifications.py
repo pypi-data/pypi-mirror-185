92cc5642f13237c1a70f01d0e740d7dcc937faec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:37 +0000

    Add package MakeOrBreak/1.0.0.25/MakeOrBreak-1.0.0.25-py3-none-any.whl

---
+++ packages/MakeOrBreak/MakeOrBreak-1.0.0.25-py3-none-any.whl/MakeOrBreak/notifications.py
@@ -0,0 +1,39 @@
+from platform import system
+import os
+
+import MakeOrBreak.utils as utils
+# import MakeOrBreak.dataReader as dataReader
+
+CONTENT_TYPE_MAPPING = {
+   0: "compliments",
+   1: "insults"
+}
+
+def getNotificationContent():
+   contentType = CONTENT_TYPE_MAPPING.get(utils.generateRdmNumber(0, len(CONTENT_TYPE_MAPPING) - 1))
+   contentData = utils.readDataFromFile(contentType)
+
+   return contentData[utils.generateRdmNumber(0, len(contentData) - 1)]
+
+def createNotification():
+   content = getNotificationContent()
+   os_system = system()
+   command = None
+
+   if os_system == "Darwin":
+      command = f'''
+      osascript -e 'display notification "{content}" with title "MakeOrBreak"'
+      '''
+   elif os_system == "Linux":
+      command = f'''
+      notify-send "MakeOrBreak" "{content}"
+      '''
+   else:
+      raise Exception("Unknown OS detected..")
+
+   os.system(command)
+
+
+if __name__ == "__main__":
+   for i in range(10):
+      createNotification()
