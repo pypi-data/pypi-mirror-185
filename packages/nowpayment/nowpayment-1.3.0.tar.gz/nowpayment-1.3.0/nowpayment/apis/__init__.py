baef0e064e30f944d22e8e90abfd228e06b0bcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:47:28 +0000

    Add package nowpayment/1.3.0/nowpayment-1.3.0.tar.gz

---
+++ packages/nowpayment/nowpayment-1.3.0.tar.gz/nowpayment-1.3.0/nowpayment/apis/__init__.py
@@ -0,0 +1,56 @@
+from typing import Union
+
+import requests
+
+
+class Empty:
+    pass
+
+
+class BaseAPI:
+    """Base API class.
+
+    Args:
+        api_key (str): API key.
+    """
+
+    def __init__(self, api_key: str, jwt_token: str = None):
+        self.api_key = api_key
+        self.jwt_token = jwt_token
+
+    def _request(self, method: str, path: str, headers: Union[None, dict] = Empty, **kwargs):
+        """
+        This is the method to make a request to the API.
+
+        :param method: HTTP method
+        :param path: API path
+        :param headers: HTTP headers
+        :param kwargs: See: https://requests.readthedocs.io/en/master/api/#main-interface
+        :return: API response.
+        """
+        set_headers: dict = {
+            'Content-Type': 'application/json',
+            'x-api-key': self.api_key
+        }
+        if self.jwt_token:
+            set_headers['Authorization'] = self.jwt_token
+        if headers is not Empty:
+            set_headers = headers
+        url = f"https://api.nowpayments.io/v1/{path}"
+        req = requests.request(
+            method,
+            url,
+            headers=set_headers,
+            **kwargs
+        )
+        if req.status_code == 200:
+            return req.json()
+        return req.content
+
+    def get_api_status(self) -> dict:
+        """This is a method for obtaining the status of the API.
+
+        Returns:
+            dict: Status of the API.
+        """
+        return self._request('GET', "status")
---
+++ packages/nowpayment/nowpayment-1.3.0.tar.gz/nowpayment-1.3.0/nowpayment/apis/payout.py
@@ -0,0 +1,75 @@
+import json
+
+from nowpayment.apis import BaseAPI
+from nowpayment.decorators import jwt_required
+
+
+class PayoutAPI(BaseAPI):
+
+    def login(self, email: str, password: str) -> dict:
+        """
+        This is the method to log in to the payout system.
+
+        :param email: email
+        :param password: password
+        :return: JWT token.
+        :rtype: dict
+        """
+        data = {
+            'email': email,
+            'password': password
+        }
+        data = json.dumps(data)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        return self._request('POST', "auth", data=data, headers=headers)
+
+    def create_payout(self, address: str, currency: str, amount: str, **kwargs) -> dict:
+        """
+        This is the method to create a payout.
+
+        :param address: the address where you want to send funds
+        :param currency: payout currency
+        :param amount: amount of the payout. Must not exceed 6 decimals (i.e. 0.123456)
+        :param kwargs: See: https://documenter.getpostman.com/view/7907941/S1a32n38?version=latest#21331cbf-c7c0-45ff-9709-0653f31d3803
+        :return: Payout data.
+        :rtype: dict
+        """
+        data = {
+            "address": address,
+            "currency": currency,
+            "amount": amount,
+            **kwargs
+        }
+        return self._request('POST', "payout", data=data)
+
+    def get_payout_status(self, payout_id: str) -> dict:
+        """
+        This is the method to get the payout status.
+
+        :param payout_id: payout id
+        :return: Payout data.
+        :rtype: dict
+        """
+        return self._request('GET', f"payout/{payout_id}")
+
+    def get_balance(self) -> dict:
+        """
+        This is the method to get the payout balance.
+
+        :return: Payout balance.
+        :rtype: dict
+        """
+        return self._request('GET', "balance")
+
+    @jwt_required
+    def verify_payout(self, withdrawals_id: str) -> dict:
+        """
+        This is the method to verify the payout.
+
+        :param withdrawals_id: withdrawals id
+        :return: Payout data.
+        :rtype: dict
+        """
+        return self._request('POST', f"payout/{withdrawals_id}/verify")
