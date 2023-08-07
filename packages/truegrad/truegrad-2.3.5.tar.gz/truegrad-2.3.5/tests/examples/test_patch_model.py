f6afccd6f2b402b4ddc9509f3befac173ea6b49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:30 +0000

    Add package truegrad/2.3.5/truegrad-2.3.5.tar.gz

---
+++ packages/truegrad/truegrad-2.3.5.tar.gz/truegrad-2.3.5/tests/examples/test_patch_model.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision.models import alexnet
+
+from truegrad.optim import TGAdamW
+from truegrad.utils import patch_model
+
+
+def test():
+    model = alexnet()
+    optim = TGAdamW(model.parameters())
+
+    patch_model(model)
+
+    inp = torch.randn((2, 3, 224, 224))
+    tgt = torch.randint(0, 1000, (2,))
+
+    for i in range(10):
+        loss = torch.nn.functional.cross_entropy(model(inp), tgt)
+        loss.backward()
+        optim.step()
+        optim.zero_grad()
