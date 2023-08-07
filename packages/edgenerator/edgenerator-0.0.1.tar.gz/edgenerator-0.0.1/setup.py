d7ba561978d9497f5a987340b008664f44fee1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:25 +0000

    Add package edgenerator/0.0.1/edgenerator-0.0.1.tar.gz

---
+++ packages/edgenerator/edgenerator-0.0.1.tar.gz/edgenerator-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Basic package'
+LONG_DESCRIPTION = 'A Basic package with more text'
+
+# Setting up
+setup(
+    name="edgenerator",
+    version=VERSION,
+    author="SiebeLeDe ",
+    author_email="<siebe.lekannegezegddeprez@student.uva.nl>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy>=1.23.4', 'matplotlib>=3.6.2'],
+    keywords=['python', 'energy diagram', 'energy diagram generator'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+#        "Operating System :: Unix",
+#       "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
