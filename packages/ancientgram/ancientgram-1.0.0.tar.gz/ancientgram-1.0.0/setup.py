5587df63735b8e69cdac29d4bdfa1f8ffaa94941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:08 +0000

    Add package ancientgram/1.0.0/ancientgram-1.0.0.tar.gz

---
+++ packages/ancientgram/ancientgram-1.0.0.tar.gz/ancientgram-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ancientgram',
+    version='1.0.0',    
+    description='A Telegram Bot Api Framework for Python',
+    url='https://github.com/khadga9/ancientgram',
+    author='khadga',
+    license='MIT',
+    packages=['ancientgram'],
+    install_requires=['requests'                 
+                      ])
