156bb54faaefaf1433d93c280948f85ac0519378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:12 +0000

    Add package dict-objectify/0.0.2/dict-objectify-0.0.2.tar.gz

---
+++ packages/dict-objectify/dict-objectify-0.0.2.tar.gz/dict-objectify-0.0.2/dict_objectify/__init__.py
@@ -0,0 +1,20 @@
+from dict_objectify.base import (Base, FieldBase, MixinBase,
+                                 NONE_EQUIVALENT_VALUES, dict_base_hash)
+from dict_objectify.fields import (Array, Bool, Datetime, EnumField, Float,
+                                   Integer, Text)
+
+__version__ = '0.0.2'
+__all__ = [
+    Base,
+    MixinBase,
+    FieldBase,
+    NONE_EQUIVALENT_VALUES,
+    dict_base_hash,
+    Array,
+    Bool,
+    Datetime,
+    EnumField,
+    Float,
+    Integer,
+    Text
+]
