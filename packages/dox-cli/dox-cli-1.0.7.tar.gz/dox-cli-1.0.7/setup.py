0c04df4a88489b3bd14e8b85d2cd1559a5bf9167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:55 +0000

    Add package dox-cli/1.0.7/dox-cli-1.0.7.tar.gz

---
+++ packages/dox-cli/dox-cli-1.0.7.tar.gz/dox-cli-1.0.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def read(path):
+    return open(path, 'r').read()
+
+
+setup_options = dict(
+    name='dox-cli',
+    version='1.0.7',
+    description='Cli to create project structure',
+    long_description=read('README.md'),
+    author='Bijay Das <imbijaydas@gmail.com>',
+    entry_point="""
+        [console_scripts]
+        dox=cli:main
+    """,
+    license='MIT License',
+    extras_require={},
+    python_requires='>=3.7',
+    project_urls={
+        'Source': 'https://github.com/bijaydas/dox-cli',
+    },
+)
+
+setup(**setup_options)
