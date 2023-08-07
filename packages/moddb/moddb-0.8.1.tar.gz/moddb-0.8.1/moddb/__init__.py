a7dd0bc85eb3c2fdc2bb34c7a91d83bd344d4025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:15 +0000

    Add package moddb/0.8.1/moddb-0.8.1.tar.gz

---
+++ packages/moddb/moddb-0.8.1.tar.gz/moddb-0.8.1/moddb/__init__.py
@@ -0,0 +1,11 @@
+from .base import search, parse_page, parse_results, login, logout, front_page, rss
+from .utils import LOGGER, BASE_URL, soup, request, get_page, Object
+from .enums import *
+from .pages import *
+from .client import Client, Thread
+
+import requests
+
+SESSION = requests.Session()
+
+__version__ = "0.8.1"
