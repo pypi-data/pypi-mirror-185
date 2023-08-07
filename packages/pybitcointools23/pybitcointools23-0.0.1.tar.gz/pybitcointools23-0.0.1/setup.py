1e1d5e5bfcb3d7a8b208cea43a202d86851123f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:59 +0000

    Add package pybitcointools23/0.0.1/pybitcointools23-0.0.1.tar.gz

---
+++ packages/pybitcointools23/pybitcointools23-0.0.1.tar.gz/pybitcointools23-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='pybitcointools23',
+      version='1.37',
+      description='Python Crypto Coin Tools',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      author='Mikhail Antonov',
+      author_email='allelementaryfor@gmail.com',
+      url='https://github.com/allelementary/pybitcointools',
+      packages=find_packages(),
+      scripts=['cryptotool'],
+      license="MIT License",
+      include_package_data=True,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Topic :: Security :: Cryptography',
+      ],
+      )
